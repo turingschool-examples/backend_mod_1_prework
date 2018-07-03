c24cc0ce8c34de4d48a33fa3d07ce8f1ba7c0711
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-07-03 15:17:06 -0600

    Adds day 2 exercises

---
+++ day_2/exercises/arrays.rb
@@ -0,0 +1,37 @@
+# In the exercises below, write your own code where indicated
+# to achieve the desired result. You should be able to run this
+# file from your terminal with the command `ruby day_2/exercises/arrays.rb`
+
+# example: write code below that will print an array of animals.
+# Store the array in a variable.
+animals = ["Zebra", "Giraffe", "Elephant"]
+p animals
+
+# Write code below that will print an array of states. Store the array in a variable.
+states = #YOUR CODE HERE
+p states
+
+# Write code below that will print an array of foods. Store the array in a variable.
+# YOUR CODE HERE
+
+# example: Write code below that will print the number of elements in array of
+# animals from above.
+p animals.count
+
+# Write code below that will print the number of elements in the array of
+#foods from above.
+# YOUR CODE HERE
+
+# Write code below that will print "Zebra" from the animals array
+# YOUR CODE HERE
+
+# Write code below that will print the last item from the foods array.
+# YOUR CODE HERE
+
+# Write code below that uses a method to add "lion" to the animals array and
+# print the result
+# YOUR CODE HERE
+
+# Write code below that removes the last item of food from the foods array and
+# print the result
+# YOUR CODE HERE
---
+++ day_2/exercises/iteration.rb
@@ -0,0 +1,26 @@
+# In the exercises below, write your own code where indicated
+# to achieve the desired result. You should be able to run this
+# file from your terminal with the command `ruby day_2/exercises/iteration.rb`
+
+# example: Write code below that iterates through a list of animals and print
+# each animal.
+animals = ["Zebra", "Giraffe", "Elephant"]
+
+animals.each do |animal|
+  p animal
+end
+
+# Write code below that iterates through a list of animals and prints "The
+# <animal> is awesome!" for each animal.
+
+animals.each do |animal|
+  # YOUR CODE HERE
+end
+
+# Create an array of foods and then iterate over that array to print "Add
+# <food> to shopping list" for each food item.
+# YOUR CODE HERE
+
+# Create an array of numbers and then iterate over that array to print doubles
+# of each of the number.
+# YOUR CODE HERE
