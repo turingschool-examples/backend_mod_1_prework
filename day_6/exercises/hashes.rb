f5a289fd032b8f2e9bddbe02366a7aaf0a49220f
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-07-05 08:09:58 -0600

    Adds day 6 exercises

---
+++ day_6/exercises/hashes.rb
@@ -0,0 +1,25 @@
+# In the exercises below, write your own code where indicated
+# to achieve the desired result. You should be able to run this
+# file from your terminal with the command `ruby day_6/exercises/arrays.rb`
+
+# example: Write code below to print a hash that holds grocery store inventory
+foods = {apples: 23, grapes: 507, eggs: 48}
+p foods
+
+# Write code below that will print a hash of animals and their number
+# at the zoo. (an inventory of animals)
+zoo = #YOUR CODE HERE
+p zoo
+
+# Using the zoo that you created above, print all the keys in the hash.
+# YOUR CODE HERE
+
+# Using the zoo that you created above, print all the values in the hash.
+# YOUR CODE here
+
+# Using the zoo taht you created above, print the value of the first item in
+# the hash
+# YOUR CODE HERE
+
+# Add an animal to the zoo hash and print the updated hash.
+# YOUR CODE HERE
