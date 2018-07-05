809e03bdc40bb1faccdab9066269e4b34cd2014b
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-07-05 07:57:42 -0600

    Adds day 5 exercises

---
+++ day_5/exercises/person.rb
@@ -0,0 +1,5 @@
+# Create a person class with at least 2 attributes and 2 behaviors.  Call all
+# person methods below the class so that they print their result to the
+# terminal.
+
+#YOUR CODE HERE
