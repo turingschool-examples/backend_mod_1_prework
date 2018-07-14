96782c1df43d9022bcc336e353aae9fa368a2a5c
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-07-14 16:34:22 -0600

    7th exercise day 1

---
+++ day_1/ex7.rb
@@ -0,0 +1,14 @@
+print "How old are you?"
+age = gets.chomp
+print "How tall are you?"
+height = gets.chomp
+print "How much do you weigh?"
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+print "How many times have you eaten today?"
+eat = gets.chomp
+print "How many times were you hungry, though?"
+hungry = gets.chomp
+print "So you were hungry #{hungry} times, but you only ate #{eat} time. Psh, psh."
