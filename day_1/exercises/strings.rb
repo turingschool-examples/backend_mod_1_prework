06b2658dd79d78851f7b24c05b993721ca4800ba
Amy Petrie <37280041+amypetrie@users.noreply.github.com>
2018-07-23 09:58:39 -0500

    Update strings.rb

--- day_1/exercises/strings.rb
+++ day_1/exercises/strings.rb
@@ -6,8 +6,7 @@
 p "Alan Turing"
 
 # Write code below to print `Welcome to Turing!` to the terminal:
-p "Welcome to Turing!"
+p #YOUR CODE HERE
 
 # Write code below to print `99 bottles of pop on the wall...`:
-print "99 bottles of pop on the wall..."
-
+# YOUR CODE HERE
