d0b151ea88be4939766c4fbf59c5ada183574d8b
Isaac Falkenstine <isaac@falkenstine.com>
2018-07-25 13:33:04 -0600

    Adds Day 2 Work

--- day_2/exercises/iteration.rb
+++ day_2/exercises/iteration.rb
@@ -13,14 +13,27 @@ end
 # Write code below that iterates through a list of animals and prints "The
 # <animal> is awesome!" for each animal.
 
+count = 0
 animals.each do |animal|
-  # YOUR CODE HERE
+  p "The #{animals.at(count)} is awesome!" # I assume that there are eaiser ways to do this -Isaac
+  count = count + 1
 end
 
 # Create an array of foods and then iterate over that array to print "Add
 # <food> to shopping list" for each food item.
-# YOUR CODE HERE
+foods = ["Pizza", "Hot Dogs", "Burgers", "Pasta"]
+num = 0
+foods.each do |food|
+  p "Add #{foods.at(num)} to the shopping list"
+  num = num + 1
+end
 
 # Create an array of numbers and then iterate over that array to print doubles
 # of each of the number.
-# YOUR CODE HERE
+numbers = ["1", "2", "3", "4", "5", "6"]
+vn = 0
+numbers.each do |number|
+  p numbers.at(vn)
+  p numbers.at(vn)
+  vn = vn + 1
+end
