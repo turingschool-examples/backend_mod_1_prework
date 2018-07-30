872ef148e3e143713a114936d630cb8986f44c4b
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-07-30 10:04:10 -0600

    10 little monkeys iteration one no plurals

---
+++ day_7/10_little_monkeys.rb
@@ -0,0 +1,8 @@
+@monkey = 0
+while @monkey <= 9
+@monkey += 1
+  puts  "#{@monkey} little monkeys jumping on the bed,
+     One fell off and bumped his head,
+     Mama called the doctor and the doctor said,
+     'No more monkeys jumping on the bed!'"
+end
