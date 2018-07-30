ffb354eca7e3bb5c1f579202955013a32ee46409
Meghan Stang <mstang15@gmail.com>
2018-07-30 16:17:41 -0600

    Adds Day 7 Work

---
+++ day_7/10_little_monkeys.rb
@@ -0,0 +1,28 @@
+monkeys = 10
+
+
+def monkey_song(n)
+  if n != 1
+  puts "#{n} little monkeys jumping on the bed,
+  One fell off and bumped his head,
+  Mama called the doctor and the doctor said,
+  'No more monkeys jumping on the bed!'"
+
+elsif n == 1
+  puts "#{n} little monkey jumping on the bed,
+  He fell off and bumped his head,
+  Mama called the doctor and the doctor said,
+  'Get those monkeys right to bed!'"
+  end
+end
+
+monkey_song(monkeys)
+monkey_song(monkeys-1)
+monkey_song(monkeys-2)
+monkey_song(monkeys-3)
+monkey_song(monkeys-4)
+monkey_song(monkeys-5)
+monkey_song(monkeys-6)
+monkey_song(monkeys-7)
+monkey_song(monkeys-8)
+monkey_song(monkeys-9)
