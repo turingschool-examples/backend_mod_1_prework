cd94aad527b656d2c5deaff79e38da42e9dd35e6
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-07-30 10:07:48 -0600

    solved for pluralizations

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -1,8 +1,15 @@
-@monkey = 0
-while @monkey <= 9
-@monkey += 1
-  puts  "#{@monkey} little monkeys jumping on the bed,
+monkey = 0
+while monkey <= 9
+monkey += 1
+if monkey == 1
+  puts "#{monkey} little monkey jumping on the bed,
      One fell off and bumped his head,
      Mama called the doctor and the doctor said,
      'No more monkeys jumping on the bed!'"
+   else
+  puts  "#{monkey} little monkeys jumping on the bed,
+     One fell off and bumped his head,
+     Mama called the doctor and the doctor said,
+     'No more monkeys jumping on the bed!'"
+   end
 end
