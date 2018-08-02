c2a167cdae288f1f2d3ddebe0578a7e55f152f73
Noah Flint <noah.flint24@gmail.com>
2018-08-01 20:38:39 -0600

    adds day 1

---
+++ day_1/ex7v2_test1.rb
@@ -0,0 +1,8 @@
+puts "first number please"
+first = gets.chomp.to_i
+
+puts "Second number please"
+second = gets.chomp.to_i
+
+answer = first + second
+puts "The calculation is #{first} + #{second} = #{answer}"
