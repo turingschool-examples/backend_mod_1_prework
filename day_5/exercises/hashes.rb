310c01fb78a8325b052d54204cd2de1933f9f2c8
Isaac Falkenstine <isaac@falkenstine.com>
2018-08-07 07:41:40 -0600

    Adds Day 5 Work

--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -8,18 +8,22 @@ p foods
 
 # Write code below that will print a hash of animals and their number
 # at the zoo. (an inventory of animals)
-zoo = #YOUR CODE HERE
+zoo = {"lions" => 5, "tigers" => 3, "bears" => 8}
 p zoo
 
 # Using the zoo that you created above, print all the keys in the hash.
-# YOUR CODE HERE
+
+zoo.each_key {|key| puts key}
 
 # Using the zoo that you created above, print all the values in the hash.
-# YOUR CODE here
+#
+
+zoo.each_value {|value| puts value}
 
 # Using the zoo taht you created above, print the value of the first item in
 # the hash
-# YOUR CODE HERE
+puts zoo.first
 
 # Add an animal to the zoo hash and print the updated hash.
-# YOUR CODE HERE
+zoo[6] = "monkeys"
+puts zoo
