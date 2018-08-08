d44370e93ceab5c28e31171a7b842c6d3c37eef4
Dan Hutchinson <danhutch5280@gmail.com>
2018-08-07 22:20:53 -0600

    Adds Day 7 Work

--- day_7/10_little_monkeys_dhutch.rb
+++ day_7/10_little_monkeys_dhutch.rb
@@ -1,58 +1,95 @@
-# 10_little_monkeys_dhutch.rb by Dan Hutchinson
+## 10_little_monkeys_dhutch.rb by Dan Hutchinson
 
-monkeys = 10
+# monkeys = 10
+#
+# while monkeys > 0 do
+#   if monkeys == 1
+#     puts "#{monkeys} little monkey jumping on the bed,"
+#   else
+#     puts "#{monkeys} litte monkeys jumping on the bed,"
+#   end
+#
+#   puts "One fell off and bumped his head,"
+#   puts "Mama called the doctor and the doctor said,"
+#
+#   if monkeys == 1
+#     puts "No more monkeys jumping on the bed!"
+#   else
+#     puts "Get those monkeys straight to bed!"
+#   end
+#
+#   puts ""
+#   monkeys -= 1
+# end
 
-while monkeys > 0 do
-  if monkeys == 1
-    puts "#{monkeys} little monkey jumping on the bed,"
-  else
-    puts "#{monkeys} litte monkeys jumping on the bed,"
-  end
-
-  puts "One fell off and bumped his head,"
-  puts "Mama called the doctor and the doctor said,"
-
-  if monkeys == 1
-    puts "No more monkeys jumping on the bed!"
-  else
-    puts "Get those monkeys straight to bed!"
-  end
-
-  puts ""
-  monkeys -= 1
-end
-
-# The following is an alternative way i wanted to try this, after reading about my peer's approaches on Slack.
-# After I got it to work properly I also started having some fun with it and practicing conditionals to adjust the grammar for subject-verb agreement. 
+## The following is an alternative way i wanted to try this, after reading about my peer's approaches on Slack.
+## After I got it to work properly I also started having some fun with it and practicing conditionals to adjust the grammar for subject-verb agreement.
 
 
-monks = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
+# monks = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
+#
+# puts "-" * 20
+#
+# monks.reverse.each do |value|
+#
+#   if value == "One"
+#     puts "#{value} little monkey jumping on the bed,"
+#   else
+#     puts "#{value} litte monkeys jumping on the bed,"
+#   end
+#
+#   if value == "Zero"
+#     puts "None were left to bump their heads,"
+#   else
+#     puts "One fell off and bumped his head,"
+#   end
+#
+#   puts "Mama called the doctor and the doctor said,"
+#
+#   if value == "Zero"
+#     puts "No more monkeys jumping on the bed!"
+#   elsif value == "One"
+#     puts "Get that monkey straight to bed!"
+#   else
+#     puts "Get those monkeys straight to bed!"
+#   end
+#
+#   puts ""
+# end
+
+## New attempt, with user input for starting number of monkeys!
 
 puts "-" * 20
 
-monks.reverse.each do |value|
+puts "With how many monkeys would you like to start?"
+print "> "
 
-  if value == "One"
-    puts "#{value} little monkey jumping on the bed,"
+keys = $stdin.gets.to_i
+
+puts "*" * 20
+puts ""
+
+while keys > 0 do
+  if keys == 1
+    puts "#{keys} little monkey jumping on the bed,"
   else
-    puts "#{value} litte monkeys jumping on the bed,"
+    puts "#{keys} litte monkeys jumping on the bed,"
   end
 
-  if value == "Zero"
-    puts "None were left to bump their heads,"
+  if keys == 1
+    puts "He fell off and bumped his head,"
   else
     puts "One fell off and bumped his head,"
   end
 
   puts "Mama called the doctor and the doctor said,"
 
-  if value == "Zero"
+  if keys == 1
     puts "No more monkeys jumping on the bed!"
-  elsif value == "One"
-    puts "Get that monkey straight to bed!"
   else
     puts "Get those monkeys straight to bed!"
   end
 
   puts ""
+  keys -= 1
 end
