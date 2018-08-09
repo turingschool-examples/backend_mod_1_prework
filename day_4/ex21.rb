97ad83c586f65a3e8d33adf6dcc22e82afdcf9a5
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-08-09 17:19:11 -0600

    adds day_4

---
+++ day_4/ex21.rb
@@ -0,0 +1,42 @@
+def add(a, b)
+  puts "ADDING #{a} + #{b}"
+   a + b
+end
+
+def subtract(a, b)
+  puts "SUBTRACTING #{a} - #{b}"
+   a - b
+end
+
+def multiply(a, b)
+  puts "MULTIPLYING #{a} * #{b}"
+   a * b
+end
+
+def divide(a, b)
+  puts "DIVIDING #{a} / #{b}"
+   a / b
+end
+
+
+puts "Let's do some math with just functions!"
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+
+# A puzzle for the extra credit, type it in anyway.
+puts "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+puts "That becomes: #{what}. Can you do it by hand?"
+ a = 50/2
+ b = 180 * a
+ c = 74 - b
+ d = 35 + c
+ puts d
---
+++ day_4/launchschool.rb
@@ -0,0 +1,31 @@
+# def greeting(name)
+#   "Hello, " + name + ". How are you doing?"
+# end
+#
+# puts greeting("Nikki")
+
+
+# x = 2
+# puts x = 2
+# p name = "Joe"
+# four = "four"
+# print something = "nothing"
+
+#
+# def multiply(num1, num2)
+#   num1 * num2
+# end
+#
+# puts multiply(5, 5)
+
+
+def scream(words)
+     words = words + "!!!!"
+     puts words
+   end
+
+   scream("Yippeee")
+
+
+#6
+#you are only providing one argument for a method that asks for more.
