4bed81ec297338d90e4f08524d34cf37fa0a314e
maybemushrooms <thecraftedgem@tuta.io>
2018-08-09 14:00:01 -0600

    day_6

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -3,3 +3,25 @@
 # terminal.
 
 #YOUR CODE HERE
+class Person
+  attr_reader :name,
+              :age,
+              :running
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @running = false
+  end
+
+  def run
+    @running = true
+  end
+end
+
+eric = Person.new("Eric", 20)
+
+p eric.name
+p eric.age
+p eric.running
+p eric.run
+p eric.running
