1aa4375a5d78f933b788ac91938b0d397206e5d4
Meghan Stang <mstang15@gmail.com>
2018-08-10 12:36:06 -0600

    Added little_monkeys for any n

---
+++ day_7/n_little_monkeys.rb
@@ -0,0 +1,22 @@
+puts "How many monkeys are jumping on the bed?"
+print ">"
+monkeys = gets.chomp.to_i
+
+def monkey_song(n)
+    if n!=1
+      puts "#{n} little monkeys jumping on the bed,
+      One fell off and bumped his head,
+      Mama called the doctor and the doctor said,
+      'No more monkeys jumping on the bed!'"
+      monkey_song(n-1)
+    elsif n ==1
+      puts "#{n} little monkey jumping on the bed,
+      He fell off and bumped his head,
+      Mama called the doctor and the doctor said,
+      'Get those monkeys right to bed!'"
+
+  end
+
+end
+
+monkey_song(monkeys)
