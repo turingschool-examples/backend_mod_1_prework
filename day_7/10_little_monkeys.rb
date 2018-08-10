530c47ed167590c51fda437310102959cfb3b069
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-08-10 13:00:42 -0600

    ten little monkies with hash

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -1,26 +1,23 @@
+number_of_monkeys = {1=> "One",
+           2=> "Two",
+           3=> "Three",
+           4=> "Four",
+           5=> "Five",
+           6=> "Six",
+           7=> "Seven",
+           8=> "Eight",
+           9=> "Nine",
+           10=> "Ten"
+           }
 monkey = 0
 while monkey <= 9
 monkey += 1
 if monkey == 1
-  puts "#{monkey} little monkey jumping on the bed,"
+  puts "#{number_of_monkeys[monkey]} little monkey jumping on the bed"
   else
-  puts  "#{monkey} little monkeys jumping on the bed,"
+  puts "#{number_of_monkeys[monkey]} little monkies jumping on the bed"
 end
 puts "One fell off and bumped his head,
 Mama called the doctor and the doctor said,
 'No more monkeys jumping on the bed!'"
 end
-
-number_of_monkeys = {1: "one",
-           2: "two",
-           3: "three",
-           4: "four",
-           5: "five",
-           6: "six",
-           7: "seven",
-           8: "eight",
-           9: "nine",
-           10: "ten"
-           }
-
-puts number_of_monkeys(10)
