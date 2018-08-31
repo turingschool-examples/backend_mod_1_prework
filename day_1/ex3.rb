6f617c0541093c544c04fa0bc78b39a07ab06dc9
Leah Miller <lkmiller.87@gmail.com>
2018-08-31 10:32:29 -0400

    adds day 1

---
+++ day_1/ex3.rb
@@ -0,0 +1,32 @@
+# this line introduces the activity
+puts "I will now count my chickens:"
+# this line divides 30 by 6 and adds that to 25
+puts "Hens #{25.0 + 30 / 6}"
+# this line multiplies 25 by 3 and uses the % function to produce the remainder and subtracts this from 100
+puts "Rooster #{100.0 - 25 * 3 % 4}"
+# this line prints the string activity
+puts "Now I will count the eggs:"
+# this line uses order of operations to calculate the sum
+puts 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# this line prints the string activity
+puts "Is it true that 3 + 2 < 5 - 7?"
+# this line asks if 5 is less than -2
+puts 3.0 + 2 < 5 - 7
+# this line prints the string, including the addition of 3 + 2
+puts "What is 3 + 2? #{3.0 + 2}"
+# this line prints the string, including the subtraction of 5 - 7
+puts "What is 5 - 7? #{5.0 - 7}"
+# this line prints the string activity
+puts "Oh, that's why it's false."
+# this line prints the string activity
+puts "How about some more."
+# this line prints the string and the query is 5 greater than -2
+puts "Is it greater? #{5.0 > -2}"
+# this line prints the string and the query is 5 greater than or equal to -2
+puts "Is it greater or equal? #{5.0 >= -2}"
+# this line prints the string and the query is 5 less than or equal to -2
+puts "Is it less or equal? #{5.0 <= -2}"
+# I added these lines to uncover the % function
+puts "What does this do? #{3.0 % 4}"
+# I feel pretty good about myself right now
+puts "How about now? #{4.0 % 1}"
