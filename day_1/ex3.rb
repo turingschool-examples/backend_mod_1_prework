8bf89595e20a67e49514e5147a2fc7bad06bf4d0
William Petty <william@snakeweed.com>
2018-09-03 18:03:13 +0100

    adds day 1

---
+++ day_1/ex3.rb
@@ -0,0 +1,36 @@
+# Announce the sequence
+puts "I will now count my chickens:"
+# Count the chickens and display the answer
+puts "chickens #{25.0 + 30.0 / 6.0}"
+# Count roosters; display the answer
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# Announce a further sequence to count eggs
+puts "Now I will count the eggs:"
+
+# Calculate and display the number of eggs
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Pose a question regarding relative values
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Calculate and display the answer to the above question
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# Calculate and display the value left of the < sign
+puts "What is 3 + 2? #{3.0 + 2.0}"
+# Calculate and display the value right of the < sign
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+# State a conclusion regarding the above values
+puts "Oh, that's why it's false."
+
+# Transition to some more calculations
+puts "How about some more."
+
+# Print whether 5 is greater than -2
+puts "Is it greater? #{5.0 > -2.0}"
+# Print whether 5 is greater or equal to -2
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# Print whether 5 is less than or equal to -2
+puts "Is it less or equal? #{5.0 <= -2.0}"
