80e801a86c3dab5ecf38aa22c9ab92823abec6f9
Jennica Stiehl <jennica.stiehl@gmail.com>
2018-09-04 21:09:03 -0600

    changes made to ex3

--- day_1/exercises/ex3.rb
+++ day_1/exercises/ex3.rb
@@ -1,33 +1,31 @@
-+ #plus does addition
-- #minus does subtraction
-/ #slash does division
-* #asterisk does multiplication
-% #percent does division and provides the remainder
-< #less than does evaluation
-> #greater than does evaluation
-<= #less-than-equal
-<= #greater-than-equal
+# + #plus does addition
+# - #minus does subtraction
+# / #slash does division
+# * #asterisk does multiplication
+# % #percent does division and provides the remainder
+# < #less than does evaluation
+# > #greater than does evaluation
+# <= #less-than-equal
+# <= #greater-than-equal
 
 puts "I will now count my chickens:"
 
 puts "Hens #{25 + 30 /6}"
-puts "Roosters #{100- 25 * 3 % 4}"
-
+puts "Roosters #{100- 25 * 3 % 4}" # 100-((25*3)modulo 4) = 97
+#
 puts "Now I will count the eggs:"
-
-puts 3 + 2 + 1 - 5 + 4 % 2 
-
-puts "It is true that 3 + 2 < 5 - 7?"
-
+#
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#
 puts "It is true that 3 + 2 < 5 - 7?"
 
 puts 3 + 2 < 5 - 7
-
+#
 puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7 #{5 - 7}"
-
+puts "What is 5 - 7? #{5 - 7}"
+#
 puts "Oh, that's why it's false."
-
+#
 puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 <= -2}"
+puts "Is it greater or equal? #{5 >= -2}"
 puts "Is it less or equal? #{5 <= -2}"
