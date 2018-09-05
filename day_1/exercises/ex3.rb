b286a0933395974db774b7358f00b1e7cd80ce2b
Jennica Stiehl <jennica.stiehl@gmail.com>
2018-09-05 10:06:48 -0600

    addition to ex 3

--- day_1/exercises/ex3.rb
+++ day_1/exercises/ex3.rb
@@ -10,22 +10,24 @@
 
 puts "I will now count my chickens:"
 
-puts "Hens #{25 + 30 /6}"
-puts "Roosters #{100- 25 * 3 % 4}" # 100-((25*3)modulo 4) = 97
+puts "Hens #{(25 + 30 /6).to_f}"
+
+# 25*3 = 75 and 75/4 = 18, remainder of 3, 100-3=97
+puts "Roosters #{(100- 25 * 3 % 4).to_f}" # 100-((25*3)modulo 4) = 97
 #
 puts "Now I will count the eggs:"
 #
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts (3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 #
-puts "It is true that 3 + 2 < 5 - 7?"
+puts "It is true that #{3 + 2 < 5 - 7.0}?"
 
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2 < 5 - 7
 #
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 3 + 2? #{3 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7}"
 #
 puts "Oh, that's why it's false."
 #
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 >= -2}"
-puts "Is it less or equal? #{5 <= -2}"
+puts "Is it greater? #{5 > -2.0}"
+puts "Is it greater or equal? #{5 >= -2.0}"
+puts "Is it less or equal? #{5 <= -2.0}"
