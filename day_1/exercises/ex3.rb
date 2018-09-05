5da32dfda58193a6a9b54428672eff3dec336827
Jennica Stiehl <jennica.stiehl@gmail.com>
2018-09-04 20:48:54 -0600

    3 new exercises

---
+++ day_1/exercises/ex3.rb
@@ -0,0 +1,33 @@
++ #plus does addition
+- #minus does subtraction
+/ #slash does division
+* #asterisk does multiplication
+% #percent does division and provides the remainder
+< #less than does evaluation
+> #greater than does evaluation
+<= #less-than-equal
+<= #greater-than-equal
+
+puts "I will now count my chickens:"
+
+puts "Hens #{25 + 30 /6}"
+puts "Roosters #{100- 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 
+
+puts "It is true that 3 + 2 < 5 - 7?"
+
+puts "It is true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7 #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 <= -2}"
+puts "Is it less or equal? #{5 <= -2}"
