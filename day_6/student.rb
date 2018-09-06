8a2aa83bbb07fc679dbe3a85b7639574bc7ac73d
Brad Niedt <bradleyniedt@gmail.com>
2018-09-06 17:33:56 -0600

    Adds Day 7 Caesar Program

--- day_6/student.rb
+++ day_6/student.rb
@@ -1,9 +1,15 @@
 class Student
-  attr_accessor :first_name, :last_name, :primary_number
+  attr_accessor :first_name, :favorite_number
+
+  def initialize(first_name, favorite_number)
+    @first_name = first_name
+    @favorite_number = favorite_number
+  end
 
   def introduction(target)
     puts "Hi #{target}, I'm #{first_name}!"
   end
+
   def favorite_number
     7
   end
