2b11811a4dca12d7e5411d3238c3edac64e2250d
Justin Mauldin <justin.mauldin7@gmail.com>
2018-09-07 15:03:43 -0600

    adds day 1

---
+++ day_1/ex11.rb
@@ -0,0 +1,26 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+
+
+# print "What is your name? "
+# x = gets.chomp
+# print "What is your favorite color? "
+# y = gets.chomp
+# print "What is your favorite food? "
+# z = gets.chomp
+# puts "#{x} is your name, and you like the color #{y} and #{z} is your favorite food!"
+
+# print "Enter a number. "
+# x = gets.chomp.to_i
+# print "enter another number. "
+# y = gets.chomp.to_i
+# z = x + y
+#
+# puts "#{x} plus #{y} is equal to #{z}."
---
+++ day_1/ex3.rb
@@ -0,0 +1,39 @@
+# + does addition
+# - does subtraction
+# / does division
+# * does multiplicaton
+# % does division and gives you the remainder
+# < tells you if its less than by out putting true or false
+# > tells you if its greater than by out putting true or false
+# <= tells you if its less than our equal to by out putting true or false
+# >= tells you if its greater than our equal to by out putting true or false
+
+# prints the text in line 12
+puts "I will now count my chickens:"
+
+#prints the text in line 15 + does the math in brackets
+puts "Hens #{25.24 + 30.55 / 6.0}"
+#prints the text in line 17 + does the math in brackets
+puts "Rooserts #{100.11 - 25.44 * 3.89 % 4.11}"
+#prints the text in line 19
+puts "Now I will count the eggs:"
+#prints the result of the math done in line 21
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#prints the text in line 23
+puts "Is it true that 3 + 2 < 5 - 7?"
+#prints the result of the math done in line 25
+puts 3.44 + 2.55 < 5.77 - 7.11
+#prints the text and does the math in brackets on line 27
+puts "What is 3 + 2? #{3.55 + 2.01}"
+#prints the text and does the math in brackets on line 29
+puts "What is 5 - 7? #{5.45 - 7.89}"
+#prints the text in line 31
+puts "Oh, that's why it's false."
+#prints the text in line 33
+puts "how about some more."
+#prints the text and does the math in brackets on line 35
+puts "Is it greater? #{5.13 > -2.56}"
+#prints the text and does the math in brackets on line 37
+puts "Is it greater or equal? #{5.45 >= -2.22}"
+#prints the text and does the math in brackets on line 39
+puts "Is it less or equal? #{5.10 <= -2.01}"
