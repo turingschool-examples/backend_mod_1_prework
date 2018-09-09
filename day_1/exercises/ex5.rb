d533093f1af2e3cf36efcfec5387821e1add88e7
Jennica Stiehl <jennica.stiehl@gmail.com>
2018-09-09 08:24:17 -0600

    new files

---
+++ day_1/exercises/ex5.rb
@@ -0,0 +1,21 @@
+# More Variables and Printing
+
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+height_centimeters = height * 2.54
+weight_kilograms = weight * 0.45
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+puts "His height in centimeters is #{height_centimeters}."
+puts "His weight in kilograms is #{weight_kilograms}."
