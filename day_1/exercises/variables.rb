9dff6840682f49e44a87f87cdea5d8b004846dd1
Jennica Stiehl <jennica.stiehl@gmail.com>
2018-09-12 04:48:12 -0600

    adding new exercises

--- day_1/exercises/variables.rb
+++ day_1/exercises/variables.rb
@@ -4,25 +4,26 @@
 
 # example: Write code below to save your name to a variable and
 # print that variable to the terminal:
-name = "Harry Potter"
+name = "Jennica Stiehl"
 p name
 
 # Write code below to save the string 'Dobby' to a variable and
 # print that variable to the terminal:
 house_elf = "Dobby"
-#YOUR CODE HERE
+p house_elf
 
 # Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 # and print that variable to the terminal:
-# YOUR CODE HERE
+message = "Harry Potter must not return to Hogwarts"
+p message
 
 # example: Write code below to add 2 to the variable `students` and
 # print the result:
 students = 22
 students += 2
-p students
+p students + 2
 
 # Write the code below to subract 2 students from the `students` variable and
 # print the result:
 #YOUR CODE HERE
-p students
+p students - 2
