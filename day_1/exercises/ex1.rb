92f511e740a38306acb37f0357396612808804b1
Jennica Stiehl <jennica.stiehl@gmail.com>
2018-09-13 21:11:43 -0600

    completed 1-5)

--- day_1/exercises/ex1.rb
+++ day_1/exercises/ex1.rb
@@ -1,8 +1,14 @@
 puts "Hello World!"
 # puts "Hello Again"
+puts "Hello Again!"
 # puts "I like typing this."
+puts "I like typing this."
 # puts "This is fun."
+puts "This is fun."
 # puts "Yay! Printing."
+puts "Yay! Printing."
 # puts "I'd much rather you 'not'."
+puts "I'd much rather you 'not'."
 # puts 'I "said" do not touch this.'
+puts 'I "said" do not touch this.'
 puts "Print another line."
--- day_1/exercises/ex3.rb
+++ day_1/exercises/ex3.rb
@@ -17,13 +17,13 @@ puts "Roosters #{(100- 25 * 3 % 4).to_f}" # 100-((25*3)modulo 4) = 97
 #
 puts "Now I will count the eggs:"
 #
-puts (3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+puts (3.to_f + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 #
 puts "It is true that #{3 + 2 < 5 - 7.0}?"
 
 puts 3.0 + 2 < 5 - 7
 #
-puts "What is 3 + 2? #{3 + 2.0}"
+puts "What is 3 + 2? #{3 + 2.to_f}"
 puts "What is 5 - 7? #{5.0 - 7}"
 #
 puts "Oh, that's why it's false."
