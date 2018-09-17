fb8081a873248d0869ee4326a689b85133a6c24f
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-09-16 22:23:20 -0600

    Adds Day 7 work

---
+++ day_7/fizzbuzz.rb
@@ -0,0 +1,11 @@
+(1..INFINITY).each do |number|
+  if number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else
+    puts number
+  end
+end
