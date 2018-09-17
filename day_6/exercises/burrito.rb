532ce0b2538eb4eaeacb88ad5953969d69de0a88
William Petty <william@snakeweed.com>
2018-09-17 14:24:27 +0100

    Adds Day 6 Work

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -6,14 +6,36 @@
 
 class Burrito
   attr_reader :protein, :base, :toppings
+
   def initialize(protein, base, toppings)
     @protein  = protein
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(new_topping)
+    @toppings << new_topping
+  end
+
+  def remove_topping(unwanted_topping)
+    @toppings.delete(unwanted_topping)
+  end
+
+  def change_protein(new_protein)
+    @protein = new_protein
+  end
+
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+dinner.add_topping("jalepenos")
+p dinner.toppings
+
+dinner.remove_topping("guacamole")
+p dinner.toppings
+
+dinner.change_protein("Lamb")
+p dinner.protein
