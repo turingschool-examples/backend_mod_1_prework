86cb368c62029b50b703b53ebe5f38e80a25eda6
Julia Marco <marcootj@gmail.com>
2018-09-18 13:28:09 +0100

    Adds Day 4 Work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -11,15 +11,24 @@ print_name
 
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-  # YOUR CODE HERE
+  p "#{name}"
 end
 
 print_name("Albus Dumbledore")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
+def sum(num_1, num_2)
+  p "#{num_1} + #{num_2} = #{num_1 + num_2}"
+end
+
+sum(2, 3)
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+def another_movie(woman, man)
+  p "My favorite movie is #{woman} & #{man}"
+end
+
+another_movie("Bonnie", "Clyde")
