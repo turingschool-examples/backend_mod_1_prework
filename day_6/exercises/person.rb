f158cfd6adf7dd8d66d98fc1e28c732c2e8852e2
Ali Benetka <ali.benetka@gmail.com>
2018-09-19 14:30:07 -0600

    Add Day 6 work

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -3,3 +3,42 @@
 # terminal.
 
 #YOUR CODE HERE
+class Person
+  attr_reader :name, :height, :age
+
+  def initialize(name, height, age)
+    @name = name
+    @height = height
+    @age = age
+  end
+
+  def run
+    puts "#{name} likes to run!"
+  end
+
+  def birthday
+    puts "April 22"
+  end
+
+  def info
+    puts "#{name} is a student who is #{age} years old and is #{height} tall."
+  end
+
+
+end
+
+
+
+
+
+  #create objects
+student1 = Person.new("Ali", "5ft4in", "29")
+
+
+  #call methods
+p student1.name
+p student1.height
+p student1.age
+p student1.info
+p student1.run
+p student1.birthday
