74bb6594ff8487fcd7b5b0b68154c6da38194bad
Rene Casco <rene.casco@icloud.com>
2018-09-26 00:39:51 -0600

    adds day 1

---
+++ day_1/ex3.rb
@@ -0,0 +1,51 @@
+# prints I will now count my chickens:
+puts "I will now count my chickens:"
+
+# Prints Hens and  the result of 25 + (30 ÷ 6)
+puts "Hens #{ 25.0 + 30.0 / 6.0}"
+
+# Prints Roosters and the result of 100 - ((25x3) modulo 4)
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# prints Now I will count the eggs:
+puts "Now I will count the eggs:"
+
+# prints the result of 3 + 2 + 1 - 5 + (4 modulo 2) - (1 ÷ 4) + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# prints the string
+puts "Is it true that 3.1 + 2.2 < 5.1 - 7.3?"
+
+# prints true or false of (3.1+2.2)<(5.1-7.3)
+puts 3.1 + 2.2 < 5.1 - 7.3
+
+# prints What is 3.1 + 2.2? and the result of 3.1 + 2.2
+puts "What is 3 + 2? #{3.1 + 2.2}"
+
+# prints What is 5.1 - 7.3? and the result of 5.1 - 7.3
+puts "What is 5.1 - 7.3? #{5.1 - 7.3}"
+
+# prints Oh, that's why it's false,
+puts "Oh, that's why it's false,"
+
+# prints How about some more.
+puts "How about some more."
+
+# prints Is it greater? and true or false of 5.3 > -2.8
+puts "Is it greater? #{5.3 > -2.8}"
+
+# prints Is it greater or equal? and true or false of 5.3 >= -2.8
+puts "Is it greater or equal? #{5.3 >= -2.8}"
+
+# prints Is it less or equal? and true or false of 5.3 <= -2.8
+puts "Is it less or equal? #{5.3 <= -2.8}"
+
+# + does addition
+# - does subtraction
+# / does division
+# * does multiplication
+# % does modulo
+# < returns a boolean of less than comparisson
+# > returns a boolean of greater than comparisson
+# <= returns a boolean of less than or equal to comparisson
+# >= returns a boolean of greater than or equal to comparisson
