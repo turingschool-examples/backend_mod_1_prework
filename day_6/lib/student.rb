10b1fe50f8551e44e21907809c8722a000991c99
Rene Casco <rene.casco@icloud.com>
2018-09-30 14:01:20 -0600

    Student Class Completed

--- day_6/lib/student.rb
+++ day_6/lib/student.rb
@@ -1,2 +1,20 @@
 class Student
+  attr_reader :first_name,
+              :last_name,
+              :primary_phone_number
+
+  def initialize(first_name, last_name, primary_phone_number)
+    @first_name = first_name
+    @last_name = last_name
+    @primary_phone_number = primary_phone_number
+  end
+
+  def introduction(target)
+    "Hi #{target}, I'm #{first_name}!"
+  end
+
+  def favorite_number
+    7
+  end
+
 end
--- day_6/test/student_test.rb
+++ day_6/test/student_test.rb
@@ -5,8 +5,25 @@ require './lib/student'
 class StudentTest < Minitest::Test
 
   def test_it_exists
-    frank = Student.new
+    frank = Student.new("Frank", "Costello", "800 888 8888")
     assert_instance_of Student, frank
+  end
+
+  def test_it_has_first_attributes
+    frank = Student.new("Frank", "Costello", "800 888 8888")
+    assert_equal "Frank", frank.first_name
+    assert_equal "Costello", frank.last_name
+    assert_equal "800 888 8888", frank.primary_phone_number
+  end
+
+  def test_introduction_introduces_student
+    frank = Student.new("Frank", "Costello", "800 888 8888")
+    assert_equal "Hi Katrina, I'm Frank!", frank.introduction("Katrina")
+  end
+
+  def test_favorite_number_returns_expected_value
+    frank = Student.new("Frank", "Costello", "800 888 8888")
+    assert_equal 7, frank.favorite_number
   end
 
 end
