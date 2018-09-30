f1576206b8e34bb6a5e062945fa9be38dce00dab
Rene Casco <rene.casco@icloud.com>
2018-09-30 15:30:31 -0600

    burrito class completed

--- day_6/exercises/lib/burrito.rb
+++ day_6/exercises/lib/burrito.rb
@@ -13,12 +13,16 @@ class Burrito
     @toppings = toppings
   end
 
-  
+  def add_topping(topping)
+    toppings << topping
+  end
+
+  def remove_topping(topping)
+    toppings.delete("salsa")
+  end
+
+  def change_protein(new_protein)
+    @protein = new_protein
+  end
+
 end
-
-
-
-dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
-p dinner.protein
-p dinner.base
-p dinner.toppings
--- day_6/exercises/test/burrito_test.rb
+++ day_6/exercises/test/burrito_test.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/burrito.rb'
+require './lib/burrito'
 
 class BurritoTest < Minitest::Test
 
@@ -14,6 +14,23 @@ class BurritoTest < Minitest::Test
     assert_equal "Beans", dinner.protein
     assert_equal "Rice", dinner.base
     assert_equal ["cheese", "salsa", "guacamole"], dinner.toppings
+  end
+
+  def test_add_topping_adds_topping_to_array
+    dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
+    assert_equal ["cheese", "salsa", "guacamole", "cilantro"], dinner.add_topping("cilantro")
+  end
+
+  def test_remove_topping_removes_topping_from_array
+    dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
+    dinner.remove_topping("salsa")
+    assert_equal ["cheese", "guacamole"], dinner.toppings
+  end
+
+  def test_change_protein_changes_protein_value
+    dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
+    dinner.change_protein("beef")
+    assert_equal "beef", dinner.protein
   end
 
 end
