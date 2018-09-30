b532b9f018085567961408211f541d03465950be
Rene Casco <rene.casco@icloud.com>
2018-09-29 19:22:27 -0600

    WIP Accessor Methods in Action section

--- day_6/lib/good_dog.rb
+++ day_6/lib/good_dog.rb
@@ -1,11 +1,21 @@
 class GoodDog
-  attr_accessor :name
+  attr_accessor :name,
+                :height,
+                :weight
 
-  def initialize(name)
+
+  def initialize(name, height, weight)
     @name = name
+    @height = height
+    @weight = weight
   end
 
   def speak
     "#{name} says arf!"
   end
+
+  def get_info
+    "#{name} weighs #{weight} and is #{height} tall."
+  end
+
 end
--- day_6/test/good_dog_test.rb
+++ day_6/test/good_dog_test.rb
@@ -5,29 +5,25 @@ require './lib/good_dog'
 class GoodDogTest < Minitest::Test
 
   def test_it_exists
-    sparky = GoodDog.new("Sparky")
-    fido = GoodDog.new("Fido")
+    sparky = GoodDog.new("Sparky", "12 inches", "10 lbs")
     assert_instance_of GoodDog, sparky
-    assert_instance_of GoodDog, fido
   end
 
   def test_it_has_attributes
-    sparky = GoodDog.new("Sparky")
-    fido = GoodDog.new("Fido")
+    sparky = GoodDog.new("Sparky", "12 inches", "10 lbs")
     assert_equal "Sparky", sparky.name
-    assert_equal "Fido", fido.name
+    assert_equal "12 inches", sparky.height
+    assert_equal "10 lbs", sparky.weight
   end
 
   def test_that_speak_returns_specific_Arf
-    sparky = GoodDog.new("Sparky")
-    fido = GoodDog.new("Fido")
+    sparky = GoodDog.new("Sparky", "12 inches", "10 lbs")
     assert_equal "Sparky says arf!", sparky.speak
-    assert_equal "Fido says arf!", fido.speak
   end
 
-  def test_it_can_change_name
-    sparky = GoodDog.new("Sparky")
-    name = "Spartacus"
-    assert_equal "Spartacus", sparky.name
+  def test_get_info_gets_object_attributes
+    sparky = GoodDog.new("Sparky", "12 inches", "10 lbs")
+    assert_equal "Sparky weighs 10 lbs and is 12 inches tall.",
+    sparky.get_info 
   end
 end
