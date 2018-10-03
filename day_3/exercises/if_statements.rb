94e362d8b594ccc591dfcc6d4ed543c8822927b9
Rob Stringer <stringer525@gmail.com>
2018-10-02 18:36:36 -0600

    adds day 3

--- day_3/exercises/if_statements.rb
+++ day_3/exercises/if_statements.rb
@@ -9,18 +9,18 @@
   # if it is snowy, print "coat"
   # if it is icy, print "yak traks"
 
-  weather = 'snowy'
+  weather = 'sunny'
 
   if weather == 'sunny'
-    p "sunscreen"
+    puts "sunscreen"
   elsif weather == 'rainy'
-    p "umbrella"
+    puts "umbrella"
   elsif weather == 'snowy'
-    p "coat"
+    puts "coat"
   elsif weather == 'icy'
-    p "yak traks"
+    puts "yak traks"
   else
-    p "good to go!"
+    puts "good to go!"
   end
 
 # Manipulate the variable 'weather' to see if you can print something other
@@ -42,11 +42,16 @@
 # When num_quarters = 2, program should print "I have enough money for a gumball"
 # When num_quarters = 3, program should print "I have enough money for a gumball"
 
-num_quarters = 0
-
-puts "I have enough money for a gumball"
-puts "I don't have enough money for a gumball"
-
+num_quarters = 1
+if num_quarters == 0
+  puts "I don't have enough money for a gumball"
+elsif num_quarters == 1
+  puts "I don't have enough money for a gumball"
+elsif num_quarters == 2
+  puts "I have enough money for a gumball"
+elsif num_quarters == 3
+  puts "I have enough money for a gumball"
+end
 
 #####################
 # Using the variables defined below, determine if you have the
@@ -59,5 +64,15 @@ puts "I don't have enough money for a gumball"
 # When cups_of_flour = 2 and has_sauce = true, your program should print "I can make pizza"
 # When cups_of_flour = 3 and has_sauce = true, your program should print "I can make pizza"
 
-cups_of_flour = 1
+cups_of_flour = 3
 has_sauce = true
+
+if cups_of_flour == 1 || has_sauce == true
+  puts "I cannot make pizza!"
+elsif cups_of_flour == 1 || has_sauce == false
+  puts "I cannot make pizza!"
+elsif cups_of_flour == 2 || has_sauce == true
+  puts "I can make pizza!"
+elsif cups_of_flour == 3 || has_sauce == true
+  puts "I can make pizza!"
+end
--- day_3/questions.md
+++ day_3/questions.md
@@ -2,12 +2,35 @@
 
 1. What is a conditional statement? Give three examples.
 
+Conditional statements are true false.  Some common conditional statements are
+
+== >= <= > <
+
 1. Why might you want to use an if-statement?
+
+An if statement is a good way to branch off code.  If the user inputs something, you can change how the code responds, which is important for user interaction
 
 1. What is the Ruby syntax for an if statement?
 
+if x == example
+  puts "string or something else"
+end
+
 1. How do you add multiple conditions to an if statement?
+
+elsif and else
 
 1. What is the Ruby syntax for an if/elsif/else statement?
 
+if x == example
+  puts "string or something else"
+elsif == example2
+  puts "string or something else"
+else  
+  puts "Anything"
+end
+
+
 1. Other than an if-statement, can you think of any other ways we might want to use a conditional statement?
+
+An else statement can be used to help a user who isn't properly inputting information.  
