09284cfa13387a602cf0f130c65671964306af70
Rob Stringer <stringer525@gmail.com>
2018-11-01 19:26:48 -0600

    adds day 4 work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -4,22 +4,37 @@
 
 # example: Write a method below that, when called will print your name
 def print_name
-  p "Severus Snape"
+  puts ('Rob')
 end
 
 print_name
 
+
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-  # YOUR CODE HERE
+  puts ('Rob')
 end
 
-print_name("Albus Dumbledore")
+print_name('ARGUE ME')
+
+
+
+
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
+def number_method(num)
+  num = (3 + 5)
+end
+
+puts number_method(3)
+
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+def string_mash_method(stringA, stringB)
+  puts stringA + " " + stringB
+end
+
+puts string_mash_method("hey", "yall")
