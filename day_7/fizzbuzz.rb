11286ce46fda2875b0f0282388608eed3e435c89
Erin King <eohone@gmail.com>
2018-11-06 10:17:12 -0700

    Adds Day 7 Work

---
+++ day_7/fizzbuzz.rb
@@ -0,0 +1,36 @@
+## FizzBuzz
+arr = *(1..100)
+  arr.each do |x|
+    if x % 3 == 0
+      puts "Fizz"
+    end
+
+    if x % 5 == 0
+      puts "Buzz"
+    end
+
+    if x % 3 == 0 && x % 5 == 0
+      puts "FizzBuzz"
+    end
+
+    if x % 3 != 0 && x % 5 != 0
+      puts x
+    end
+  end
+
+
+
+
+#Create a file named fizzbuzz.rb and within that file, write a program that prints the numbers from 1 to 100 with the following rules:
+#* For any number that is a multiple of 3, print 'Fizz'
+#* For any number that is a multiple of 5, print 'Buzz'
+#* For any number that is a multiple of both 3 and 5, print 'FizzBuzz'
+#* For all other numbers, print the number.
+
+#The output of you program will look something like this:
+#```
+#=> 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, ..., 98, Fizz, Buzz
+#```
+
+### Bonus
+#Can you write the program so that it will run for any range of numbers?
