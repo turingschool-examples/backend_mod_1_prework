191c45a2b199bbb56ab887f8d730d263c881d9ec
Smitha Hosmani <smitha_hosmani@yahoo.com>
2018-11-07 12:52:00 -0700

    adds day 4

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -3,23 +3,35 @@
 # file from your terminal with the command `ruby day_4/exercises/methods.rb`
 
 # example: Write a method below that, when called will print your name
-def print_name
-  p "Severus Snape"
+def print_name(name)
+  puts name
 end
 
-print_name
+print_name("Smitha Hosmani")
 
 # Write a method that takes an argument of your name and prints your name
-def print_name(name)
-  # YOUR CODE HERE
+def print_name(first_name, last_name)
+  puts "My first name is #{first_name} and my last name is #{last_name}."
 end
 
-print_name("Albus Dumbledore")
+print_name("Smitha", "Hosmani")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
+def add(a, b)
+  puts "Adding #{a} + #{b}"
+  return a + b
+end
+
+sum = add(20, 10)
+
+puts "Gives us a sum of #{sum}"
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+def combo(thing1, thing2)
+  return "#{thing1} and #{thing2}"
+end
+
+puts "What goes well together? #{combo("peanut butter", "jelly")}"
--- day_4/questions.md
+++ day_4/questions.md
@@ -1,11 +1,20 @@
 ## Day 4 Questions
 
 1. In your own words, what is the purpose of a method?
+A method can be a programmer designed set of instructions that can store values in variables.  That method can be called/used repeatedly throughout the program/globally or locally.
 
 1. In the space below, create a method named `hello` that will print `"Sam I am"`.
+def  hello()
+  puts "Sam I am"
+end
 
 1. Create a method name `hello_someone` that takes an argument of `name` and prints `"#{name} I am"`.
+def hello_someone(name)
+  puts "#{name} I am"
+end
 
 1. How would you call or execute the method that you created above?
-
+puts "#{hello_someone(Smitha)} I am"
 1. What questions do you still have about methods in Ruby?
+I don't know what I don't know.
+I don't feel confident in my knowledge of methods.  I feel like I am guessing, I am not sure.
