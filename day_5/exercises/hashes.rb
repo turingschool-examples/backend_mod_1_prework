a07f176d0c39d33d2a6ba39780a893f9f30c40c6
Rob Stringer <stringer525@gmail.com>
2018-11-07 16:54:11 -0700

    adds day 5 work

--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -4,22 +4,24 @@
 
 # example: Write code below to print a hash that holds grocery store inventory
 foods = {apples: 23, grapes: 507, eggs: 48}
-p foods
+puts "There are #{foods[:apples]} apples, #{foods[:grapes]} grapes, and #{foods[:eggs]} eggs in stock."
 
 # Write code below that will print a hash of animals and their number
 # at the zoo. (an inventory of animals)
-zoo = #YOUR CODE HERE
-p zoo
+zoo = {zebras: 22, elephants: 80, cockroaches: 12000000}#YOUR CODE HERE
+puts "There are #{zoo[:zebras]} zebras, #{zoo[:elephants]} elephants, and #{zoo[:cockroaches]} cockroaches at the zoo."
 
 # Using the zoo that you created above, print all the keys in the hash.
-# YOUR CODE HERE
+puts zoo.keys
 
 # Using the zoo that you created above, print all the values in the hash.
-# YOUR CODE here
+puts zoo.values
 
 # Using the zoo taht you created above, print the value of the first item in
 # the hash
-# YOUR CODE HERE
+puts {zoo[:zebras]}
 
 # Add an animal to the zoo hash and print the updated hash.
 # YOUR CODE HERE
+zoo[:Jaguar] = 8
+puts zoo
--- day_5/questions.md
+++ day_5/questions.md
@@ -5,9 +5,17 @@
 1. In the space below, create a Hash stored to a variable named `pet_store`.  This hash should hold an inventory of items and the number of that item that you might find at a pet store.
 
 1. given the following `states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}`, how would you access the value `"Iowa"`?
+puts states[1]
 
 1. With the same hash above, how would we get all the keys?  All the values?
+puts states.keys [0, 1, 2]
+puts states.values [0, 1, 2]
 
 1. What is another example of when we might use a hash?  In this case, why is a hash better than an array?
 
+Hashes can be better than array when you need a string and value to be attached together.  It gives you the ability to link a string and a number
+
+
+
 1. What questions do you still have about hashes?
+How do we actually use these things to make functional software?
