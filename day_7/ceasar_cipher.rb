a8d1447bfead9146e20d609afe806b58f60af306
JennicaStiehl <jennica.stiehl@gmail.com>
2018-11-14 11:09:55 -0700

    adds refactor on CC

--- day_7/ceasar_cipher.rb
+++ day_7/ceasar_cipher.rb
@@ -1,19 +1,15 @@
 class Decode
 
-  def initialize(alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+  def initialize(alphabet = (65.chr..90.chr).to_a + (97.chr..122.chr).to_a)
     @alphabet = alphabet
-    @special_char_array = ['!','@','#','$','%','^','&','*','(',')','-','=','+','[',']',' ','?','.','~','`',',','/']
-  end
-
-  def split_alphabet
-    @alphabet.upcase.split(//)
+    @special_char_array = (32.chr..47.chr).to_a + (58.chr..64.chr).to_a + (91.chr..96.chr).to_a + (123.chr..126.chr).to_a
   end
 
   def make_messsage_array(message)
-    message.upcase.split(//)
+    message.split(//)
   end
 
-  def decipher(message, shifts = 1, alpha = split_alphabet)
+  def decipher(message, shifts = 1, alpha = @alphabet)
     message_array = make_messsage_array(message)
     message_array.map do |letter|
       if @special_char_array.any? { |char| char == letter } == true
@@ -22,11 +18,10 @@ class Decode
         index = alpha.index(letter) - shifts
         alpha.slice(index)
       end
-    end.join
+    end.join.upcase
   end
 
 end
 
 ceasar = Decode.new
-# p ceasar.split_alphabet
 p ceasar.decipher("Hello World!",5)
