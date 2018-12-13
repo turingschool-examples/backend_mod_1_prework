b7e264b35db10480d1f02a28581d8bee323c001e
sojurner <pykim0591@gmail.com>
2018-12-12 21:26:54 -0700

    Add ex7.rb

---
+++ day_1/ex7.rb
@@ -0,0 +1,10 @@
+# puts adds a new line to the end of each argument
+# print does not
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
