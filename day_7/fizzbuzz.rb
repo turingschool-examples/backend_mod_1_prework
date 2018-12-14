90194a98a7e27b5db9cb213fe27cd18718bc453a
sojurner <pykim0591@gmail.com>
2018-12-13 21:45:16 -0700

    add fizzbuzz solution

---
+++ day_7/fizzbuzz.rb
@@ -0,0 +1,18 @@
+def fizzbuzz()
+  n = 1
+
+  while n < 100
+    if n % 3 == 0 && n % 5 == 0
+      print "FizzBuzz, "
+    elsif n % 3 == 0
+      print "Fizz, "
+    elsif n % 5 == 0
+      print "Buzz, "
+    else
+      print "#{n}, "
+    end
+    n+=1
+  end
+end
+
+fizzbuzz()
