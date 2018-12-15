73d8a85580d18824b2b150df32533226d1189bc2
sojurner <pykim0591@gmail.com>
2018-12-15 08:36:02 -0700

    Completes person exercise

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -3,3 +3,22 @@
 # terminal.
 
 #YOUR CODE HERE
+
+class Person
+  attr_reader :name :legs :hair
+
+  def initialize(name, legs, hair)
+    @name = name
+    @legs = legs
+    @hair = hair
+  end
+
+  def walk 
+    p "#{@name} is movien #{legs} legs"
+  end
+
+  def shower
+    p "#{@name} washed his #{@hair} hair"
+end
+
+paul = Person.new("Paul", 2, 'black')
