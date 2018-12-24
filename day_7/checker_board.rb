f6a8d94328a53cf526014ad6e09112a8a8ab3453
Paul Kim <35910428+sojurner@users.noreply.github.com>
2018-12-23 18:43:00 -0700

    Update checker_board.rb

--- day_7/checker_board.rb
+++ day_7/checker_board.rb
@@ -1,22 +1,16 @@
 def checker_board(size)
-  template = 1.upto(size)
-
-  oddArr = template.map do |num|
-    if num % 2 == 0
-      num = "X"
-    else
-      num = " "  
-    end
-  end
-
-  template.each do |count|
-    if count % 2 == 0
-      puts oddArr * ""
-    else
-      puts oddArr.join.gsub(" X", "X ")
-    end
+  template = (1..size).to_a
+  oddArr = template.map {|num| num % 2 == 0 ? "X" : " "}
+  
+  if size > 1
+    template.each {|count| count % 2 == 0 ? count = oddArr * "" : count = oddArr.join.gsub(" X", "X ")
+      puts count
+    }
+  else
+    puts "size has to be greater than #{size}"
   end
 end
+
 
 checker_board(5)
 checker_board(10)
