f22ccfcbfc9587eb2ba45fd45dc0023a9ee5bf83
Paul Kim <35910428+sojurner@users.noreply.github.com>
2018-12-23 18:28:50 -0700

    Update ceasar_cipher.rb

--- day_7/ceasar_cipher.rb
+++ day_7/ceasar_cipher.rb
@@ -4,7 +4,7 @@ def ceasar_cipher(str, shift_val)
   split_arr = str.upcase.split(//)
 
   split_arr.map {|char|
-    char == " " ? char : rotated_alpha[alphabet.find_index(char)]
+    char == " " ? char : shifted_alphabet[alphabet.find_index(char)]
   }.join("")
  
 end
