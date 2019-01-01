7c8bb49ab2b908e1cbc622919eabf97b7fb4109f
Paul Kim <35910428+sojurner@users.noreply.github.com>
2018-12-31 22:52:22 -0700

    Update fizzbuzz.rb

--- day_7/fizzbuzz.rb
+++ day_7/fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(start, last)
   while start <= last
     print start % 3 == 0 && start % 5 == 0 ? 
-    "FizzBuzz, " : start % 3 == 0 ? 
-    "Fizz, " : start % 5 == 0 ? 
-    "Buzz, " : "#{start}, "
+    " FizzBuzz " : start % 3 == 0 ? 
+    " Fizz " : start % 5 == 0 ? 
+    " Buzz " : " #{start} "
     start += 1
   end
 end
