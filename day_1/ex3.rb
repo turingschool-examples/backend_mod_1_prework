8736b7f797c99408803b2f056ce54c6770e0829b
Chi Tran <chidtran@gmail.com>
2019-01-04 16:43:40 -0700

    adds day 1

--- day_1/ex3.rb
+++ day_1/ex3.rb
@@ -5,7 +5,7 @@ puts "Hens #{25.0 + 30.0 / 6.0}"
 puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 # print string
 puts "Now I will count the eggs:"
-# print solution
+# print computation solution
 puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 # print string
 puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
