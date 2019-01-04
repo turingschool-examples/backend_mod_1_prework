0d2453f25962a63bb08a81bb083bdc750258d682
Paul Kim <35910428+sojurner@users.noreply.github.com>
2019-01-04 15:06:04 -0700

    Update checker_board.rb

--- day_7/checker_board.rb
+++ day_7/checker_board.rb
@@ -1,11 +1,8 @@
 def checker_board(size)
   template = (1..size).to_a
-  oddArr = template.map {|num| num % 2 == 0 ? "X" : " "}
-  evenArr = template.map {|num| num % 2 == 0 ? " " : "X"}
+  evenArr = template.map {|num| num % 2 == 0 ? "X" : " "}
+  oddArr = template.map {|num| num % 2 == 0 ? " " : "X"}
   
-  template.each {|count| puts count % 2 == 0 ? oddArr * "" : evenArr * ""} if size > 1
+  template.each {|count| puts count % 2 == 0 ? evenArr * "" : oddArr * ""} if size > 1
   puts "size has to be greater than #{size}" unless size > 1
 end
-
-checker_board(5)
-checker_board(10)
