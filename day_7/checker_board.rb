a5fd5a54dd7fcd4e9db2a283e24f64cdd02e5146
Paul Kim <35910428+sojurner@users.noreply.github.com>
2019-01-07 22:18:51 -0700

    Update checker_board.rb

--- day_7/checker_board.rb
+++ day_7/checker_board.rb
@@ -1,8 +1,8 @@
 def checker_board(size)
   template = (1..size).to_a
-  evenRow = template.map {|num| num % 2 == 0 ? "X" : " "} * ""
-  oddRow = template.map {|num| num % 2 == 0 ? " " : "X"} * ""
+  evenRow = template.map {|num| num.even? ? "X" : " "} * ""
+  oddRow = template.map {|num| num.odd? ? "X" : " "} * ""
   
-  template.each {|count| puts count % 2 == 0 ? evenRow : oddRow } if size > 1
+  template.each {|count| puts count.even? ? evenRow : oddRow } if size > 1
   puts "size has to be greater than #{size}" unless size > 1
 end
