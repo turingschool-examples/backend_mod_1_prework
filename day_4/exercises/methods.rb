84ae084a6384904bc889d647c10803ea9e591832
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-01-08 11:13:37 -0700

    Adds Day 4 Work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -11,15 +11,26 @@ print_name
 
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-  # YOUR CODE HERE
+  p name
 end
 
-print_name("Albus Dumbledore")
+print_name("Glynnis O'Connell")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
+def sum(a, b)
+  return a + b
+end
+
+p sum(2, 4)
+
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+
+def movie(male, female)
+  p "When #{male} met #{female}."
+end
+
+movie("Harry", "Sally")
