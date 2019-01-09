44bd6e3f31c9656b7bfe5808dcf9a79c74283305
Paul Kim <35910428+sojurner@users.noreply.github.com>
2019-01-09 07:46:33 -0700

    Update ceasar_cipher.rb

--- day_7/ceasar_cipher.rb
+++ day_7/ceasar_cipher.rb
@@ -9,12 +9,11 @@ class CeasarCipher
     
   def shift(shift_val)
   shifted_alphabet = @alphabet.rotate(shift_val * -1)
-  
   @phrase.upcase.gsub(/./) {|char|
     char == " " ? char : shifted_alphabet[alphabet.find_index(char)]
   }
   end
-
+  
 end
 
 x = CeasarCipher.new('Hello world')
