809d5b065c1819bf2e36a2b87eeff103c71c6a65
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-01-09 12:06:41 -0700

    Adds Day 4 Work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -4,22 +4,34 @@
 
 # example: Write a method below that, when called will print your name
 def print_name
-  p "Severus Snape"
+  p "Jeremy Bennett"
 end
 
 print_name
 
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-  # YOUR CODE HERE
+  p "#{name}"
 end
 
-print_name("Albus Dumbledore")
+print_name("Jeremy Bennett")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
+def sum(a, b)
+  p a + b
+end
+
+sum(4, 6)
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+
+def longer_string(man, woman)
+  a = "When #{man}"
+  b = " Meets #{woman}"
+  p a << b
+end
+
+longer_string("Harry", "Sally")
--- day_4/questions.md
+++ day_4/questions.md
@@ -2,10 +2,26 @@
 
 1. In your own words, what is the purpose of a method?
 
+A method gives the programmer the ability to manipulate an object or several objects in a consistent way, or to manipulate an object in several different ways.
+
 1. In the space below, create a method named `hello` that will print `"Sam I am"`.
+
+def hello()
+  p "Sam I am"
+end
+
+hello
 
 1. Create a method name `hello_someone` that takes an argument of `name` and prints `"#{name} I am"`.
 
+def hello_someone(name)
+  p "#{name} I am"
+end
+
+hello_someone(Sam)
+
 1. How would you call or execute the method that you created above?
+
+hello_someone(Sam)
 
 1. What questions do you still have about methods in Ruby?
