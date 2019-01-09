3fc97bada15b812d694413854f45d7a8f8db418c
Mills Provosty <millsprovosty@gmail.com>
2019-01-09 08:08:27 -0600

    Adds Day 6

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -5,27 +5,70 @@
 # 3. change_protein
 
 class Burrito
-  attr_reader :protein, :base, :toppings
-  def initialize(protein, base, toppings)
+  attr_reader :protein, :base, :toppings, :add_topping, :remove_topping, :change_protein
+  def initialize(protein, base, toppings, add_topping, remove_topping, change_protein)
     @protein  = protein
     @base     = base
     @toppings = toppings
 
-    def add_toppings(top_1, top_2)
+    def add_toppings(add_toppings)
+      puts "I would like to add #{add_toppings} to my burrito."
+    end
 
+    def remove_topping(remove_topping)
+      puts "I don't think I want #{remove_topping} on my burrito."
+    end
+
+    def change_protein(change_protein)
+      puts "And I believe I'd like to change the beans to #{change_protein}."
     end
   end
 end
 
 
 
-dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
-dinner.add_toppings("bacon", "sour cream")
+
+dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"], "Choose your own topping", "Remove a topping", "Change my protien")
 puts "What are my protein options?"
 puts dinner.protein
-puts "Great. What is the base?"
-puts dinner.base
-puts "And my topping options?"
-puts dinner.toppings
-puts "Is there anything else I could get?"
-puts "Yes, how about some #{top_1} or #{top_2}?"
+puts "If you're ok with that, say 'Yes'. If not, say 'No' "
+answer = $stdin.gets.chomp
+
+if answer == "Yes"
+  puts "Great Choice."
+elsif answer == "No"
+  puts "Please enter what you would like:"
+  change_protein = gets
+  puts "I would like #{change_protein}"
+end
+
+puts "Great. Your base option is #{dinner.base}."
+puts "And your topping options are #{dinner.toppings}, unless you'd like to choose your own topping."
+puts "If you're ok with those toppings, say 'Yes', if not, say 'No'."
+answer = $stdin.gets.chomp
+
+if answer == "Yes"
+  puts "Sounds good!"
+
+elsif answer == "No"
+  puts "Which toppings would you like to remove: Cheese? Salsa? Guacamole?"
+  answer = $stdin.gets.chomp
+
+    if answer == "Cheese"
+      puts "Ok, keep the Salsa and Guac."
+
+    elsif answer == "Salsa"
+      puts "Ok, Keep the Cheese and Guacamole."
+
+    elsif answer == "Guacamole"
+      puts "Ok, Keep the Cheese and Salsa."
+
+    else
+      puts "It sounds like you'd like to build your own."
+      puts "What toppings would you like to add? Enter:"
+      add_toppings = gets
+      puts "I would like to add #{add_toppings} to my burrito."
+    end
+  end
+
+puts "Coming right up!"
