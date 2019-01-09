4414075fd5399b2cfbdf1a9559f183d1fdfc90fb
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-01-09 11:05:49 -0700

    adding corrected dog.rb

--- day_6/exercises/dog.rb
+++ day_6/exercises/dog.rb
@@ -3,17 +3,35 @@
 # hunger status.
 
 class Dog
-  attr_reader :name
+  attr_reader :name, :breed, :age, :hungry
 
-  def initialize(name)
+  def initialize(name, breed, age)
     @name   = name
+    @breed = breed
+    @age = age
+    @hungry = true
+  end
+
+  def dog_info
+    puts "#{name} is a #{breed} and is #{age} years old."
+  end
+
+  def eat
+    @hungry = false
   end
 
   def play
-    puts "#{name} is hungry!"
+    @hungry = true
+  end
+
+  def bark
+    puts "woof!"
   end
 end
 
-fido = Dog.new("Fido")
-puts fido.name
+fido = Dog.new("Fido", "cavapoo", 3)
+fido.dog_info
+fido.eat
+puts fido.hungry
 fido.play
+puts fido.hungry
