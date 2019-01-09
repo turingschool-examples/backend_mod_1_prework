a627925a3f6e1384ce77486ccfc5a04e5cc53d50
Santiago Caraveo <santiagocbrvg@hotmail.com>
2019-01-09 01:28:48 -0700

    Adds Day 7 Work

---
+++ day_7/10_little_monkeys.rb
@@ -0,0 +1,15 @@
+print "how many monkey jumping on the bed would you like: "
+
+def monkey(numberm)
+  puts "#{numberm} little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said,
+Get those monkeys right to bed!"
+end
+x = gets.chomp
+monkey(x)
+
+
+#if monkey == "0"
+#  puts "there is not monkeys :( "
+#end
---
+++ day_7/10_little_monkeysIF.rb
@@ -0,0 +1,87 @@
+print "how many monkey jumping on the bed would you like: "
+x = gets.chomp
+#monkeys = ["one", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+#monkeys[2]
+i = x
+if x == "0"
+  puts "there is not monkeys :("
+
+elsif x == "1"
+  puts "----------------------------------------"
+  puts "One little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+puts "----------------------------------------"
+
+elsif x == "2"
+  puts "----------------------------------------"
+  puts "two little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+puts "----------------------------------------"
+
+elsif x == "3"
+  puts "----------------------------------------"
+  puts "three little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+puts "----------------------------------------"
+
+elsif x == "4"
+  puts "----------------------------------------"
+  puts "fore little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+puts "----------------------------------------"
+
+elsif x == "5"
+  puts "----------------------------------------"
+  puts "five little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+puts "----------------------------------------"
+
+elsif x == "6"
+  puts "----------------------------------------"
+  puts "six little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+puts "----------------------------------------"
+
+elsif x == "7"
+  puts "----------------------------------------"
+  puts "seven little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+puts "----------------------------------------"
+
+elsif x == "8"
+  puts "----------------------------------------"
+  puts "eight little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+puts "----------------------------------------"
+
+elsif x == "9"
+  puts "----------------------------------------"
+  puts "nine little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+puts "----------------------------------------"
+
+elsif x == "10"
+  puts "----------------------------------------"
+  puts "ten little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+puts "----------------------------------------"
+
+elsif x > "10"
+  puts "----------------------------------------"
+  puts "#{i} little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+puts "----------------------------------------"
+
+
+end
