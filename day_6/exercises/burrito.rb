7482ddabba35f1c025c5bd16566219b1e56d7f2a
Corey Sheesley <corey.sheesley@gmail.com>
2019-01-13 12:36:47 -0700

    Day 6 Work - Updated

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -6,6 +6,7 @@
 
 class Burrito
   attr_accessor :protein, :base, :toppings
+
   def initialize(protein, base, toppings)
     @protein  = protein
     @base     = base
@@ -13,24 +14,28 @@ class Burrito
   end
 
   def add_topping(add_topping)
-    p "#{add_topping} has been added to your burrito."
+    self.toppings << add_topping # code to add topping to toppings array
+
   end
 
   def remove_topping(remove_topping)
-    p "#{remove_topping} has been removed from your burrito."
+    self.toppings.delete(remove_topping)# code to delete topping from toppings array
   end
 
   def change_protein(new_protein)
-    self.protein = new_protein
-    puts "Protein has been changed to #{self.protein}."
+    self.protein = new_protein # code to change protein state
+  end
+
+  def burrito_details
+    p "Protein: #{@protein} - Base: #{@base} - Toppings: #{@toppings.join(", ")}"
   end
 end
 
-dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
-p dinner.protein
-p dinner.base
-p dinner.toppings
+dinner = Burrito.new("beans", "rice", ["cheese", "salsa", "guacamole"])
 
 dinner.add_topping("sour cream")
+dinner.add_topping("bacon")
 dinner.remove_topping("salsa")
 dinner.change_protein("barbacoa")
+
+dinner.burrito_details
