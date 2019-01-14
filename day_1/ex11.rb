c10aa983b804ac2afb4dc111a3cc8dac5008c502
Zach Leach <zachleach@techoverco.com>
2019-01-14 11:46:08 -0700

    Added exercise 11 to backend prework.

---
+++ day_1/ex11.rb
@@ -0,0 +1,14 @@
+print "How old are you?"
+puts "\n"
+puts "-------- Be sure to type in a number ----------"
+age = gets.chomp
+print "How tall are you? "
+puts "\n"
+puts "-------- Be sure to type in a number ----------"
+height = gets.chomp
+print "How much do you weight?"
+puts "\n"
+puts "-------- Be sure to type in a number ----------"
+weight = gets.chomp
+
+puts "So, you're #{age} years old, #{height} tall, and weigh #{weigh} pounds."
