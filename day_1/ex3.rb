0499a31530376cacea56438335d92014c0a23d9c
William Homer <billyhomeriii@gmail.com>
2019-02-09 16:23:43 -0700

    Complete step 4 of Day1

---
+++ day_1/ex3.rb
@@ -0,0 +1,23 @@
+puts "I will nbow count my chickens:"
+# calculates the number of Hens and Roosters
+puts "Hens #{25 + 30 / 6.5}"
+puts "Roosters #{100 - 25 * 3 %4}"
+
+puts "Now I will count the eggs:"
+# count's each egg and displayed it.
+puts 3.5 + 2 + 1 - 5.9 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 -7?"
+# Determines if the above is true or false
+puts 3 + 2 < 5 -7
+# Add's two numbers and subtracts two numbers
+puts "what is 3 + 2? #{3 + 2}"
+puts "What is 5.2 - 7.6? #{5.2 - 7.6}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+# determines if 5 > 2 is greater, greater or equal or less than or equal.
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
---
+++ day_1/tip.rb
@@ -0,0 +1,5 @@
+puts "Determine a 20% tip on a $76.78 dollar bill"
+# Determine the tip amount of 20%
+puts "A 20% tip will increase the bill: $#{(76.78 * 0.2).round(2)}"
+# Determine the total bill amount with 20% tip
+puts "The new total of the bill will be: $#{(76.78 * 1.2).round(2)}"
