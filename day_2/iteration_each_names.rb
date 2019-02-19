180f708f96fd8f87b84a3a28bf8f3ab43f9bba5f
Alexander Mathieu <mathieu.alexanderm@gmail.com>
2019-02-18 20:19:57 -0700

    Modifies Day 2 Work

--- day_2/iteration_each_names.rb
+++ day_2/iteration_each_names.rb
@@ -3,3 +3,13 @@ names = ["Alice Smith", "Bob Evans", "Roy Rogers"]
 names.each do |full_name|
   puts full_name
 end
+
+p names.first
+
+p names.last
+
+p names.map { |name| name.gsub(/[^A-Z]/, '') }
+
+p "#{names.last} has #{names.last.length} characters... including the space."
+
+p names.join.delete(' ').length
