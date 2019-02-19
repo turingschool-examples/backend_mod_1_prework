8a5aa205e0998165509c2e02bd7b9e4448b4dec0
James Cape <james.cape@gmail.com>
2019-02-18 20:36:58 -0700

    Adds Day 5 Work

--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -3,23 +3,30 @@
 # file from your terminal with the command `ruby day_5/exercises/hashes.rb`
 
 # example: Write code below to print a hash that holds grocery store inventory
-foods = {apples: 23, grapes: 507, eggs: 48}
-p foods
+#foods = {
+#  apples => 23,
+#  grapes => 507,
+#  eggs => 48
+#}
+#p foods
 
 # Write code below that will print a hash of animals and their number
 # at the zoo. (an inventory of animals)
-zoo = #YOUR CODE HERE
+zoo = [
+  {"monkeys" => 5, "elephants" => 18, "zebras" => 9}
+]
 p zoo
 
 # Using the zoo that you created above, print all the keys in the hash.
-# YOUR CODE HERE
+p zoo.map {|x| x.keys}
 
 # Using the zoo that you created above, print all the values in the hash.
-# YOUR CODE here
+p zoo.map {|x| x.values}
 
-# Using the zoo taht you created above, print the value of the first item in
+# Using the zoo that you created above, print the value of the first item in
 # the hash
-# YOUR CODE HERE
+p zoo.map {|x| x.values.first}
 
 # Add an animal to the zoo hash and print the updated hash.
-# YOUR CODE HERE
+zoo[1] = "bats"
+p zoo
