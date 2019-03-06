6c037e123967a61f7b9c47a5cea8348ffd416f30
Ryan Miller <ryanmillergm@gmail.com>
2019-03-06 15:27:03 -0700

    Updated day 7 work

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -1,29 +1,25 @@
 ## 10 Little Monkeys
 
 # Create a file named 10_little_monkeys.rb and within that file, write a program that will print the following nursery rhyme, but for *10* monkeys.
+require 'rubygems'
+require 'to_words'
 
 def little_monkeys(num)
-counter = num
-
-  while counter > 0
-
-puts "#{counter} little monkeys jumping on the bed,"
-puts "one fell off and bumped his head,"
-puts "Mama called the doctor and the doctor said,
-/'No more monkeys jumping on the bed!/''"
-
-counter -= 1
-end
+  counter = num
+  while counter > 1
+    puts "#{counter.to_words} little monkeys jumping on the bed,"
+    puts "one fell off and bumped his head,"
+    puts "Mama called the doctor and the doctor said,
+    /'No more monkeys jumping on the bed!/''"
+  counter -= 1
+  end
 
 
 
-puts "One little monkey jumping on the bed,
-  He fell off and bumped his head,
-  Mama called the doctor and the doctor said,
-  /'Get those monkeys right to bed!/'"
-end
+  puts "One little monkey jumping on the bed,
+    He fell off and bumped his head,
+    Mama called the doctor and the doctor said,
+    /'Get those monkeys right to bed!/'"
+  end
 
 little_monkeys(10)
-
-### Bonus
-# Can you write the program so that it will run for any number of monkeys?
