7265dcb3b7624ac1c28c7308ee39d95062e50247
Nathan Thomas <NathanThomas@Nathans-MacBook-Air.local>
2019-03-14 09:56:15 -0700

    Update changes

--- day_1/ex6.rb
+++ day_1/ex6.rb
@@ -1,23 +1,35 @@
+#setting the value of types_of_people to 10
 types_of_people = 10
-x = "There are #{types_of_people} types of people."
-binary = "binary"
+# setting the value of types_of_people to a sting.
+x = 'There are #{types_of_people} types of people.'
+# setting the value of binary to binary
+binary = 'binary'
+# setting the value of the variable do_not to don't.
 do_not = "don't"
+# setting the value of y to a string and inserting the values of two additional variables binary and do_not.
 y = "Those who know #{binary} and those who #{do_not}."
-
+# printing the value of x.
 puts x
-
+# printing the value of y.
 puts y
-
+# printing a string with the value of x.
 puts "I said #{x}."
-
-puts "I said #{y}."
-
+# printing a string with the value of y.
+puts "I also said #{y}."
+# setting the value of hilarious to false
 hilarious = false
+# setting the value of joke_evaluation to a string and adding the value of hilarious to the string.
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
-
+# pringing the value of joke_evaluation
 puts joke_evaluation
-
+# setting the value of w to a string
 w = "This is the left side of..."
+# setting the value of e to a string
 e = "a string with a right side."
-
+# printing the values of w plus the values of e.
 puts w + e
+
+# Adding the two strings w and e with a + makes a longer string because using the + concatenates...
+#the values of both w and e making one single sring.
+
+# When you change strings to single quotes instead of double quotes it confuses the same character used in abbrievations.
