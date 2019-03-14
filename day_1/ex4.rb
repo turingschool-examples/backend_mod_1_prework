c712decefad6597a53ffc99737258bdd3efe3f6d
Nathan Thomas <NathanThomas@Nathans-MacBook-Air.local>
2019-03-14 09:46:46 -0700

    Add comments

--- day_1/ex4.rb
+++ day_1/ex4.rb
@@ -1,10 +1,18 @@
+#cars equals 100
 cars = 100
+#spacein a car is 4
 space_in_a_car = 4
+# drivers equals 30
 drivers = 30
+#passengers = 90
 passengers = 90
+#cars_not_driven equals 100 - 30
 cars_not_driven = cars - drivers
+# cars_driven equals the same number as there are drivers
 cars_driven = drivers
+#carpool_capacity equals the number of cars driven times space_in_car
 carpool_capacity = cars_driven * space_in_a_car
+#average_passengers_per_car = number of passengers divided by cars_driven
 average_passengers_per_car = passengers / cars_driven
 
 puts "There are #{cars} cars available."
