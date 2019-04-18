d754fefca404b1b6e6d0db796e33405800b1a7ea
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-04-18 16:48:26 -0600

    Add day 1

---
+++ day_1/ex3.rb
@@ -0,0 +1,37 @@
+# Printing
+puts "I will now count my chickens:"
+
+# Printing and calculating
+puts "Hens #{25.0 + 30 / 6}"
+# Printing and calculating
+puts "Roosters #{100.0 - 25 * 3 % 4}"
+
+# Printing
+puts "Now I will count the eggs:"
+
+# Calculating
+puts 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Printing and calculating
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Calculating
+puts 3.0 + 2 < 5 - 7
+
+# Printing and calculating
+puts "What is 3 + 2? #{3.0 + 2}"
+# Printing and calculating
+puts "What is 5 - 7? #{5.0 - 7}"
+
+# Printing
+puts "Oh, that's why it's false."
+
+# Printing
+puts "How about some more."
+
+# Printing and calculating
+puts "Is it greater? #{5.0 > -2}"
+# Printing and calculating
+puts "Is it greater or equal? #{5.0 >= -2}"
+# Printing and calculating
+puts "Is it less or equal? #{5.0 <= -2}"
