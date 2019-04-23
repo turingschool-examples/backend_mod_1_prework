112ca2ef38995d0c3ae71104851ceac7821932d0
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-04-23 14:06:47 -0600

    Add study drills

--- day_1/ex6.rb
+++ day_1/ex6.rb
@@ -1,21 +1,40 @@
+# Delcaring a variable
 types_of_people = 10
+# Declaring a variable that contains a variable within a string
 x = "There are #{types_of_people} types of people."
+# Declaring a variable
 binary = "binary"
+# Declaring a variable
 do_not = "don't"
+# Delcaring a variable that contains two variables within a string
 y = "Those who know #{binary} and those who #{do_not}."
 
+# Print variable x
 puts x
+# Print variable y
 puts y
 
+# Print a string and variable
 puts "I said: #{x}."
+# Print a string and variable
 puts "I also said: '#{y}'."
 
+# Declaring a variable
 hilarious = false
-joke_evaluation = "Isn't that joke so funny?! #{false}"
+# Declaring a variable within a string
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+# Print variable joke_evaluation
 puts joke_evaluation
 
+# Declaring a variable
 w = "This is the left side of..."
+# Declaring a variable
 e = "a string with a right side."
 
+# Print two variables
 puts w + e
+
+# Study Drills
+# 4. Adding 'w' and 'e' combines the two strings within the variables
+# 5. Nothing changes. The string still prints normally
