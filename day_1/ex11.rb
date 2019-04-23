f6e396383d5664a6565975b50cd9410bce87c8ce
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-04-23 14:11:19 -0600

    Add study drill questions

--- day_1/ex11.rb
+++ day_1/ex11.rb
@@ -6,3 +6,13 @@ print "How much do you weigh? "
 weight = gets.chomp
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+print "What's your first name? "
+first_name = gets.chomp
+print "What's your last name? "
+last_name = gets.chomp
+
+puts "Nice to meet you #{first_name} #{last_name}, my name is Computer."
+
+# Study Drills
+# 1. Takes string input from a user
