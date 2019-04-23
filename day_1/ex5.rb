78c317db1e5a3ab77af496dfcbc7f5fc2cf020ee
username <wnt925@gmail.com>
2019-04-23 14:28:01 -0600

    Add comments

--- day_1/ex5.rb
+++ day_1/ex5.rb
@@ -1,11 +1,19 @@
+#sets var my_name equal to Zed Shaw
 my_name = 'Zed A. Shaw'
+#sets var my_age equal to intergewr 35
 my_age = 35 # not a lie in 2009
+#sets variable my_height equal to 74 inches
 my_height = 74 * 2.54 # cm
+#sets var my_weight eqwual to 180 lbs
 my_weight = 180 * 0.453 #kilo
+#sets var my_eyes equal to string blue
 my_eyes = 'Blue'
+#sets variable my_teeth equal to string white
 my_teeth = 'White'
+#sets variable mya_hair equal to string brown
 my_hair = 'Brown'
 
+#uses string interpolation to print string + varaible value inside of the string
 puts "Let's talk about #{my_name}."
 puts "He's #{my_height} cm. tall."
 puts "He's #{my_weight} kilo heavy."
