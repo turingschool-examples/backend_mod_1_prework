f468d981d44b171ae76b1f385a8bca88a5d3c5e6
username <wnt925@gmail.com>
2019-04-23 14:30:46 -0600

    Edit exercises

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -4,32 +4,31 @@
 
 # example: Write a method below that, when called will print your name
 def print_name
-  p "Severus Snape"
+  p "Will Thompson"
 end
 
 print_name
 
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-name = "Will Thompson"
-
+p "#{name}"
 end
 
+print_name("Will Thompson")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
 def sum(num1, num2)
-  puts num1 + num2
+num1 + num2
 end
 
-puts sum(1, 2)
+p sum(1, 2)
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
-
 def string_compile(dog, breed)
-  puts "#{dog} is an #{breed}"
+  p "#{dog} is an #{breed}"
 end
 
 string_compile("Rio", "Akita")
