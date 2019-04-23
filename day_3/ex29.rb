3865e9819de5d364d3b42711cc326e8b7fa7dbba
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-04-23 14:29:43 -0600

    Add study drill questions

---
+++ day_3/ex29.rb
@@ -0,0 +1,43 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats
+  puts "Too many cats! The world is doomed!"
+end
+
+if people > cats
+  puts "Not many cats! The world is saved!"
+end
+
+if people < dogs
+  puts "The world is drooled on!"
+end
+
+if people > dogs
+  puts "The world is dry!"
+end
+
+dogs += 5
+
+if people >= dogs
+  puts "People are greater than or equal to dogs."
+end
+
+if people <= dogs
+  puts "People are less than or equal to dogs."
+end
+
+if people == dogs
+  puts "People are dogs."
+end
+
+if people >= cats
+  puts "Test"
+end
+
+# Study Drills
+# 1. If the statement is true, the block is executed
+# 2. For readability
+# 3. Nothing happens
+# 4.
