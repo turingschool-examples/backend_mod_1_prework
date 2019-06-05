241a57616efbb24a0e61300df906d4a31e8788ca
Fenton Taylor <fentontaylor@gmail.com>
2019-06-05 17:02:21 -0600

    Finished ex1.rb

---
+++ day_1/ex1.rb
@@ -0,0 +1,15 @@
+# puts "Hello World!"
+# puts "Hello Again"
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay! Printing."
+# puts "I'd much rather you 'not'."
+# puts 'I "said" do not touch this.'
+
+if 4 > 3
+  puts "Yep."
+else
+  puts "Nope."
+end
+
+puts "This is the only line you can see!"
