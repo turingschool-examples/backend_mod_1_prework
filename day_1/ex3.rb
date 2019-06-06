21dd77fbb83300af197892924e43f98e2c2c55b7
Fenton Taylor <fentontaylor@gmail.com>
2019-06-05 20:49:47 -0600

    Finished ex3.rb and study drills

--- day_1/ex3.rb
+++ day_1/ex3.rb
@@ -3,31 +3,31 @@
 puts "I will now count my chickens:"
 
 # outputs the string with ruby calculation 25 + 30 / 6 = 25 + 5 = 30
-puts "Hens #{25 + 30 / 6}" # "Hens 30"
+puts "Hens #{25.0 + 30 / 6}" # "Hens 30"
 
 # outputs the string with ruby calculation 100 - 25 * 3 % 4 = 100 - 75 % 4 = 100 - 3 = 97
-puts "Roosters #{100 - 25 * 3 % 4}" # Roosters 97
+puts "Roosters #{100.0 - 25 * 3 % 4}" # Roosters 97
 
 # outputs the string
 puts "Now I will count the eggs:"
 
 # outputs the calculation
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 # outputs the string
-puts "Is it true that 3 + 2 < 5 - 7"
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0"
 
 # outputs boolean because of <
-puts 3 + 2 < 5 - 7 # false
+puts 3.0 + 2 < 5 - 7 # false
 
 # outputs the string with calculation
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
 
 # outputs the string
 puts "Oh, that's why it's false."
 
 # outputs the string with boolean calculation
-puts "Is is greater? #{5 > -2}" # true
-puts "Is it greater or equal? #{5 >= -2}" # true
-puts "Is it less or equal? #{5 <= -2}" # false
+puts "Is is greater? #{5.0 > -2.0}" # true
+puts "Is it greater or equal? #{5.0 >= -2.0}" # true
+puts "Is it less or equal? #{5.0 <= -2.0}" # false
---
+++ day_1/ex3_1.rb
@@ -0,0 +1,4 @@
+# This is ex3 study drill number 3
+# Find something you need to calculate and write a new .rb file that does it.
+
+puts "The answer to life, the universe, and everything is #{(9 + 3 - 5) * (30 / 6 + 1)}."
