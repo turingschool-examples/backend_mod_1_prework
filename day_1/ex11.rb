3fee8c6c21a301275b1d9ac38dfeae570fea9496
Fenton Taylor <fentontaylor@gmail.com>
2019-06-06 10:37:06 -0600

    Finished ex11.rb with age calculation

---
+++ day_1/ex11.rb
@@ -0,0 +1,14 @@
+print "What year were you born? "
+bday_year = gets.chomp.to_i
+print "How tall are you? (inches) "
+height = gets.chomp
+print "How much do you weigh? (pounds) "
+weight = gets.chomp
+
+puts "\nSo, you're #{height} inches tall and weigh #{weight} pounds."
+
+today = Time.new
+year = today.year
+year = year.to_i
+
+puts "\nYou will be #{year - bday_year} years old by the end of this year."
