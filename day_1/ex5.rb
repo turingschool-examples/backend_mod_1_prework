8c05904aa07d5424c4eb76635ed436f27f1cfa3c
Fenton Taylor <fentontaylor@gmail.com>
2019-06-06 09:30:39 -0600

    Finished ex5.rb

---
+++ day_1/ex5.rb
@@ -0,0 +1,18 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 #inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+height_cm = height * 2.54
+weight_kg = weight * 0.454
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall. (That's #{height_cm} cm.)"
+puts "He's #{weight} pounds heavy. (That's #{weight_kg} kg.)"
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight}, I get #{age + height + weight}."
