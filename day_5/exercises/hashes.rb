c10349d0f65e7122d5988032ff33f6a236537197
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-06-06 22:05:01 -0600

    Modify hashes exercise

--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -8,7 +8,7 @@ puts foods
 
    #Alternatively, in simplified hash:
 
-   foods = {apples: 23, grapes: 507, eggs: 48}
+   # foods = {apples: 23, grapes: 507, eggs: 48}
 
 # Write code below that will print a hash of animals and their number
 # at the zoo. (an inventory of animals)
@@ -16,7 +16,7 @@ zoo = {'penguins' => 22, 'sea otters' => 23, 'panda bears' => 24}
 puts zoo
 
    #Alternatively, in simplified hash:
-   zoo = {penguins: 22, sea_otters: 23, panda_bears: 24}
+   # zoo = {penguins: 22, sea_otters: 23, panda_bears: 24}
 
 # Using the zoo that you created above, print all the keys in the hash.
 zoo = {'penguins' => 22, 'sea otters' => 23, 'panda bears' => 24}
@@ -31,7 +31,7 @@ zoo = {'penguins' => 22, 'sea otters' => 23, 'panda bears' => 24}
 puts zoo['penguins']
 
    #Alternatively, in simplified hash:
-   puts zoo[:penguins]
+   # puts zoo[:penguins]
 
 # Add an animal to the zoo hash and print the updated hash.
 zoo = {'penguins' => 22, 'sea otters' => 23, 'panda bears' => 24}
