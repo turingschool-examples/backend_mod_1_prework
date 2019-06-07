f8c92b7457aa47dbdc62746bc3747e6d1ef0357b
Fenton Taylor <fentontaylor@gmail.com>
2019-06-07 10:05:29 -0600

    Finish ex18.rb and say.rb

---
+++ day_4/ex18.rb
@@ -0,0 +1,22 @@
+def print_two(*args)
+  arg1, arg2 = args
+  puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+def print_two_again(arg1, arg2)
+  puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+def print_one(arg1)
+  puts "arg1: #{arg1}"
+end
+
+def print_none()
+  puts "I got nothin'."
+end
+
+
+print_two("Fenton", "Taylor")
+print_two_again("Hi", "Ho!")
+print_one("Uno!")
+print_none()
