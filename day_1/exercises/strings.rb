3cf8f277695030da65445a9bfea799b65231cec3
Alice Post <ap2322@gmail.com>
2019-06-08 15:21:05 -0600

    Add exercises for day 1 ruby files

--- day_1/exercises/strings.rb
+++ day_1/exercises/strings.rb
@@ -3,10 +3,21 @@
 # file from your terminal with the command `ruby day_1/exercises/strings.rb`
 
 # example: Write code below to print your name to the terminal:
-p "Alan Turing"
+puts "Alice Post"
 
 # Write code below to print `Welcome to Turing!` to the terminal:
-p #YOUR CODE HERE
+puts "Welcome to Turing"
 
 # Write code below to print `99 bottles of pop on the wall...`:
-# YOUR CODE HERE
+bottles = 99
+puts "#{bottles} bottles of pop on the wall, #{bottles} bottles of pop!"
+puts "Take one down, pass it around, #{bottles - 1} bottles of pop on the wall."
+
+# full song in comment block (between =begin and =end) here:
+=begin
+98.times do
+  bottles -= 1
+  puts "#{bottles} bottles of pop on the wall, #{bottles} bottles of pop!"
+  puts "Take one down, pass it around, #{bottles - 1} bottles of pop on the wall."
+end
+=end
