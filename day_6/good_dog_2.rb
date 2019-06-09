705462eec822b36a59db620c850e603166ddd4d9
Fenton Taylor <fentontaylor@gmail.com>
2019-06-09 17:55:44 -0600

    Update good_dog_2 with attr_accessor methods

---
+++ day_6/good_dog_2.rb
@@ -0,0 +1,33 @@
+class GoodDog
+  attr_accessor :name, :height, :weight #attr_reader for getting attributes, attr_writer for setting attributes
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
+  end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{self.name} weighs #{self.weight} and is #{self.height} tall."
+  end
+
+  def speak
+    "#{name} says Arf!"
+  end
+end
+
+
+sparky = GoodDog.new("Sparky", "12 inches", "10 lbs")
+puts sparky.speak
+puts sparky.info
+sparky.change_info('Spartacus', '24 inches', '45 lbs')
+puts sparky.info
+
+fido = GoodDog.new("Fido", "20 inches", "30 lbs")
+puts fido.speak
+puts fido.name
