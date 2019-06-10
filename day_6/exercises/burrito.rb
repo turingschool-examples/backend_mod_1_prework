22a2a2f0dc0059546c374fb202406ed3f1130b9b
Fenton Taylor <fentontaylor@gmail.com>
2019-06-09 19:36:42 -0600

    Finish burrito.rb methods: add_topping, remove_topping, change_protein

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -5,15 +5,37 @@
 # 3. change_protein
 
 class Burrito
-  attr_reader :protein, :base, :toppings
+  attr_accessor :protein, :base, :toppings
   def initialize(protein, base, toppings)
     @protein  = protein
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(topping)
+    self.toppings << topping
+  end
+
+  def remove_topping(topping)
+    self.toppings.delete(topping)
+  end
+
+  def change_protein(new_protein)
+    self.protein = new_protein
+  end
+
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
-p dinner.protein
-p dinner.base
-p dinner.toppings
+puts dinner.protein
+puts dinner.base
+puts "#{dinner.toppings}"
+
+dinner.add_topping("sour cream")
+puts "#{dinner.toppings}"
+
+dinner.remove_topping("cheese")
+puts "#{dinner.toppings}"
+
+dinner.change_protein("carnitas")
+puts dinner.protein
