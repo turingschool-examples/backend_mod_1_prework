a9136a70a74f07a4ac49075dd9f20457c49ebc91
Scott Payton <spayton.c@gmail.com>
2019-06-10 00:25:57 -0600

    Add work for day 7 10_little_monkies

---
+++ day_7/10_little_monkeys.rb
@@ -0,0 +1,58 @@
+
+i = 0
+loop do
+  i += 1
+  if i == 1
+    puts "One little monkey jumping on the bed,
+      > He fell off and bumped his head,
+      > Mama called the doctor and the doctor said,
+      >'Get those monkeys right to bed!"
+  elsif i ==2
+    puts "two little monkey jumping on the bed,
+    > He fell off and bumped his head,
+    > Mama called the doctor and the doctor said,
+    >'Get those monkeys right to bed!"
+  elsif i ==3
+    puts "three little monkey jumping on the bed,
+    > He fell off and bumped his head,
+    > Mama called the doctor and the doctor said,
+    >'Get those monkeys right to bed!"
+  elsif i ==4
+    puts "four little monkey jumping on the bed,
+    > He fell off and bumped his head,
+    > Mama called the doctor and the doctor said,
+    >'Get those monkeys right to bed!"
+  elsif i ==5
+    puts "five little monkey jumping on the bed,
+    > He fell off and bumped his head,
+    > Mama called the doctor and the doctor said,
+    >'Get those monkeys right to bed!"
+  elsif i ==6
+    puts "six little monkey jumping on the bed,
+    > He fell off and bumped his head,
+    > Mama called the doctor and the doctor said,
+    >'Get those monkeys right to bed!"
+  elsif i ==7
+    puts "seven little monkey jumping on the bed,
+    > He fell off and bumped his head,
+    > Mama called the doctor and the doctor said,
+    >'Get those monkeys right to bed!"
+  elsif i ==8
+    puts "eight little monkey jumping on the bed,
+    > He fell off and bumped his head,
+    > Mama called the doctor and the doctor said,
+    >'Get those monkeys right to bed!"
+  elsif i == 9
+    puts "nine little monkey jumping on the bed,
+    > He fell off and bumped his head,
+    > Mama called the doctor and the doctor said,
+    >'Get those monkeys right to bed!"
+  elsif i == 10
+    puts "ten little monkey jumping on the bed,
+    > He fell off and bumped his head,
+    > Mama called the doctor and the doctor said,
+    >'Get those monkeys right to bed!"
+  else
+    break
+  end
+end
