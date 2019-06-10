5695501abcb38d6b3860aeac4f03edcb7f11bddf
Scott Payton <spayton.c@gmail.com>
2019-06-10 16:58:58 -0600

    Updated dog.rb day_6 work

--- day_6/exercises/dog.rb
+++ day_6/exercises/dog.rb
@@ -5,7 +5,7 @@
 class Dog
   attr_reader :breed, :name, :age
 
-  def initialize(breed, name, age)
+  def initialize(breed, name, age, hungry)
     @breed  = breed
     @name   = name
     @age    = age
@@ -18,18 +18,18 @@ class Dog
 
   def eat
     @hungry = false
+    puts "Fido just ate, is he hungry?...#{@hungry}"
   end
 
   def play
-    puts hungry(true)
+    @hungry = true
+    puts "Fido just played, is he hungry?...#{@hungry}"
   end
 end
 
-fido = Dog.new("Bernese", "Fido", 4)
-puts fido.breed
-puts fido.name
-puts fido.age
-puts fido.hungry
-puts fido.eat
-puts fido.hungry
-puts fido.play
+fido = Dog.new("Bernese", "Fido", 4, true)
+p fido.breed
+p fido.name
+p fido.age
+fido.eat
+p fido.play
