1d67a4501f93a782a21e39fd2f599d9ede0d1bb0
Fenton Taylor <fentontaylor@gmail.com>
2019-06-10 13:47:51 -0600

    Encoding functioning properly

--- day_7/ceasar_cipher.rb
+++ day_7/ceasar_cipher.rb
@@ -1,5 +1,5 @@
-print "Would you like to encode or decode text? 1: encode 2: decode  -> "
-coding = gets.chomp.to_i
+# print "Would you like to encode or decode text? 1: encode 2: decode  -> "
+# coding = gets.chomp.to_i
 
 print "What shift value would you like to use?  -> "
 shift = gets.chomp.to_i
@@ -17,13 +17,23 @@ def clean_text(text)
 end
 
 def encode(text, shift)
-  key = *('A'..'Z')
-  cipher = key.pop(shift) + key
-
 
 end
 
-p coding
-p shift
+key = *('A'..'Z')
+cipher = key.last(shift) + key.first(26 - shift)
 text = clean_text(text)
-p text
+text_length = text.length
+
+a = *(0..text_length - 1)
+
+code_text = ""
+for i in a do
+  if text[i] == " "
+    code_text[i] = " "
+  else
+    code_text[i] = cipher[key.rindex(text[i])]
+  end
+end
+
+puts "Encoded Text: #{code_text}"
