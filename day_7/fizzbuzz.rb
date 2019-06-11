d94004823f28fa5e8ab6dafb61569ae21d757dcb
Scott Payton <spayton.c@gmail.com>
2019-06-11 14:04:17 -0600

    Finalize day_7 work

--- day_7/fizzbuzz.rb
+++ day_7/fizzbuzz.rb
@@ -2,7 +2,9 @@ puts('Lets play fizzbuzz')
 #number = gets()
 #puts('Enter the number you would like to go up to...')
 1.upto(200) do |i|
+  #line below says if i, when divided by 0 returns no remainder (same for 5) do the following
   if i % 3 == 0 && i % 5 ==0
+    #execute if above statement is true
     puts "fizzbuzz"
   elsif i % 3 == 0
     puts "fizz"
