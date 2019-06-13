ee32694b02ab9f491716fa1439d1f47cf4a456f4
Fenton Taylor <fentontaylor@gmail.com>
2019-06-13 10:24:50 -0600

    Rewrite translate_text method with enumerable, remove comments

--- day_7/ceasar_cipher.rb
+++ day_7/ceasar_cipher.rb
@@ -8,46 +8,41 @@ print "Input text -> "
 text = gets.chomp
 
 def clean_text(text)
-  text = text.upcase                    # convert to upper case
-  text = text.gsub(/[[:punct:]]/, "")   # remove any punctuation
-  text = text.gsub(/\d/, "")            # remove any numbers
-  text = text.gsub(/[\s]$/, "")         # remove whitespace at end of line
-  text = text.gsub(/[\s]{2,}/, " ")     # remove excess whitespace between words
-  text                                  # return the cleaned text
+  text = text.upcase
+  text = text.gsub(/[[:punct:]]/, "")
+  text = text.gsub(/\d/, "")
+  text = text.gsub(/[\s]$/, "")
+  text = text.gsub(/[\s]{2,}/, " ")
+  text
 end
 
 def translate_text(text, shift, coding)
-  key = *('A'..'Z')                                 # create A-Z array
-  cipher = key.last(shift) + key.first(26 - shift)  # create shifted alphabet
-  text = clean_text(text)                           # clean user input text
-  text_length = text.length                         # get length of user text
-  a = *(0..text_length - 1)                         # create indexing array
-  code_text = ""      # initialize string to fill in with encode/decode algorithm
+  key = *('A'..'Z')
+  cipher = key.last(shift) + key.first(26 - shift)
+  text = clean_text(text)
+  text = text.split("")
 
-  if coding == 1      # user wants to encode text
-    for i in a do
-      if text[i] == " "     # if string index is a space, keep it as a space
-        code_text[i] = " "
+  if coding == 1
+    code_text =  text.collect do |letter|
+
+      if letter == " "
+        " "
       else
-        # if string index is a letter, add equivalent cipher letter from
-        # alphabet array to code_text string
-        code_text[i] = cipher[key.rindex(text[i])]
+        cipher[key.rindex(letter)]
       end
     end
-    code_text   # return code_text string of ENCODED text
-
-  else      # user wants to decode text
-    for i in a do
-      if text[i] == " "     # if string index is a space, keep it as a space
-        code_text[i] = " "
-      else
-        # if string index is a letter, add equivalent alphabet letter from
-        # cipher array to code_text string
-        code_text[i] = key[cipher.rindex(text[i])]
-      end
-    end
-    code_text   # return code_text string of DECODED text 
   end
+
+  if coding == 2
+    code_text = text.collect do |letter|
+      if letter == " "
+        " "
+      else
+        key[cipher.rindex(letter)]
+      end
+    end
+  end
+  return code_text.join
 end
 
 if coding == 1
