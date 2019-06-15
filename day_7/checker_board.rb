36feac15e3368cbff4440e3174fe80f00146a174
Fenton Taylor <fentontaylor@gmail.com>
2019-06-14 18:53:07 -0600

    Prints nicely without border

---
+++ day_7/checker_board.rb
@@ -0,0 +1,14 @@
+print "What side length do you desire? --> "
+side_length = gets.chomp.to_i
+board_array = Array.new(side_length ** 2)
+board_array = board_array.each_with_index do |space, i|
+  if (i + 1).odd? == true
+    board_array[i] = "X"
+  else
+    board_array[i] = " "
+  end
+end
+
+board_array.each_slice(side_length) do |row|
+  puts row.join(" ")
+end
