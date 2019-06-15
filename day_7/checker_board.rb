d1db495ade21cd1ff8efbf3a859ea7cd4d868733
Fenton Taylor <fentontaylor@gmail.com>
2019-06-14 19:22:06 -0600

    Prints nicely for odd side_length. Simplify board_array creation

--- day_7/checker_board.rb
+++ day_7/checker_board.rb
@@ -1,18 +1,22 @@
 print "What side length do you desire? --> "
 side_length = gets.chomp.to_i
-board_array = Array.new(side_length ** 2)
-board_array = board_array.each_with_index do |space, i|
-  if (i + 1).odd? == true
-    board_array[i] = "X"
-  else
-    board_array[i] = " "
+
+if side_length.even? == true
+  board_array = ["X", " "] * (side_length ** 2 / 2)
+else
+  board_array = ["X", " "] * (side_length ** 2 / 2) + ["X"]
+end
+
+
+puts "-" * (side_length * 2 + 1)
+
+if side_length.odd? == true
+  board_array.each_slice(side_length) do |row|
+    puts "|" + row.join(" ") + "|"
   end
+else
+  
 end
 
-puts "|" + "-" * (side_length * 2 - 1) + "|"
 
-board_array.each_slice(side_length) do |row|
-  puts "|" + row.join(" ") + "|"
-end
-
-puts "|" + "-" * (side_length * 2 - 1) + "|"
+puts "-" * (side_length * 2 + 1)
