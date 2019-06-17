335783967301362107cc8e9ad1937cd65dbf29da
Christopher Kelling <cj.kelling88@gmail.com>
2019-06-16 19:34:07 -0600

    Add Day 4 Word

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -4,22 +4,31 @@
 
 # example: Write a method below that, when called will print your name
 def print_name
-  p "Severus Snape"
+  puts "Severus Snape"
 end
 
 print_name
 
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-  # YOUR CODE HERE
+  puts name
 end
 
 print_name("Albus Dumbledore")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
+def add (a, b)
+  puts a + b
+end
+
+add(10, 24)
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+def concat (man, woman)
+  puts "When " + man + " Met " + woman
+end
+
+concat("Harry", "Sally")
