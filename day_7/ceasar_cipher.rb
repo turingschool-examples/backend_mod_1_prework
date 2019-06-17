f59906b79adb9931643d5da682d9ee4bc1d0990f
Fenton Taylor <fentontaylor@gmail.com>
2019-06-17 14:00:51 -0600

    Add function to read text file for encoding

--- day_7/ceasar_cipher.rb
+++ day_7/ceasar_cipher.rb
@@ -1,16 +1,26 @@
-print "Would you like to encode or decode text? 1: encode 2: decode  -> "
+print "Would you like to encode or decode text?  1: ENCODE  2: DECODE  -> "
 coding = gets.chomp.to_i
 
 print "What shift value would you like to use?  -> "
 shift = gets.chomp.to_i
 
-print "Input text -> "
-text = gets.chomp
+print "Would you like to input text, or read a file from current working directory?  1: INPUT  2: FILE -> "
+input_type = gets.chomp.to_i
+
+if input_type != 2
+  print "Input text -> "
+  text = gets.chomp
+else
+  print "Input file name: "
+  file_name = Dir.pwd + "/" + gets.chomp
+  text = File.read(file_name)
+end
 
 def clean_text(text)
   text = text.upcase
   text = text.gsub(/[[:punct:]]/, "")
   text = text.gsub(/\d/, "")
+  text = text.gsub(/\s/, " ")
   text = text.gsub(/[\s]$/, "")
   text = text.gsub(/[\s]{2,}/, " ")
   text
