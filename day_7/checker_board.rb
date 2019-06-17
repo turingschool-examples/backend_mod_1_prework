8f5d3aaab0e529c8d3b121b58ee0f3af417835ce
Fenton Taylor <fentontaylor@gmail.com>
2019-06-17 12:50:38 -0600

    Fix so that prints nicely for any side_length

--- day_7/checker_board.rb
+++ day_7/checker_board.rb
@@ -1,22 +1,19 @@
 print "What side length do you desire? --> "
 side_length = gets.chomp.to_i
 
-if side_length.even? == true
-  board_array = ["X", " "] * (side_length ** 2 / 2)
-else
-  board_array = ["X", " "] * (side_length ** 2 / 2) + ["X"]
-end
-
+tiles = ['X', ' '] * side_length
+row_num = *(1..side_length)
+odd_row = tiles[0..side_length-1].join(' ')
+even_row = tiles[1..side_length].join(' ')
 
 puts "-" * (side_length * 2 + 1)
 
-if side_length.odd? == true
-  board_array.each_slice(side_length) do |row|
-    puts "|" + row.join(" ") + "|"
+row_num.each do |row|
+  if row.odd? == true
+    puts ['|', odd_row, '|'].join
+  else
+    puts ['|', even_row, '|'].join
   end
-else
-  
 end
-
 
 puts "-" * (side_length * 2 + 1)
