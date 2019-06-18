0db4bf8eab2be79c7a9670bbc8134f9f73cff9b1
Fenton Taylor <fentontaylor@gmail.com>
2019-06-18 13:00:10 -0600

    Add cheat sheet function

--- day_7/fizzbuzz_2.rb
+++ day_7/fizzbuzz_2.rb
@@ -24,9 +24,39 @@ class FizzBuzz
   def say_fzbz
     numbers.collect {|num| fzbz(num)}
   end
+
+  def fizzes
+    numbers.find_all {|n| n % 3 == 0 && !(n % 5 == 0)}
+  end
+
+  def buzzes
+    numbers.find_all {|n| n % 5 == 0 && !(n % 3 == 0)}
+  end
+
+  def fizzbuzzes
+    numbers.find_all {|n| n % 3 == 0 && n % 5 == 0}
+  end
+
+  def cheat_sheet
+    puts "Fizzes:     #{fizzes.join(', ')}"
+    puts "Buzzes:     #{buzzes.join(', ')}"
+    puts "FizzBuzzes: #{fizzbuzzes.join(', ')}"
+  end
 end
 
 print "How high do you want to count?  --> "
 high = gets.chomp.to_i
 
-puts FizzBuzz.new(high).say_fzbz.join(", ")
+this_fzbz = FizzBuzz.new(high)
+puts this_fzbz.say_fzbz.join(", ")
+
+puts "\n"
+
+print 'Do you want to know all the "Fizzes," "Buzzes," and "FizzBuzzes?" [ y / n ] --> '
+choice = gets.chomp
+
+if choice =='y' || choice =='Y'
+  this_fzbz.cheat_sheet
+else
+  puts "Okay, bye!"
+end
