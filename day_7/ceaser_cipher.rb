fe59f0d9486b45010d7c79803d6a865e0a94ab6e
Corina Allen <StarPerfecrt@gmail.com>
2019-06-24 14:15:30 -0600

    Remove excess code

--- day_7/ceaser_cipher.rb
+++ day_7/ceaser_cipher.rb
@@ -1,5 +1,4 @@
 # Ceaser Cipher
-require "pry"
 
 letters = Array.new
 
@@ -10,7 +9,7 @@ shift = gets.chomp.to_i
 
 letters.push(string.split(//))
 
-alphabet = {"A" => 1, "B" => 2, "C" => 3, "D" => 4, "E" => 5, "F" => 6, "G" => 7, "H" => 8, "I" => 9, "J" => 10, "K" => 11, "L" => 12, "M" => 13, "N" => 14, "O" => 15, "P" => 16, "Q" => 17, "R" => 18, "S" => 19, "T" => 20, "U" => 21, "V" => 22, "W" => 23, "X" => 24, "Y" => 25, "Z" => 26}
+alphabet = {"A" => 1, "B" => 2, "C" =>  3, "D" => 4, "E" => 5, "F" => 6, "G" => 7, "H" => 8, "I" => 9, "J" => 10, "K" => 11, "L" => 12, "M" => 13, "N" => 14, "O" => 15, "P" => 16, "Q" => 17, "R" => 18, "S" => 19, "T" => 20, "U" => 21, "V" => 22, "W" => 23, "X" => 24, "Y" => 25, "Z" => 26}
 
 encoded = Hash[letters.flatten.collect { |letter| [letter, alphabet[letter]] } ]
 
