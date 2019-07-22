b12b2f232ea5447187d6727c3f8dc0cfc38fa6ff
Michael Cooper <macoop2363@mgmail.com>
2019-07-22 16:01:25 -0600

    printing lines with puts

---
+++ day_1/exercises/ex1.rb
@@ -0,0 +1,26 @@
+
+lines = [
+    "Hello World!",
+    "Hello Again",
+    "I like typing this.",
+    "This is fun.",
+    "Yay! Printing.",
+    "I'd much rather you 'not'.",
+    'I "said" do not touch this.',
+    "Is this what study drill 1 wants?"
+]
+
+# Standard ex1
+puts lines.slice(0, 7)
+puts ''
+
+# study drills 1
+puts lines
+puts ''
+
+# study drills 2
+# prints a random line
+puts lines[rand(0..7)]
+
+# study drills 3
+# this is a comment. Comments are for programmers to read
