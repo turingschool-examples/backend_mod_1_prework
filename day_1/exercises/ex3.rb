ebee1a47dcb27c7db091e61e3b4641251becef11
Michael Cooper <macoop2363@mgmail.com>
2019-07-22 17:15:44 -0600

    added comments and changed integers to floats as requested

--- day_1/exercises/ex3.rb
+++ day_1/exercises/ex3.rb
@@ -1,23 +1,33 @@
 puts "I will now count my chickens:"
 
-puts "Hens #{25 + 30 / 6}"
-puts "Roosters #{100 - 25 * 3 % 4}"
+# compute 25 + (30 / 6)
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# compute 100 - ((25 * 3) mod 4)
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 puts "Now I will count the eggs:"
 
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# compute 3+2+1-5+ (4mod2) - (1 / 4) + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 puts "Is it true that 3 + 2 < 5 - 7?"
 
-puts 3 + 2 < 5 - 7
+# is 5 less than -2?
+puts 3.0 + 2.0 < 5.0 - 7.0
 
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7? #{5 - 7}"
+# print 3 + 2 and 5 - 7
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
 
 puts "Oh, that's why it's false."
 
 puts "How about some more."
 
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 >= -2}"
-puts "Is it less or equal? #{5 <= -2}"
+# is 5 greater than -2?
+puts "Is it greater? #{5.0 > -2.0}"
+
+# is 5 greater than or equal to -2?
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+
+# is 5 less than or equal to -2?
+puts "Is it less or equal? #{5.0 <= -2.0}"
