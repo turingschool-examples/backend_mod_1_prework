de2d5d641182401af822cbbc5ba748e92b5545f4
Sebastian Sloan <sebastiansloan94@gmail.com>
2019-07-22 08:51:22 -0600

    Updating Forgotten Files For day_1

---
+++ day_1/Budget.rb
@@ -0,0 +1,20 @@
+puts "Do we make enough money to pay the bills?"
+
+puts "First we have to look at our total income"
+puts "Total Income:"
+
+puts 1369.66 * 2 + 2000
+
+puts "Total Bills:"
+
+puts "Total Houseing:"
+
+puts 400 + 1100
+
+puts "Total Auto Expenses:"
+
+puts 366 + 305.16 + 133 + 195
+
+puts "Total Debt:"
+
+puts 25 + 160 + 45.70 + 171 + 60 + 25
