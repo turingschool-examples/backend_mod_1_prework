473db80619e9a5d71412d3539dfd6225ca16f2be
Devin Kapla <devinkapla@gmail.com>
2019-07-25 10:34:09 -0600

    day 2 done

---
+++ day_2/array_methods.md
@@ -0,0 +1,9 @@
+.last gives you the last element in the array.
+.sort will rearrange the and give you an new array according to the sort algorithim.
+.join will combine all elements of the array.
+.index will help a dev find a specific address of an element in array.
+.include will return true or false if the element is present.
+.each will cycle through each element in the array.
+.collect will do the same as above and return a new array.
+.first will return the first element in the array.
+.shuffle will return a new array that is the shuffled elements of thge old array.
--- day_2/exercises/iteration.rb
+++ day_2/exercises/iteration.rb
@@ -13,14 +13,16 @@ end
 # Write code below that iterates through a list of animals and prints "The
 # <animal> is awesome!" for each animal.
 
-animals.each do |animal|
-  # YOUR CODE HERE
-end
+animals.each {|animal| p "#{animal} is awesome"}
+
 
 # Create an array of foods and then iterate over that array to print "Add
 # <food> to shopping list" for each food item.
-# YOUR CODE HERE
+foods = ["Beer", "Pizza", "Brain"]
+foods.each {|food| p "Add #{food} to shoppping list"}
 
 # Create an array of numbers and then iterate over that array to print doubles
 # of each of the number.
+nums = [1,2,3,4]
+nums.each {|num| p "#{num}#{num}"}
 # YOUR CODE HERE
