57a2a0641a50c7afef0973fa6705d2ee0a3826c0
Daniel <danmoran.pro@gmail.com>
2019-07-26 09:24:30 -0600

    Complete 10_little_monkeys

---
+++ day_7/lib/10_little_monkeys.rb
@@ -0,0 +1,14 @@
+class Monkey
+
+  @nums = (1..5)
+
+    @nums.reverse_each do |num|
+    if num > 1
+      puts"
+        #{num} little monkeys jumping on the bed,
+        One fell off and bumped his head,
+        Mama called the doctor and the doctor said,
+        No more monkeys jumping on the bed!"
+     end
+ end
+end
