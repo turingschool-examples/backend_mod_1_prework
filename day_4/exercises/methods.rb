861cf7c8e89e0afd63b472dd9b4b31b8fdec231c
Devin Kapla <devinkapla@gmail.com>
2019-07-26 17:48:42 -0600

    Day 4 done

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -11,15 +11,24 @@ print_name
 
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-  # YOUR CODE HERE
+  p name
 end
 
 print_name("Albus Dumbledore")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
+def sum_of_two(first, second)
+  p first + second
+end
+sum_of_two(34, 632)
 # YOUR CODE HERE
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+def concat_words(first, second)
+  p first + second
+end
+sum_of_two("The void is eternal", " and I'll sleep when I'm dead")
+concat_words("The void is eternal", " and I'll sleep when I'm dead")
