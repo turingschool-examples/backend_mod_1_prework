d06edcb1fe35e30ff1abeb4bdddab5e447052705
Mary Lang <mcl86@cornell.edu>
2019-07-27 08:23:32 -0700

    Update monkeys

--- day_7/10_little_monkeys_test.rb
+++ day_7/10_little_monkeys_test.rb
@@ -1,28 +1,3 @@
-=begin
-## 10 Little Monkeys
-
-Create a file named 10_little_monkeys.rb and within that file, write a program
- that will print the following nursery rhyme, but for *10* monkeys.
-
-> Three little monkeys jumping on the bed,
-> One fell off and bumped his head,
-> Mama called the doctor and the doctor said,
-> "No more monkeys jumping on the bed!"
->
-> Two little monkeys jumping on the bed,
-> One fell off and bumped his head,
-> Mama called the doctor and the doctor said,
-> "No more monkeys jumping on the bed!"
->
-> One little monkey jumping on the bed,
-> He fell off and bumped his head,
-> Mama called the doctor and the doctor said,
-> "Get those monkeys right to bed!"
-
-### Bonus
-Can you write the program so that it will run for any number of monkeys?
-=end
-
 def to_word(count)
 
 numb_to_name = {
