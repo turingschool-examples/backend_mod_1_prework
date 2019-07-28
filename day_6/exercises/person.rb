dcc5ea055d365df41b3887f160d67f16ae099aa6
Michael Cooper <macoop2363@gmail.com>
2019-07-28 15:33:52 -0600

    Created Person class for assignment

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -2,4 +2,33 @@
 # person methods below the class so that they print their result to the
 # terminal.
 
-#YOUR CODE HERE
+class Person
+    attr_accessor :first_name, :last_name
+    attr_reader :is_awake
+
+    def initialize(first, last)
+        @first_name = first
+        @last_name = last
+        @is_awake = true
+    end
+
+    def greeting
+        puts "Hello, I'm #{@first_name} #{@last_name}!"
+    end
+
+    def sleep
+        @is_awake = false
+    end
+
+    def wake_up
+        @is_awake = true
+    end
+end
+
+steve = Person.new("Steve", "Harrington")
+steve.greeting
+puts "Is Steve awake? #{steve.is_awake}"
+steve.sleep
+puts "Is Steve awake? #{steve.is_awake}"
+steve.wake_up
+puts "Is Steve awake? #{steve.is_awake}"
