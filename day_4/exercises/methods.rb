b8654ea5a10936871d987ed5a01d85e522d9806d
Michael Cooper <macoop2363@gmail.com>
2019-07-27 19:14:10 -0600

    Filled the code for the given exercises

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -11,15 +11,27 @@ print_name
 
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-  # YOUR CODE HERE
+  p name
 end
 
 print_name("Albus Dumbledore")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
+def add(x, y)
+  p x + y
+end
+
+add(5, 7)
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+
+def concatenate(str1, str2)
+  p "str1: #{str1}"
+  p "str2: #{str2}"
+  p "str1 + str2: #{str1 + str2}"
+end
+
+concatenate("manbear", "pig")
