c3759e21ffbb65b6bd87535a1a7654e3cfd20b8a
Michael Cooper <macoop2363@gmail.com>
2019-07-28 15:13:57 -0600

    Completed the exercise for 'Objects, Attributes, and Methods'

---
+++ day_6/student.rb
@@ -0,0 +1,18 @@
+# Exercise for "Objects, Attributes, and Methods"
+
+class Student
+    attr_accessor :first_name, :last_name, :primary_phone_number
+
+    def introduction(target)
+        puts "Hi #{target}, I'm #{first_name}!"
+    end
+
+    def favorite_number
+        7
+    end
+end
+
+frank = Student.new
+frank.first_name = "Frank"
+frank.introduction("Katrina")
+puts "Frank's favorite number is #{frank.favorite_number}."
