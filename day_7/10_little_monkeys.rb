e33967fd9bee98a3fa3d2c775240dbb3b5eb881f
Alan Turing <tybalogh@Tylers-MacBook-Pro.local>
2019-07-29 15:47:32 -0600

    Add Day 7 Work

---
+++ day_7/10_little_monkeys.rb
@@ -0,0 +1,66 @@
+#Ten little monkeys
+animal = "monkeys"
+monkeys = 10
+name = ["Joey", "Lulu", "Brett", "Gabe", "Susie", "Tom", "Julie", "Ashley", "Danny", "Carl"]
+
+
+puts "#{monkeys} little #{animal} jumping off the bed,
+#{name[0]} fell off and bumped his head,
+Mama called the doctor and the doctor said,
+'No more #{animal} jumping off the bed!''\n\n"
+#prints 10 monkeys
+
+puts "#{monkeys -= 1} little #{animal} jumping off the bed,
+#{name[1]}fell off and bumped her head,
+Mama called the doctor and the doctor said,
+'No more #{animal} jumping off the bed!'\n\n"
+#prints 9 monkeys
+
+puts "#{monkeys -= 1} little #{animal} jumping off the bed,
+#{name[2]} fell off and bumped his head,
+Mama called the doctor and the doctor said,
+'No more #{animal} jumping off the bed!'\n\n"
+#prints 8 monkeys
+
+puts "#{monkeys -= 1} little #{animal} jumping off the bed,
+#{name[3]} fell off and bumped his head,
+Mama called the doctor and the doctor said,
+'No more #{animal} jumpng off the bed!'\n\n"
+#prints 7 monkeys
+
+puts "#{monkeys -= 1} little #{animal} jumping off the bed,
+#{name[4]} fell off and bumped her head,
+Mama called the doctor and the doctor said,
+'No more #{animal} jumping off the bed!'\n\n"
+#prints 6 monkeys
+
+puts "#{monkeys -= 1} little #{animal} jumping off the bed,
+#{name[5]} fell off and bumped his head,
+Mama called the doctor and the doctor said,
+'No more #{animal} jumping off the bed!'\n\n"
+#prints 5 monkeys
+
+puts "#{monkeys -= 1} little #{animal} jumping off the bed,
+#{name[6]} fell off and bumped her head,
+Mama called the doctor and the doctor said,
+'No more #{animal} jumping off the bed!'\n\n"
+#prints 4 monkeys
+
+puts "#{monkeys -= 1} little #{animal} jumping off the bed,
+#{name[7]} fell off and bumped her head,
+Mama called the doctor and the doctor said,
+'No more #{animal} jumping off the bed!'\n\n"
+#prints 3 monkeys
+
+puts "#{monkeys -= 1} little #{animal} jumping off the bed,
+#{name[8]} fell off and bumped his head,
+Mama called the doctor and the doctor said,
+'No more #{animal} jumping off the bed!'\n\n"
+#prints 2 monkeys
+
+puts "#{monkeys -= 1} little #{animal} jumping on the bed,
+#{name[9]}fell off and bumped his head,
+Mama called the doctor and the doctor said,
+'No more #{animal} jumping off the bed!'\n\n"
+
+puts "No more #{animal} jumping on the bed!"
---
+++ day_7/checker_board.rb
@@ -0,0 +1,11 @@
+#checker board
+# W is the white boxes on the board and '' is the black boxes on the board
+puts "Checkers\n\n"
+
+puts 'W W W W W W W W'
+puts ' W W W W W W W '
+puts 'W W W W W W W W'
+puts ' W W W W W W W '
+puts 'W W W W W W W W'
+puts ' W W W W W W W W'
+puts 'W W W W W W W W '
