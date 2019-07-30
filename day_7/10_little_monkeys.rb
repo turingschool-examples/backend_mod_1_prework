46735bec666554bb789ceeb5ede3f4888ee49e8a
Michael Cooper <macoop2363@mgmail.com>
2019-07-29 19:44:17 -0600

    Added comments.

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -1,6 +1,7 @@
+# include number class
 require_relative './number'
 
-
+# Constants in the rhyme
 REPEATED_RHYME = "little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\""
 LAST_RHYME = "One little monkey jumping on the bed,\nHe fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"Get those monkeys right to bed!\""
 
@@ -8,9 +9,11 @@ LAST_RHYME = "One little monkey jumping on the bed,\nHe fell off and bumped his 
 valid = false
 user_input = 0
 
+# Continue asking until there is a valid input
 while valid == false do
     print "How many monkeys are there?  "
     user_input = gets.chomp.to_i
+
     if user_input > 0
         num_monkeys = Number.new(user_input)
         valid = true
@@ -19,9 +22,11 @@ while valid == false do
     end
 end
 
+# Repeat for the amount of monkeys
 while num_monkeys.num > 1 do 
     puts num_monkeys.num_word.capitalize + REPEATED_RHYME + "\n\n"
     num_monkeys.decriment
 end
 
+# Print the last paragraph of the rhyme.
 puts LAST_RHYME
