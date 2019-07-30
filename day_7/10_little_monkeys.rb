ca4b8b7b9662fbd58791938ec08ce85af73687c7
Michael Cooper <macoop2363@mgmail.com>
2019-07-29 19:36:32 -0600

    Added user input for how many monkeys

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -1,5 +1,27 @@
 require_relative './number'
 
-test = Number.new(1800004)
 
-puts test.num_word
+REPEATED_RHYME = "little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\""
+LAST_RHYME = "One little monkey jumping on the bed,\nHe fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"Get those monkeys right to bed!\""
+
+
+valid = false
+user_input = 0
+
+while valid == false do
+    print "How many monkeys are there?  "
+    user_input = gets.chomp.to_i
+    if user_input > 0
+        num_monkeys = Number.new(user_input)
+        valid = true
+    else
+        puts "Invalid input! Input a positive integer!"
+    end
+end
+
+while num_monkeys.num > 1 do 
+    puts num_monkeys.num_word + REPEATED_RHYME + "\n\n"
+    num_monkeys.decriment
+end
+
+puts LAST_RHYME
