ffdabe5b1809d7776fd3f1a49f33b02c81e8045c
Tyla Devon <tyladevon@gmail.com>
2019-08-01 15:54:38 -0600

    Add day 6 work

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -2,4 +2,31 @@
 # person methods below the class so that they print their result to the
 # terminal.
 
-#YOUR CODE HERE
+class Person
+    attr_reader :name, :gender_pronouns, :age, :country_of_origin
+
+      def initialize(name, gender_pronouns, age, country_of_origin)
+        @name = name
+        @gender_pronouns = gender_pronouns
+        @age = age
+        @country_of_origin = country_of_origin
+      end
+
+      def speak
+          "Hello!"
+      end
+
+      def introduce
+          "Hi, my name is @{name}"
+      end
+end
+
+tyla = Person.new("Tyla", "she/hers", 33, "USA")
+
+p tyla.name
+p tyla.gender_pronouns
+p tyla.age
+p tyla. country_of_origin
+
+tyla.speak
+tyla.introduce
