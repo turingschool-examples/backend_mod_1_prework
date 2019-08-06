719c9a6c8e7ff2cfb7466b16c030a4bf99d8a395
Alice Post <ap2322@gmail.com>
2019-08-06 14:46:10 -0600

    Restructure into class and method structure

--- day_7/caesar_cipher.rb
+++ day_7/caesar_cipher.rb
@@ -2,31 +2,40 @@
 # encode it based on a shift value
 # provided by the user.
 
-# Variables Needed: Starting alphabet, shift value, cipher value
-alphabet_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-alphabet_lower = 'abcdefghijklmnopqrstuvwxyz'
-shift_value = 0
-cipher_message = ""
+# TODO:  put this in a class and method structure
 
-puts "What is your message?"
-print "> "
-message_to_encode = gets.chomp
+class CeasarCipher
+  def get_message
+    puts "What is your message?"
+    print "> "
+    message_to_encode = gets.chomp
+  end
 
-puts "How many letters would you like to shift your message?"
-print "> "
-shift_value = gets.chomp.to_i
+  def get_shift_value
+    puts "How many letters would you like to shift your message?"
+    print "> "
+    shift_value = gets.chomp.to_i
+  end
 
-# cycle through each letter in the message
-message_to_encode.split("").each do |letter|
-  if alphabet_lower.include? letter
-    new_alpha_index_lower = (alphabet_lower.index(letter) + shift_value)% 26
-    cipher_message += alphabet_lower[new_alpha_index_lower]
-  elsif alphabet_upper.include? letter
-    new_alpha_index_upper = (alphabet_upper.index(letter) + shift_value) % 26
-    cipher_message += alphabet_upper[new_alpha_index_upper]
-  else # spaces
-    cipher_message += letter
+  def encode(message_to_encode, shift_value)
+    alphabet_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alphabet_lower = 'abcdefghijklmnopqrstuvwxyz'
+    cipher_message = ""
+
+    message_to_encode.split("").each do |letter|
+      if alphabet_lower.include? letter
+        new_alpha_index_lower = (alphabet_lower.index(letter) - shift_value)% 26
+        cipher_message += alphabet_lower[new_alpha_index_lower]
+      elsif alphabet_upper.include? letter
+        new_alpha_index_upper = (alphabet_upper.index(letter) - shift_value) % 26
+        cipher_message += alphabet_upper[new_alpha_index_upper]
+      else # spaces
+        cipher_message += letter
+      end
+    end
+    puts "Your message in cipher is: " + cipher_message
   end
 end
 
-puts "Your message in cipher is: " + cipher_message
+cipher = CeasarCipher.new
+cipher.encode("Hello World", 5)
