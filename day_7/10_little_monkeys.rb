08753767fbdfbceded6b8f97db49419b15394ed5
Tyler <52803986+tybalogh@users.noreply.github.com>
2019-08-10 12:26:21 -0600

    Update 10_little_monkeys.rb

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -1,66 +1,15 @@
 #Ten little monkeys
-animal = "monkeys"
-monkeys = 10
-name = ["Joey", "Lulu", "Brett", "Gabe", "Susie", "Tom", "Julie", "Ashley", "Danny", "Carl"]
+numbers = ["Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One"]
 
-
-puts "#{monkeys} little #{animal} jumping off the bed,
-#{name[0]} fell off and bumped his head,
+for number in numbers
+if number == "One"
+  puts "#{number} little monkey jumping on the bed,
+He fell off and bumped his head,
 Mama called the doctor and the doctor said,
-'No more #{animal} jumping off the bed!''\n\n"
-#prints 10 monkeys
-
-puts "#{monkeys -= 1} little #{animal} jumping off the bed,
-#{name[1]}fell off and bumped her head,
+'Get those monkeys right to bed!'\n\n"
+else
+  puts "#{number} little monkeys jumping on the bed,
+One fell off and bumped his head,
 Mama called the doctor and the doctor said,
-'No more #{animal} jumping off the bed!'\n\n"
-#prints 9 monkeys
-
-puts "#{monkeys -= 1} little #{animal} jumping off the bed,
-#{name[2]} fell off and bumped his head,
-Mama called the doctor and the doctor said,
-'No more #{animal} jumping off the bed!'\n\n"
-#prints 8 monkeys
-
-puts "#{monkeys -= 1} little #{animal} jumping off the bed,
-#{name[3]} fell off and bumped his head,
-Mama called the doctor and the doctor said,
-'No more #{animal} jumpng off the bed!'\n\n"
-#prints 7 monkeys
-
-puts "#{monkeys -= 1} little #{animal} jumping off the bed,
-#{name[4]} fell off and bumped her head,
-Mama called the doctor and the doctor said,
-'No more #{animal} jumping off the bed!'\n\n"
-#prints 6 monkeys
-
-puts "#{monkeys -= 1} little #{animal} jumping off the bed,
-#{name[5]} fell off and bumped his head,
-Mama called the doctor and the doctor said,
-'No more #{animal} jumping off the bed!'\n\n"
-#prints 5 monkeys
-
-puts "#{monkeys -= 1} little #{animal} jumping off the bed,
-#{name[6]} fell off and bumped her head,
-Mama called the doctor and the doctor said,
-'No more #{animal} jumping off the bed!'\n\n"
-#prints 4 monkeys
-
-puts "#{monkeys -= 1} little #{animal} jumping off the bed,
-#{name[7]} fell off and bumped her head,
-Mama called the doctor and the doctor said,
-'No more #{animal} jumping off the bed!'\n\n"
-#prints 3 monkeys
-
-puts "#{monkeys -= 1} little #{animal} jumping off the bed,
-#{name[8]} fell off and bumped his head,
-Mama called the doctor and the doctor said,
-'No more #{animal} jumping off the bed!'\n\n"
-#prints 2 monkeys
-
-puts "#{monkeys -= 1} little #{animal} jumping on the bed,
-#{name[9]}fell off and bumped his head,
-Mama called the doctor and the doctor said,
-'No more #{animal} jumping off the bed!'\n\n"
-
-puts "No more #{animal} jumping on the bed!"
+'No more monkeys jumping on the bed!'\n\n" end
+end
