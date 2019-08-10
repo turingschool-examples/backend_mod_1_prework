83345f021fdd52ab408334d45a32e84c1934da98
Tyler <52803986+tybalogh@users.noreply.github.com>
2019-08-10 12:28:14 -0600

    Update burrito.rb

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -5,15 +5,19 @@
 # 3. change_protein
 
 class Burrito # => class
-  attr_reader :protein, :base, :toppings # => attributes
-  def initialize(protein, base, toppings) # => methods
+  attr_reader :protein, :base, :toppings, :plate, :sides# => attributes
+  def initialize(protein, base, toppings, plate, sides) # => methods
     @protein  = protein
     @base     = base
     @toppings = toppings
+    @plate = plate
+    @sides = sides
   end
 end
 
-dinner = Burrito.new("Chicken", "Rice", ["cheese", "salsa", "sour cream"])
+dinner = Burrito.new("Chicken", "Rice", ["cheese", "salsa", "sour cream"], "Plastic plate", "Beans")
 p dinner.protein # => prints method "Chicken"
 p dinner.base # => prints method "Rice"
 p dinner.toppings # => prints method ["cheese", "salsa", "sour cream"]
+p dinner.plate # => prints method "Plastic plate"
+p dinner.sides # => prints method "Beans"
