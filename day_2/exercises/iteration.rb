5858339a55192058eca53b8ad479df8e266021e3
msimon42 <msimon@uab.edu>
2019-08-21 21:48:50 -0500

    day 2 exercises

--- day_2/exercises/iteration.rb
+++ day_2/exercises/iteration.rb
@@ -14,13 +14,20 @@ end
 # <animal> is awesome!" for each animal.
 
 animals.each do |animal|
-  # YOUR CODE HERE
+  puts "The #{animal} is awesome"
 end
 
 # Create an array of foods and then iterate over that array to print "Add
 # <food> to shopping list" for each food item.
-# YOUR CODE HERE
+foods = ["apple", "milk", "cheese", "butter"]
 
+foods.each do |food|
+  puts "Add #{food} to grocery list"
+end
 # Create an array of numbers and then iterate over that array to print doubles
 # of each of the number.
-# YOUR CODE HERE
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+
+nums.each do |num|
+  puts num * 2
+end   
