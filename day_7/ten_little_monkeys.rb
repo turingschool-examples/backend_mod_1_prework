a0a68a53ea271e417cb4f1088fed36ccc3bb1aeb
Joshua O'Bannon <josho@Joshuas-MBP.domain>
2019-08-22 19:54:25 -0600

    Change Ten Monkeys

--- day_7/ten_little_monkeys.rb
+++ day_7/ten_little_monkeys.rb
@@ -1,9 +1,22 @@
 counter = 0
-puts "Please enter a number of Monkey jumping on the bed: "
+puts "Please enter a number of Monkey jumping on the bed (up to ten): "
 num_of_monkeys = $stdin.gets.chomp.to_i
 
+num_index = {
+    1 => "One",
+    2 => "Two",
+    3 => "Three",
+    4 => "Four",
+    5 => "Five",
+    6 => "Six",
+    7 => "Seven",
+    8 => "Eight",
+    9 => "Nine",
+    10 => "Ten"
+}
+
 while (counter < (num_of_monkeys)) do
-  puts "#{num_of_monkeys - counter} little monkeys jumping on the bed, "
+  puts "#{num_index[(num_of_monkeys - counter)]} little monkeys jumping on the bed, "
   puts "One fell off and bumped his head, "
   puts "Mama called the doctor and the doctor said,"
   puts "No more monkeys jumping on the bed!"
