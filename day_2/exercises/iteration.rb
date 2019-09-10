c2917563759f39c6ca08b743e2448aacb9dabff3
Raymond Nguyen <rayknguyen@gmail.com>
2019-09-09 22:09:41 -0600

    iteration complete

--- day_2/exercises/iteration.rb
+++ day_2/exercises/iteration.rb
@@ -13,14 +13,36 @@ end
 # Write code below that iterates through a list of animals and prints "The
 # <animal> is awesome!" for each animal.
 
+animals = ["Zebra", "Giraffe", "Elephant"]
+
 animals.each do |animal|
-  # YOUR CODE HERE
+
+  print "The #{animal} is awesome!"
+
 end
+
 
 # Create an array of foods and then iterate over that array to print "Add
 # <food> to shopping list" for each food item.
 # YOUR CODE HERE
 
+foods = ["milk","bread","sugar"]
+
+foods.each do |food|
+
+  print "Add #{food} to shopping list."
+
+end
+
+
 # Create an array of numbers and then iterate over that array to print doubles
 # of each of the number.
 # YOUR CODE HERE
+
+numbers = ["1","2","3"]
+
+numbers.each do |number|
+
+  print number* 2
+
+end
