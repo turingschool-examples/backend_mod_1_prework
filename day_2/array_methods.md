fec9bcb9464882416f36783c89e0431fc31d6430
William Meighan <williamdmeighan@gmail.com>
2019-09-12 15:07:13 -0600

    Add Day 2 Work

---
+++ day_2/array_methods.md
@@ -0,0 +1,26 @@
+.sort
+  This method will sort the elements of string in alphabetical order, and integers in ascending numerical order. Can also be formatted to sort in descending alphabetical or numerical order, as well as other possible orders.
+
+each
+  Allows iteration through each element of an array. You can also use the index of each element (numerical value) as a parameter by running each!.
+
+join
+  Using join will combine all elements of an array into one string.
+
+index
+  Running index will return the index number associated with the element that you are running the index on. Index numbers always run left to right, starting with 0 for the first element of an array.
+
+include?
+  This method will tell you if a listed element is present within an array.
+
+collect
+  Using this command, you can add a value to each element within an array.
+
+first
+  This command will return the first element of an array.
+
+last
+  This command will return the last element of an array.
+
+shuffle
+  This command will re-order the elements of an array at random.
--- day_2/exercises/arrays.rb
+++ day_2/exercises/arrays.rb
@@ -5,33 +5,36 @@
 # example: write code below that will print an array of animals.
 # Store the array in a variable.
 animals = ["Zebra", "Giraffe", "Elephant"]
-p animals
+print animals
 
 # Write code below that will print an array of states. Store the array in a variable.
-states = #YOUR CODE HERE
-p states
+states = ["Alabama", "Alaska", "Arizona", "Arkansas"]
+print states
 
 # Write code below that will print an array of foods. Store the array in a variable.
-# YOUR CODE HERE
+foods = ["pizza", "hotdog", "hamburger", "french fries"]
+print foods
 
 # example: Write code below that will print the number of elements in array of
 # animals from above.
-p animals.count
+puts animals.count
 
 # Write code below that will print the number of elements in the array of
 #foods from above.
-# YOUR CODE HERE
+puts foods.count
 
 # Write code below that will print "Zebra" from the animals array
-# YOUR CODE HERE
+puts animals[0]
 
 # Write code below that will print the last item from the foods array.
-# YOUR CODE HERE
+puts animals.last
 
 # Write code below that uses a method to add "lion" to the animals array and
 # print the result
-# YOUR CODE HERE
+animals.insert(3, "lion")
+puts animals
 
 # Write code below that removes the last item of food from the foods array and
 # print the result
-# YOUR CODE HERE
+foods.delete_at(3)
+puts foods
