8727362bb4f20d7dda2d80db76c677d5d65ac122
Andrew Reid <areid.sj@gmail.com>
2019-09-13 06:04:20 -0600

    Exercises 1-5

---
+++ day_1/ex3.rb
@@ -0,0 +1,32 @@
+#Announces counting of chickens, presumably after they've hatched.
+puts "I will now count my chickens:"
+
+#Calculates 25 + 5 = 30
+puts "Hens #{25.0 + 30.0 / 6.0}"
+#Calculates 100 - 3 = 97
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+puts "Now I will count the eggs:"
+#Calculates 6 - 5 + 0 - 0 + 6 = 7
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#Calculates 5 < -2 = false
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+#Calculates 5
+puts "What is 3 + 2? #{3.0 + 2.0}"
+#Calculates -2
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+#Calculates 5 >-2 = true
+puts "Is it greater? #{5.0 > -2.0}"
+#Calculates 5 >= 2 = true
+puts "Is it greater or equal? #{5.0 >= 2.0}"
+#Calculates 5 <= -2 = false
+puts "Is it less or equal? #{5.0 <= -2.0}"
---
+++ day_1/ex5.rb
@@ -0,0 +1,21 @@
+name = 'Andrew Reid'
+age = 40 #true story
+height = 70 #inches
+weight = 230 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth}} depending on the coffee."
+
+#this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+puts "My height in centimeters is #{height * 2.54} cm."
+puts "My weight in kilograms is #{weight * 0.453592} kgs."
+puts "My age in dog years is #{age * 7} years."
