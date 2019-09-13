43716ce85783ba36c78c6725512b592c83871acc
Andrew Reid <areid.sj@gmail.com>
2019-09-13 16:50:04 -0600

    Complete strings exercise

--- day_1/exercises/strings.rb
+++ day_1/exercises/strings.rb
@@ -3,10 +3,14 @@
 # file from your terminal with the command `ruby day_1/exercises/strings.rb`
 
 # example: Write code below to print your name to the terminal:
-p "Alan Turing"
+p "Andrew Reid"
 
 # Write code below to print `Welcome to Turing!` to the terminal:
-p #YOUR CODE HERE
+p "Welcome to Turing!"
 
 # Write code below to print `99 bottles of pop on the wall...`:
-# YOUR CODE HERE
+counter = 99
+counter.times do
+  p "#{counter} bottles of beer on the wall..."
+  counter = counter - 1
+end
