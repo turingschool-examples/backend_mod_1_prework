43fc6b3bcc61897b15f7b5676ae1a4e605c3d0b4
William Meighan <williamdmeighan@gmail.com>
2019-09-16 13:01:12 -0600

    Add Day 4 Work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -4,22 +4,32 @@
 
 # example: Write a method below that, when called will print your name
 def print_name
-  p "Severus Snape"
+  puts "Will Meighan"
 end
 
 print_name
 
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-  # YOUR CODE HERE
+  puts name
 end
 
-print_name("Albus Dumbledore")
+print_name("Will Meighan")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
+def sum (a, b)
+  puts a + b
+end
+
+sum(99, 100)
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+
+def love(man, woman)
+  puts "#{man} met #{woman} at karaoke night. The rest is history."
+end
+
+love("Sonny", "Cher")
