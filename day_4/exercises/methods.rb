620a20370e227bc186083598d6e97dd5d91a8f32
sasloan <sebastiansloan94@gmail.com>
2019-09-17 14:50:19 -0600

    Complete day_4 exercise methods

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -11,15 +11,22 @@ print_name
 
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-  # YOUR CODE HERE
+  p "Sebastian Sloan"
 end
 
 print_name("Albus Dumbledore")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
+sum_of_2 = [2,2]
+
+p sum_of_2.sum
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+
+wizard_name = "Harry Potter"
+adventure = "Socerors Stone"
+
+p "My favorite book is #{wizard_name} and the #{adventure}."
