5ce8903b1849260ead44259a0cf26be1ba506334
Chase.Gaddis <cgaddis36@gmail.com>
2019-09-17 22:16:02 -0600

    Day 6 Work

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -3,3 +3,17 @@
 # terminal.
 
 #YOUR CODE HERE
+class Person
+    attr_reader :first_name, :last_name, :home_town
+
+    def initialize(first_name, last_name, home_town)
+      @first_name = first_name
+      @last_name = last_name
+      @home_town = home_town
+    end
+  end
+
+  holly = Person.new("Holly", "Bryan", "Asheville")
+  puts holly.first_name
+  puts holly.last_name
+  puts holly.home_town
