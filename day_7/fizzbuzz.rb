1ea4beeb5712a42912006453d5f6d7535db3841a
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-09-18 17:50:56 -0600

    Add Day 7 projects

---
+++ day_7/fizzbuzz.rb
@@ -0,0 +1,13 @@
+i = 0
+until i == 100
+  i += 1
+  if i % 3 == 0 && i % 5 == 0
+    puts "FizzBuzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  elsif i % 5 == 0
+    puts "Buzz"
+  else
+    puts i
+  end
+end
