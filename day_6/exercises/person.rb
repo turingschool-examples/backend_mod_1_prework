5485377ae2a94492a62b595d7b3d252ce08ff296
sasloan <sebastiansloan94@gmail.com>
2019-09-19 13:25:54 -0600

    Complete person exercise

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -2,4 +2,27 @@
 # person methods below the class so that they print their result to the
 # terminal.
 
-#YOUR CODE HERE
+class Person
+  attr_reader :name, :age, :journal 
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @journal = []
+  end
+
+  def talk
+    p "Hello my name is #{@name} and I am #{@age} years old."
+  end
+
+  def write(phrase)
+    @journal << phrase
+  end
+end
+
+sebastian = Person.new("Sebastian", 28)
+
+p sebastian.name
+p sebastian.age
+p sebastian.talk
+p sebastian.write("Good Morning, Please meet me in Clarke Hall before school.")
