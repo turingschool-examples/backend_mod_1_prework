fad793ee9316f01367e8a42199c9b7df4f28cc2e
Phil DeLong <philjdelong@gmail.com>
2019-09-19 14:41:55 -0600

    Finalize ex1

--- day_1/ex1.rb
+++ day_1/ex1.rb
@@ -1,9 +1,24 @@
-puts "Hello World!"
-#puts "Hello Again"
+#print another line
+puts "Hello World!
+"
 puts "I like typing this."
 puts "This is fun."
-#puts "Yay! Printing."
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
+puts "Another line."
 
+#prints only one line
+#puts "Hello World!"
+#puts "I like typing this."
+#puts "This is fun."
+#puts "I'd much rather you 'not'."
+#puts 'I "said" do not touch this.'
+puts "Another line."
+
+#place # infront of a line. (this makes the line a comment)
+#puts "Hello World!"
+puts "I like typing this."
+puts "This is fun."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
 puts "Another line."
