9f1778ae109065919889b5b7447611d93c2dd5d9
Phil DeLong <philjdelong@gmail.com>
2019-09-19 15:13:31 -0600

    FInalize ex6.rb

--- day_1/ex6.rb
+++ day_1/ex6.rb
@@ -1,38 +1,53 @@
-#assigning a variable to address the variety of objects.
+# assigning a variable to address the variety of objects.
 types_of_people = 10
-#assigning x a string value that call types_of_people variable within the string.
+
+# assigning x a string value that call types_of_people variable within the string.
 x = "There are #{types_of_people} types of people."
-#assigning a variable to binary
+
+# assigning a variable to binary
 binary = "binary"
-#assigning a variable to do_not
+
+# assigning a variable to do_not
 do_not = "don't"
-#assigning a string variable to y
+
+# this puts a string into another string
+# assigning a string variable to y
 y = "Those who know #{binary} and those who #{do_not}."
 
-#print the variable x to the terminal
+# print the variable x to the terminal
 puts x
-#print the variable y to the terminal
+
+# print the variable y to the terminal
 puts y
 
-#print this string to the terminal. The string pulls variable x.
+# this puts a string into another string
+# print this string to the terminal. The string pulls variable x.
 puts "I said: #{x}."
+
+# this puts a string into another string
 #print this string to the terminal. The string pulls variable y.
 puts "I also said: '#{y}'."
 
-#assigning boolean to variable hilarious
+# assigning boolean to variable hilarious
 hilarious = false
+
 #assigning a string to variable joke_evaluation
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
-#print the variable joke_evaluation to the terminal.
+# print the variable joke_evaluation to the terminal.
 puts joke_evaluation
 
-#assigning a sting to variable w
+# assigning a sting to variable w
 w = "This is the left side of..."
-#assigning a sting to variable e
-e = "a string with a right side."
 
-#print the variables w and e
+#assigning a sting to variable e
+e = 'a string with a right side.'
+
+# print the variables w and e
 puts w + e
 
-#by adding two stings using variables, we can make a longer string by telling the computer to print them together.
+# by adding two stings using variables, we can make a longer string by telling
+# the computer to print them together.
+
+# if you change the string from single to double quote, the printed message
+# will be the same. 
