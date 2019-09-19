d5040606a029d0d4578d37a9e083a26937ab2a28
Phil DeLong <philjdelong@gmail.com>
2019-09-19 14:51:52 -0600

    FInalize ex3

---
+++ day_1/ex3.rb
@@ -0,0 +1,33 @@
+puts "I will now count my chickens:"
+
+# using a string to identify the quantity by incorporating a formula within '{}'
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+#using a string to identify an action.
+puts "Now I will count the eggs:"
+
+#utilizing each of the function keys in a single equation.
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 -1.0 / 4.0 + 6.0
+
+#no brackets were used, so the integers are displayed within the string.
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+#utilizing an equation outside of the quotes will calculate and print.
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+#using an 'or' function will print a boolean.
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
+
+#using irb to calculate new equation
+puts "Is it greater? #{(5.5 / 6.0)/5 > 1}"
+=> false 
