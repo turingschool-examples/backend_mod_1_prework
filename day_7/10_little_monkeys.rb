1924977d402a41653bc53af69a8f138e055a259e
Madelyn Romero <m.romero013@gmail.com>
2019-09-19 02:20:53 -0600

    Add 10_little_monkeys final version (without flexible range)

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -1,34 +1,20 @@
-# Currently Working, with numerals
-
-numberMonkeys = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-
-numberMonkeys.each do |monkey|
-  if (monkey - 1) > 1
+monkeyHash = {10 => "Ten", 9 => "Nine", 8 => "Eight", 7 => "Seven", 6 => "Six", 5 => "Five", 4 => "Four", 3 => "Three", 2 => "Two", 1 => "One", 0 => "Zero"}
 
 
-    puts "#{monkey - 1} little monkeys jumping on the bed,
-  One fell off and bumped his head,
-  Mama called the doctor and the doctor said,
-  'No more monkeys jumping on the bed!'"
-elsif
-  (monkey - 1) == 1
-  puts "One little monkey jumping on the bed,
-  He fell off and bumped his head,
-  Mama called the doctor and the doctor said,
-  'No more monkeys jumping on the bed!'"
-
+monkeyHash.each do |numeral, string|
+  if numeral > 1
+    puts "#{string} little monkeys jumping on the bed,
+One fell down and bumped his head,
+Mama called the doctor and the doctor said,
+'No more monkeys jumping on the bed!'"
+  elsif
+    numeral == 1
+    puts "#{string} little monkey jumping on the bed,
+He fell down and bumped his head,
+Mama called the doctor and the doctor said,
+'No more monkeys jumping on the bed!'"
+  else
+    numeral == 0
     puts "Now there's no more monkeys jumping on the bed!"
   end
 end
-
-
-# How to convert string number to integer?
-    # Just need to define "ten" = 10?
-
-# Create loop:
-    # "x little monkeys", if x >= 11 (?) run nursery rhyme with x - 1 (?) monkeys.
-    # if x = 1, end loop
-
-    # Need to put hash in right before spits out rhyme? Do the whole thing
-    # with integers, and then right before it is displayed turn integers
-    # into string words?
