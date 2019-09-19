9efbc8f8ac5851d42263f998ed060d17c35c0830
sasloan <sebastiansloan94@gmail.com>
2019-09-19 13:11:07 -0600

    Complete burrito challenge

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -11,9 +11,25 @@ class Burrito
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(topping)
+    @toppings << topping
+  end
+
+  def remove_topping(topping)
+    @toppings.shift
+    @toppings
+  end
+
+  def change_protein(new_protien)
+    @protien = new_protien
+  end
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+p dinner.add_topping("Sour Cream")
+p dinner.remove_topping("Cheese")
+p dinner.change_protein("Steak")
