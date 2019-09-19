1d8baf1c2da02f4ad32dbd3f5e13d4d5a2ce56ab
Wren Steitle <lauren.steitle@gmail.com>
2019-09-18 18:00:55 -0600

    correct day 5 exercise hashes

--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -3,7 +3,7 @@
 # file from your terminal with the command `ruby day_5/exercises/hashes.rb`
 
 # example: Write code below to print a hash that holds grocery store inventory
-foods = {"apples" => 23, "grapes" => 507, "eggs" => 48}
+foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code below that will print a hash of animals and their number
