c953630e393927e237a65cc9d183a39e796aceb7
sasloan <sebastiansloan94@gmail.com>
2019-09-20 15:17:57 -0600

    Complete ceasar_cipher project

--- day_7/ceasar_cipher.rb
+++ day_7/ceasar_cipher.rb
@@ -1,4 +1,41 @@
+class CeasarCipher
+  attr_reader :message, :shift
 
+  def initialize
+    @message = message
+    @shift = shift
+  end
+
+  def encode(message, shift)
+    # this code takes the message submitted from the class object
+    # 'cipher' and sepperates the letters in the string by using .char
+    # and then using the .map method allows us to convert each
+    # letter. Now .ord is what allows out code to turn a letter
+    # into a numberical signifigance.
+    encrypt = message.chars.map do |letter|
+      letter.ord
+    end
+    # Now here we are creating a new array called shift,
+    # this array takes the array of encrypt made from our message
+    # earlier and uses .map to allow us to influence each element
+    # in the enumerable again. in this case we are taking the numerical
+    # signifigance of the letter and adding the shift the user imputed into
+    # it in this case we are adding '5' so now the method will use Ruby's built
+    # in recognition of what each symbols numberical value is and add 5 too it
+    # in order to change it's numberical value to that of a different symbol.
+    shifted = encrypt.map { |letter| letter + shift}
+    # finally we need to take our new numbers and turn them back into symbols
+    # aka letters. so we are going to map over each one of our element numbers
+    # then use .char to turn them back into their symbols then we will use .join
+    # to combine them back into a word that will of course make no sense. lol .
+    shifted.map {|letter| letter.chr}.join
+  end
+
+end
+
+cipher = CeasarCipher.new
+
+p cipher.encode("Hello World", 5)
 
 
 
