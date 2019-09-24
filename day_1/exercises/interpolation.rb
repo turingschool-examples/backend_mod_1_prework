ebc303594476c81bdb74265a2d986a20b5567f7a
Anthony Gomes <code@tonygom.es>
2019-09-23 18:30:58 -0600

    Add Day 1

--- day_1/exercises/interpolation.rb
+++ day_1/exercises/interpolation.rb
@@ -3,21 +3,21 @@
 # file from your terminal with the command `ruby day_1/exercises/interpolation.rb`
 
 # example: Write code that combines the variables below into another string:
-name = "Ron"
-team = "Chudley Cannons"
-
-p "The #{team} are #{name}'s favorite Quidditch team"
+# name = "Ron"
+# team = "Chudley Cannons"
+#
+# puts "The #{team} are #{name}'s favorite Quidditch team"
 
 # Write code that combines the variables below into a string that reads "The
 # quick red fox jumped over the lazy brown dog":
 speedy = "quick red fox"
 slow_poke = "lazy brown dog"
 
-p # YOUR CODE HERE
+puts "The #{speedy} jumped over the #{slow_poke}"
 
 # Write code that combines the variables below into a string that reads "In a
 # predictable result, the tortoise beat the hare!"
 slow_poke = "tortoise"
 speedy = "hare"
 
-# YOUR CODE HERE
+puts "In a predictable result, the #{slow_poke} beat the #{speedy}!"
