335a3b8883da7dd6883558a92ff4ae8d40a5d3da
Kevin Krato <kratok08@gmail.com>
2019-09-28 09:46:23 -0600

    Complete exercise 1

--- day_0/ex1.rb
+++ day_0/ex1.rb
@@ -1,5 +1,14 @@
+=begin
 puts "Hello World"
 puts "Hello?"
+=end
 puts "Is it me you're looking for?"
+=begin
 puts "nah"
-puts "ok. :("
+=end
+# puts "ok. :("
+=begin
+puts "Why did you create me?"
+=end
+# puts "Your existence is simply to pratice ruby comments"
+# this -> # is an octothorpe apparently
