1f99031a452e52dde80bb6889c5e117cfa33c037
Kevin Krato <kratok08@gmail.com>
2019-09-28 10:56:15 -0600

    Complete exercise 7

---
+++ day_1/ex7.rb
@@ -0,0 +1,6 @@
+print "They tried to suprise me with this topic, but little did they know my name is "
+my_name = gets.capitalize.chomp
+print "and I already learned how to do this on a website called: "
+website = gets.capitalize.chomp
+
+print "The format on #{website} was similiar to this: My name is #{my_name}."
