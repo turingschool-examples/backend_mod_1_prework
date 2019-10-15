a11af6d85b67baead470e3f571b0feb3ea016938
Will Dunlap <dunlapww@gmail.com>
2019-10-14 21:07:27 -0600

    Add Day 2 Work

--- day_2/exercises/iteration.rb
+++ day_2/exercises/iteration.rb
@@ -14,13 +14,26 @@ end
 # <animal> is awesome!" for each animal.
 
 animals.each do |animal|
-  # YOUR CODE HERE
+  puts "The #{animal} is awesome!"
 end
 
 # Create an array of foods and then iterate over that array to print "Add
 # <food> to shopping list" for each food item.
-# YOUR CODE HERE
+foods = ["beets", "peanut butter", "apples"]
+foods.each {|i| puts "Add #{i} to the shopping list"}
 
 # Create an array of numbers and then iterate over that array to print doubles
 # of each of the number.
-# YOUR CODE HERE
+
+numbers = [1,3,5]
+#not sure what we mean by print doubles.
+
+#Below doubles each value in set:
+numbers.collect do |i|
+  puts 2*i
+end
+
+#Below prints each value in set twice
+numbers.each do |i|
+  puts "#{i}#{i}"
+end
