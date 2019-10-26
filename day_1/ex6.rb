1d61f4feff70c70925d49cf084eb86d6bdbb6723
Alex Lynch <mlynch5187@gmail.com>
2019-10-26 15:33:25 -0400

    Add day 1

---
+++ day_1/ex6.rb
@@ -0,0 +1,23 @@
+# types of people is a variable
+types_of_people = 10
+# x will always represent the states line
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+#the puts commands will essentially write what x and y stand for
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+#joke_evaluation will write out the statement above
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
