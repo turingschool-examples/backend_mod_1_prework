6e6a11783577e56a0bed6ec705462a706ca4fca2
Steven Anderson <steven.a.anderson2012@gmail.com>
2019-10-28 23:08:11 -0600

    Added good_dog.rb for practice

---
+++ day_6/good_dog.rb
@@ -0,0 +1,31 @@
+# FILE: good_dog.rb
+# PROGRAMMER: Steven Anderson
+# DATE: 28 October 2019
+# DESCRIPTION: An example file for learning about classes. Example from launch school.
+
+# Modules allow for using methods in other classes called mixing in.
+module Speak
+  def speak(sound)
+    puts "#{sound}"
+  end
+end
+
+class GoodDog
+  include Speak # using include effectively "mixes in" the method from a module.
+end
+
+class HumanBeing
+  include Speak
+end
+
+# .ancestors looks up method lookup chain
+puts "---GoodDog Ancestor Chain---"
+puts GoodDog.ancestors
+puts "---HumanBeing Ancestor Chain---"
+puts HumanBeing.ancestors
+
+sparky = GoodDog.new # creates a new instance of the GoodDog class. ** Instantiation **
+sparky.speak("Arf!")
+bob = HumanBeing.new # Instantiates HumanBeing
+bob.speak("Hello!")
+
