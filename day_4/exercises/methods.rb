bf574ba8aff4faf8ba2d2df5226e5532cd97e1d6
jerry <jerry@jerrys-MBP.attlocal.net>
2019-11-02 01:44:08 -0400

    Add Day 4 Work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -4,22 +4,34 @@
 
 # example: Write a method below that, when called will print your name
 def print_name
-  p "Severus Snape"
+  p "Will Kunz"
 end
 
 print_name
 
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-  # YOUR CODE HERE
+  p name
 end
 
 print_name("Albus Dumbledore")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
+def sum( x, y)
+	return x + y
+end
+
+sum( 1, 2)
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+
+def conc(sta, stb)
+	puts "When #{sta} met #{stb}."
+end
+
+conc("Bob", "Phil")
+
+
--- day_4/questions.md
+++ day_4/questions.md
@@ -1,11 +1,20 @@
 ## Day 4 Questions
 
 1. In your own words, what is the purpose of a method?
+specified task that can be called whenever desired to complete a task
 
 1. In the space below, create a method named `hello` that will print `"Sam I am"`.
+def hello()
+	puts "Sam I am"
+end
 
 1. Create a method name `hello_someone` that takes an argument of `name` and prints `"#{name} I am"`.
+def hello_someone(name)
+	puts "#{name} I am"
+end
 
 1. How would you call or execute the method that you created above?
+hello("whatever name I want to include in these brackets")
 
 1. What questions do you still have about methods in Ruby?
+How does ruby handle void methods?
