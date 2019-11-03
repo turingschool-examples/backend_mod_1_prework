4746c93f9bb47967de7f5eb18130224767f510d9
Stephanie Friend <stephaniefriend91@gmail.com>
2019-11-03 11:47:33 -0700

    Add day 7 work

---
+++ day_7/10_little_monkeys.rb
@@ -0,0 +1,18 @@
+def monkeys(n)
+
+  while n > 1
+    puts "#{n} little monkeys jumping on the bed,
+    One fell off and bumped his head,
+    Mama called the doctor and the doctor said,
+    'No more monkeys jumping on the bed!'"
+    n -= 1
+  end
+end
+
+n = 7
+monkeys(n)
+
+puts "1 little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said,
+'Get those monkeys right to bed!'"
