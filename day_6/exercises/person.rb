890f94bf6d7f22652c76652acff24794aa0d0095
Tyler Tomlinson <twtomlinson89@gmail.com>
2019-11-03 19:01:57 -0500

    Add day 6 work

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -1,5 +1,27 @@
 # Create a person class with at least 2 attributes and 2 behaviors.  Call all
 # person methods below the class so that they print their result to the
 # terminal.
+class Person
+  attr_reader :hair, :name, :tired,
 
-#YOUR CODE HERE
+  def initialize(hair, name, tired)
+    @breed  = hair
+    @name   = name
+    @tired = true
+  end
+
+  def sleep
+    @tired = false
+  end
+
+  def speak
+    p "hello"
+  end
+end
+
+
+tyler = Person.new("blonde", "Tyler")
+p tyler.age
+p tyler.name
+tyler.sleep
+p tyler.tired
