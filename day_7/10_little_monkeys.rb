87af30cfa7e83729805e0c21fd0ed60723622dfd
Jordan Williams <jordan.wallace.williams@gmail.com>
2019-11-11 20:49:36 -0700

    Update monkey output to strings

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -1,23 +1,23 @@
-require 'pry'
-
 # The variable monkeys can be adjusted to any number range and it will work with the nursery_rhyme method.
-# Example: monkeys = (1..100)
-monkeys = (1..10)
 
-def nursery_rhyme(monkey)
-  if monkey > 1
-    puts "#{monkey} little monkeys jumping on the bed,"
-    puts "One fell off and bumped his head,"
-    puts "Mama called the doctor and the doctor said,"
-    puts "No more monkeys jumping on the bed!"
-    puts " "
-  elsif monkey == 1
-    puts "#{monkey} little monkey jumping on the bed,"
-    puts "He fell off and bumped his head,"
-    puts "Mama called the doctor and the doctor said,"
-    puts "Get those monkeys right to bed!"
+monkeys = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
+
+def nursery_rhyme(monkeys)
+  monkeys.reverse_each do |monkey|
+    if monkey == "One"
+      puts " "
+      puts "#{monkey} little monkey jumping on the bed,"
+      puts "He fell off and bumped his head,"
+      puts "Mama called the doctor and the doctor said,"
+      puts "Get those monkeys right to bed!"
+    else
+      puts " "
+      puts "#{monkey} little monkeys jumping on the bed,"
+      puts "One fell off and bumped his head,"
+      puts "Mama called the doctor and the doctor said,"
+      puts "No more monkeys jumping on the bed!"
+    end
   end
 end
 
-# Reverse iterate through monkeys using the nursery_rhyme method
-monkeys.reverse_each { |monkey| nursery_rhyme(monkey) }
+nursery_rhyme(monkeys)
