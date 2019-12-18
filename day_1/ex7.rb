6cbbb218b7c85ad68a0edc0f6d5b4a8a61e6cf6c
David Tran <davidttemail@gmail.com>
2019-12-18 01:59:11 -0700

    Add day 1

---
+++ day_1/ex7.rb
@@ -0,0 +1,16 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+print "What's your name? "
+name = gets.chomp
+if name == "David"
+  puts "Welcome back David!"
+else
+  puts "Who are you stranger?"
+end
