4f9fe341f29ce22aef835feaa8b91a95aaa41033
Mike Hernandez <hernandezmichael321@gmail.com>
2019-12-19 14:01:42 -0700

    Add ex7 from learn ruby the hard way

---
+++ day_1/ex7.rb
@@ -0,0 +1,13 @@
+print "How old are you? "
+age = gets.chomp
+
+print "How tall are you? "
+height = gets.chomp
+
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall, and #{weight} heavy."
+
+# Study Drills
+# gets.chomp is a method gets stores information entered into a variable.  However, it also incudes the return key.  .chomp removes the return key entry.
