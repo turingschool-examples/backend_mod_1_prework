2e7888b9e6d9a3d46d34f8606cfb56d2c040dd56
DvDTran <57076945+DvDTran@users.noreply.github.com>
2019-12-21 00:25:07 -0700

    Update ex3.rb

--- day_1/ex3.rb
+++ day_1/ex3.rb
@@ -8,8 +8,7 @@ puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 # Puts a message
 puts "Now I will count the eggs:"
-# Puts the result of the calculation (3 + 2 + 1 - 5 + 6) because (1 / 4) and
-# (4 % 2) after dividing have a remainder of 0
+# Puts the result of the calculation (3 + 2 + 1 - 5 + 6) because (1 / 4) and (4 % 2) after dividing have a remainder of 0
 puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # Puts a message
@@ -30,6 +29,6 @@ puts "Oh, that's why it's false."
 puts "How about some more."
 
 # More boolean questions, with true/false as the output within the string
-puts "Is it greater? #{5.0 > -2.0}"
-puts "Is it greater or equal? #{5.0 >= -2.0}"
-puts "Is it less or equal? #{5.0 <= -2.0}"
+puts "Is it greater? #{5.0 > -2.0}" # => true
+puts "Is it greater or equal? #{5.0 >= -2.0}" # => true
+puts "Is it less or equal? #{5.0 <= -2.0}" # => false
