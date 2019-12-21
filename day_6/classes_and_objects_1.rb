b0eea204d0c45b6ff517dcb828b90bf503927108
DvDTran <57076945+DvDTran@users.noreply.github.com>
2019-12-21 15:32:57 -0700

    Adjust format

--- day_6/classes_and_objects_1.rb
+++ day_6/classes_and_objects_1.rb
@@ -2,14 +2,17 @@ class GoodDog
   def initialize(name)
     @name = name
   end
+  
 # The @ symbol is an instance variable. The variable exists for as long as the object exists. Meaning we can permanently store data.
   def get_name
     @name
   end
+  
 # "set_name=" is a setter method
   def set_name=(name)
     @name = name
   end
+  
 # Establishing a setter/getter for all attributes that can change is A LOT of code instead we can use 'attr_accessor'
   def speak
     "#{@name} says Arf!"
@@ -35,21 +38,24 @@ class GoodDog
   attr_accessor :name, :height, :weight
 
 # We can initialize all 3 variables at a time. 'def initialize(name)' => 'def initalize(n, h, w)'
-  def initalize(n, h, w)
-    @name = n
-    @height = h
-    @weight = w
+  def initalize(name, height, weight)
+    @name = name
+    @height = height
+    @weight = wweight
   end
+  
 # Instead of instance variables, since we're using a getter method, we should reference the method name
   def speak
     "#{name} says Arf!"
   end
+  
 # We can add a function to change all 3 variables at a time
-  def change_info(n, h, w)
-    @name = n
-    @height = h
-    @weight = w
+  def change_info(name, height, weight)
+    @name = name
+    @height = height
+    @weight = weigiht
   end
+  
 # The setter 'name=' creates a local variable.. Instead of doing that, we can tell the program to call the setter method defined in the accessor by changing our variables to 'self.name'
   def info
     "#{name} weighs #{weight} and is #{height} tall."
