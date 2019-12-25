fe0429c8278202ff47dbc3eb48a16428e583cb94
Mike Hernandez <hernandezmichael321@gmail.com>
2019-12-25 08:40:11 -0700

    Add my own code to rb2

---
+++ day_5/ex2b.rb
@@ -0,0 +1,28 @@
+states = {
+  "Florida" => "FL",
+  "Virginia" => "VA",
+  "Colorado" => "CO"
+}
+
+capital_cities = {
+  "Florida" => "Tallahassee",
+  "Virginia" => "Richmond",
+  "Colorado" => "Denver"
+}
+
+times = {
+  "Florida" => 26,
+  "Virginia" => 4,
+  "Colorado" => 6
+}
+capital_cities.each do |state, capital|
+  puts "I lived in #{capital}, #{state}."
+end
+
+times.each do |key, value|
+  puts "I lived in #{key} for #{value} years."
+end
+
+
+#
+# puts "I lived in #{capital_cities["Florida"]}, #{states["Florida"]} for #{times["Florida"]} years."
