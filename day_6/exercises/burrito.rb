e87849a3daa0da45a819cb8050a66d9b0ca1ccc3
Mike Hernandez <hernandezmichael321@gmail.com>
2019-12-29 16:57:05 -0700

    Add methods to burrito class

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -6,14 +6,43 @@
 
 class Burrito
   attr_reader :protein, :base, :toppings
-  def initialize(protein, base, toppings)
+  def initialize(protein, base, toppings=["cheese","salsa"])
     @protein  = protein
     @base     = base
     @toppings = toppings
   end
+  # I want to know what's happening to my burrito and the methods I call to it.
+  # Here's a method that simply tells me what is on my burrito.  I can call the
+  # arguments with the attr_reader method, not the instance variable.
+  def description
+    "Your dinner is a #{protein} burrito in a #{base} with #{toppings.join" & "}."
+  end
+  # I changed the default value of toppings to an array.  To add to my topppings I will keep
+  # my existing toppings and add with push.  I'll push in the new topping as an argument to the toppings array
+  # and since the default values of this exercise were done with attr_reader, not writer, I will use the
+  # intance variable.
+  def add_topping(new_top)
+    @toppings = @toppings.push(new_top)
+  end
+
+  # I'll remove toppings using the pop array method.
+  def remove_topping
+    @toppings.pop
+  end
+  #  I'll use the instance varaible to change the protein.  I could also use attr_writer or accessor
+  # to write these as a metod return instead of changing the instance variable.
+  def change_protein(new_p)
+    @protein = new_p
+  end
+
 end
 
-dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
-p dinner.protein
-p dinner.base
-p dinner.toppings
+dinner = Burrito.new("chicken", "bowl")
+
+p dinner.description
+dinner.add_topping("jalapenos")
+p dinner.description
+dinner.remove_topping
+p dinner.description
+dinner.change_protein("steak")
+p dinner.description
