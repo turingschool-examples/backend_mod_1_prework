777c88c91bd53f1412c292f63bc4952052d87b7a
Jesse Gietzen <gietzenjesse@gmail.com>
2019-12-28 18:15:26 -0700

    Add second answer

--- day_4/ex5.rb
+++ day_4/ex5.rb
@@ -7,3 +7,6 @@ end
 scream("Yippeee")
 
 # get rid of the return and it will modify words and puts "Yippeee!!!!"
+
+# puts words will print "Yippeee!!!!" and return nil.
+# p words will print "Yippeee!!!!" and return "Yippeee!!!!"
