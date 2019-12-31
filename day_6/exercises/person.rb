8876380d8720ed87219cbbba444bc13b359ddc57
Jesse Gietzen <gietzenjesse@gmail.com>
2019-12-30 21:30:34 -0700

    Add Day 6 Work

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -2,4 +2,39 @@
 # person methods below the class so that they print their result to the
 # terminal.
 
-#YOUR CODE HERE
+class Person
+  attr_accessor :hangry, :itchy, :hair_color
+
+  def initialize(h, i, hc)
+    @hangry = h
+    @itchy = i
+    @hair_color = hc
+  end
+
+  def feed
+    puts "Put a burrito in it!"
+    @hangry = false
+    puts "Ah, much better!"
+  end
+
+  def scratch
+    puts "Yay, back scratches!"
+    @itchy = false
+  end
+
+  def dye(new_color)
+    @hair_color = new_color
+  end
+
+end
+
+hank = Person.new(true, true, "brown")
+p "hank's hangry status is: #{hank.hangry}."
+p "hank's back is itchy: #{hank.itchy}."
+p "hank's hair is #{hank.hair_color}."
+hank.feed
+hank.scratch
+hank.dye("blue")
+p "hank's hangry status is: #{hank.hangry}."
+p "hank's back is itchy: #{hank.itchy}."
+p "hank's hair is #{hank.hair_color}."
