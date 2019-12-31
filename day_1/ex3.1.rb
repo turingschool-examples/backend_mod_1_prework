3c86265628bbe4e13cc638436270deb2ce55619e
Kelsha_Darby <ohanaipo@gmail.com>
2019-12-30 21:36:30 -0700

    Add day 1

---
+++ day_1/ex3.1.rb
@@ -0,0 +1,11 @@
+puts "Time to calculate the amount of water in my fish tanks"
+
+puts "55 Gallon #{55 / 4 * 3} Gal"
+puts "20 Gallon #{20 / 6 * 5} Gal"
+puts "10 Gallon #{10 / 7 * 6} Gal"
+
+puts "Now the number of Axolotls"
+
+puts "In 55 Gal #{2 + 1}"
+puts "In 20 Gal #{2 - 1}"
+puts "In 10 Gal #{3 - 1}"
