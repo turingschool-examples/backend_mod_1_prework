dfecb736d3e5543b9c74d9c85ef2bbb8b2934e93
Maxwell Baird <bairdm71@gmail.com>
2020-01-06 18:02:55 -0700

    Finish day_1

---
+++ day_1/ex6.rb
@@ -0,0 +1,33 @@
+#Creates an variable that holds a value of 10
+types_of_people = 10
+#Inserts the variable into a new variable that is a string
+x = "There are #{types_of_people} types of people."
+#Creates variable called binary that holds a string
+binary = "binary"
+#Created another variable called do_not that holds a string
+do_not = "don't"
+#Combineds the previous two variables with a new third one to create a new long string
+y = "Those who know #{binary} and those who #{do_not}."
+#Prints out variable x that holds one other variable
+puts x
+#Prints out variable y which holds two other variables inside it
+puts y
+#prints out variable x inside a string
+puts "I said : #{x}."
+#Prints out variable y inside a string
+puts "I also said: '#{y}'."
+#Created a boolean variable
+hilarious = false
+#Create a variable that had inserted the variable hilarious
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+#Prints out the previous variable
+puts joke_evaluation
+#Create a variable that holds a string
+w = "This is the left side of ..."
+#Created a variable that holds a string
+e = "a string with a right side."
+#Combines the two string variable and prints out one line in the terminal
+puts w + e
+#There are four strings inserted into another strings, twice on line 10, once on 16 and once on 18.
+#If you use single quote instead on double quote the computer will still print it out as a string.
+#Using + symbol with two strings cause those two strings to become one whole string instead of two different strings.
--- day_1/questions.md
+++ day_1/questions.md
@@ -1,17 +1,29 @@
 ## Day 1 Questions
 
 1. How would you print the string `"Hello World!"` to the terminal?
+`p "Hello World!"`
 
 1. What is the character you would use to indicate comments in a ruby file?
+The character is # also known as an octothorpe
 
 1. Explain the difference between an integer and a float?
+The difference between an integer and a float is that is when the computer is calculating
+the values it will round the integer numbers to a whole number if there is a decimal point,
+while for the float it will not round up to a whole number.
 
 1. In the space below, create a variable `animal` that holds the string `"zebra"`
+`animal = "zebra"`
 
 1. How would you print the string `"zebra"` using the variable that you created above?
+`p animal`
 
 1. What is interpolation? Use interpolation to print a sentence using the variable `animal`.
+interpolation is when you insert a variable into a string. For example,
+`puts "bob's favorite animal is #{animal}"`
 
 1. How do we get input from a user? What is the method that we would use?
+In order to get input from a user in the terminal we use the get method which ask the user to
+type into the terminal and press enter in order to submit it.
 
 1. Name and describe two common string methods.
+length method tells us how many characters there are in the strings, while the split method breaks the string into substrings based on the space character. With the split method you can also input an argument that breaks down the string with a different character instead.
