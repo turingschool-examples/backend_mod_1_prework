c692d8a61d4569ab5af65e8a35b62cf0b6906601
Maxwell Baird <bairdm71@gmail.com>
2020-01-08 13:40:02 -0700

    add a study drill

---
+++ day_1/ex3Calc.rb
@@ -0,0 +1,3 @@
+puts "A 10 kilometer run in miles is #{10 * 0.621371}"
+puts "While a 5 mile run in kilometers is #{5 * 1.60934}"
+puts "Therefore you want to run a 10 kilometer run right? #{10 * 0.621371 < 5 * 1.60934}"
