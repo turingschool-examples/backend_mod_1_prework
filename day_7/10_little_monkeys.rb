7647af2c93e2f7a2a49bf6f3cb54b22577c469b9
Chris Stankus <chrisstankus99@gmail.com>
2020-01-09 09:04:42 -0700

    Add Day 7 Work

---
+++ day_7/10_little_monkeys.rb
@@ -0,0 +1,11 @@
+monkeys = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
+puts "One little monkey jumping on the bed,"
+puts "He fell off and bumped his head,"
+puts "Mama called the doctor and the doctor said,"
+puts "Get those monkeys right to bed!"
+monkeys.each do |monkey|
+  puts "#{monkey} little monkey jumping on the bed,"
+  puts "One fell off and bumped his head,"
+  puts "Mama called the doctor and the doctor said,"
+  puts "No more monkeys jumping on the bed!"
+end
