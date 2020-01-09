0a4c842919d8c8456450fdd3569f4ddeaacfc287
Colin Alexander <colintalex@gmail.com>
2020-01-09 13:03:05 -0700

    Adds Completed Day 5

--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -8,18 +8,32 @@ p foods
 
 # Write code below that will print a hash of animals and their number
 # at the zoo. (an inventory of animals)
-zoo = #YOUR CODE HERE
+zoo = {zebras: 6, giraffes: 3, meerkats: 36, penguins: 12}
 p zoo
 
 # Using the zoo that you created above, print all the keys in the hash.
-# YOUR CODE HERE
+zoo.each do |name, number|
+  puts "The keys of the zoo hash are #{name}."
+end
 
 # Using the zoo that you created above, print all the values in the hash.
-# YOUR CODE here
+zoo.each do |name, number|
+  puts "The values of the zoo hash are #{number}."
+end
+
 
 # Using the zoo taht you created above, print the value of the first item in
 # the hash
-# YOUR CODE HERE
+p zoo[:zebras]
 
 # Add an animal to the zoo hash and print the updated hash.
 # YOUR CODE HERE
+zoo[:lions] = 8
+p zoo
+
+states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}
+p states['IA']
+
+states.each do |abbrev, state|
+  p abbrev
+end
