3c32c50e0b7ad350fc35a565a261e3d53dee7503
Margo Flewelling <flewelling.margo@gmail.com>
2020-01-12 12:02:38 -0700

    Add Day 6 Work

---
+++ day_6/practice.rb
@@ -0,0 +1,14 @@
+class Student
+end
+
+class Student
+  attr_accessor :first_name, :last_name, :primary_phone_number
+
+  def introduction(target)
+     puts "Hi #{target}, I'm #{first_name}!"
+   end
+ end
+
+frank= Student.new
+frank.first_name = 'Frank'
+frank.introduction('Katrina')
