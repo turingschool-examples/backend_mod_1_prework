309004d4103b065c0fe42773df71d48fd1ffd0ad
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-12 09:02:17 -0700

    Allow negative number input

--- day_7/checkerboard.rb
+++ day_7/checkerboard.rb
@@ -2,10 +2,21 @@
 # building this as a class with method calls would be a little less cumbersome but in the end it all works
 # using new array calls and lots of variables
 
-print "How wide will this board be?  >"
-width = gets.chomp.to_i
-print "How tall will this board be?  >"
-height = gets.chomp.to_i
+print "How wide will this board be?  > "
+get_width = gets.chomp.to_i
+if get_width < 0
+  width = get_width * -1
+else
+  width = get_width
+end
+
+print "How tall will this board be?  > "
+get_height = gets.chomp.to_i
+if get_height < 0
+  height = get_height * -1
+else
+  height = get_height
+end
 
 big_even = height / 2
 
@@ -28,6 +39,9 @@ end
 even = wide_even.join("") + "\n" + wide_odd.join("") + "\n"
 odd = wide_even.join("")
 
+system "clear"
+print "\nHere is you #{get_width} X #{get_height} checkerboard\n\n"
+
 if height == 1
   print odd
 elsif height == 2
@@ -37,3 +51,5 @@ elsif height % 2 == 1
 elsif height % 2 == 0
   puts even * big_even
 end
+
+print "\n\n"
