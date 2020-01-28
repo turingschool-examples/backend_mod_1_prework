2075072930376cb3ac198cb3fd6d5a0881141c1e
Taylor Keller <takeller17@gmail.com>
2020-01-28 13:59:30 -0700

    Add Day 4 Work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -11,15 +11,25 @@ print_name
 
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-  # YOUR CODE HERE
+  p name
 end
 
 print_name("Albus Dumbledore")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
 
+def add(x, y)
+  p x + y
+end
+
+add(5, 5)
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+
+def join_string(word1, word2)
+  p word1 + ' ' + word2
+end
+
+join_string("hi", "five")
