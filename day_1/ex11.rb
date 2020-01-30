bf085bdf6ebebbc806570c95837e97ffcbedc36a
Kayla Frankum <frankumk@gmail.com>
2020-01-29 21:05:41 -0800

    Add day 1

---
+++ day_1/ex11.rb
@@ -0,0 +1,19 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy?"
+y_or_n = gets.chomp
+#gets. chomp is used to take string input from users. a = gets. chomp : gets. chomp takes input from the user through the keyboard and store it in the variable a.
+
+print "Do you like to code? "
+likes_to_code = gets.chomp
+print "Do you like to climb? "
+likes_to_climb = gets.chomp
+
+print "Trying this string thing"
+a = gets.chomp.to_i
+puts a
---
+++ day_1/ex3.rb
@@ -0,0 +1,28 @@
+puts "I will now count my chickens:"
+# addition and division with order of operations
+puts "Hens #{25 + 30 / 6}"
+
+#100 - ((25*3) Remainder after / 4)
+puts "Roosters #{100 - 25.0 * 3 % 4.0}"
+
+#order of perations
+puts "Now I will count the eggs:"
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#compares both sides
+puts 3.1 + 2 < 5 - 7
+
+#math inside the hash bracket..NICE
+puts "What is 3 + 2? #{3.0+2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+#More comparisons
+puts "Is it greater? #{5.0 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
