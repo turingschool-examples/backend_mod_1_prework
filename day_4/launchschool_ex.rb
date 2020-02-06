f90cda2c2fce5f9eedaf1a24e4086d3c29f05807
Kayla Frankum <frankumk@gmail.com>
2020-02-05 22:20:52 -0800

    Add Day 4 Work

---
+++ day_4/launchschool_ex.rb
@@ -0,0 +1,28 @@
+#1
+
+def greeting(name)
+  "Welcome #{name}"
+end
+
+puts greeting("Kayla")
+
+#1. 2
+#2. nil
+#3. Joe
+#4. four
+#5. nill
+
+def multiply(x,y)
+  x * y
+end
+
+p multiply(2,2)
+
+
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
