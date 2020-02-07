c1bc72348f46807cb53835567991be06d24b1579
Jamie Tran <anh.jamietran@gmail.com>
2020-02-06 19:13:47 -0700

    Adds completed ex11.rb

---
+++ day_1/ex11.rb
@@ -0,0 +1,24 @@
+# Asking For Input
+
+=begin
+Most of what software does is:
+take input from a person,
+change it,
+print out something to show how it changed.
+=end
+
+# gets.chomp takes the user's input and chomps the terminal newline
+
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+print "What is your name? "
+name = gets.chomp
+
+puts "Hi, #{name}, nice to meet you."
