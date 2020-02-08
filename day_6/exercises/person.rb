d8ffb9c793bb20c3b593d912c5f766f2e6c2c5dc
Jamie Tran (Anh) <31839316+gothgoth@users.noreply.github.com>
2020-02-08 02:26:28 -0700

    Update person.rb

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -3,3 +3,30 @@
 # terminal.
 
 #YOUR CODE HERE
+
+class Person
+  attr_reader :name, :origin_planet
+  
+  def initialize(name, origin_planet)
+    @name = name
+    @origin_planet = origin_planet
+  end
+  
+  def greet
+      "I am #{@name}! I am from #{origin_planet}!"
+  end
+  
+  def eye_contact
+      "#{@name} stares intently."
+  end
+
+end
+
+grok = Person.new("Grok", "Bla'Ath'n")
+
+bitmer = Person.new("Bit-Mer", "Sn-ltA")
+
+p grok.greet
+p bitmer.eye_contact
+p bitmer.greet
+p grok.eye_contact
