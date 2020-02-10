ca2def0f82e5fa9bb3eacadbffd925b00f85236a
Derek Borski <derek.borski@gmail.com>
2020-02-10 12:01:50 -0700

    Add day 1

---
+++ day_1/ex6.rb
@@ -0,0 +1,36 @@
+# Variable for types of people that exist
+types_of_people = 10
+# Variable that has a string that calls the different types of people
+x = "There are #{types_of_people} types of people."
+# Variable for the string binary
+binary = "binary"
+# Variable for don't
+do_not = "don't"
+# Variable for the punchline of the joke
+y = "Those who know #{binary} and those who #{do_not}."
+
+# prints variable x
+puts x
+# prints variable y
+puts y
+
+# Prints string with variable x
+puts "I said: #{x}."
+# Prints string with variable y
+puts "I also said: '#{y}'."
+
+# Defines variable hilarious as false
+hilarious = false
+# Defines variable joke_evaluation with a string that calls hilarious variable
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# Prints the variable joke_evaluation
+puts joke_evaluation
+
+# Defines variable w with a string
+w = "This is the left side of..."
+# Defines variable e with a string
+e = "a string with a right side."
+
+# Prints the sum of variables w + e
+puts w + e
