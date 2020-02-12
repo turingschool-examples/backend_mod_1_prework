61ccbd6460d6f42d44ee4285ac04efe60422c4b6
Melanie Tran <melaniexx02@gmail.com>
2020-02-12 15:03:37 -0700

    Add day 5 work

---
+++ day_5/test.rb
@@ -0,0 +1,25 @@
+pet_store = {
+  dog_food: 32,
+  collars: 11,
+  leashes: 9,
+  toys: 43
+}
+
+p pet_store
+
+
+states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}
+p states["IA"]
+p states.keys
+p states.values
+
+
+about_me = {
+  "Name" => "Melanie",
+  "Age" => "25",
+  "Location" => "Denver",
+  "Astrology" => "Pisces"
+}
+
+p about_me
+p about_me["Age"]
