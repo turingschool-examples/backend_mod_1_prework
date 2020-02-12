33a1b860a600c6ce9840181fb3e9829c4c7f997b
Melanie Tran <melaniexx02@gmail.com>
2020-02-11 22:01:40 -0700

    Add Day 4 work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -11,15 +11,32 @@ print_name
 
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-  # YOUR CODE HERE
+  puts name
 end
 
+print_name("Melanie Tran")
 print_name("Albus Dumbledore")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
+def add(a, b)
+  puts "Adding #{a} + #{b}"
+  return a + b
+end
+
+sum = add(2, 3)
+puts "The sum is #{sum}."
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+
+def concatenation(day, weather)
+  puts "On #{day}, it will be #{weather}."
+end
+
+puts "Here is this week's forecast!"
+concatenation("Monday", "rainy")
+concatenation("Tuesday", "snowy")
+concatenation("Wednesday", "windy")
+concatenation("Thursday", "terrible and cloudy")
