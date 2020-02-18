45b7089a7bfb5eb9bd47d69b329d92d69c5ce8cf
Danny Ramos <danramos21@gmail.com>
2020-02-17 22:44:44 -0700

    Add day 1

---
+++ day_1/ex2.rb
@@ -0,0 +1,8 @@
+# A comment. this is so you can read your program later.
+# Anything after the # is ignored by ruby.
+
+puts "I could have code like this." # and the commend after is ignored
+
+# puts "this wont run"
+
+puts "this will run"
