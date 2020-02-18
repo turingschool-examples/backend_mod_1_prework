988d637aa55f1f1f7c6f78f5357ec83046233fc5
Jane Greene <janegreenecasa@gmail.com>
2020-02-18 16:21:59 -0700

    Add day 1

---
+++ day_1/ex5.rb
@@ -0,0 +1,18 @@
+name = 'Zed A. Shaw'
+age = 35 #not a lie in 2009
+height = 74.0 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height / 12.0} feet tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually he's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+#this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight}
+I get #{age + height + weight}."
