c0fcbaa70ea4c37793aa242a02a87517d4bd3d81
Danny Ramos <danramos21@gmail.com>
2020-02-19 23:41:09 -0700

    Add Day 3 Work

--- day_3/exercises/if_statements.rb
+++ day_3/exercises/if_statements.rb
@@ -26,7 +26,21 @@
 # Manipulate the variable 'weather' to see if you can print something other
 # than 'coat'
 
+weather = 'icy'
 
+if weather == 'sunny'
+  p "sunscreen"
+elsif weather == 'rainy'
+  p "umbrella"
+elsif weather == 'snowy'
+  p "coat"
+elsif weather == 'icy'
+  p "yak traks"
+else
+  p "good to go!"
+end
+
+By changing the variable to anything else but `snowy` it will change the output.
 ##################
 # Using the num_quarters variable defined below, determine
 # if you have enough money to buy a gumball. A gumball costs
@@ -44,8 +58,12 @@
 
 num_quarters = 0
 
-puts "I have enough money for a gumball"
-puts "I don't have enough money for a gumball"
+f num_quarters >= 2
+  p "I have enough money for a gumball"
+elsif num_quarters =< 1
+  p "I don't have enough money for a gumball"
+end
+
 
 
 #####################
@@ -61,3 +79,15 @@ puts "I don't have enough money for a gumball"
 
 cups_of_flour = 1
 has_sauce = true
+
+if cups_of_flour == 1 && has_sauce
+  p "I cannot make pizza"
+elsif cups_of_flour == 1 && has_sauce == false
+  p "I cannot make pizza"
+elsif cups_of_flour == 2 && has_sauce
+  p "I can make pizza"
+elsif cups_of_flour == 2 && has_sauce == false
+  p "I can make pizza"
+elsif cups_of_flour == 3 && has_sauce
+  p "I can make pizza"
+end
