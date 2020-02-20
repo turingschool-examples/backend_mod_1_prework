5d716f4d0cbcadf5d67c6e69a19bbb41bf704cfa
stellakunzang <stella.k.bonnie@gmail.com>
2020-02-20 13:45:41 -0700

    Complete hashes exercises

--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -8,18 +8,22 @@ p foods
 
 # Write code below that will print a hash of animals and their number
 # at the zoo. (an inventory of animals)
-zoo = #YOUR CODE HERE
-p zoo
+zoo = {"zebras" => 15, "elephants" => 5, "lions" => 2}
+puts zoo
 
 # Using the zoo that you created above, print all the keys in the hash.
-# YOUR CODE HERE
+
+puts zoo.keys
 
 # Using the zoo that you created above, print all the values in the hash.
-# YOUR CODE here
 
-# Using the zoo taht you created above, print the value of the first item in
+puts zoo.values
+
+# Using the zoo that you created above, print the value of the first item in
 # the hash
-# YOUR CODE HERE
+
+puts zoo["zebras"]
 
 # Add an animal to the zoo hash and print the updated hash.
-# YOUR CODE HERE
+zoo["tigers"] = 7
+puts zoo
