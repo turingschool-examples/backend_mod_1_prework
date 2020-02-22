451016cde43c8ba83f85f7cb64d63889a7902313
Eric Hale <eric_hale@comcast.net>
2020-02-22 10:33:32 -0700

    Add escape to quotes on 7 and 12, Add int_to_str

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -1,16 +1,21 @@
 def little_monkeys
-      gets.chomp.to_i.downto (1) do |x|
-    if x != 1
-      puts "#{x} little monkeys jumping on the bed,
+      gets.chomp.to_i.downto (1) do |num|
+    if num != 1
+      puts "#{int_to_str[num]} little monkeys jumping on the bed,
       One fell off and bumped his head,
       Mama called the doctor and the doctor said,
-      'Get those monkeys right to bed!'"
+      \"No more monkeys jumping on the bed!\""
     else
-      puts "#{x} little monkey jumping on the bed,
+      puts "#{int_to_str[num]} little monkey jumping on the bed,
       he fell off and bumped his head,
       Mama called,the doctor and the doctor said,
-      'Get those monkeys right to bed!'"
+      \"Get those monkeys right to bed!\""
     end
-  end
+    def int_to_str
+    {1 => 'One', 2 => 'Two', 3 => 'Three', 4 => 'Four',
+    5 => 'Five', 6 => 'Six', 7 => 'Seven', 8 => 'Eight', 9 => 'Nine', 10 => 'Ten'}
+    end
+ end
 end
+puts "How many monkeys are there?"
 p little_monkeys
