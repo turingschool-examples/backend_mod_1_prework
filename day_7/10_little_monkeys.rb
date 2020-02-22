9947f56d9635025363e6cb9cb06f9a9a62e01a66
Eric Hale <eric_hale@comcast.net>
2020-02-22 08:54:40 -0700

    Add gets.chomp.to_i to method

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -1,15 +1,16 @@
 def little_monkeys
-      (1..gets.chomp.to_i).each do |x|
+      gets.chomp.to_i.downto (1) do |x|
     if x != 1
-      puts '#{x} little monkeys jumping on the bed,
+      puts "#{x} little monkeys jumping on the bed,
       One fell off and bumped his head,
       Mama called the doctor and the doctor said,
-      "Get those monkeys right to bed!"'
+      'Get those monkeys right to bed!'"
     else
-      puts '#{x} little monkey jumping on the bed,
+      puts "#{x} little monkey jumping on the bed,
       he fell off and bumped his head,
       Mama called,the doctor and the doctor said,
-      "Get those monkeys right to bed!"'
+      'Get those monkeys right to bed!'"
     end
   end
 end
+p little_monkeys
