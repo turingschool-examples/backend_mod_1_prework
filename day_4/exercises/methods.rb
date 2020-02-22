8a5c06f04c5c52bc74740ed8bec574cd691180d8
Danny Ramos <danramos21@gmail.com>
2020-02-21 17:24:38 -0700

    Add Day 4 Work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -7,19 +7,43 @@ def print_name
   p "Severus Snape"
 end
 
-print_name
+  def print_name
+    puts "Danny Ramos"
+  end
+
+  puts print_name
+  => "Danny"
 
 # Write a method that takes an argument of your name and prints your name
-def print_name(name)
-  # YOUR CODE HERE
-end
+  def print_name(name)
+    puts name + "."
+  end
 
-print_name("Albus Dumbledore")
+  print_name("Danny")
+  => Danny.
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
+  def add(a,b)
+    puts "ADDING #{a} + #{b}"
+    return a + b
+  end
+
+add (2, 4)
+
+ADDING 2 + 4
+=> 6
+
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+
+  def modes_of_transportation (car, bike)
+    puts "A #{car} is faster than a #{bike}"
+  end
+
+modes_of_transportation("Jeep", "Fixie")
+
+A Jeep is faster than a Fixie
+=> nil
