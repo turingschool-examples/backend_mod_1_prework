c35f8debd14b981f7925fedc4d66f46ea213cb6c
Mad Hallé <mad.halle@icloud.com>
2020-02-23 13:04:29 -0700

    Add day 3 work

--- day_3/exercises/if_statements.rb
+++ day_3/exercises/if_statements.rb
@@ -9,7 +9,7 @@
   # if it is snowy, print "coat"
   # if it is icy, print "yak traks"
 
-  weather = 'snowy'
+  weather = 'sunny'
 
   if weather == 'sunny'
     p "sunscreen"
@@ -42,10 +42,13 @@
 # When num_quarters = 2, program should print "I have enough money for a gumball"
 # When num_quarters = 3, program should print "I have enough money for a gumball"
 
-num_quarters = 0
+num_quarters = 3
+if num_quarters < 2
+  p "I don't have enough money for a gumball"
+elsif num_quarters >= 2
+  p "I have enough money for a gumball"
+end
 
-puts "I have enough money for a gumball"
-puts "I don't have enough money for a gumball"
 
 
 #####################
@@ -59,5 +62,12 @@ puts "I don't have enough money for a gumball"
 # When cups_of_flour = 2 and has_sauce = true, your program should print "I can make pizza"
 # When cups_of_flour = 3 and has_sauce = true, your program should print "I can make pizza"
 
-cups_of_flour = 1
-has_sauce = true
+cups_of_flour = 3
+has_sauce = false
+
+
+if cups_of_flour >=2 and has_sauce == true
+  p "I can make pizza"
+else
+  p "I cannot make pizza"
+end
---
+++ day_3/exercises/practice.rb
@@ -0,0 +1,33 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats
+  p "Too many cats! The world is doomed!"
+end
+
+if people > cats
+  p "Not many cats! The world is saved!"
+end
+
+if people < dogs
+  p "The world is drooled on!"
+end
+
+if people > dogs
+  puts "The world is dry!"
+end
+
+dogs += 5
+
+if people >= dogs
+  puts "People are greater than or equal to dogs."
+end
+
+if people <= dogs
+  p "People are less than or equal to dogs."
+end
+
+if people == dogs
+  p "People are dogs."
+end
