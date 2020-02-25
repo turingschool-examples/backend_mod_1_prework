5ca855039618529c27ddd10375f49410a168d9e8
Derek Borski <derek.borski@gmail.com>
2020-02-25 00:44:44 -0700

    10_little_monkeys.rb initial commit

---
+++ day_7/10_little_monkeys.rb
@@ -0,0 +1,29 @@
+monkey = "Ten little monkeys jumping on the bed,
+One fell off and bumped his head,
+Mama called the doctor and the doctor said,
+\"No more monkeys jumping on the bed!\""
+
+monkey_one = "One little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said,
+\"Get those monkeys right to bed!\""
+
+puts monkey
+puts
+puts monkey.sub 'Ten', 'Nine'
+puts
+puts monkey.sub 'Ten', 'Eight'
+puts
+puts monkey.sub 'Ten', 'Seven'
+puts
+puts monkey.sub 'Ten', 'Six'
+puts
+puts monkey.sub 'Ten', 'Five'
+puts
+puts monkey.sub 'Ten', 'Four'
+puts
+puts monkey.sub 'Ten', 'Three'
+puts
+puts monkey.sub 'Ten', 'Two'
+puts
+puts monkey_one
