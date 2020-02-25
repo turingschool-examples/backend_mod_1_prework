ede1a7cc60975f79fb948a241bcbef5a43b56025
Arique Aguilar <arique.aguilar@gmail.com>
2020-02-25 16:23:04 -0700

    Study drills for ex11

---
+++ day_1/ex11.rb
@@ -0,0 +1,24 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+
+# Study Drills
+    #1
+        # the gets part of gets.chomp is used to get input from the user
+        # the chomp part of gets.chomp gets rid of the \n at the end of
+            # the string.
+    #2
+      #This example didn't make sense to me
+        # =>        x = gets
+        # =>        y = gets
+        # =>        puts x+y
+
+        # =>        x = gets.chomp
+        # =>        y = gets.chomp
+        # =>        puts x+y
