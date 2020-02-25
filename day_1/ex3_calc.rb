25c1b1476818f7cf6af01814e153ca4e02278ad2
Arique Aguilar <arique.aguilar@gmail.com>
2020-02-25 13:33:32 -0700

    Ex3 Calculator

---
+++ day_1/ex3_calc.rb
@@ -0,0 +1,12 @@
+puts "Calculating TANF Hours"
+
+puts "On average, there are 30 days in the month"
+puts "I need to complete 135 hours a month"
+
+puts "In January, there are 23 weekdays to complete all my hours."
+puts "I worked #{135/23} hours per day, to meet my hours."
+
+puts "In February there are 20 weekdays to work during the week."
+puts "Which means I will have to work #{135/20} per day to meet my hours."
+puts "Am I tired? #{5 <= 2}"
+puts "Am I ready? #{5 > 2}"
