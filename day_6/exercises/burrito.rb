e6a46b00a02a12e82497eb149f02917d8848a64f
Derek Borski <derek.borski@gmail.com>
2020-02-24 21:54:37 -0700

    Added a few examples

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -11,9 +11,24 @@ class Burrito
     @base     = base
     @toppings = toppings
   end
+  def add_topping
+    puts "You are having a burrito with #{toppings.last}."
+  end
+
+  def remove_topping
+    puts "Would you like to remove #{toppings.first} from your burrito?"
+  end
+
+  def change_protein
+    @protein1 = steak
+    puts "Would you like to change your protein from #{protein} to #{@protein1}?"
+  end
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+p dinner.add_topping
+p dinner.remove_topping
+p dinner.change_protein
--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -2,4 +2,36 @@
 # person methods below the class so that they print their result to the
 # terminal.
 
-#YOUR CODE HERE
+class Person
+
+ attr_accessor :height, :weight, :smiling, :eating
+ def initialize(height, weight, smiling, eating)
+   @height = height
+   @weight = weight
+   @smiling = smiling
+   @eating = eating
+ end
+
+ def tall(number)
+   @height += number
+   puts "You are #{number} feet tall"
+ end
+
+ def current_height
+   puts "You are now #{@height} feet tall."
+ end
+
+ def is_smiling(true_false)
+   @smiling = true_false
+   puts "Are you smiling? #{true_false}"
+ end
+end
+
+person1 = Person.new(6, 180, true, false)
+p person1.height
+p person1.weight
+p person1.smiling
+p person1.eating
+person1.tall(5)
+person1.current_height
+person1.is_smiling(true)
