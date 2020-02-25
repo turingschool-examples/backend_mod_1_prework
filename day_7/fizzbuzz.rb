ae9bee5132ef9103322ae270d41ba41c2c223948
Danny Ramos <danramos21@gmail.com>
2020-02-24 19:19:49 -0700

    Add Day 7 Work

---
+++ day_7/fizzbuzz.rb
@@ -0,0 +1,34 @@
+`FIZZBUZZ`
+
+
+100.times do |n|
+  n += 1
+  if n % 3 == 0 && n % 5 == 0
+    puts "FizzBuzz"
+  elsif n % 3 == 0
+    puts "Fizz"
+  elsif n % 5 == 0
+    puts "Buzz"
+  else
+    puts n
+  end
+end
+
+
+`BONUS`
+
+def fizzbuzz(lower_number, higher_number)
+  (lower_number..higher_number).each do |n|
+    if n % 3 == 0 && n % 5 == 0
+      puts "FizzBuzz"
+    elsif n % 3 == 0
+      puts "Fizz"
+    elsif n % 5 == 0
+      puts "Buzz"
+    else
+      puts n
+    end
+  end
+end
+
+fizzbuzz(25,50)
