db71b718444cdac370363e89fc075cb107d47583
Jane Greene <janegreenecasa@gmail.com>
2020-02-25 16:22:52 -0700

    Add Day 7 Work

---
+++ day_7/fizzbuzz.rb
@@ -0,0 +1,14 @@
+
+
+
+for i in 1..100
+  if i % 5 == 0 && i % 3 == 0
+      puts "fizzbuzz"
+  elsif i % 3 == 0
+      puts "fizz"
+  elsif i % 5 == 0
+      puts "buzz"
+  else
+      puts i
+  end
+end
