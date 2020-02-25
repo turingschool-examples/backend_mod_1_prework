a1803111e1054d5b5163f390ae8512b51a24d312
Kiera Allen <kieraaallen@gmail.com>
2020-02-24 20:09:40 -0700

    Add FizzBuzz and Monkey Projects

---
+++ day_7/fizzbuzz.rb
@@ -0,0 +1,30 @@
+# Write a program that prints the numbers from 1 to 100 with the following rules:
+# ~ For any number that is a multiple of 3, print 'Fizz'
+# ~ For any number that is a multiple of 5, print 'Buzz'
+# ~ For any number that is a multiple of both 3 and 5, print 'FizzBuzz'
+# ~ For all other numbers, print the number.
+# The output should look something like this:
+# => 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, 13, 14, FizzBuzz, ..., 98,
+# Fizz, Buzz
+
+def fizzbuzz(n)
+  if n % 3 == 0 && n % 5 == 0
+    "FizzBuzz"
+  elsif n % 3 == 0
+    "Fizz"
+  elsif n % 5 == 0
+    "Buzz"
+  else
+    n
+  end
+end
+
+puts "How many numbers would you like to FizzBuzz with?"
+max_value = gets.chomp.to_i
+while max_value <= 0
+  puts "No, idiot. A number after 1 or the number 1. Love, Dwight."
+  max_value = gets.chomp.to_i
+end
+(1..max_value).each do |n|
+  puts fizzbuzz(n)
+end
