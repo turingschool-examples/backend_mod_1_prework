e0e33b5157a33c808ec075976a67ec7a810e9748
Arique Aguilar <arique.aguilar@gmail.com>
2020-02-25 15:00:26 -0700

    2020 TANF Average Hours Code

---
+++ day_1/ex4_calc.rb
@@ -0,0 +1,45 @@
+tanf_hours_met = 135.0
+jan_work_days = 23
+feb_work_days = 20
+mar_work_days = 22
+apr_work_days = 22
+may_work_days = 21
+jun_work_days = 22
+jul_work_days = 23
+aug_work_days = 21
+sep_work_days = 22
+oct_work_days = 22
+nov_work_days = 21
+dec_work_days = 23
+
+
+
+jan_av_hr_pr_day = tanf_hours_met / jan_work_days
+feb_av_hr_pr_day = tanf_hours_met / feb_work_days
+mar_av_hr_pr_day = tanf_hours_met / mar_work_days
+apr_av_hr_pr_day = tanf_hours_met / apr_work_days
+may_av_hr_pr_day = tanf_hours_met / may_work_days
+jun_av_hr_pr_day = tanf_hours_met / jun_work_days
+jul_av_hr_pr_day = tanf_hours_met / jul_work_days
+aug_av_hr_pr_day = tanf_hours_met / aug_work_days
+sep_av_hr_pr_day = tanf_hours_met / sep_work_days
+oct_av_hr_pr_day = tanf_hours_met / oct_work_days
+nov_av_hr_pr_day = tanf_hours_met / nov_work_days
+dec_av_hr_pr_day = tanf_hours_met / dec_work_days
+
+
+
+puts "Calculating TANF Hours"
+
+puts "In January I have to work #{jan_av_hr_pr_day} hours per day to meet my hours."
+puts "In February I have to work #{feb_av_hr_pr_day} hours per day to meet my hours."
+puts "In March I have to work #{mar_av_hr_pr_day} hours per day to meet my hours."
+puts "In April I have to work #{apr_av_hr_pr_day} hours per day to meet my hours."
+puts "In May I have to work #{may_av_hr_pr_day} hours per day to meet my hours."
+puts "In June I have to work #{jun_av_hr_pr_day} hours per day to meet my hours."
+puts "In July I have to work #{jul_av_hr_pr_day} hours per day to meet my hours."
+puts "In August I have to work #{aug_av_hr_pr_day} hours per day to meet my hours."
+puts "In September I have to work #{sep_av_hr_pr_day} hours per day to meet my hours."
+puts "In October I have to work #{oct_av_hr_pr_day} hours per day to meet my hours."
+puts "In November I have to work #{nov_av_hr_pr_day} hours per day to meet my hours."
+puts "In December I have to work #{dec_av_hr_pr_day} hours per day to meet my hours."
