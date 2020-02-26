1077bb700ee759f70a34c0ab7dcd92758f0751ca
Arique Aguilar <arique.aguilar@gmail.com>
2020-02-25 19:55:05 -0700

    Added Ex 11

--- day_1/ex11.rb
+++ day_1/ex11.rb
@@ -1,20 +1,20 @@
-print "How old are you? "
-age = gets.chomp
-print "How tall are you? "
-height = gets.chomp
-print "How much do you weigh? "
-weight = gets.chomp
+#print "How old are you? "
+#age = gets.chomp
+#print "How tall are you? "
+#height = gets.chomp
+#print "How much do you weigh? "
+#weight = gets.chomp
 
-puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+#puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
 
 
 # Study Drills
     #1
-        # the gets part of gets.chomp is used to get input from the user
+        # the gets part of gets.chomp you can ask the user a question that returns as a string.
         # the chomp part of gets.chomp gets rid of the \n at the end of
             # the string.
     #2
-      #This example didn't make sense to me
+      #This example didn't make sense to me, getting a new one.
         # =>        x = gets
         # =>        y = gets
         # =>        puts x+y
@@ -22,3 +22,14 @@ puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
         # =>        x = gets.chomp
         # =>        y = gets.chomp
         # =>        puts x+y
+
+      #This example Names helped me understand better.  Even though I couldn't get the strings to have a space in between.
+        #print "What is your name?"
+        #name = gets.chomp
+        #print "Hello #{name}, nice to meet you!"
+        #print "What's your favorite color?"
+        #color = gets.chomp
+        #print "Cool! That's a wonderful color."
+        #print "So your name is #{name} and your favorite color is #{color}, what else should I know about you?"
+        #random_fact = gets.chomp
+        #print " And you like to #{random_fact}, interesting!"
