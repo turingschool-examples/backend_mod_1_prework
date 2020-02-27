af8fbdcdeeb44a7f4d24a6cab9ba89e34ef79c60
Arique Aguilar <arique.aguilar@gmail.com>
2020-02-27 10:41:00 -0700

    Add arguements and period to method

--- day_4/methods_reading.rb
+++ day_4/methods_reading.rb
@@ -1,8 +1,8 @@
-def say
-  # method body goes here
+def say(words)
+  puts words + "."
 end
 
-puts "hello"
-puts "hi"
-puts "how are you"
-puts "I'm fine"
+say("hello")
+say("hi")
+say("how are you")
+say("I'm fine")
