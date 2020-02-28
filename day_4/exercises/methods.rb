63f219a15c8a6c149de4d0750b207ab9d5db550c
James Belta <james.belta@gmail.com>
2020-02-28 12:21:11 -0700

    Add Day 4 Work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -11,15 +11,29 @@ print_name
 
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-  # YOUR CODE HERE
+  p name
 end
 
 print_name("Albus Dumbledore")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
+def sum(a, b)
+  p "The sum of #{a} and #{b} is: #{a + b}"
+  return a + b
+end
+
+sum(2,5)
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+
+def team(team1, team2)
+  p "Today's match is the #{team1}, vs the #{team2}"
+end
+
+home = "Yankees"
+away = "Red Socks"
+
+team(home, away)
