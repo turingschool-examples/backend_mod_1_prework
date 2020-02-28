5cd79839fd8a0b30a9a97eb0b0ee9f8d1740480b
Sage Lee <sagemcklee@gmail.com>
2020-02-28 14:16:38 -0700

    Add Fizz and Monkey Projects

---
+++ day_7/10_little_monkeys.rb
@@ -0,0 +1,18 @@
+
+number =["Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"]
+
+number.each do |n|
+  puts "#{n} little monkeys jumping on the bed,
+One fell off and bumped his head,
+Mama called the doctor and the doctor said,
+'No more monkeys jumping on the bed!'"
+puts "      "
+
+end
+
+part_two = "One little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said,
+'Get those monkeys right to bed!'"
+
+puts part_two
