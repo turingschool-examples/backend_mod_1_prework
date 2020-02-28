969b009c31b72ec7a5381c4c150506e6c344ef3b
Sage Lee <sagemcklee@gmail.com>
2020-02-27 23:22:01 -0700

    Add Day 6 Work

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -3,3 +3,25 @@
 # terminal.
 
 #YOUR CODE HERE
+class Person
+attr_accessor :name, :hair_color
+
+def initialize(name, hair_color)
+  self.hair_color = hair_color
+  self.name = name
+
+end
+
+def change_color(color)
+  @hair_color = color
+end
+
+def speak
+  puts "Hello my name is #{self.name},I have #{self.hair_color} hair."
+end
+end
+
+sally = Person.new("Sally", "blonde")
+puts sally.speak
+  sally.change_color('brunette')
+  puts sally.speak
