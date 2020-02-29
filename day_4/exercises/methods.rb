b4081ad9662fbcf31d457b7edda3c6ca6bbb6049
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-02-28 22:27:25 -0700

    Add Day 4 Work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -4,22 +4,42 @@
 
 # example: Write a method below that, when called will print your name
 def print_name
-  p "Severus Snape"
+  puts "Severus Snape"
 end
 
 print_name
 
 # Write a method that takes an argument of your name and prints your name
 def print_name(name)
-  # YOUR CODE HERE
+  puts name
 end
 
 print_name("Albus Dumbledore")
 
 # Write a method that takes in 2 numbers and prints their sum, then call that
 # method.
-# YOUR CODE HERE
+def sum (x,y)
+  #optionsal eturn x + y
+  new_sum = x + y
+  puts "The sum of the two numbers is #{new_sum}"
+end
+puts "Please enter first number: "
+x = gets.chomp.to_i
+puts "Please enter second number: "
+y = gets.chomp.to_i
+sum(x,y)
+# optional answer = sum(x,y)
+#optional puts "The sum of the two numbers is #{answer}"
+# optional puts "The sum of the two numbers is " + sum(x,y).to_s
 
 # Write a method that takes in two strings and prints a concatenation
 # of those two strings, for example the arguments could be (man, woman) and
 # the end result might be "When Harry Met Sally".  Then, call that method.
+
+def add(man, woman)
+  str = "When "
+  str2 = "met "
+  return str.concat(man) + " " + str2.concat(woman)
+end
+
+puts add("Harry","Sally")
--- day_4/questions.md
+++ day_4/questions.md
@@ -1,11 +1,17 @@
 ## Day 4 Questions
 
 1. In your own words, what is the purpose of a method?
-
+* the purpose of a method is to take some input and manipulate it then return
+a value or store a value.
 1. In the space below, create a method named `hello` that will print `"Sam I am"`.
-
+`def hello
+  puts "Sam I am"
+end
+hello`
 1. Create a method name `hello_someone` that takes an argument of `name` and prints `"#{name} I am"`.
-
+`def hello_someone(name)
+  puts "#{name} I am"`
 1. How would you call or execute the method that you created above?
-
+* You would run `hello_someone`
 1. What questions do you still have about methods in Ruby?
+None 
