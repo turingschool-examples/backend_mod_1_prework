9453332f1f9865fa81e664b16d4a762b235590eb
Arique Aguilar <arique.aguilar@gmail.com>
2020-02-29 16:28:00 -0700

    Exercises for Day 6 complete all code functioning

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -3,3 +3,44 @@
 # terminal.
 
 #YOUR CODE HERE
+
+class Person
+  attr_reader :first_name, :feelings
+
+  def introduction(first_name)
+    "Hi, my name is #{first_name}."
+  end
+
+  def express(feelings)
+    "Today, I feel #{feelings}."
+  end
+
+end
+
+mom = Person.new
+p mom.introduction("Arique")
+p mom.express("scared")
+
+
+
+########THIS ROUND FAILED, TRY AGAIN!
+#class Person
+#   attr_reader :smiles, :tears
+#   def initialize(smiles, tears)
+#     @happy = smiles
+#     @sad = tears
+#   end
+#   def happy(smiles)
+#     puts "I am all smiles! #{smiles}"
+#   end
+#
+#   def sad(tears)
+#     puts "I am all tears. #{tears}"
+#   end
+#
+# end
+#
+# mother = Person.new
+# p Person.happy("Give me a high five!")
+# p Person.sad("Pass me the tissue.")
+###############
