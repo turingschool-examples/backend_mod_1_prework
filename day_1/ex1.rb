270e21f93a8bae96d186d8bccd9b78da31e96020
Austin Aspaas <araspaas@live.com>
2020-02-29 19:18:12 -0700

    edited ex1.rb

---
+++ day_1/ex1.rb
@@ -0,0 +1,12 @@
+puts #{}"Hello World!"
+puts #{}"Hello again."
+puts #{}"I like typing this."
+puts #{}"This is fun."
+puts "Yay! printing."
+puts #{}"I'd much rather you 'not'."
+puts #'I "said"do not touch this.'
+puts #{}"this is the other line"
+
+Put # a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+
+# by putting a # it sees the line as a comment. so ruby ignores this when running the code.
