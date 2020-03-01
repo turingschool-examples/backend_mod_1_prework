b0c23c50b8b358facf2892f734068602a7713eaf
Antonio Jackson <antoniojackson@gmail.com>
2020-03-01 15:50:47 -0700

    Add Day 6 Work

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -6,10 +6,23 @@
 
 class Burrito
   attr_reader :protein, :base, :toppings
+
   def initialize(protein, base, toppings)
     @protein  = protein
     @base     = base
     @toppings = toppings
+  end
+
+  def add_topping(topping)
+    @toppings = toppings << topping
+  end
+
+  def remove_topping(topping)
+    toppings.delete(topping)
+  end
+
+  def change_protein(protein)
+    @protein = protein
   end
 end
 
@@ -17,3 +30,8 @@ dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+p dinner.add_topping("lettuce")
+dinner.remove_topping("salsa")
+p dinner.toppings
+dinner.change_protein("Chicken")
+p dinner.protein
--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -2,4 +2,29 @@
 # person methods below the class so that they print their result to the
 # terminal.
 
-#YOUR CODE HERE
+class Person
+  attr_reader :name, :age, :awake
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @awake = true
+  end
+
+  def go_to_sleep
+    @awake = false
+  end
+
+  def wake_up
+    @awake = true
+  end
+
+  def celebrate_birthday
+    @age += 1
+  end
+end
+
+p tony = Person.new("Tony", 31)
+p tony.go_to_sleep
+p tony.wake_up
+p tony.celebrate_birthday
