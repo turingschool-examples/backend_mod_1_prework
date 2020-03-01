f7539d00879b9c98d5f63e6af1f8d7f5141cd0d2
Austin Aspaas <araspaas@live.com>
2020-02-29 20:43:45 -0700

    Added code to ex3.rb

---
+++ day_1/ex3.rb
@@ -0,0 +1,28 @@
+# Types out "I will now count my chickens:"
+puts "I will now count my chickens:"
+
+# Types out Hens then does the math between the {} and the types that out in the same line.
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# Types out Roosters then does the math between the {} and the types that out in the same line.
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+# Types out "Now i will count the eggs:"
+puts "Now i will count the eggs:"
+# Does the math
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+# Types out "Is it true that 3 + 2 < 5 - 7?"
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+# will give be a boolean return value.
+puts 3.0 + 2.0 < 5.0 - 7.0
+# Types "what is 3 + 2 then does the math between the {} in gives you the return value. same for the one below with its numbers."
+puts "what is 3.0 + 2.0? #{3.0 + 2.0}"
+puts "what is 5.0 - 7.0? #{5.0 - 7.0}"
+# Types out "oh, that's why it's false."
+puts "oh, that's why it's false."
+# Types out "how about some more"
+puts "how about some more"
+# Types out "Is it greater?" then gives a boolean return value
+puts "Is it greater? #{5.0 > -2.0}"
+#Types "Is it greater or equal?" then gives a boolean return value
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# Types out "Is it less or equal?" then give a boolean return value
+puts "Is it less or equal? #{5.0 <= -2.0}"
