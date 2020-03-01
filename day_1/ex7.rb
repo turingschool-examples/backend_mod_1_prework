ec8972bed933b1b937b21a127681e8d2096fd162
Grant Dempsey <grant.dempsey.1@Grants-MacBook-Pro.local>
2020-02-29 23:15:29 -0700

    Day1 Edit

---
+++ day_1/ex7.rb
@@ -0,0 +1,32 @@
+print "How old are you? "
+
+age = gets.chomp
+
+print "how tall are you? "
+
+height = gets.chomp
+
+print "How much to you weigh? "
+
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+puts "Study Drills"
+puts "1. gets command takes the user input and returns it as a string value"
+puts "chomp command removes the line break that happens with gets"
+
+print "How much money is in your wallet? "
+
+wallet = gets.chomp
+
+print "How much money is in your bank account? "
+
+bank_account = gets.chomp
+
+print "How much money in dollars did you invest in Bitcoin? "
+
+bitcoin = gets.chomp
+
+puts "You have this amount in dollars from your wallet #{wallet}, bank account #{bank_account},
+and bitcoin #{bitcoin}."
