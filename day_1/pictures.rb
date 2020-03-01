b8aa2c1ac3efcd3dc3901e7cbfb4e74c2e52e956
Grant Dempsey <grant.dempsey.1@Grants-MacBook-Pro.local>
2020-02-29 17:30:03 -0700

    Day_1_Update

---
+++ day_1/pictures.rb
@@ -0,0 +1,10 @@
+puts "How many pictures are in my offic?"
+
+puts "Wall 1 has #{7 + 4} pictures on it"
+puts "Wall 2 has #{2 + 1} pictures on it"
+puts "Wall 3 has #{2 * 2} pictures on it"
+puts "Wall 4 has #{1800/237 * 0} pictures on it"
+
+puts "How many pictures are in my office?"
+
+puts 7+ 4 + 2 + 1 + 2 * 2 + (1800/237 * 0)
