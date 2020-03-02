975f37f69adc4fdc3f6c6451bf948bdcc72aa640
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-03-02 15:48:27 -0700

    Add 10_little_monkeys.rb

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -15,23 +15,20 @@ numbers = {
     2 => "Two",
     1 => "One",
   }
+
 puts "Enter the amount of times you want the nursery rhyme to print (1-15): "
 amount_of_times = gets.to_i
-i=0
-if amount_of_times <= 0
-  puts "Can not print zero or less times"
-end
-for i in i..amount_of_times-1
-  if amount_of_times >= 2
-    puts "#{numbers[amount_of_times]} little monkeys jumping on the bed,
+
+amount_of_times.downto(1) do |n|
+  if n == 1
+    puts "One little monkey jumping on the bed,
+    He fell off an bumped his head,
+    Mama called the doctor and the doctor said,
+    Get those monkeys right to bed!"
+  else
+    puts "#{numbers[n]} little monkeys jumping on the bed,
     One fell off and bumped his head,
     Mama called the doctor and the doctor said,
     No more monkeys jumping on the bed!"
-    amount_of_times -= 1
-  else
-    puts "One little monkey jumping on the bed,
-    He fell off and bumped his head,
-    Mama called the doctor and the doctor said,
-    Get those monkeys right to bed!"
   end
 end
