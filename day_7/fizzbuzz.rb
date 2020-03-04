71d4b106c7a9c33ce6da1ac8d7f5c9b9bf3c14ef
Austin Aspaas <araspaas@live.com>
2020-03-03 18:33:49 -0700

    Add day 7

---
+++ day_7/fizzbuzz.rb
@@ -0,0 +1,15 @@
+fizz_array = []
+number_array = Array (1..100)
+
+number_array.each {|x|
+
+if (((x % 3) == 0.0) && ((x % 5) == 0.0))
+  puts "FizzBuzz"
+elsif ((x % 3) == 0.0)
+  puts "Fizz"
+elsif ((x % 5) == 0.0)
+  puts "Buzz"
+else
+  puts x
+end
+}
