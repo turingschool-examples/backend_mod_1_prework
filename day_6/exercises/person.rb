631352c79be2ee17d8244f16e3e640d4461e1986
jKwibe <kwibemerci12@gmail.com>
2020-03-26 20:25:31 -0600

    Add Day 6 Work

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -1,5 +1,58 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
 # YOUR CODE HERE
+
+class Person
+  attr_reader :name, :age, :height, :weight
+
+  def initialize(name,age, height, weight)
+    @name = name
+    @height = height #in meters
+    @weight = weight # in kilograms
+    @age = age
+  end
+
+  def work_out
+    @weight -=  3
+    puts "My new - weight is #{@weight}"
+  end
+
+  def eat_fatty_food
+    @weight += 5
+    p "my new + weight #{weight}"
+  end
+
+  def bmi_count
+    bmi = @weight.to_f / (@height.to_f ** 2)
+
+    p "Final weight weight:  #{@weight}kg"
+    p "Height:  #{@height}m"
+    if bmi >= 25.0
+      return "My bmi is #{(bmi*10).round / 10.0} are obese => Check on your diet and workout "
+    elsif bmi >=  18.5
+      return "My bmi is  #{(bmi*10).round / 10.0} => I am healthy"
+    else
+      return "My bmi   #{(bmi*10).round / 10.0}is less than average => Eat healthy fats"
+    end
+
+  end
+
+
+
+  def introduction
+    puts "Hi! my name is #{@name.capitalize} nice to meet you"
+    puts "I am #{@age} years old"
+    puts bmi_count
+
+  end
+
+end
+
+person1 = Person.new("kwibe",26, 1.79, 80)
+
+person1.eat_fatty_food
+person1.eat_fatty_food
+person1.work_out
+person1.introduction
--- day_6/questions.md
+++ day_6/questions.md
@@ -1,13 +1,38 @@
 ## Day 6 Questions
 
 1. In your own words, what is a Class?
+  - A class will tell an object what it should know about itself (attributes) as well as what that object can do (methods).
 
 1. What is an attribute of a Class?
+  - An attribute is a characteristic shared by all the instances of a class.
 
 1. What is behavior of a Class?
+  - A behavior for a class is an action that each instance of the class can perform. These behaviors known as methods.
 
 1. In the space below, create a Dog class with at least 2 attributes and 2 behaviors:
+  ```
+    class Dog
+      attr_reader :breed, :age, :color, :is_sick
+
+      def initialize(b, a, c)
+        @breed = b
+        @age = a
+        @color = c
+        @is_sick = false
+      end
+
+      def call_vet
+        @is_sick = true
+      end
+
+      def bark(sound)
+        p sound
+      end
+    end
+  ```
 
 1. How do you create an instance of a class?
+  - To instantiate an object of a class, we use the method `new` to which we pass as arguments, the values that will be used to initialize the instance variables.
 
 1. What questions do you still have about classes in Ruby?
+  - What is the difference between `@instance_variable = value` and `self.instance_variable` ? Are these two syntaxes equivalent?
