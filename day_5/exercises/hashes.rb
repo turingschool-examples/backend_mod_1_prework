e691ce54a344dfea25f2edee251bc935745cd05c
Dani Coleman <danicoleman00@gmail.com>
2020-03-28 15:22:59 -0600

    Add Day 5 Work

--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby day_5/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,21 +8,38 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {elephants: 12, tigers: 15, giraffes: 10, zebras: 20, lions: 14}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+zoo = {elephants: 12, tigers: 15, giraffes: 10, zebras: 20, lions: 14}
+puts zoo.keys
 
-# Write code that prints all of the 'values' of the zoo variable 
+
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+zoo = {elephants: 12, tigers: 15, giraffes: 10, zebras: 20, lions: 14}
+puts zoo.values
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
-# YOUR CODE HERE
+zoo = {elephants: 12, tigers: 15, giraffes: 10, zebras: 20, lions: 14}
+puts zoo.values[0]
 
-# Write code that adds an animal to the zoo hash. 
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+zoo = {elephants: 12, tigers: 15, giraffes: 10, zebras: 20, lions: 14}
+zoo[:monkeys] = 7
+puts zoo
+
+# Extra practice
+# Write code that prints the value of key of the second animal of the zoo variable
+zoo = {elephants: 12, tigers: 15, giraffes: 10, zebras: 20, lions: 14}
+puts zoo.values[1]
+
+# Write code that changes the value of the giraffes and adds an animal in the zoo varialbe
+zoo = {elephants: 12, tigers: 15, giraffes: 10, zebras: 20, lions: 14}
+zoo[:giraffes] = 18
+zoo[:monkeys] = 7
+puts zoo
---
+++ day_5/exercises/questions_check.rb
@@ -0,0 +1,11 @@
+pet_store = {"dog_food" => 200, "cat_food" => 175, "fish" => 350, "rodents" => 75}
+puts pet_store
+
+states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}
+puts states.values[1]
+
+states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}
+puts states.keys
+
+states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}
+puts states.values
