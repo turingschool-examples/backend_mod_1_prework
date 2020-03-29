65dd96d12517c73ad3b7e63ca4b370f75cc43bd6
Chandler Hulstrom <chulstro11@gmail.com>
2020-03-29 10:07:52 -0600

    Completed third set of exercises

---
+++ day_1/exercises/ex3.rb
@@ -0,0 +1,33 @@
+# Explanation for Hen and Rooster count
+puts "I will now count my chickens"
+
+# Calculations for chicken count
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# Declaration of counting intent
+puts "Now I will count my eggs:"
+
+# Random complicated arithmetic
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0- 1.0 / 4.0 + 6.0
+
+# Querie to be investigated in the near future
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# The aforementioned querie
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# More questions of arithmetic
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+# Observation of data
+puts "Oh, that's why it's false."
+
+# Continuing with less than greater than practice
+puts "How about some more."
+
+# A few boolean questions
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
