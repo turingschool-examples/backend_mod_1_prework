1b93586a9d1d262e9459c648569d3802a7575d9e
Chandler Hulstrom <chulstro11@gmail.com>
2020-03-29 10:10:10 -0600

    Completed sixth set of exercises

---
+++ day_1/exercises/ex6.rb
@@ -0,0 +1,28 @@
+# This is our vartiable list
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+# Statement that there are 10 types of people
+puts x
+puts y
+
+# gotta reaffirm the joke
+puts "I said: #{x}."
+puts "I also said:'#{y}'."
+
+# Now searching for validation
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# Oh darn, not good validation
+puts joke_evaluation
+
+# Couple strings regarding a sentence
+w = "This is the left side of..."
+e = "a string with a right side."
+
+# math with letters and strings and convolution
+puts w + e # This is a longer string because it's just adding the string letters together
