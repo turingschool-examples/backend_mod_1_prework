3a4e31f2431fff00fa5f335670a5c99a3870ac8d
Kathy Bui <kathy.bui1992@gmail.com>
2020-03-31 13:37:26 -0600

    Type ex 3 example. Add comments on what each lines does

---
+++ day_1/exercises/ex3.rb
@@ -0,0 +1,30 @@
+#prints string
+puts "I will now count my chickens"
+#prints "Hens" and the answer to the 25+30/6 which is 30
+puts "Hens #{25+30/6}"
+#prints "Roosters" and the answer to 100-25*3%4 which is 97
+puts "Roosters #{100-25*3%4}"
+#prints string
+puts "Now I will count the eggs:"
+#prints answer to arithmatic
+puts 3+2+1-5+4%2-1/4+6
+#prints string
+puts "Is it true that 3 + 2 < 5 - 7?"
+#prints boolean for 3+2<5-7 which is false
+puts 3 + 2 < 5 - 7
+#prints what is in the string and then the answer which is after the #
+puts "What is 3 + 2? #{3 + 2}"
+#prints what is in the string and then the answer which is after the #
+puts "What is 5 - 7? #{5 - 7}"
+#prints string
+puts "Oh, that's why it is false."
+#prints string
+puts "How about some more."
+#prints string "It is greater?" followed by whether the equation was true or false, in this case true
+puts "It is greater? #{5> -2}"
+#prints string "It is greater or equal?" followed by whether the equation was true or false, in this case true.
+puts "Is it greater or equal? #{5> -2}"
+#prints string "It is less or equal?" followed by whether the equation was true or false, in this case alse.
+puts "Is it less or equal? #{5<= -2}"
+
+#the #{} runs the math entered and just returns the answer, so when you print it you will only see the answer. 
