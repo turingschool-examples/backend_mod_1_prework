500004fea4897a260e8222c92b67a89f6bff0423
Arique Aguilar <arique.aguilar@gmail.com>
2020-03-30 23:22:41 -0500

    Day 3 exercises, complete and ready for edit

---
+++ day_3/exercises/new_game.rb
@@ -0,0 +1,58 @@
+puts "You're in a dark room.  Pick door #1 or door #2."
+print "> "
+
+door = $stdin.gets.chomp
+
+if door == "1"
+puts "You'ved entered the dark layer of a witch."
+puts "1. Test drive the wand."
+puts "2. Swallow a potion."
+puts "3. Pet the black cat."
+
+puts "> "
+  witch = $stdin.gets.chomp
+
+  if witch == "1"
+    puts "You spread pixie dust all around you and die of anaphylactic shock.  Good job!"
+
+  elsif witch == "2"
+    puts "You start to glow and float and explode into a new star.  Good job!"
+
+  elsif witch == "3"
+    puts "Purrrrrrfect!  The cat walks away to reveal two boxes.  Box #1 or Box #2?"
+
+    puts "> "
+      cat_box = $stdin.gets.chomp
+
+      if cat_box == "1"
+        puts "You have an unclaimed million dollar lotto ticket!  You run to cash it in but trip over the cat and die.  Good job!"
+
+      elsif cat_box == "2"
+        puts "Poisous snakes jump out and bite your lip, you fall paralyzed.  Good job!"
+
+      else
+      puts "Pick one or two!  It's that easy! - screams the creator of this game.  Your computer explodes.  Good job!"
+      end
+  else
+      puts "Your computer will explode in 3...2...1...BOOM! Good job!"
+    end
+
+
+elsif door == "2"
+  puts "You've entered the basement of a mortuary.  Do you open storage door #1 or #2?"
+
+  puts "> "
+    mortuary = $stdin.gets.chomp
+
+    if mortuary == "1"
+      puts "It's a surprise party.  But it scares you so much you die.  Good job!"
+    elsif mortuary == "2"
+      puts "You won the game and die of excitment. Good job!"
+    else
+      puts "Play this game right!  Boom!  Goes your computer!"
+    end
+
+else
+  puts "Run this game again and die respectfully!  Geeze!"
+
+end
--- day_3/questions.md
+++ day_3/questions.md
@@ -1,13 +1,43 @@
 ## Day 3 Questions
 
 1. What is a conditional statement? Give three examples.
+A conditional statement values true or false and is typically used in if statement methods.
+
+Ex_1: num == 5
+
+Ex_2: num && num1
+
+Ex_3: num || num1
 
 1. Why might you want to use an if-statement?
 
+> If I want the computer to make a series of choices automatically without me getting in the way.
+
 1. What is the Ruby syntax for an if statement?
+
+```
+if variable_name == "set terms"
+puts "Write your result"
+end
+```
 
 1. How do you add multiple conditions to an if statement?
 
+You use elsif for all of your conditions, and you use else for your last condition.
+
+
 1. Provide an example of the Ruby syntax for an if/elsif/else statement:
 
+```
+if variable_name == "set terms here"
+  puts "Write your result"
+elsif variable_name == "set different terms"
+  puts "Write different result"
+else variable_name (leave blank)
+  puts "Write different result."
+end
+```
+
 1. Other than an if-statement, can you think of any other ways we might want to use a conditional statement?
+
+> If I want to know if an element exists within an array, I can use conditional statements to find out.
