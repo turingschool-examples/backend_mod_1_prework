551f2c3e4c57796f667bec63bb57161dba8ba928
jKwibe <kwibemerci12@gmail.com>
2020-03-30 22:04:58 -0600

    reduce code

--- day_7/checker_board.rb
+++ day_7/checker_board.rb
@@ -4,46 +4,30 @@ class CheckerBoard
 
   def checker_board(board_size)
     puts "Checker Board"
-      columns = []
+      x_columns = []
+      space_columns =[]
       i = 0
+      j = 0
   # printing the line with "X" first
       until i == board_size
         i += 1
         if i.odd? #The same as if i % 2 != 0
-          columns.push("X") # alternative of push => <<
+          x_columns << "X"
+          space_columns << " " # alternative of push => <<
         else
-          columns.push( " ")
+          x_columns << " "
+          space_columns << "X"
         end
       end
-      x_first_line = columns.join
+      x_first_line = x_columns.join
+      space_first_line = space_columns.join
 
-    #printing the line with " " first
-    i = 0
-    columns = []
-      until i == board_size
-        i += 1
-        if i.odd?
-          columns << " " #used << instead of push
-        else
-           columns << "X"
-        end
-      end
-       space_first_line = columns.join
-
-       #Reset the i variable to zero
-       #Prints the checkers board
-
-       i = 0
-
-       until i == board_size
-           i += 1
-          if i.odd?
-            puts x_first_line
-          else
-            puts space_first_line
-          end
+       until j == board_size
+           j += 1
+      puts (j.odd?)? x_first_line :  space_first_line
 
        end
+
   end
 
 end
