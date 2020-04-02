c21a27cab0d1e4499fd5e172dfa7e3de1515eaf8
Dani Coleman <danicoleman00@gmail.com>
2020-04-01 18:00:34 -0600

    Add FizzBuzz Ruby File

---
+++ day_7/fizzbuzz.rb
@@ -0,0 +1,31 @@
+# Instance method with function name = fizzbuzz and argument number
+def fizzbuzz(n)
+    if (n % 3 == 0)
+      puts "Fizz"
+    end
+    if (n % 5 == 0)
+      puts "Buzz"
+    end
+    if !((n % 3 == 0) || (n % 5 == 0))
+      puts n
+    end
+  end
+# iteration of numbers 1-100
+  (1..100).each {|n| fizzbuzz n}
+
+# Bonus (Just change the iteration spread in the each method)
+  def fizzbuzz(n)
+      if (n % 3 == 0)
+        puts "Fizz"
+      end
+      if (n % 5 == 0)
+        puts "Buzz"
+      end
+      if !((n % 3 == 0) || (n % 5 == 0))
+        puts n
+      end
+    end
+
+    (1..500).each {|n| fizzbuzz n}
+
+# used this source for help https://gist.github.com/wilkinsbrian/e6fc05fbd6ab96e87cd0    
