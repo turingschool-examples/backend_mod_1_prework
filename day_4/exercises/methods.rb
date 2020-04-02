132b77035d5158e2af010927e77f7ed1e8c46890
Kathy Bui <kathy.bui1992@gmail.com>
2020-04-01 20:48:32 -0600

    Add notes

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -12,16 +12,26 @@ print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  p "#{name}"
 end
 
 print_name("Albus Dumbledore")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method:
-# YOUR CODE HERE
+def add_numbers(number1, number2)
+  p number1 + number2
+end
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
+add_numbers(1,3)
+
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
 # Then call your method:
+
+def concatenation_strings(str1, str2)
+  p "#{str1} and #{str2}"
+end
+
+concatenation_strings("fall down seven times", "stand up eight")
--- day_4/exercises/mutates.rb
+++ day_4/exercises/mutates.rb
@@ -7,3 +7,18 @@ end
 p "Before no_mutate method: #{a}"
 no_mutate(a)
 p "After no_mutate method: #{a}"
+
+def add_three(number)
+  number + 3
+end
+
+returned_value = add_three(4)
+puts returned_value
+
+def add_three(number)
+  return number + 3
+  number + 4
+end
+
+returned_value = add_three(4)
+puts returned_value
