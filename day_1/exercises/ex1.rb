507146ae4183f1e999c8696dfb5c9dbab64eab89
jKwibe <kwibemerci12@gmail.com>
2020-04-01 22:41:30 -0600

    Add day 1

---
+++ day_1/exercises/ex1.rb
@@ -0,0 +1,13 @@
+# #puts "Hello World!"
+# #puts "Hello Again"
+ puts "I like typing this."
+# puts "This is fun."
+ puts "Yay! Printing."
+# puts "I'd much rather you 'not'."
+# puts 'I "said" do not touch this.'
+
+
+puts "print another line"
+
+
+#  Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
