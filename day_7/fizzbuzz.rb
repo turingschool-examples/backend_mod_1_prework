d3acbfc427d4304c3bb71390be1af03d5da4dfd9
Kwibe Merci <kwibemerci12@gmail.com>
2020-04-04 14:02:21 -0600

    refactor fizzbuzz

--- day_7/fizzbuzz.rb
+++ day_7/fizzbuzz.rb
@@ -19,4 +19,18 @@ def fizz_buzz(min,max)
  p new_arr.join(", ")
 end
 
-fizz_buzz(1, 100)
+# fizz_buzz(1, 100)
+
+# Second solution for the if else statement
+
+def fizz_buzz_2 (min = 1, max =10)
+  range = (min..max).to_a
+
+  range.inject(""){|memo, num|
+    (num % 3 ==0 && num % 5 == 0)? memo << "FizzBuzz " :
+    (num % 5 == 0) ? memo << "Buzz " :
+    (num % 3 == 0) ? memo << "Fizz " : memo << num.to_s + " "
+  }
+
+end
+p fizz_buzz_2
