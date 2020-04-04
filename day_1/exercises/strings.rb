e355624102c87c428a5ed891fb0e5d152f7e5561
Jonathan Wilson <53539820+Jonathan-M-Wilson@users.noreply.github.com>
2020-04-04 16:06:25 -0600

    Update strings.rb

--- day_1/exercises/strings.rb
+++ day_1/exercises/strings.rb
@@ -7,7 +7,7 @@
 p "Alan Turing"
 
 # Write code that prints `Welcome to Turing!` to the terminal:
-p 'Welcome to Turing!'
+p 
 
 # Write code that prints `99 bottles of pop on the wall...` to the terminal:
-p `99 bottles of pop on the wall...`
+p 
