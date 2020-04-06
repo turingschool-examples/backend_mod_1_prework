eb91e5a08afe0d4bcf6469c057a94847e0ac4e15
Dani Coleman <danicoleman00@gmail.com>
2020-04-06 09:01:33 -0600

    Add Day 1 Work

--- day_1/exercises/ex4.rb
+++ day_1/exercises/ex4.rb
@@ -1,31 +1,31 @@
-# cars = 100
+cars = 100
 
-# space_in_a_car = 4.0
+space_in_a_car = 4.0
 
-# drivers = 30
+drivers = 30
 
-# passengers = 90
+passengers = 90
 
-# cars_not_driven = cars - drivers
+cars_not_driven = cars - drivers
 
-# cars_driven = drivers
+cars_driven = drivers
 
-# carpool_capacity = cars_driven * space_in_a_car
+carpool_capacity = cars_driven * space_in_a_car
 
-# average_passangers_per_car = passengers / cars_driven
+average_passangers_per_car = passengers / cars_driven
 
 # number of cars available
-puts "There are #{100} cars available."
+puts "There are #{cars} cars available."
 # number of drivers available to drive
-puts "There are only #{30} drivers available."
+puts "There are only #{drivers} drivers available."
 # number of cars not in use
-puts "There will be #{100 - 30} empty cars today."
+puts "There will be #{cars_not_driven} empty cars today."
 # total number of people that can ride
-puts "We can trasnport #{30 * 4} people today."
+puts "We can trasnport #{carpool_capacity} people today."
 # total number of people that need a ride
-puts "We have #{90} to carpool today."
+puts "We have #{passengers} to carpool today."
 # maximun number of people per car
-puts "We need to put about #{90 / 30} in each car."
+puts "We need to put about #{average_passangers_per_car} in each car."
 
 # Study Drills
 
