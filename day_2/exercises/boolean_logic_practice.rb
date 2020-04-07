65b80f62a0c5f27a8825037221b24c62e0699094
Sienna Kopf <princess.kopf@gmail.com>
2020-04-07 12:52:18 -0600

    Add exercise files

---
+++ day_2/exercises/boolean_logic_practice.rb
@@ -0,0 +1,16 @@
+## 1. Eqaulity operators in ruby: !=, ==, <, >, <=, >=
+# <=> combined comparison. if a ==b retuirns 0, a > b returns 1, if b > a returns -1
+# === test for equality within clause of a case statemnt
+# .eql? true if same type of data & same value
+# equal? returms true if both have same obj id
+
+## 2.
+!= #not equal
+== #eqauls
+< #less than
+> #greater than
+<= #less than or equal to
+>= #greater than or equal to
+<=> # combined comparison
+.eql? #dot equal # QUESTION:
+equal? #equal?
