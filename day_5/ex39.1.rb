16aa8d8ff94f0ff702a072d84b53a45bc4001743
Michael Chickillo <truthvendor@gmail.com>
2020-04-10 16:36:54 -0600

    Add Day 5 Work

---
+++ day_5/ex39.1.rb
@@ -0,0 +1,32 @@
+  cities = {
+  'Montana' => 'Billings',
+  'Ohio' => 'Cleveland',
+  'Colorado' => 'Denver',
+  'Idaho' => 'Boise',
+  'Texas' => 'Austin',
+  'Georgia' => 'Atlanta',
+  '66' => 'Palm Beach',
+  '67' => 'Vero Beach',
+  '22' => 'Double Pepperoni Pizza',
+  'krtj' => 'Apricot Blonde',
+
+
+  }
+  puts '-' * 25
+  puts "Georgia has: #{cities['Georgia']}"
+  puts "Idaho has: #{cities['Idaho']}"
+  puts "Colorado has a city named #{cities['Colorado']}"
+  puts "Florida has a city named #{cities['66']} and a city named #{cities['67']}"
+  puts "Flatten has a city named #{cities['Idaho']}"
+  puts "I love to eat #{cities['22']} while drinking #{cities['krtj']}"
+
+fruit = {'oranges' => 3, 'apples' => 2, 'plums' => 17
+
+}
+fruit['gp3'] = 221
+fruit['pr3'] = 8
+  puts '-' * 22
+  puts "I want to eat #{fruit['apples']} apples."
+  puts "I want to eat #{fruit['gp3']} grapes."
+  puts "I want to eat #{fruit['pr3']}"
+  p fruit
--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby day_5/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,21 +8,22 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {'Zebras' => 7, 'Lions' => 3, 'Monkeys' => 4921} # WOW, that's a lot of monkeys!!
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+puts zoo.keys
 
-# Write code that prints all of the 'values' of the zoo variable 
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+puts zoo.values
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
-# YOUR CODE HERE
+puts "#{zoo ['Zebras']}"
 
-# Write code that adds an animal to the zoo hash. 
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+zoo['Ostrich'] = '5'
+p zoo
