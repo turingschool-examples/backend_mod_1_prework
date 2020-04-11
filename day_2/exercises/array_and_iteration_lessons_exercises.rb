0cb34f00e9fa76b4f24d47df02b700ff2c47ae62
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-04-10 22:52:37 -0600

    Complete additional Day 2 exercises

--- day_2/exercises/array_and_iteration_lessons_exercises.rb
+++ day_2/exercises/array_and_iteration_lessons_exercises.rb
@@ -10,14 +10,49 @@ end
 numbers = [1, 2, 3, 4]
 numbers.each do |n|
   p "#{n}" * 2
-  end
+end
 ### triples
 numbers.each do |n|
   p "#{n}" * 3
-  end
+end
 
 ## If you had the same array, how would you only print out the even numbers? What about the odd numbers?
-##### COMPLETE LATER
+### print only evens
+numbers.each do |n|
+  if n.even?
+    p n
+  end
+end
+### print only odds
+numbers.each do |n|
+  if n.odd?
+    p n
+  end
+end
+
+## How could you create a new array which contains each number multipled by 2?
+new_numbers = []
+numbers.each do |n|
+  new_numbers << n * 2
+end
+p new_numbers
+
+## Given an array of first and last names, e.g. [“Alice Smith”, “Bob Evans”, “Roy Rogers”], how would you print out the full names line by line?
+names = ["Alice Smith", "Bob Evans", "Roy Rogers"]
+names.each do |name|
+  p name
+end
+### How would you print out only the first name?
+##### names.each do |name|
+  ##### name.split
+
+### How would you print out only the last name?
+
+### How could you print out only the initials?
+
+### How can you print out the last name and how many characters are in it?
+
+### How can you create an integer which represents the total number of characters in all the names?
 
 # exercises from https://learnrubythehardway.org/book/ex28.html
 p true && true
