053053ffc5a404c0208003a477277e8479ec1c97
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-04-11 00:25:04 -0600

    Reorganize Day 2 exercise files

--- day_2/exercises/iteration_and_each.rb
+++ day_2/exercises/iteration_and_each.rb
@@ -1,4 +1,3 @@
-# exercises from https://backend.turing.io/module1/lessons/iteration_and_each
 ## example
 names = ["Megan", "Brian", "Sal"]
 names.each do |name|
@@ -53,47 +52,3 @@ end
 ### How can you print out the last name and how many characters are in it?
 
 ### How can you create an integer which represents the total number of characters in all the names?
-
-# exercises from https://learnrubythehardway.org/book/ex28.html
-p true && true
-## true
-p false && true
-## false
-p 1 == 1 && 2 == 1
-## false
-p "test" == "test"
-## true
-p 1 == 1 || 2 != 1
-## true
-p true && 1 == 1
-## true
-p false && 0 != 0
-## false
-p true || 1 == 1
-## true
-p "test" == "testing"
-## false
-p 1 != 0 && 2 == 1
-## false
-p "test" != "testing"
-## true
-p "test" == 1
-## false
-p !(true && false)
-## true
-p !(1 == 1 && 0 != 1)
-## false
-p !(10 == 1 || 1000 == 1000)
-## false
-p !(1 != 10 || 3 == 4)
-## false
-p !("testing" == "testing" && "Zed" == "Cool Guy")
-## true
-p 1 == 1 && (!("testing" == 1 || 1 == 0))
-## true
-p "chunky" == "bacon" && (!(3 == 4 || 3 == 3))
-## false
-p 3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))
-## false
-p 3 != 4 && !("testing" != "test" || "Ruby" == "Ruby")
-## false
