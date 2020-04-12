97d353d23fcc1b047c7ccd81780d8ba3cde57c57
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-04-11 19:08:51 -0600

    Add Day 6 Work

--- day_6/exercises/tests.rb
+++ day_6/exercises/tests.rb
@@ -1,19 +1,3 @@
-## Day 6 Questions
-
-1. In your own words, what is a Class?
-
-A class is a set of attributes stored in variables that are specific to an item or thing we are referencing.  Instead of creating variables for each separate item, we can instead create a class that like items can use.  The reading from launchschool.com puts it best: "Think of classes as molds and objects as the things you produce out of those molds."
-
-2. What is an attribute of a Class?
-
-Attributes of a class are ways to describing aspects within a class.  For instance an `Airplane` class might have attributes such as `propulsion_type, wingspan, fuel_capacity`.
-
-3. What is behavior of a Class?
-
-Behaviors of classes are their capabilities, these are defined by the methods they are given access to.  Methods give classes the functionality to utilize attributes within as well as manipulate date passed into the class.
-
-4. In the space below, create a Dog class with at least 2 attributes and 2 behaviors:
-```
 class Dog
   attr_reader :vaccinated, :groomed, :weight
   def initialize(vaccinated, groomed, weight)
@@ -78,13 +62,3 @@ p "--------------------------"
 p "Vaccinated? #{dog_2.vaccinated}"
 p "Groomed? #{dog_2.groomed}"
 p "Weight? #{dog_2.weight}"
-
-```
-
-5. How do you create an instance of a class?
-
-From the `Dog` class I wrote above, I created a new instance and gave it attribut4es like this: `dog_1 = Dog.new(true, false, 180)`
-
-6. What questions do you still have about classes in Ruby?
-
-I would like to see more real life examples of how classes are utilized in Ruby in production environments.
