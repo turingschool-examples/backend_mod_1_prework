5db6556efe6e3f228570d6e0671b55e1ae231334
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-04-13 17:48:46 -0600

    Add Day 6 Work

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -1,4 +1,4 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
@@ -11,9 +11,33 @@ class Burrito
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(topping)
+    @toppings << topping
+    puts "You have added #{topping} to your burrito toppings. Your burrito toppings are now: #{@toppings}."
+    # come back later to edit toppings interpolation to be printed as a string
+  end
+
+  def remove_topping(topping)
+    @toppings.delete(topping)
+    puts "You have removed #{topping} from your burrito toppings. Your burrito toppings are now: #{@toppings}."
+  end
+
+  def change_protein(protein)
+    @protein = protein
+    puts "You have switched your burrito protein to #{protein}."
+  end
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+dinner.add_topping("sour cream")
+p dinner.toppings
+p dinner.remove_topping("cheese")
+p dinner.toppings
+dinner.change_protein("chicken")
+p dinner.protein
+
+# why does it allow for protein to be changed if it was initialized as a getter variable
--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -1,5 +1,34 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
-# YOUR CODE HERE
+class Person
+  attr_reader :first_name, :last_name
+  attr_accessor :age, :city_of_residence
+
+  def initialize(first_name, last_name, age, city_of_residence)
+    @first_name = first_name
+    @last_name = last_name
+    @age = 0
+    @city_of_residence = "Los Angeles"
+  end
+
+  def born
+    puts "#{first_name} #{last_name} was born today in #{city_of_residence}."
+  end
+
+  def time_passes(number_of_years)
+    @age += number_of_years
+    puts "#{first_name} is now #{age} years old."
+  end
+
+  def move_cities(city_of_residence)
+    @city_of_residence = city_of_residence
+    puts "#{first_name} moved to #{city_of_residence}."
+  end
+end
+
+sally = Person.new("Sally", "Smith", 0, "Los Angeles")
+sally.born
+sally.time_passes(12)
+sally.move_cities("Denver")
