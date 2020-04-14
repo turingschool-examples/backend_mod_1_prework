9005d9ddbc61bfaac4063057027681667ce2b69e
Sienna Kopf <princess.kopf@gmail.com>
2020-04-14 14:14:18 -0600

    Add 10 little monkeys solution using to_words gem

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -1,9 +1,11 @@
-numbers = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten']
+# numbers = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten']
+require 'to_words'
 
-monkey_range = (9..1)
+monkey_range = (10..2)
 
-(monkey_range.first).downto(monkey_range.last).each do |num|
-  puts "#{numbers[num]} little monkeys jumping on the bed,"
+(monkey_range.first).downto(monkey_range.last).each do |number|
+  number = number.to_words.capitalize
+  puts "#{number} little monkeys jumping on the bed,"
   puts "One fell off and bumped his head,"
   puts "Mama called the doctor and the doctor said,"
   puts "No more monkeys jumping on the bed!"
@@ -12,6 +14,6 @@ end
 puts "One little monkey jumping on the bed,"
 puts "He fell off and bumped his head,"
 puts "Mama called the doctor and the doctor said,"
-puts "Get those monkeys right to bed
+puts "Get those monkeys right to bed!"
 
-## to make this problem work for any number of monkeys, the easiest thing to do is install a ruby gem such as the to_words or humanize gem. Then you would implement this in the loop to print each number as a string word. I was having trouble installing gems to my terminal so I took the analog path.
+## gem 'to_words' makes the program work for all concievable numbers of monkeys
