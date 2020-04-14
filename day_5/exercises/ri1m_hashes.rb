b872622ce541c6de9f821ddb179fce3f0f2580c8
“Taija” <taija.warbelow@google.com>
2020-04-13 20:43:37 -0700

    Add Day 5 Work

---
+++ day_5/exercises/ri1m_hashes.rb
@@ -0,0 +1,13 @@
+# hash = unordered collection of key-value pairs
+# => is a rocket
+
+# the first part is the key the bit after the rocket is the value
+# the key is the address of the value which is the data
+# keys must be unique
+produce = {"apples" => 3, "Oranges" => 1, "carrots" => 12}
+
+# add something to a Hash
+produce["grapes"} = 221]
+
+# simplified syntax may be used if you are not using strings as keys or values
+produce = {apples: 3, oranges: 1, carrots: 12}
