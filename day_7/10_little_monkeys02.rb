ba4acf7ff2349d7d81a27d9ac52ea8e0239aa627
Michael Chickillo <truthvendor@gmail.com>
2020-04-15 13:12:57 -0600

    Add day 7 work

---
+++ day_7/10_little_monkeys02.rb
@@ -0,0 +1,16 @@
+10.downto(2) do |x|
+  puts "#{x} little monkeys jumping on the bed,"
+  puts "One fell off and bumped his head,"
+  puts "Mama called the doctor and the doctor said,"
+  puts "'No more monkeys jumping on the bed!'"
+  puts " "
+
+end
+  puts "1 little monkey jumped on the bed,"
+  puts "He fell off and bumped his head,"
+  puts "Mama called the doctor and the doctor said,"
+  puts "Get those monkeys right to bed!"
+
+
+# Bonus  = To have 99 monkeys jump on the bed modify line 1 to:
+# 99.downto(2) do |x|
---
+++ day_7/high_level.md
@@ -0,0 +1,6 @@
+### Caeser Cipher
+* First you would need to use get.chomps to collect user input.
+* Then you would need to use ascii to convert the letters to numbers.
+* Then you would take the user input in number form now and add or subtract the
+* number of shifts that you want.
+* Lastly you would convert the numbers back into letters to get your cipher.
