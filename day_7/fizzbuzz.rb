15b773dc51cd8bf776c5ee4a42ae1a4636ed67b7
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-11 22:28:43 -0600

    Create limitations for user input

--- day_7/fizzbuzz.rb
+++ day_7/fizzbuzz.rb
@@ -16,8 +16,16 @@ def fizzbuzz(number)
   end
 end
 
-puts "Please enter an integer number for fizzbuzz to run!"
+puts "Please enter a positive integer number between 1 and  1000000 for fizzbuzz to run!"
 
-input_number = gets.chomp.to_i
+input_number = gets.chomp.delete(",").to_i
 
-puts fizzbuzz(input_number)
+if input_number > 1000000
+  puts "Please enter a number between 1 and 1000000"
+elsif input_number.negative?
+  puts "Please add a POSITIVE integer"
+elsif input_number.is_a?(Integer)
+  puts fizzbuzz(input_number)
+else
+  puts "Please enter a positive integer number if you want the fizzbuzz to run!"
+end
