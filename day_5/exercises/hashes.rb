7163cd6f9cbe8818b2d01cc9b25836d13aa6cdc5
Jose Lopez <joselopez11394@gmail.com>
2020-05-14 15:39:46 -0400

    Add Day 5 Work

--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby day_5/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,21 +8,25 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {tiger: 2, monkey: 10, snake: 20}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+zoo.each do |animals, amount|
+  p animals
+end
 
-# Write code that prints all of the 'values' of the zoo variable 
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+zoo.each do |animals, amount|
+  p amount
+end
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
-# YOUR CODE HERE
-
-# Write code that adds an animal to the zoo hash. 
+p zoo[:tiger]
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+zoo[:Zebra] = 10
+p zoo
--- day_5/questions.md
+++ day_5/questions.md
@@ -1,13 +1,21 @@
 ## Day 5 Questions
 
 1. What is a Hash, and how is it different from an Array?
-
+  - A hash is a place to store objects that has a value to it, while an array is just a list of objects
 1. In the space below, create a Hash stored to a variable named `pet_store`.  This hash should hold an inventory of items and the number of that item that you might find at a pet store.
-
+pet_store = {dog_food: 100, toys: 1000, fish: 50}
 1. Given the following `states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}`, how would you access the value `"Iowa"`?
-
+  - states["IA"]
 1. With the same hash above, how would we get all the keys?  How about all the values?
+  - states.each do |key, value|
 
+      puts key(How you get all the keys)
+
+      puts value(How you get all the values)
+
+    end
 1. What is another example of when we might use a hash?  In your example, why is a hash better than an array?
-
+  - student_grades - {"Jose" => "A", "Mike" => "B", "Bob" => "C"}
+    -In these examples we are storing students grades.This is better with a hash instead of an array because in an array it will be harder to ask for a student and return me there grades.
 1. What questions do you still have about hashes?
+  - No questions
