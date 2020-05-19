311ea1490fefc63318c443a60e7385a49a1e3cec
Kevin Cuadros <kev040798@gmail.com>
2020-05-19 15:39:19 -0400

    Add Day 7 Work

---
+++ day_7/10_little_monkeys.rb
@@ -0,0 +1,12 @@
+monkey = ["Ten", "Nine", "Eight", "Seven", "Six", "Five","Four", "Three", "Two", "One"]
+monkey.each do |ele|
+  1.times {p "#{ele} little monkey(s) jumping on the bed, One fell off and bumped his head, Mama called the doctor and the doctor said,  'No more monkeys jumping on the bed!'"}
+end
+
+# Bonus
+puts "Enter number of monkey's"
+num = gets.chomp.to_i
+r = num..1
+
+
+(r.first).downto(r.last).each {|ele| puts "#{ele} little monkey(s) jumping on the bed, One fell off and bumped his head, Mama called the doctor and the doctor said,  'No more monkeys jumping on the bed!'" }
