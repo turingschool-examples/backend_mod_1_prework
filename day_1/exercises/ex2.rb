c36b09c962bccdcc4c44cbab66b8740b1bcde141
Leah Riffell <leah.riffell@gmail.com>
2020-05-21 11:25:02 -0400

    Add day 1 ex 1-2

---
+++ day_1/exercises/ex2.rb
@@ -0,0 +1,9 @@
+# Commenting is a helpful thing
+# Anything after an #octothorpe (aka hashtag) is ignored by ruby
+
+puts "I could have code like this." #and this will be ignored
+
+# A comment can also be used to "disable" or comment out a piece of code:
+# puts "This won't run."
+
+puts "This will run."
