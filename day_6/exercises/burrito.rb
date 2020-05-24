033cd702ffbc9c0e72bcfa5e9a776eb3a325c770
Logan Riffell <lkriffell@gmail.com>
2020-05-24 17:28:52 -0400

    Add Day 6 Work

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -1,19 +1,40 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
 # 3. change_protein
 
 class Burrito
-  attr_reader :protein, :base, :toppings
+  attr_accessor :protein, :base, :toppings
   def initialize(protein, base, toppings)
     @protein  = protein
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(t)
+    self.toppings = toppings << t
+  end
+
+  def remove_topping(t)
+    toppings.delete(t)
+  end
+
+  def change_protein(p)
+    self.protein = p
+  end
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
+p dinner.protein
+p dinner.base
+p dinner.toppings
+puts '-' * 10
+
+dinner.add_topping("lettuce")
+dinner.remove_topping("salsa")
+dinner.change_protein("steak")
+
 p dinner.protein
 p dinner.base
 p dinner.toppings
