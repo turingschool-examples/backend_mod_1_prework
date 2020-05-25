439ec3bed78a5d2df17de19a36b23300586aab71
Eduardo Parra <helloeduardo@icloud.com>
2020-05-25 15:55:46 -0600

    Add day 1

---
+++ day_1/exercises/ex1.rb
@@ -0,0 +1,11 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "this is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+# Study Drills
+puts "This is another line."
+# puts "This line will not print because it's a comment."
