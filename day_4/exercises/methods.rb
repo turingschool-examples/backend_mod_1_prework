f2b157c8a0a8eed517cd3f6bb63da100bd667f43
Travis McKinstry <mckinstrytg@gmail.com>
2020-05-26 09:22:46 -0600

    Add Day 4 Work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -12,16 +12,27 @@ print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  puts "#{name}"
 end
 
 print_name("Albus Dumbledore")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method:
-# YOUR CODE HERE
+def practice_methods(a, b)
+  return a + b
+end
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
+puts practice_methods(2, 5)
+
+
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
 # Then call your method:
+
+def people(name1, name2)
+  return "#{name1} never really like #{name2} because #{name2} insisted they were perfect."
+end
+
+people("George", "Jim")
