e646c846133029a29b8b0374f18999f5d18bf727
Roberto Basulto <roberto.basulto085@gmail.com>
2020-05-27 22:56:39 -0400

    Day 3 Complete

---
+++ day_3/exercises/ex4.rb
@@ -0,0 +1,16 @@
+def water_status(minutes)
+  if minutes < 7
+    puts "The water is not boiling yet."
+  elsif minutes == 7
+    puts "It's just barely boiling"
+  elsif minutes == 8
+    puts "It's boiling!"
+  else
+    puts "Hot! Hot! Hot!"
+  end
+end
+
+puts "How long has the water been heating up?"
+time = gets.chomp
+
+water_status(time.to_i)
