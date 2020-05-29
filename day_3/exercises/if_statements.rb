391ff9c2e17917371e540ebcdade53c0004d261b
fieldstyler <tdfields93@gmail.com>
2020-05-28 21:01:05 -0600

    Add day 3 work

--- day_3/exercises/if_statements.rb
+++ day_3/exercises/if_statements.rb
@@ -3,14 +3,14 @@
 # file by entering the following command in your terminal:
 # `ruby day_3/exercises/if_statements.rb`
 
-# Example: Using the weather variable below, write code that decides 
+# Example: Using the weather variable below, write code that decides
 # what you should take with you based on the following conditions:
   # if it is sunny, print "sunscreen"
   # if it is rainy, print "umbrella"
   # if it is snowy, print "coat"
   # if it is icy, print "yak traks"
 
-  weather = 'snowy'
+  weather = "sunny"
 
   if weather == 'sunny'
     p "sunscreen"
@@ -26,7 +26,24 @@
 
 # Experiment with manipulating the value held in variable 'weather'
 # to print something other than 'coat'
+# Answer to this is above. I also experimented below with user input.
+puts "Are the current weather conditions: sunny, rainy, snowy, icy, or other?"
 
+print "> "
+
+  weather = $stdin.gets.chomp
+puts " "
+  if weather == 'sunny'
+    puts "I should grab some sunscreen"
+  elsif weather == 'rainy'
+    puts "I need an umbrella"
+  elsif weather == 'snowy'
+    puts "I should get my coat"
+  elsif weather == 'icy'
+    puts "yak traks"
+  else
+    puts "I think I'm good to go!"
+  end
 
 ##################
 # Using the num_quarters variable defined below, determine
@@ -35,21 +52,44 @@
 
 # Right now, the program will print
 # out both "I have enough money for a gumball" and
-# "I don't have enough money for a gumball". Write a 
+# "I don't have enough money for a gumball". Write a
 # conditional statement that prints only one or the other.
 
 # Experiment with manipulating the value held within num_quarters
 # to make sure both conditions can be achieved.
-
+puts " "
 num_quarters = 0
 
-puts "I have enough money for a gumball"
-puts "I don't have enough money for a gumball"
+ if num_quarters >= 2
+   puts "I have enough money for a gumball"
+ else
+  puts "I don't have enough money for a gumball"
+end
+
+puts " "
+num_quarters1 = 3
+
+ if num_quarters1 >= 2
+   puts "I have enough money for a gumball"
+ else
+  puts "I don't have enough money for a gumball"
+end
+
+puts " "
+puts "How many quarters do you have?"
+print "> "
+quarters = $stdin.gets.chomp
+
+ if quarters >= "2"
+  puts "You have enough money for a gumball"
+ else
+  puts "You don't have enough money for a gumball"
+end
 
 
 #####################
 # Using the variables defined below, write code that will tell you
-# if you have the ingredients to make a pizza. A pizza requires 
+# if you have the ingredients to make a pizza. A pizza requires
 # at least two cups of flour and sauce.
 
 # You should be able to change the variables to achieve the following outputs:
@@ -60,6 +100,63 @@ puts "I don't have enough money for a gumball"
 
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
-
+#Example 1
+puts " "
 cups_of_flour = 1
 has_sauce = true
+
+if cups_of_flour >= 2 && has_sauce == true
+  puts "I can make pizza"
+else
+  puts "I cannot make pizza"
+end
+
+#Example 2
+cups_of_flour_1 = 5
+has_sauce_1 = false
+
+if cups_of_flour_1 >= 2 && has_sauce_1 == true
+  puts "I can make pizza"
+else
+  puts "I cannot make pizza"
+end
+
+#Example 3
+cups_of_flour_2 = 2
+has_sauce_2 = true
+
+if cups_of_flour_2 >= 2 && has_sauce_2 == true
+  puts "I can make pizza"
+else
+  puts "I cannot make pizza"
+end
+
+#Example 4
+cups_of_flour_3 = 3
+has_sauce_3 = true
+
+if cups_of_flour_3 >= 2 && has_sauce_3 == true
+  puts "I can make pizza"
+else
+  puts "I cannot make pizza"
+end
+
+
+#Example with user input
+puts " "
+puts "How many cups of flour do you have?"
+print "> "
+
+cups_of_flour_input = $stdin.gets.chomp
+
+puts "Do you have pizza sauce?"
+print "> "
+has_sauce_input = $stdin.gets.chomp
+
+if cups_of_flour_input >= "2" && (has_sauce_input == "yes" || has_sauce_input == "Yes")
+  puts "I can make pizza"
+else
+  puts "I cannot make pizza"
+end
+
+puts " "
--- day_3/questions.md
+++ day_3/questions.md
@@ -2,12 +2,48 @@
 
 1. What is a conditional statement? Give three examples.
 
+```
+ **A conditional statement is something that evaluates whether a statement is true or false. 'if' expressions, 'elsif' expressions, and 'else' expressions are all examples of conditional statements. It the 'if' statement is true, then the code will run with whatever command is given. If the 'if' statement is false, then the code will move to the first 'elsif' statement. If the 'elsif' statement is true, then it will run the code. It will repeat this process for all 'elsif' statements depending on how many there are. There could be infinite 'elsif' statements. Lastly, if the 'if' statement and all 'elsif' statements are false (or there are no 'elsif' statements), then the code will run the 'else' statement.**
+ ```
+
 1. Why might you want to use an if-statement?
+
+```
+**You would use an if-statement when you want your code to do something** *if and only if* **a certain condition is met. If the condition is not met, then the code will not run, or you can tell the code to execute something else when the if-statement is not met.**
+```
 
 1. What is the Ruby syntax for an if statement?
 
+```
+**if** *variable* == **string/number** *(Can be any conditional operator, not just ==)*
+  **puts "Some quote"** *(Or execute whatever code you want, not just printing strings)*
+**else**
+  **puts "Some other quote"
+**end**
+```
+
 1. How do you add multiple conditions to an if statement?
+
+```
+**elsif and else would be the the way to add multiple conditions to an if statement. If you want just two conditions, using if and else would be all you would need. If you want three or more conditions, then you would use one if-statement, greater than or equal to 1 elsif-statement (>=), and one else-statement.**
+```
 
 1. Provide an example of the Ruby syntax for an if/elsif/else statement:
 
+```
+*puts "What's your favorite food?"
+food = gets.chomp
+
+if food == "pizza"
+  puts "Correct!"
+elsif food == "ice cream"
+  puts "YESSS"
+else
+  puts "Wrong."
+end*
+```
+
 1. Other than an if-statement, can you think of any other ways we might want to use a conditional statement?
+
+```
+I think an *unless* conditional statement would be useful. I think this would be used very similarly to an *if-statement*, but in a scenario where most inputs the coder is looking for are **true** and only a few inputs are **false**. "Do whatever you want *unless* there's a pandemic." Most of the time there isn't a pandemic, but sometimes it happens. You wouldn't lead with "If there's a pandemic, do this. If not, do the other thing." :)
