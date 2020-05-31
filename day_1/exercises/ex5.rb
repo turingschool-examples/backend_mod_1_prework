123ee7f87dd8bae31542f1fdf76bcb618ee22648
Phil DeFraties <philip.defraties@gmail.com>
2020-05-30 19:00:19 -0600

    added exercise 5

---
+++ day_1/exercises/ex5.rb
@@ -0,0 +1,17 @@
+name = 'Zed A. Shaw'
+age = 35 # nota lie in 2009
+height = 74 * 2.54 # inches converted to centimeters
+weight = 180 / 2.2046 # lbs converted to kilograms
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} centimeters tall."
+puts "He's #{weight} kilograms heavy"
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
