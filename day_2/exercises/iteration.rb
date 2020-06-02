446d056051dcbcb662e2ccbf33f96165b1a0af71
Brett <64513280+BJSherman80@users.noreply.github.com>
2020-06-02 11:16:11 -0600

    added_code_blocks

--- day_2/exercises/iteration.rb
+++ day_2/exercises/iteration.rb
@@ -5,29 +5,34 @@
 
 # Example: Write code that iterates through a list of animals
 # and print each animal:
+```
 animals = ["Zebra", "Giraffe", "Elephant"]
 
 animals.each do |animal|
   p animal
 end
-
+```
 # Write code that iterates through a list of animals and prints
 # "The <animal> is awesome!" for each animal:
-
+```
 animals.each do |animal|
    p "The #{animal} is awesome!"
 end
-
+```
 # Write code that stores an array of foods in a variable,
 # then iterates over that array to print
 # "Add <food> to shopping "list" for each food item:
+```
 foods = ["apple", "meat", "pizza", "ramen"]
 
 foods.each do |food|
   p "add #{food} to shopping list."
 end
+```
 # Write code that stores an array of numbers in a variable,
 # then iterates over that array to print doubles of each number:
+```
 numbers = [1,2,3,4,5,6]
 
 numbers.each{ |x| puts x , x }
+```
