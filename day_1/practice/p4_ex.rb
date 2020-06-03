e2939c4ba73f862378008530c50314b6b0c7c475
Carson Jardine <carsonjardine@gmail.com>
2020-06-02 20:45:57 -0600

    add exercises

---
+++ day_1/practice/p4_ex.rb
@@ -0,0 +1,24 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
+
+puts "\aHello world\a"
+
+print "Say", "hello", "to", "the", "world", "\n"
