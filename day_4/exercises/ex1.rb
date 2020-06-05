ce2dc3ec0027b1de72c01828b62f41e7274706e1
Corey Carter <crc2231@gmail.com>
2020-06-05 15:53:52 -0600

    Add Day 4 work

---
+++ day_4/exercises/ex1.rb
@@ -0,0 +1,24 @@
+def say(words='hello')
+  puts words + '.'
+end
+
+say()
+say("hi")
+say("how are you")
+say("I'm fine")
+
+a = 5
+
+def some_method
+  a = 3
+end
+
+puts a
+
+[1, 2, 3].each do |num|
+  puts num
+end
+
+def print_num(num)
+  puts num
+end
