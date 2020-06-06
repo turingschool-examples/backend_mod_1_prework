18472d3f7d7901514f7227615f11b10c0cb90a98
Corey Carter <crc2231@gmail.com>
2020-06-06 13:13:13 -0600

    Add Day 5 Work

---
+++ day_5/exercises/ex2.rb
@@ -0,0 +1,13 @@
+#produce = {"apples" => 3, "oranges" => 1, "carrots" => 12}
+
+produce = {apples: 3, oranges: 1, carrots: 12}
+
+puts "There are #{produce['oranges']} oranges in the fridge."
+
+produce["grapes"] = 221
+produce
+produce["oranges"] = 6
+
+puts produce
+puts produce.keys
+puts produce.values
