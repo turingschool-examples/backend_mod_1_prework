6a4ae243b38c29eadd054777593b7c882d19613c
Megan Goulding <megan.m.goulding@gmail.com>
2020-06-05 23:12:18 -0600

    Add Day 5 Work

--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby day_5/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,21 +8,27 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {zebras: 2, lions: 4, monkies:6, elephants:3}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+zoo = {zebras: 2, lions: 4, monkies:6, elephants:3}
+p zoo.keys
 
-# Write code that prints all of the 'values' of the zoo variable 
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+zoo = {zebras: 2, lions: 4, monkies:6, elephants:3}
+p zoo.values
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
-# YOUR CODE HERE
+zoo = {zebras: 2, lions: 4, monkies:6, elephants:3}
+p zoo.fetch(:zebras)
 
-# Write code that adds an animal to the zoo hash. 
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+zoo = {zebras: 2, lions: 4, monkies:6, elephants:3}
+zoo["tigers"] = 3
+p zoo
+zoo = {:zebras=>2, :lions=>4, :monkies=>6, :elephants=>3, "tigers"=>3}
--- day_5/questions.md
+++ day_5/questions.md
@@ -3,11 +3,38 @@
 1. What is a Hash, and how is it different from an Array?
 
 1. In the space below, create a Hash stored to a variable named `pet_store`.  This hash should hold an inventory of items and the number of that item that you might find at a pet store.
+```
+pet_store = {dog_toys: 15, birds: 13, cat_leashes: 8, flea_shampoo: 24}
 
+p pet_store
+```
 1. Given the following `states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}`, how would you access the value `"Iowa"`?
+```
+irb(main):001:0> states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}
+=> {"CO"=>"Colorado", "IA"=>"Iowa", "OK"=>"Oklahoma"}
+irb(main):002:0> states.values_at("IA")
+=> ["Iowa"]
+```
 
-1. With the same hash above, how would we get all the keys?  How about all the values?
+1. With the same hash above, how would we get all the keys?  How about all the values
+```
+p states.keys
+
+p states.values
+```
 
 1. What is another example of when we might use a hash?  In your example, why is a hash better than an array?
 
+Hash allows us to be more specific and organized in a collection.
+
+```
+person = {name: 'bob', height: '6 ft', weight: '160 lbs', hair: 'brown'}
+
+person.each do |key, value|
+  puts "Bob's #{key} is #{value}"
+end
+```
+
 1. What questions do you still have about hashes?
+
+no questions yet, just need more practice.
