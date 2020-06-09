44434e002bf0d9ed9ad5788f1d666e06dae5dfe0
Brett Turing <bjsherman80@gmail.com>
2020-06-09 12:37:55 -0600

    Created 10 little monkeys program

---
+++ day_7/10_little_monkeys.rb
@@ -0,0 +1,23 @@
+puts "--------10 Little Monkeys--------"
+
+
+
+def monkeys(monkeys1, monkeys2, monkeys3 )
+  puts  "#{monkeys1} little monkeys jumping on the bed,
+   One fell off and bumped his head,
+  Mama called the doctor and the doctor said,
+   'No more monkeys jumping on the bed!'
+
+  #{monkeys2} little monkeys jumping on the bed,
+   One fell off and bumped his head,
+   Mama called the doctor and the doctor said,
+   'No more monkeys jumping on the bed!'
+
+   #{monkeys3} little monkey jumping on the bed,
+   He fell off and bumped his head,
+   Mama called the doctor and the doctor said,
+    'Get those monkeys right to bed!' "
+end
+
+
+monkeys("Three", "Two", "One")
