d0135a0627f0fa5cbaff835ada7d9d2d0171445d
Leah Riffell <leah.riffell@gmail.com>
2020-06-10 15:45:37 -0600

    Update 10 monkeys to run for any number of monkeys

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -1,18 +1,20 @@
+require 'humanize'
+
 class LittleRhymes
-  def initialize(*number)
-    @monkeys = [*number]
+  def initialize(number)
+    @monkeys = (1 .. number).to_a.reverse
   end
 
   def sing_nursey_rhyme
     @monkeys.each_with_index do |monkey, index|
-      if index == @monkeys.index(@monkeys.last)
-        print "#{monkey.gsub(/\w+/, &:capitalize)} little monkey jumping on the bed,\n One fell off and bumped his head,\n Mama called the doctor and the doctor said,\n \"Get those monkeys right to bed!\"\n\n"
+      if monkey == @monkeys.last
+        print "#{monkey.humanize.gsub(/\w+/, &:capitalize)} little monkey jumping on the bed,\n One fell off and bumped his head,\n Mama called the doctor and the doctor said,\n \"Get those monkeys right to bed!\"\n\n"
       else
-        print "#{monkey.gsub(/\w+/, &:capitalize)} little monkeys jumping on the bed,\n One fell off and bumped his head,\n Mama called the doctor and the doctor said,\n \"No more monkeys jumping on the bed!\"\n\n"
+        print "#{monkey.humanize.gsub(/\w+/, &:capitalize)} little monkeys jumping on the bed,\n One fell off and bumped his head,\n Mama called the doctor and the doctor said,\n \"No more monkeys jumping on the bed!\"\n\n"
       end
     end
   end
 end
 
-ten_monkeys = LittleRhymes.new('ten', 'nine', 'eight', 'seven', 'six', 'five', 'four', 'three', 'two', 'one')
+ten_monkeys = LittleRhymes.new(10)
 ten_monkeys.sing_nursey_rhyme
