7db5d312d736c8145092ee227a7bd11ecfe03a8f
Brett Turing <bjsherman80@gmail.com>
2020-06-19 15:10:45 -0600

    Modified code

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -1,24 +1,11 @@
 puts "--------10 Little Monkeys--------"
 
+monkeys = (1..20)
 
-
-def monkeys(monkeys1, monkeys2, monkeys3 )
-  puts  "#{monkeys1} little monkeys jumping on the bed,
-   One fell off and bumped his head,
-  Mama called the doctor and the doctor said,
-   'No more monkeys jumping on the bed!'
-
-  #{monkeys2} little monkeys jumping on the bed,
-   One fell off and bumped his head,
-   Mama called the doctor and the doctor said,
-   'No more monkeys jumping on the bed!'
-
-   #{monkeys3} little monkey jumping on the bed,
-   He fell off and bumped his head,
-   Mama called the doctor and the doctor said,
-    'Get those monkeys right to bed!' "
-end
-
-
-monkeys("Ten", "Nine", "Eight")
+monkeys.each do |monkey|
+  puts "#{monkey} little monkeys jumping on the bed,
+    One fell off and bumped his head,
+    Mama called the doctor and the doctor said,
+   'No more monkeys jumping on the bed!'"
+ end
 #Bonus: Change parameters when you call the method for any number of monkeys.
