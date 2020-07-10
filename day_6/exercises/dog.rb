f64e806f61eed1462a25c09ff6b2dc8814b32423
George Soderholm <georgesoderholm@gmail.com>
2020-07-09 21:13:33 -0600

    Add Day 6 work

--- day_6/exercises/dog.rb
+++ day_6/exercises/dog.rb
@@ -3,13 +3,39 @@
 # print the dog's hunger status.
 
 class Dog
-  attr_reader :breed, :name, :age, :hungry
+  attr_reader :current_hair_length, :breed, :name, :age, :hungry
 
-  def initialize(breed, name, age)
+  def initialize(current_hair_length, breed, name, age)
+    @current_hair_length = current_hair_length
     @breed  = breed
     @name   = name
     @age    = age
+    @ideal_hair_length = 0.5
+    p @stomach_level = 1
     @hungry = true
+  end
+
+  def groom(hair_length)
+    if hair_length > @ideal_hair_length
+      hair_length -= 0.25
+    end
+  end
+
+  def eat
+    @hungry = false
+    @stomach_level += 5
+  end
+
+  def poop
+    if @stomach_level > 10
+      p @stomach_level
+      @stomach_level = 0
+      @hungry = true
+      puts "That took care of some business!"
+    else
+      p @stomach_level
+      puts "Nope, just a little tinkle!"
+    end
   end
 
   def bark
@@ -20,12 +46,10 @@ class Dog
     @hungry = true
   end
 
-  def eat
-    @hungry = false
-  end
+
 end
 
-fido = Dog.new("Bernese", "Fido", 4)
+fido = Dog.new(0.67, "Bernese", "Fido", 4)
 p fido.breed
 p fido.name
 p fido.age
@@ -34,3 +58,9 @@ fido.eat
 p fido.hungry
 fido.play
 p fido.hungry
+fido.eat
+fido.eat
+fido.poop
+fido.poop
+fido.eat
+fido.poop
