031b3e72538ff4a990387fd02a75fef4a71b0cfd
Eugene Theriault <hybridbassist@gmail.com>
2020-07-16 09:42:01 -0700

    Remove extraneous comparison operators

--- day_7/checker_board.rb
+++ day_7/checker_board.rb
@@ -6,7 +6,7 @@ number = gets.chomp.to_i
 range = (1..number).to_a
 
 range.each do |row|
-  print "X " * number if row.even? == true
-  print " X" * number if row.odd? == true
+  print "X " * number if row.even?
+  print " X" * number if row.odd? 
   print "\n"
 end
