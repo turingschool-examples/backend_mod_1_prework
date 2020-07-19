9c38ebb945dfb09c5d254f193207e65f05fb1767
Aidan Murray <aidanhansm@gmail.com>
2020-07-19 15:53:59 -0600

    Add day 3 work

---
+++ day_3/exercises/ex3.rb
@@ -0,0 +1,40 @@
+puts "You enter a dark room with two doors.  Do you go through door #1 or door #2?"
+
+print "> "
+door = $stdin.gets.chomp
+
+if door == "1"
+  puts "There is a giant bear here eating a cheese cake.  What do you do?"
+  puts "1. Take the cake"
+  puts "2. Scream at the bear"
+
+  print "> "
+  bear = $stdin.gets.chomp
+
+  if bear == "1"
+    puts "The bear eats your face off. Good job!"
+  elsif bear == "2"
+    puts "The bear eats your legs off. Good job!"
+  else
+    puts "Well, doing %s is probably better. Bear runs away." % bear
+  end
+
+
+elsif door == "2"
+  puts "You stare into the endless abyss at Cthulu's retina."
+  puts "1. Blueberries"
+  puts "2. Yellow jacket clothespins"
+  puts "3. Understanding revolvers yelling melodies"
+
+  print "> "
+  insanity = $stdin.gets.chomp
+
+  if insanity == "1" || insanity == "2"
+    puts "your body survives and your brain is mush"
+  else
+    puts "your eyes are mush"
+  end
+
+else
+  puts "FALL ON A KNIFE AND DIE YOU MOFO"
+end
