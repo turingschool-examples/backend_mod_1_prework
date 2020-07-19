4dad22f9703451725d9f86792d9e3815f0fd2a73
Aidan Murray <aidanhansm@gmail.com>
2020-07-18 22:18:59 -0700

    Add day 1

---
+++ day_1/ex2.rb
@@ -0,0 +1,9 @@
+# A comment for later stuff
+# Anything after the # is ignored by ruby
+
+puts "Code not in a comment" #now it is
+
+# Use comments to disable code for a bit
+# puts "lol this won't do it"
+
+puts "This will do it"
---
+++ day_1/ex5.rb
@@ -0,0 +1,17 @@
+name = 'Zed A. Shaw'
+age = 35 #lol u old
+height = 74 #inches
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height * 2.54} cm tall."
+puts "He's #{weight * 0.453592} kg light."
+puts "Not that heave tbh."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His toofs are usually #{teeth} depending on coffee." #EEEEEWWWWWWW coffee sucks
+
+# lol tricky
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
