df30a68afc84f60ae88d091901277548c21dae97
Greg Mitchell <g2mitchell3003@gmail.com>
2020-07-20 14:45:10 -0600

    Complete exercise and questions

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -1,4 +1,4 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
@@ -11,9 +11,28 @@ class Burrito
     @base     = base
     @toppings = toppings
   end
+  def add_topping(new_topping)
+    toppings.push(new_topping)
+  end
+  def remove_topping(old_topping)
+    if toppings.rindex(old_topping) != nil
+      toppings.delete_at(toppings.rindex(old_topping))
+    else
+      puts "There is no #{old_topping} on this burrito"
+    end
+  end
+  def change_protein(new_protein)
+    @protein = new_protein
+  end
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+dinner.add_topping("Pico de Gallo")
+p dinner.toppings
+dinner.remove_topping("Sour Cream")
+p dinner.toppings
+dinner.change_protein("Chicken")
+p dinner.protein
--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -1,5 +1,57 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
 # YOUR CODE HERE
+class Person
+  attr_reader :height, :weight, :depression, :anxious
+  def initialize(height, weight, depression, anxious)
+    @height = height
+    @weight = weight
+    @depression = depression
+    @anxious = anxious
+  end
+  def grow(growth)
+    @height += growth
+  end
+  def gain_weight(weight_gained)
+    @weight += weight_gained
+  end
+  def depress
+    @depression = true
+  end
+  def make_anxious
+    @anxious = true
+  end
+  def therapy
+    @anxious = false
+    @depression = false
+  end
+  def shrink(amount_shrunk)
+    @height -= amount_shrunk
+  end
+  def lose_weight(weight_lost)
+    @weight -= weight_lost
+  end
+end
+
+greg = Person.new(160, 145, true, false)
+p greg.height
+p greg.weight
+p greg.depression
+p greg.anxious
+greg.grow(4)
+greg.gain_weight(15)
+greg.depress
+greg.make_anxious
+p greg.height
+p greg.weight
+p greg.depression
+p greg.anxious
+greg.shrink(1)
+greg.therapy
+greg.lose_weight(6)
+p greg.height
+p greg.weight
+p greg.depression
+p greg.anxious
