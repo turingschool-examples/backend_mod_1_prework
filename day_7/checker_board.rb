5a7d7c1829c7a4ad1974e1c488bb3c35bfd7a89e
Eugene Theriault <hybridbassist@gmail.com>
2020-07-19 22:23:43 -0700

    Remove extraneous array

--- day_7/checker_board.rb
+++ day_7/checker_board.rb
@@ -1,12 +1,9 @@
 puts "Input number of rows: "
 print "> "
-
 number = gets.chomp.to_i
 
-range = (1..number).to_a
-
-range.each do |row|
+(1..number).each do |row|
   print "X " * number if row.even?
-  print " X" * number if row.odd? 
+  print " X" * number if row.odd?
   print "\n"
 end
