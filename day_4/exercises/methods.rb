d9bd58fd885dfa7e6beff7f46aa4fca5156c7da5
Megan Campbell <megan.campbell26@yahoo.com>
2020-07-21 11:06:00 -0600

    Remove duplicate

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -12,24 +12,6 @@ print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
-end
-
-print_name("Albus Dumbledore")
-# In the below exercises, write code that achieves
-# the desired result. To check your work, run this
-# file by entering the following command in your terminal:
-# `ruby day_4/exercises/methods.rb`
-
-# Example: Write a method that when called will print your name:
-def print_name
-  p "Severus Snape"
-end
-
-print_name
-
-# Write a method that takes a name as an argument and prints it:
-def print_name(name)
   p name
 end
 
@@ -55,12 +37,3 @@ def wizards(good, evil)
 end
 
 wizards("Harry Potter", "Voldemort")
-
-# Write a method that takes in 2 numbers as arguments and prints
-# their sum. Then call your method:
-# YOUR CODE HERE
-
-# Write a method that takes in two strings as arguments and prints
-# a concatenation of those two strings. Example: The arguments could be
-# (man, woman) and the end result might output: "When Harry Met Sally".
-# Then call your method:
