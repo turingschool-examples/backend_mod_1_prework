6f1ceb4ab6891507c7496dd7877f74fe128a7122
Cydnee Owens <cks410@yahoo.com>
2020-07-21 14:26:49 -0600

    Add Day 6 Work

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -1,5 +1,33 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
-# YOUR CODE HERE
+class Person
+    attr_accessor :name, :favorite_candies, :favorite_color
+    def initialize(name, favorite_candies, favorite_color)
+      @name = name
+      @favorite_candies = favorite_candies
+      @favorite_color = favorite_color
+    end
+
+    def speak
+      p "Hello I am #{@name}"
+    end
+
+    def change_fav_color(color)
+      self.favorite_color = color
+    end
+
+    def add_fav_candy(candy)
+      self.favorite_candies.push(candy)
+    end
+  end
+
+  person = Person.new("Victor", ["Reese's", "Twix", "Snickers"], "orange")
+  p person.name
+  p person.favorite_candies
+  p person.favorite_color
+  p person.speak
+  p person.change_fav_color("blue")
+  p person.add_fav_candy("Gummies")
+  p person
