e5545472a082e430c1cacce92fe7a235aebb0154
Greg Mitchell <g2mitchell3003@gmail.com>
2020-07-21 15:35:54 -0600

    Complete exercises

---
+++ day_7/fizzbuzz.rb
@@ -0,0 +1,21 @@
+puts "pick a lower bound please"
+print "> "
+lower_bound = gets.chomp.to_i
+#lower_bound.to_i
+puts "Pick an upper bound"
+print ">"
+upper_bound = gets.chomp.to_i
+#upper_bound.to_i
+
+
+array =* (lower_bound..upper_bound)
+array.each do |number|
+  if number % 3 == 0 && number % 5 ==0
+    print "Fizzbuzz, "
+  elsif number % 3 == 0 && number % 5 != 0
+    print "Fizz, "
+  elsif number % 5 == 0 && number % 3 != 0
+    print "Buzz, "
+  else print "#{number}, "
+  end
+end
