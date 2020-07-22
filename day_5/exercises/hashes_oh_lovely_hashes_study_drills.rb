2adf5e32fbffccb8cd3ff2cd0be78f30c7bd8e54
Caryn Keffer <carynkeffer@gmail.com>
2020-07-22 16:21:57 -0600

    Add Day 5 Work

---
+++ day_5/exercises/hashes_oh_lovely_hashes_study_drills.rb
@@ -0,0 +1,175 @@
+# ************** study drill #1
+# do this same kind of mapping with different cities and states/regions
+
+# create a mapping of state to abbreviation
+states = {
+  'Washington' => 'WA',
+  'Ohio' => 'OH',
+  'Colorado' => 'CO',
+  'New Mexico' => 'NM',
+  'Maryland' => 'MD'
+}
+
+# # create a basic set of states and some cities in them
+cities = {
+  'CO' => 'Denver',
+  'NM' => 'Taos',
+  'MD' => 'Ann Arbor'
+}
+
+# add some more cities
+cities['OH'] = 'Cleveland'
+cities['WA'] = 'Seattle'
+
+# puts out some cities
+puts '/' * 10
+puts "NM State has: #{cities['NM']}}"
+puts "MD State has: #{cities['MD']}"
+
+# puts some states
+puts '/' * 10
+puts "Colorado's abbreviation is: #{states['Colorado']}"
+puts "Washington's abbreviation is: #{states['Washington']}"
+
+# # do it by usimg the state then cities dict (what is dict?)
+puts '/' * 10
+puts "Ohio has: #{cities[states['Ohio']]}"
+puts "Maryland has: #{cities[states['Maryland']]}"
+
+# puts every state abbreviation
+puts '/' * 10
+states.each do |state, abbrev|
+  puts "#{state} is abbreviated #{abbrev}"
+end
+
+# puts every city in state
+puts '/' * 10
+cities.each do |abbrev, city|
+  puts "#{abbrev} has the city #{city}"
+end
+
+# now do both at the same time
+puts '/' * 10
+states.each do |state, abbrev|
+  city = cities[abbrev]
+  puts "#{state} is abbreviated #{abbrev} and has city #{city}"
+end
+
+puts '/' * 10
+# by default ruby says "nil" when something isn't in there
+state = states['Maine']
+
+if !state
+  puts "Sorry, no Maine."
+end
+
+# default values using ||= with the nil result
+city = cities['ME']
+city ||= 'Does Not Exist'
+puts "The city for the state 'ME' is #{city}"
+
+
+# ******** study drill # 2
+find the ruby documentation for hashes and try to do even more things to them
+
+according to ruby-doc.org :
+a hash is a dictionary-like collection of unique keys and their values, also
+called associative arrays, but where an array uses integers as its index,
+a hash allows you to use any object type.
+hashes enumerate their values in the order that the corresponding keys were
+inserted.
+
+this is me writing a hash with keys and values where the keys are symbols
+states = {
+  Washington: 'WA',
+  Ohio: 'OH',
+  Colorado: 'CO',
+  New_Mexico: 'NM',
+  Maryland: 'MD'
+}
+
+cities = {
+  'CO' => 'Denver',
+  'NM' => 'Taos',
+  'MD' => 'Ann Arbor'
+}
+
+# just putting this here to see what happens
+puts states == cities
+puts states != cities
+
+# this doesn't do anything obvious?, but it also doesn't give me an error
+states.keys
+states.values
+
+cities['OH'] = 'Cleveland'
+cities['WA'] = 'Seattle'
+
+puts '/' * 10
+puts "NM State has: #{cities['NM']}"
+puts "MD State has: #{cities['MD']}"
+
+# I'm calling on my states by referencing them as symbols
+puts '/' * 10
+puts "Colorado's abbreviation is: #{states[:Colorado]}"
+puts "Washington's abbreviation is: #{states[:Washington]}"
+
+puts '/' * 10
+puts "Ohio has: #{cities[states[:Ohio]]}"
+puts "Maryland has: #{cities[states[:Maryland]]}"
+
+puts '/' * 10
+states.each do |state, abbrev|
+  puts "#{state} is abbreviated #{abbrev}"
+end
+
+puts '/' * 10
+cities.each do |abbrev, city|
+  puts "#{abbrev} has the city #{city}"
+end
+
+puts '/' * 10
+states.each do |state, abbrev|
+  city = cities[abbrev]
+  puts "#{state} is abbreviated #{abbrev} and has city #{city}"
+end
+
+puts '/' * 10
+# a different way to return a value other than `nil`
+states = Hash.new( "State not found" )
+
+puts "#{states["Michigan"]}"
+puts "#{states["Alaska"]}"
+
+
+# ******** study drill #3
+# find out what you can't do with hashes
+
+colors = {
+  'yellow_green' => 'stems',
+  'blue_green' => 'water',
+  'green_green' => 'leaves',
+  'brown_green' => 'earth'
+}
+
+numbers = {
+  'stems' => 124,
+  'water' => 356,
+  'leaves' => 423,
+  'earth' => 876
+}
+
+puts "Assign all flower stems to color ##{numbers['stems']}."
+puts "Assign all lakes and rivers to color ##{numbers['water']}."
+# this returns nothing
+puts "#{colors[2]}"
+
+# looks like I can't do this
+# colors['yellow_yellow'] = 'flowers'
+#
+# puts "Yellow is to be assigned to #{colors[yellow_yellow]}."
+
+# but can I do this?
+numbers['flowers'] = '999'
+# yes
+puts "Assign all flowers to color ##{numbers['flowers']}"
--- day_5/questions.md
+++ day_5/questions.md
@@ -2,12 +2,40 @@
 
 1. What is a Hash, and how is it different from an Array?
 
-1. In the space below, create a Hash stored to a variable named `pet_store`.  This hash should hold an inventory of items and the number of that item that you might find at a pet store.
+ **A hash is a dictionary-like collection of unique keys and their values, also called associative arrays, but where an array uses integers as its index, a hash allows you to use any object type. Hashes enumerate their values in the order that the corresponding keys were inserted.**
 
-1. Given the following `states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}`, how would you access the value `"Iowa"`?
 
-1. With the same hash above, how would we get all the keys?  How about all the values?
+2. In the space below, create a Hash stored to a variable named `pet_store`.  This hash should hold an inventory of items and the number of that item that you might find at a pet store.
 
-1. What is another example of when we might use a hash?  In your example, why is a hash better than an array?
+```ruby
+pet_store = {beta_fish: 30, fish_tanks: 23, filters: 11, gold_fish: 45, nets: 22}
+```
 
-1. What questions do you still have about hashes?
+
+3. Given the following `states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}`, how would you access the value `"Iowa"`?
+
+```ruby
+puts "#{states["IA"]}"
+# or
+puts states["IA"]
+```
+
+
+4. With the same hash above, how would we get all the keys?  How about all the values?
+
+```ruby
+puts states.keys
+puts states.values
+```
+
+
+5. What is another example of when we might use a hash?  In your example, why is a hash better than an array?
+
+**Another example of when we might use a hash is anytime we need to reference an amount of stored data. This could be... employee names/ID, pet name/medical record number.**
+
+**A hash would be better than an array in these examples because the programmer would not have to index the values of each of the keys. The key/value pairs are unique, can be in any order, and can be called on easily.**
+
+
+6. What questions do you still have about hashes?
+
+**How to store arrays in hashes?**
