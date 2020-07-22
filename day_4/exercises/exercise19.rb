b020aa4d91c8dc67205d7662825f69f9d62063f4
Connor Ferguson <cpfergus1@gmail.com>
2020-07-22 12:53:43 -0600

    Adding day_4 homework

---
+++ day_4/exercises/exercise19.rb
@@ -0,0 +1,75 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers)
+  puts "You have #{cheese_count} cheeses!"
+  puts "You have #{boxes_of_crackers} boxes of crackers!"
+  puts "Man that's enough for a party!"
+  puts "Get a blanket.\n"
+end
+#function collects two variables that can be input directly or implied. Prints missages utilizing those variables when the function is called.
+
+puts "We can just give the function numbers directly:" # print message
+cheese_and_crackers(20, 30) # Calls cheese_and_crackers method with intigers in the input parameter section
+
+
+puts "OR, we can use variables from our script:" # print message
+amount_of_cheese = 10 # define variable
+amount_of_crackers = 50 # define variable
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers) # Calls cheese_and_crackers method with variables in the input parameter section
+
+
+puts "We can even do math inside too:" #Prints message
+cheese_and_crackers(10 + 20, 5 + 6) # Calls cheese_and_crackers method with intigers added together in the input parameter section
+
+
+puts "And we can combine the two, variables and math:" #print message
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000) # Calls method "cheese_and_crackers" while adding to the defined variables amount_of_cheese and amount_of_crackers 100 and 1000 respectively
+
+
+
+#my method
+def cookie_baking (time, high_altitude)
+  if (high_altitude > 5000 && (time < 15 && time >= 12))
+    puts "At your altitude of #{high_altitude}, you will need to add 1 tbsp of flour and 3 tsp of water for your recipe!\n"
+  elsif (high_altitude < 5000 && (time < 15 && time >= 12))
+    puts "At your altitude of #{high_altitude}, you will need to add 1 tbsp of flour and 3 tsp of water for your recipe!\n"
+    puts "Your cookies will turn out perfect if you cook it for about #{time} minutes"
+  elsif (high_altitude > 5000 && time < 12)
+    puts "At your altitude of #{high_altitude}, you will need to add 1 tbsp of flour and 3 tsp of water for your recipe!"
+    puts "You need to cook your cookies longer than #{time} minutes or they will be uncooked!\n"
+  elsif (high_altitude > 5000 && time > 15)
+    puts "At your altitude of #{high_altitude}, you will need to add 1 tbsp of flour and 3 tsp of water for your recipe!"
+    puts "If you cook your cookies for #{time} minutes you will end up burning them!\n"
+  elsif (high_altitude < 5000 && time < 12)
+    puts "At your altitude of #{high_altitude} feet, you will not have to add any extra ingredients or change your baking time to ensure a good cookie!"
+    puts "You need to cook your cookies longer than #{time} minutes or they will be uncooked!\n"
+  elsif (high_altitude < 5000 && time > 15)
+    puts "At your altitude of #{high_altitude} feet, you will not have to add any extra ingredients or change your baking time to ensure a good cookie!"
+    puts "If you cook your cookies for #{time} minutes you will end up burning them!\n"
+  else
+    puts "If you do not know your cooking times or altitude, look them up on google!\n"
+  end
+end
+
+cooktime = 5
+altitude = 4550
+
+cookie_baking(cooktime, altitude)
+
+
+cooktime = 14
+altitude = 5670
+
+cookie_baking(cooktime, altitude)
+
+cookie_baking(12 + 3, 2130 + 4000)
+
+cookie_baking(13, 4300)
+
+puts "how long do you plan on cooking your cookies?"
+cooktime = gets.chomp.to_i
+
+puts "At what elevation do you currently live?"
+altitude = gets.chomp.to_i
+
+puts "Well let's see how we can help you here..."
+cookie_baking(cooktime,altitude)
--- day_4/questions.md
+++ day_4/questions.md
@@ -1,11 +1,28 @@
 ## Day 4 Questions
 
 1. In your own words, what is the purpose of a method?
+ * A method defines a personalized function to perform when called in the program. This allows a way to condense repetitive code and if changes are required for the function, you only have to do it in one area.
 
 1. Create a method named `hello` that will print `"Sam I am"`.
+  ```
+  def hello(name = "Sam I am")
+    p name
+  end
 
+  hello()
+  ```
 1. Create a method named `hello_someone` that takes an argument of `name` and prints `"#{name} I am"`.
+
+ ```
+ def hello_someone(name)
+ p "#{name} I am"
+ end
+ hello_someone("John")
+ ```
 
 1. How would you call or execute the method that you created above?
 
+  * You would call the function name which is hello_someone and in parenthesis type out your input which would be a name.
+
 1. What questions do you have about methods in Ruby?
+  * Do we design methods with the intent for it to work on a class and instance basis or just one or the other.
