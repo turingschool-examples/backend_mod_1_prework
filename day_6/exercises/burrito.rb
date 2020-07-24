c0fa9c73537a50800d60769cd9c614c65f6856b5
Connor Ferguson <cpfergus1@gmail.com>
2020-07-24 11:51:09 -0600

    Adding day_6 homework

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -1,4 +1,4 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
@@ -11,9 +11,30 @@ class Burrito
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(new_topping)
+    @toppings = @toppings + [new_topping]
+  end
+
+  def remove_topping(x_topping)
+    @toppings = @toppings - [x_topping]
+  end
+
+  def change_protein(protein)
+    @protein = protein
+  end
+
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
+p dinner.protein
+p dinner.base
+p dinner.toppings
+
+dinner.add_topping("jalapeno")
+dinner.remove_topping ("salsa")
+dinner.change_protein("chicken")
+
 p dinner.protein
 p dinner.base
 p dinner.toppings
