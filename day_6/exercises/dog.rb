20ed2fc29071b27f4e96614351c3e344ad47c034
Jodie <dearjodie@gmail.com>
2020-07-25 12:51:26 -0600

    Add Day 6 Work

--- day_6/exercises/dog.rb
+++ day_6/exercises/dog.rb
@@ -1,19 +1,24 @@
 # In the dog class below, write a `play` method that makes
-# the dog hungry. Call that method below the class, and 
+# the dog hungry. Call that method below the class, and
 # print the dog's hunger status.
-
 class Dog
-  attr_reader :breed, :name, :age, :hungry
+  attr_accessor :breed, :name, :age, :hungry
 
-  def initialize(breed, name, age)
+  def initialize(breed, name, age, hungry)
     @breed  = breed
     @name   = name
     @age    = age
-    @hungry = true
+    @hungry = hungry
   end
 
   def bark
     p "woof!"
+  end
+
+  def play(hungry)
+    puts "Playing makes #{name} hungry!"
+    puts "#{name} has played for 30 minutes. Is #{name} hungry?"
+    @hungry = hungry
   end
 
   def eat
@@ -21,10 +26,11 @@ class Dog
   end
 end
 
-fido = Dog.new("Bernese", "Fido", 4)
-p fido.breed
-p fido.name
-p fido.age
+fido = Dog.new("Bernese", "Fido", 4, "Hungry")
+# p fido.breed
+# p fido.name
+# p fido.age
+fido.play("true")
 p fido.hungry
-fido.eat
-p fido.hungry
+# p fido.eat
+# p fido.hungry
