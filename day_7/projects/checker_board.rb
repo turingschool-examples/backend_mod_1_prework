ced64185f47abce7f9d55eb686d50af79ded7a8d
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-07-27 19:26:27 -0600

    Update checker_board to take in user input

--- day_7/projects/checker_board.rb
+++ day_7/projects/checker_board.rb
@@ -1,8 +1,5 @@
-
-
-class CheckerBoard
-
-  def board_size(number)
+puts "What size would you like the checker board to be?"
+number = $stdin.gets.chomp.to_i
 
     number.times do |number|
       if number.odd?
@@ -11,22 +8,30 @@ class CheckerBoard
         puts "X X X"
       end
     end
-  end
-end
 
-eight = CheckerBoard.new
-eight.board_size(8)
-
-puts "--------"
-
-five = CheckerBoard.new
-five.board_size(5)
-
-
-
-# puts "What size would you like the checker board to be?"
-# number = $stdin.gets.chomp
+# Below was where I statred, but I realized I had made it too complicated
+# puts "------------Original code below------------"
 #
-# def array_up_to(i)
-#     (1..i).to_a
+# class CheckerBoard
+#
+#   def board_size(number)
+#
+#     number.times do |number|
+#       if number.odd?
+#         puts " X X X"
+#       else
+#         puts "X X X"
+#       end
+#     end
+#   end
 # end
+#
+# eight = CheckerBoard.new
+# eight.board_size(8)
+#
+# puts "--------"
+#
+# five = CheckerBoard.new
+# five.board_size(5)
+#
+# puts "--------"
