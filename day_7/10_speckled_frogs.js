b908c46f046c7cba70d0e908ef1fd0f840f78fb3
Michael Foy <foymikek@gmail.com>
2020-07-29 12:00:26 -0600

    Add day 7 10 spekecled frogs JS exercise

---
+++ day_7/10_speckled_frogs.js
@@ -0,0 +1,6 @@
+var i;
+for(i = 10 ; i >= 1 ; i--)
+{
+  console.log(`${i} sat on a log \n eating some most delicious bugs.\n One jumped in the pool where its nice and cool,\n then there were ${i-1} speckled frogs.`)
+}
+console.log("then there were no more speckled frogs!");
