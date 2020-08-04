03fd061aaa7d6461e94e5f29298e5d4500e7582b
Kevin Cuadros <kev040798@gmail.com>
2020-08-04 12:35:24 -0400

    Error Fix

--- day_7/fizzbuzz.rb
+++ day_7/fizzbuzz.rb
@@ -13,16 +13,16 @@ end
 
 #bonus
 puts "Enter any number"
-num = gets.chomp.to_i
+number = gets.chomp.to_i
 
-num.times do |number|
-  if  number % 3 == 0 && number % 5 == 0
+number.times do |ele|
+  if  ele % 3 == 0 && ele % 5 == 0
     puts 'FizzBuzz'
-  elsif number % 3 == 0
+  elsif ele % 3 == 0
     puts 'Fizz'
-  elsif  number % 5 == 0
+  elsif  ele % 5 == 0
     puts 'Buzz'
   else
-    puts number
+    puts ele
   end
 end
