500117f9e162e18e9c396b78a8ebb520a10afc91
Phil McCarthy <philmcc2631@gmail.com>
2020-08-19 17:50:34 -0600

    Add day 1 exercises

--- day_1/exercises/interpolation.rb
+++ day_1/exercises/interpolation.rb
@@ -8,18 +8,18 @@
 name = "Ron"
 team = "Chudley Cannons"
 
-p "The #{team} are #{name}'s favorite Quidditch team"
+puts "The #{team} are #{name}'s favorite Quidditch team"
 
 # Write code that uses the variables below to form a string that reads
 # "The quick red fox jumped over the lazy brown dog":
 speedy = "quick red fox"
 slow_poke = "lazy brown dog"
 
-p # YOUR CODE HERE
+puts "The #{speedy} jumped over the #{slow_poke}"
 
 # Write code that uses the variables below to form a string that reads
 # "In a predictable result, the tortoise beat the hare!":
 slow_poke = "tortoise"
 speedy = "hare"
 
-# YOUR CODE HERE
+puts "In a predictable result, the #{slow_poke} beat the #{speedy}"
