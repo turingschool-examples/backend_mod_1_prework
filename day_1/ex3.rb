8f7c59dd17708ef3495e9dbb992cd7a2ad1f7bba
Aiden Mendez <aidenmendez@gmail.com>
2020-08-20 22:55:42 -0600

    Completed first 3 Intro to ruby lessons.

---
+++ day_1/ex3.rb
@@ -0,0 +1,37 @@
+# Outputs string to console.
+puts "I will now count my chickens:"
+
+# Outputs the calculated number of hens and a string to the console.
+puts "Hens #{25.00 + 30.00 / 6.00}"
+# Outputs the calculated number of roosters and a string to the console.
+puts "Roosters #{100.00 - 25.00 * 3.00 % 4.00}"
+
+# Outputs a string to the console.
+puts "Now I will count the eggs:"
+
+# Computes and outputs the number of eggs to the console.
+puts 3.00 + 2.00 + 1.00 - 5.00 + 4.00 % 2.00 - 1.00 / 4.00 + 6.00
+
+# Outputs string to console.
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Computes each side of the enequality and outputs a boolean value to the console.
+puts 3.00 + 2.00 < 4.00 - 7.00
+
+# Outputs a string and a computed value.
+puts "What is 3 + 2? #{3.00 + 2.00}"
+# Outputs a string and a computed value.
+puts "What is 5 - 7? #{5.00 - 7.00}"
+
+# Outputs a string to the console.
+puts "Oh, that's why it's false."
+
+# Outputs a string to the console.
+puts "How about some more."
+
+# Outputs a string and a boolean value representing the computed inequality.
+puts "Is it greater? #{5.00 > -2.00}"
+# Outputs a string and a boolean value representing the computed inequality.
+puts "Is it greater or equal? #{5.00 >= -2.00}"
+# Outputs a string and a boolean value representing the computed inequality.
+puts "Is it less or equal? #{5.00 <= -2.00}"
