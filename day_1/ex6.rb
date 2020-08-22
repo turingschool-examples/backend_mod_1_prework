e5e65e647790d390f7e9b58f3aa15e0b9755e762
Aiden Mendez <aidenmendez@gmail.com>
2020-08-21 20:13:40 -0600

    Added ex6 & 7 and updated README

--- day_1/ex6.rb
+++ day_1/ex6.rb
@@ -1,21 +1,37 @@
+# variable definition: integer
 types_of_people = 10
+# variable definition: string with nested integer
 x = "There are #{types_of_people} types of people."
+# var definition: string
 binary = "binary"
+# var definition: string
 do_not = "don't"
+# var definition: string with nested string variable
 y = "Those who know #{binary} and those who #{do_not}."
 
+# logging variable to console
 puts x
+# logging variable to console
 puts y
 
+# logging string with nested variable to console.
 puts "I said #{x}."
+# logging string with nested variable to console.
 puts "I also said: '#{y}'."
 
+# defining variable with boolean value
 hilarious = false
+
+# defining variable containing string with nested boolean value.
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+# logging value of variable created above to console.
 puts joke_evaluation
 
-w = "This is the left side of..."
+# defining variable with string value
+w = 'This is the left side of...'
+# defining variable with string value
 e = "a string with a right side."
 
+# combining and logging the strings created above to the console.
 puts w + e
---
+++ day_1/ex7.rb
@@ -0,0 +1,22 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+
+# =====Study Drills======
+
+# chomp is a string method that returns a string 
+# with the trailing  /n (new line) character removed.
+# 'gets' is a method that gets user input as string.
+
+# Practice form
+print "What school do you go to?"
+school = gets.chomp
+
+print "What's your favorite coding language?"
+language = gets.chomp
