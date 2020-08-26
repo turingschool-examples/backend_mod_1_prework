a7883b793a412afb62a7a26eb3eb83e314b9efbd
Ben Fulton <benjamin.fulton99@gmail.com>
2020-08-26 08:42:52 -0600

    Update prework

---
+++ day_7/10_speckled_frogs.rb
@@ -0,0 +1,5 @@
+for i in (10).downto(1)
+
+  puts "#{i} speckled frogs sat on a log\neating some most delicious bugs.\nOne jumped in the pool where its nice and cool,\nthen there were #{i-1} speckled frogs.\n\n "
+
+end
---
+++ day_7/fizzbuzz.rb
@@ -0,0 +1,11 @@
+1.upto(100) do |i|
+     if i % 5 == 0 && i % 3 == 0
+       puts "FizzBuzz"
+     elsif i % 5 == 0
+       puts "Buzz"
+     elsif i % 3 == 0
+       puts "Fizz"
+     else
+       puts i
+     end
+   end
