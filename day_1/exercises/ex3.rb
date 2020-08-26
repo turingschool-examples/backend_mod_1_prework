d6d4afb4e8edcba515b9118eb15b4c619071436d
SK-Sam <yeosamuel95@gmail.com>
2020-08-25 21:51:37 -0400

    First 2 exercises completed

---
+++ day_1/exercises/ex3.rb
@@ -0,0 +1,25 @@
+# Numbers and Math
+
+puts "I will now count my chickens:"
+
+puts "Hens #{25 + 30 / 6}" #30
+puts "Roosters #{100 - 25 * 3 % 4}" #97
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 #7
+
+puts "Is it true that 3 + 2 < 5 - 7?" #False
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}" #5
+puts "What is 5 - 7? #{5 - 7}" #-2
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}" #True
+puts "Is it greater or equal? #{5 >= -2}" #true
+puts "Is it less or equal? #{5 <= -2}" #false
