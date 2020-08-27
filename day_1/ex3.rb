defa0c6ead3ab1adf0a370c0ded8a53af8eaefce
Jordan Beck <jordanfbeck@gmail.com>
2020-08-27 13:51:11 -0600

    Initial commit

---
+++ day_1/ex3.rb
@@ -0,0 +1,24 @@
+#count chickens
+puts "I will now count my chickens:"
+#count hens then roosters
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 -25 * 3 % 4}"
+#count eggs
+puts "Now I will count the eggs:"
+#does calculation
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#asks for boolean value
+puts "Is it true that 3 + 2 < 5 - 7?"
+#does calculation
+puts "3 + 2? #{3 + 2}"
+#does calculation
+puts "What is 3 + 2? #{3 + 2}}"
+puts "What is 5 - 7? #{5 - 7}"
+#shows false
+puts "Oh, that's why it's false."
+#asks question
+puts "How about some more."
+#does calculations
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
