136767de5240c0b5a09a1b3d1d493bb122604ec2
Jordan Beck <68141454+jordanfbeck0528@users.noreply.github.com>
2020-08-29 08:19:17 -0600

    Update strings.rb

--- day_1/exercises/strings.rb
+++ day_1/exercises/strings.rb
@@ -6,10 +6,10 @@
 # Example: Write code that prints your name to the terminal:
 p "Alan Turing"
 
-puts "Jordan Frederick Beck"
+p "Jordan Frederick Beck"
 
 # Write code that prints `Welcome to Turing!` to the terminal:
-p #YOUR CODE HERE
+p "Welcome to Turing!"
 
 # Write code that prints `99 bottles of pop on the wall...` to the terminal:
-# YOUR CODE HERE
+p "99 bottles of pop on the wall..."
