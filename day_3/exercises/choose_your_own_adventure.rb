11ece8e797b4e6d86b4b204be0b6586802612b55
Aiden Mendez <aidenmendez@gmail.com>
2020-08-30 15:16:41 -0600

    Update practice choose your own adventure

--- day_3/exercises/choose_your_own_adventure.rb
+++ day_3/exercises/choose_your_own_adventure.rb
@@ -8,15 +8,48 @@ puts "> "
 help = $stdin.gets.chomp
 
 if help == "1"
-    puts "You decide to investigate the source of the light..."
-    # need branch
-end
+    puts "You decide to investigate the source of the light. While you're walking down the road, you suddenly see headlights approaching. What do you do?"
+    puts "1. Wave down the driver."
+    puts "2. Hide in the ditch."
+    
+    puts "> "
+    car = $stdin.gets.chomp
 
-if help == "2"
+    if car == "1"
+        puts "You start frantically waving at the approaching vehicle. It slowly comes to a stop in front of you. You realize it's a tow truck! You explain your car troubles and he gladly offers to tow you to town. The end!"
+
+    elsif car == "2"
+        puts "Being the middle of the night, you decide to play it safe and hide in the ditch. You peek at the oncoming vehicle and realize it was a tow truck. Drats! What do you do?"
+        puts "1. Keep walking to the light in the distance."
+        puts "2. Head back to your car in hopes that the tow truck stopped to investigate."
+        
+        puts "> "
+        direction = $stdin.gets.chomp
+
+        if direction == "1"
+            puts "You decide to continue towards the light. Once you arrive you find a campfire surrounded by a bunch of wolves. They eat you. The end!"
+        
+        elsif direction == "2"
+            puts "You decide to walk back to your car. You get back to where you broke down but your car is gone! The tow truck must've taken it. The end!"
+        end
+    end
+
+elsif help == "2"
     puts "You pull out your phone and realize you have no service and your phone has 5% battery life."
     puts "what do you do next?"
     puts "1. There's a bluff about a half mile into the woods-- maybe the service is better there?"
     puts "2. I've never even changed a tire but I'll peek under the hood."
-    # need branch
+    
+    puts "> "
+    stay = $stdin.gets.chomp
+    if stay == "1"
+        puts "You decide to venture into the woods. You walk for what feels like miles. When you finally get to the bluff, you climb to the top and try to call for help when suddenly your phone dies! Looking around, you realize you can't see your car."
+        puts "You're lost in the woods. The end!"
+    elsif stay == "2"
+        puts "You take a look under the hood to see if you can figure out what the problem is. You mess with some moving parts, turn the key and to your surprise, the car starts! Good job!"
+    end
 end
+
+
+
 
