2488c742995df941ee11ad7e5c57566e23ad0041
Max Ribbans <ribbans.max@gmail.com>
2020-08-31 15:26:35 -0400

    Add day 1

---
+++ day_1/ex3.rb
@@ -0,0 +1,30 @@
+# prints a string
+puts "I will now count my chickens:"
+
+# prints a string and a calculation.
+
+puts "Hens #{25.0 + 30.0 /6.0}"
+# prints a string and a calculation.
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+# prints a string
+puts "Now I will count the eggs:"
+# prints a number
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+# prints a string
+puts "Is it true that 3 + 2 < 5 - 7?"
+# prints a boolean
+puts 3.0 + 2.0 < 5.0 - 7.0
+# prints a string and a calculation.
+puts "What is 3 + 2? #{3.0 + 2.0}"
+# prints a string and a calculation.
+puts "What is 5 - 7? #{5.0 - 7.0}"
+# prints a string
+puts "Oh, that's why it's false."
+# prints a string
+puts "How about some more."
+# prints a string and a boolean
+puts "Is it greater #{5.0 > -2.0}"
+# prints a string and a boolean
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# prints a string and a boolean
+puts "Is it less or equal? #{5.0 <= -2.0}"
---
+++ day_1/ex6.rb
@@ -0,0 +1,31 @@
+# initialize types_of_people
+types_of_people = 10
+# initialize x with types_of_people inside it
+x = "There are #{types_of_people} types of people."
+# initialize binary
+binary = "binary"
+# initialize do_not
+do_not = "don't"
+# initialize y with binary and do_not inside it
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+# initialize hilarious boolean
+hilarious = false
+# initialize joke_evaluation with hilarious inside it
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+# initialize w
+w = "This is the left side of..."
+# initialize e
+e = "a string with a right side."
+
+puts w + e
+
+# adding the string makes a longer string because the sum has both full components...
+# the strings will mostly still work with ' instead of ", except for |puts "I also said: '#{y}'."|
