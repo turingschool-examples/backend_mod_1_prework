e6b756d2c3bca9916c49f3fbab5f2a47199a4a01
SK-Sam <yeosamuel95@gmail.com>
2020-09-02 23:00:22 -0700

    Finished all exercises.

--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -8,21 +8,24 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {zebras: 1, pandas: 3, koalas: 12, monkeys: 25}
 p zoo
 
 # Write code that prints all of the 'keys' of the zoo variable 
 # you created above:
-# YOUR CODE HERE
+p zoo.keys
 
 # Write code that prints all of the 'values' of the zoo variable 
 # you created above:
-# YOUR CODE HERE
+p zoo.values
 
 # Write code that prints the value of the first animal of the zoo variable 
 # you created above:
-# YOUR CODE HERE
+p zoo[:zebras]
+p zoo[zoo.keys[0]]
 
 # Write code that adds an animal to the zoo hash. 
 # Then, print the updated hash:
-# YOUR CODE HERE
+zoo[:giraffe] = 0
+p zoo
+
