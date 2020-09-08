3facdab984b4de9dc7ecd74d3b430a36dea1d2e0
Vanessa Arruda <wanessarruda@gmail.com>
2020-09-08 11:58:29 -0600

    Day 2 Work

--- day_2/exercises/iteration_exercise.rb
+++ day_2/exercises/iteration_exercise.rb
@@ -1,69 +1,79 @@
 # If you had an array of numbers, e.g. [1,2,3,4], how do you print out the doubles of each number? Triples?
 
-# array = [1,2,3,4]
-# array.each { |x| print x * 2}
-# puts
-# array.each { |x| print x * 3}
+array = [1,2,3,4]
+array.each { |x| print x * 2}
+puts
+array.each { |x| print x * 3}
 
 # If you had the same array, how would you only print out the even numbers? What about the odd numbers?
 
-# array = [1,2,3,4]
-#
-# array.each {|x| puts x if x.even?}
+array = [1,2,3,4]
 
-# array = [1,2,3,4]
-#
-# array.each {|x| puts x if x.odd?}
+array.each {|x| puts x if x.even?}
+
+array = [1,2,3,4]
+
+array.each {|x| puts x if x.odd?}
 
 # How could you create a new array which contains each number multipled by 2?
 
-# array = [1,2,3,4]
-#
-# double_array = array.collect {|x| x * 2}
-#
-# print double_array
+array = [1,2,3,4]
+
+double_array = array.collect {|x| x * 2}
+
+print double_array
 
 
 # Given an array of first and last names, e.g. [“Alice Smith”, “Bob Evans”, “Roy Rogers”], how would you print out the full names line by line?
 
-# array = ["Alice Smith", "Bob Evans", "Roy Rogers"]
-#
-# array.each {|x| puts x}
+array = ["Alice Smith", "Bob Evans", "Roy Rogers"]
 
-# How would you print out only the first name?
+array.each {|x| puts x}
 
-# array = ["Alice Smith", "Bob Evans", "Roy Rogers"]
-#
-# array.each do |name|
-#   puts name.split.first
-# end
-
-# How would you print out only the last name?
-#
-# array = ["Alice Smith", "Bob Evans", "Roy Rogers"]
-#
-# array.each do |name|
-#   puts name.split.last
-# end
-
-How could you print out only the initials?
+How would you print out only the first name?
 
 array = ["Alice Smith", "Bob Evans", "Roy Rogers"]
 
-array.each {|name| puts name[0].split}
+array.each do |name|
+  puts name.split.first
+end
 
+# How would you print out only the last name?
+#
+array = ["Alice Smith", "Bob Evans", "Roy Rogers"]
 
+array.each do |name|
+  puts name.split.last
+end
+
+# How could you print out only the initials?
+
+array = ["Alice Smith", "Bob Evans", "Roy Rogers"]
+
+initials = []
+
+array.each {|x| puts x}
+array.map! {|x| x.split" "}
+array.each {|x| print x[0] + " "}
+puts
+array.each {|x| print x[1] + " "}
+puts
+array.each do |each|
+    initials << each.first[0] + each.last[0]
+end
+print initials
+puts
 
 # How can you print out the last name and how many characters are in it?
 
-# array = ["Alice Smith", "Bob Evans", "Roy Rogers"]
-#
-# print array.last
-# puts ""
-# print array.last.length
+array = ["Alice Smith", "Bob Evans", "Roy Rogers"]
+
+print array.last
+puts ""
+print array.last.length
 
 # How can you create an integer which represents the total number of characters in all the names?
 
-# array = ["Alice Smith", "Bob Evans", "Roy Rogers"]
-#
-# puts array.join.length
+array = ["Alice Smith", "Bob Evans", "Roy Rogers"]
+
+puts array.join.length
