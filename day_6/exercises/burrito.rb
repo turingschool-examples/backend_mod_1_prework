83d2ac0043935ad545e641b19d3df5f2e2ed4723
Adam Etzion <adametzion90@gmail.com>
2020-09-08 12:52:24 -0600

    Add Day 6 Work

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -1,15 +1,30 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
 # 3. change_protein
 
 class Burrito
-  attr_reader :protein, :base, :toppings
+  attr_accessor :protein, :base, :toppings
   def initialize(protein, base, toppings)
     @protein  = protein
     @base     = base
     @toppings = toppings
+  end
+
+  def add_topping(topping)
+    @toppings += topping
+    p "You've added #{topping}! Far out. That'll be $2.75 extra..."
+  end
+
+  def remove_topping(topping)
+    @toppings -= topping
+    p "You've removed #{topping}...damn."
+  end
+
+  def change_protein(protein)
+    @protein = protein
+    p "Righteous choice. #{protein} is great for your chi."
   end
 end
 
@@ -17,3 +32,17 @@ dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+
+# How can i get this not to print backslashes?
+dinner.add_topping(['cilantro'])
+p dinner.toppings
+
+#why am I able to do this?
+dinner.remove_topping(['chocolate'])
+p dinner.toppings
+
+dinner.remove_topping(['guacamole'])
+p dinner.toppings
+
+dinner.change_protein('tofu')
+p dinner.protein
--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -1,5 +1,32 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
 # YOUR CODE HERE
+class Person
+
+  attr_reader :name
+  attr_accessor :married
+
+  def initialize(name, married)
+    @name = name
+    @married = married
+  end
+
+  def get_hitched(partner)
+    @married = true
+    p "Congrats #{name} and #{partner}! #{name}, I hope this is the one!"
+
+  end
+
+  def get_ditched(partner)
+    @married = false
+    p "Hey #{name}, forget #{partner}. This is the first day of the rest of your life!"
+  end
+end
+
+gary = Person.new('Gary', 'false')
+
+gary.get_hitched('Barry')
+
+gary.get_ditched('Barry')
