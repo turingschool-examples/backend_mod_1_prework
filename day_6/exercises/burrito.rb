9595f9b33b848d8da79a42d6f210fa3e503e46dc
Isabelle Villasenor <villasenori@outlook.com>
2020-09-08 19:46:06 -0600

    Add Day 6 Work

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -1,19 +1,39 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
 # 3. change_protein
 
 class Burrito
-  attr_reader :protein, :base, :toppings
-  def initialize(protein, base, toppings)
+  attr_reader :protein, :base, :topping
+  def initialize(protein, base, topping)
     @protein  = protein
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(topping)
+    p "#{add_topping} has been added."
+  end
+
+  def remove_topping(topping)
+    p "#{topping} has been removed."
+  end
+
+  def change_protein(protein)
+    @protein += ", " + protein
+    p "Changed main protein to #{protein}."
+  end
+
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
-p dinner.toppings
+p dinner.topping
+dinner.change_protein("Soyfritas")
+dinner.remove_topping("salsa")
+dinner.add_topping("pico")
+p dinner.protein
+p dinner.base
+p dinner.topping
--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -1,5 +1,23 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
-# YOUR CODE HERE
+class Person
+    attr_reader :name, :age
+
+    def initialize(name, age)
+      @name   = name
+      @age    = age
+    end
+
+    def order_drink
+      if @age <= 21
+        puts "No way Jose"
+      end
+      puts "Sure"
+    end
+end
+
+
+Isabelle = Person.new("Isabelle", 25)
+p Isabelle.order_drink
