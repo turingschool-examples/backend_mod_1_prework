e1e32f2598352efd7f52d0876da89b4f3c7c36f2
Vanessa A. (she/her/BE) <69529052+nessaarruda@users.noreply.github.com>
2020-09-21 09:10:46 -0600

    Update if_statements.rb

--- day_3/exercises/if_statements.rb
+++ day_3/exercises/if_statements.rb
@@ -74,20 +74,14 @@ puts num_quarters
 
 def pizza
 
-cups_of_flour = 3
-has_sauce = true
+  cups_of_flour = 3
+  has_sauce = true
 
-if cups_of_flour = 1 && has_sauce = true
-  print "I cannot make pizza."
-elsif cups_of_flour = 5 && has_sauce = false
-  print "I cannot make pizza."
-elsif cups_of_flour = 2 && has_sauce = true
-  print "I can make pizza."
-elsif cups_of_flour = 3 && has_sauce = true
-  print "I can make pizza."
-
-end
-
+  if cups_of_flour >= 2 && has_sauce == true
+    puts "I can make pizza"
+  else
+    puts "I cannot make pizza"
+  end
 end
 
 puts pizza
