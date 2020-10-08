b73ca971a24a8e3e6c95d80e75c8bc25cfadf97d
kylejschulz <kylschulz@gmail.com>
2020-10-07 20:42:26 -0400

    Initial Commit

---
+++ day_1/ex2.rb
@@ -0,0 +1,15 @@
+#this is a practice comment
+puts "I could have code like this."
+puts "This will run."
+
+#if the octathorpe is in quotes it's treated as part of a string
+
+=begin
+This is how you block comment in ruby!
+=end
+
+##
+#This
+#is
+#another
+#way
