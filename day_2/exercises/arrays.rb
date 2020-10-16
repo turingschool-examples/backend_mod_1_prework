296e77dfba14fbc5ff70cff720a79ef0c28c9ddd
Olivia Charles <ocharles92@gmail.com>
2020-10-16 12:21:13 -0400

    Add day 2 work

--- day_2/exercises/arrays.rb
+++ day_2/exercises/arrays.rb
@@ -10,12 +10,13 @@ p animals
 
 # Write code that stores an array of states in a variable,
 # then prints that array:
-states = #YOUR CODE HERE
+states = ["Colorado", "Virginia", "Pennsylvania"]
 p states
 
 # Write code that stores an array of foods in a variable,
 # then prints that array:
-# YOUR CODE HERE
+food = ["Bread", "Milk", "Eggs"]
+p food
 
 # Example: Write code that prints the number of elements
 # in your above array of animals:
@@ -23,18 +24,15 @@ p animals.count
 
 # Write code that prints the number of elements
 # in your above array of foods:
-# YOUR CODE HERE
+p food.count
 
 # Write code that prints "Zebra" from your animals array:
-# YOUR CODE HERE
+p animals[0]
 
 # Write code that prints the last item of your foods array:
-# YOUR CODE HERE
+p food[2]
 
 # Write code that adds "lion" to your animals array
 # and prints the result (Hint- use a method):
-# YOUR CODE HERE
-
-# Write code that removes the last element from your foods array
-# and prints the result (Hint- use a method):
-# YOUR CODE HERE
+animals.push("lion")
+p animals
