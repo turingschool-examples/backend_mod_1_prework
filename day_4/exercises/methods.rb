6a0dadacb1cff9423667222027e8314444a7d744
Olivia Charles <ocharles92@gmail.com>
2020-10-19 23:30:22 -0400

    Add Day 4 Work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -11,17 +11,30 @@ end
 print_name
 
 # Write a method that takes a name as an argument and prints it:
-def print_name(name)
-  # YOUR CODE HERE
+def print_name(print_name)
+  puts "print_name: #{print_name}"
 end
 
 print_name("Albus Dumbledore")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method:
-# YOUR CODE HERE
+def add(a, b)
+  puts "ADDING #{a} + #{b}"
+  return a + b
+end
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
+num = add(17, 5)
+
+puts "number #{num} is great!"
+
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
 # Then call your method:
+
+def string(breakfast, dessert)
+  puts "#{breakfast} are better than #{dessert}!"
+end
+
+string("Bagels", "pie")
--- day_4/questions.md
+++ day_4/questions.md
@@ -1,11 +1,24 @@
 ## Day 4 Questions
 
 1. In your own words, what is the purpose of a method?
+A method is used to create a function that returns a value.
 
 1. Create a method named `hello` that will print `"Sam I am"`.
-
+```Ruby
+def hello_someone
+  puts "Sam I am"
+end
+```
 1. Create a method named `hello_someone` that takes an argument of `name` and prints `"#{name} I am"`.
-
+```Ruby
+def hello_someone(name)
+  puts "#{name} I am."
+end
+hello_someone("Frank")
+```  
 1. How would you call or execute the method that you created above?
+Calling the method with the name I require as the argument
+ex: hello_someone("Frank")
 
 1. What questions do you have about methods in Ruby?
+nothing yet. I have a lot more work to do. 
