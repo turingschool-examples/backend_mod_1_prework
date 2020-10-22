eeed8886ccf0c027413b92bb8f7d6d4e0ab4dc4d
Jacob Arellano <jmm.arellano@gmail.com>
2020-10-22 12:16:07 -0600

    initial commit

---
+++ day_1/ex3.rb
@@ -0,0 +1,24 @@
+# prints a string on the terminal explaining what's about to happen
+puts "I will now count my chickens:"
+#prints Hens and Rooster then results of the calculations in the brackets
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Rooster #{100.00 - 25.0 * 3.0 % 4.0}"
+# prints what the next calculations will be doing
+puts "Now I will count the eggs:"
+# shows that brackets aren't needed for calculations if there aren't strings
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# annoucnes next calculations
+puts "Is it true that 3 + 2 < 5 - 7?"
+# prints a new line with the a true or false depending on the answer
+puts 3 + 2 < 5 - 7
+# prints out the components of last questions and results bc of the brackets
+puts "What is 3 + 2? #{ 3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+# a line explaining the outcome
+puts "Oh, that's why it's false"
+# lets you know more calculations are coming?
+puts "How about some more?"
+# same thing as lines 15, 16S
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater? or equal? #{5 >= -2}"
+puts "Is it less or equal #{5 <= -2}"
