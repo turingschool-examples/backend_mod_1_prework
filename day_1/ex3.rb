7f9afd2fbc7d3fc121a2f7623ea12f6c8a71a647
Khoa Nguyen <KhoaNg323@hotmail.com>
2020-10-22 13:26:57 -0600

    update ex3.rb

--- day_1/ex3.rb
+++ day_1/ex3.rb
@@ -6,7 +6,7 @@ puts "Hens #{25 + 30 / 6}"
 puts "Roosters #{100 - 25 * 3 % 4}"
 # Display the quote
 puts "Now I will count the eggs:"
-# 
+#
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 puts "Is it true that 3 + 2 < 5 - 7?"
