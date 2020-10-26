cd5016a484a0f26da85fea6afff59c333b1320a1
Diana Buffone <diana.pamelaet@gmail.com>
2020-10-26 16:11:00 -0400

    Add Day 6 Work

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -1,5 +1,25 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
-# YOUR CODE HERE
+class Person
+  attr_reader :name, :favorite_game, :age
+  def initialize(name, favorite_game, age)
+    @name = name
+    @favorite_game = favorite_game
+    @age = age
+  end
+
+  def introduction
+    puts "Hola, my name is #{name} and I am #{age} years old."
+  end
+
+  def play
+    puts "Do you play #{favorite_game}?"
+  end
+end
+
+me = Person.new("Diana", "PokemonGo", 32)
+
+p me.introduction
+p me.play
