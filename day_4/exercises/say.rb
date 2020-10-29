0f6eaead999cc52c1389aa66eab7764533d23f65
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-10-28 23:32:27 -0600

    Add Day 4 Work

---
+++ day_4/exercises/say.rb
@@ -0,0 +1,20 @@
+# puts "hello"
+# puts "hi"
+# puts "how are you"
+# puts "I'm fine"
+#
+# def say(words="hello")
+#   puts words + '.'
+# end
+#
+# say()
+# say("hi")
+# say("how are you")
+# say("I'm fine")
+
+a = 5
+def some_method
+  a = 3
+end
+
+puts a 
