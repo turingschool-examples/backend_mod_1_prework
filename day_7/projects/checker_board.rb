902cb93f2246b3231ba84db6fbcafaf3b79a676b
Jacob Arellano <jmm.arellano@gmail.com>
2020-10-28 18:46:30 -0600

    bad update

--- day_7/projects/checker_board.rb
+++ day_7/projects/checker_board.rb
@@ -1,28 +1,54 @@
 class CheckerBoard
-  attr_accessor :size, :board, :x, :o
+  attr_accessor :board, :x, :o, :size
 
   def initialize(size)
-    @size = size
-    @board = ''
-    @x = 'X'
+    @board = []
+    @x = 'x'
     @o = ' '
+    @size = size
   end
 
   def draw_line
-    temp = @size + 1
-    until temp == 0
-      if @board[-1] == @o
-        @board += @x
-      elsif @board[-1] == @x
-        @board += @o
+    counter = 1
+    @board.insert(-1, @x)
+    until counter > @size
+      if @board[-1] == @x
+        @board.insert(-1, @o)
+        elsif @size == counter
+          @board.insert(-1, "\n")
       else
-        @board += @o
+        @board.insert(-1, @x)
       end
-      temp -= 1
+      counter += 1
     end
-
   end
 
+  def draw_line2
+    counter = 1
+    @board.insert(-1, @o)
+    until counter == @size
+      if @board[-1] == @o
+        @board.insert(-1, @x)
+      elsif @size == counter
+        @board.insert(-1, "\n")
+      else
+        @board.insert(-1, @o)
+      end
+      counter += 1
+    end
+  end
+
+  def print_board
+    empty = ''
+    self.draw_line
+    self.draw_line2
+    self.draw_line2
+    @board.each do |line|
+      empty += line
+    end
+  end
 end
 
-board = CheckerBoard.new(8)
+
+chess = CheckerBoard.new(4)
+chess.print_board
