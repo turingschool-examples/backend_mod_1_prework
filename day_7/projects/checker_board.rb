04645e30991e1f3e026fc842e112b1a659dc50f2
Jacob Arellano <jmm.arellano@gmail.com>
2020-10-30 11:16:01 -0600

    refactor

--- day_7/projects/checker_board.rb
+++ day_7/projects/checker_board.rb
@@ -1,16 +1,24 @@
 class CheckerBoard
-  attr_accessor :board, :x, :o, :size
+  attr_accessor :board, :x, :o, :size, :marker
 
   def initialize(size)
     @board = []
     @x = 'x'
     @o = ' '
     @size = size
+    @marker = "1"
+    self.print_board
   end
 
   def draw_line
     counter = 1
-    @board.insert(-1, @x)
+
+    if @marker == "1"
+      @board.insert(-1, @x)
+    elsif @marker == "2"
+      @board.insert(-1, @o)
+    end
+
     until counter == @size
       if @board[-1] == @x
         @board.insert(-1, @o)
@@ -19,46 +27,33 @@ class CheckerBoard
       end
       counter += 1
     end
+
     @board.insert(-1, "\n")
+
+    if @marker == "1"
+      @marker = "2"
+    elsif @marker == "2"
+      @marker = "1"
+    end
   end
 
-  def draw_line2
-    counter = 1
-    @board.insert(-1, @o)
-    until counter == @size
-      if @board[-1] == @o
-        @board.insert(-1, @x)
-      else
-        @board.insert(-1, @o)
-      end
-      counter += 1
-    end
-    @board.insert(-1, "\n")
-  end
 
   def print_board
     empty = ''
-    marker = "1"
     counter = 0
+
     until counter == @size
-      if marker == "1"
-        self.draw_line2
-        counter += 1
-        marker = "2"
-      elsif marker == "2"
-        self.draw_line
-        counter += 1
-        marker = "1"
-      end
+      self.draw_line
+      counter += 1
     end
+
     @board.each do |line|
       empty += line
     end
+
     puts empty
   end
-
 end
 
 
-chess = CheckerBoard.new(6)
-chess.print_board
+chess = CheckerBoard.new(9)
