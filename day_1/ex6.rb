08e0b8e8fb8bcfd55617a74948dd817368f09e0d
Aphilosopher30 <ashafer1@fordham.edu>
2020-10-30 13:54:57 -0600

    Add two files to day_1

---
+++ day_1/ex6.rb
@@ -0,0 +1,34 @@
+# Strings and Text
+#this is a number
+types_of_people = 10
+#this puts that nuber in a stinrg
+x = "There are #{types_of_people} types of people."
+#string
+binary = "binary"
+#string
+do_not = "don't"
+#string with string in it
+y = "Those who know #{binary} and those who #{do_not}."
+
+#printing strings
+puts x
+puts y
+
+#printing strings in more strings
+puts "I said:#{x}."
+puts "I also said: '#{y}'"
+
+#bollion
+hilarious = false
+#bollion in string
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+#printing bollion in string
+puts joke_evaluation
+
+#strings
+w = "This is the left side of..."
+e = "a string with a right side."
+
+#combining strings
+puts w + e
---
+++ day_1/ex7.rb
@@ -0,0 +1,10 @@
+#excercise 11: asking Questions
+
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "so, you're #{age} old, #{height} tall and #{weight} heavy."
