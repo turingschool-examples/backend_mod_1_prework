602c7fb83b2bad616fe61970bcada6ebebfebb5b
Harrison Blake <harrison.r.blake@gmail.com>
2020-10-30 14:02:45 -0600

    Add day 6 work

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -1,4 +1,4 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
@@ -11,9 +11,32 @@ class Burrito
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(topping)
+    self.toppings.push(topping)
+  end
+
+  def remove_topping(r_topping)
+    self.toppings.each do |topping|
+      if topping == r_topping
+        toppings.delete(topping)
+      end
+  end
+
+  def change_protein(protein)
+    @protein = protein
+  end
+end
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+
+dinner.add_topping("Avacado")
+p dinner.toppings
+dinner.remove_topping("salsa")
+p dinner.toppings
+dinner.change_protein("chicken")
+p dinner.protein
--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -1,5 +1,30 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
-# YOUR CODE HERE
+class Person
+  attr_reader :name, :job
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end
+
+  def change_job(job)
+    @job = job
+  end
+
+  def change_name(name)
+    @name = name
+  end
+
+  def info
+    "My name is #{self.name} and I work as a #{self.job}!"
+  end
+end
+
+bob = Person.new("Bob", "Architect")
+
+puts bob.info
+bob.change_job("Programmer")
+bob.change_name("Kevin")
+puts bob.info
