6c27f5425d7fbe769a43f1f28b5e44fd15671f8c
Aphilosopher30 <ashafer1@fordham.edu>
2020-10-31 12:12:38 -0600

    Add day 4 work

---
+++ day_4/exercices.rb
@@ -0,0 +1,12 @@
+# Write a program that prints a greeting message. This program should contain a method called greeting that takes a name as its parameter and returns a string.
+
+def greeting(name)
+  "hello, #{name}"
+end
+
+# Write a program that includes a method called multiply that takes two arguments and returns the product of the two numbers.
+
+def multiply(a, b)
+  print a*b
+  a * b
+end
--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -5,23 +5,37 @@
 
 # Example: Write a method that when called will print your name:
 def print_name
-  p "Severus Snape"
+  p "Andrew Shafer"
 end
 
 print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  p name
 end
 
 print_name("Albus Dumbledore")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method:
 # YOUR CODE HERE
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
+def add_numbers(x, y)
+  puts x + y
+  return x + y
+end
+
+add_numbers(2,2)
+
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
 # Then call your method:
+
+def concatenate_strings(x,y)
+  puts x + y
+  return x + y
+end
+
+concatenate_strings("something, ", "here")
