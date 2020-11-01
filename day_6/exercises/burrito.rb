c4fc04b305f3d6c81fe1cce3172c48a204a6086e
Aphilosopher30 <ashafer1@fordham.edu>
2020-10-31 23:42:32 -0600

    Add day 6 work

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -1,4 +1,4 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
@@ -11,9 +11,26 @@ class Burrito
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(topping)
+    return @protein << topping
+  end
+
+  def remove_topping
+      @protein.pop
+  end
+
+  def change_protein(new_protein)
+    @protein = new_protein
+  end
+
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+
+dinner.remove_topping
+dinner.add_topping("extra-CHEESE")
+dinner.change_protein("pork")
--- day_6/questions.md
+++ day_6/questions.md
@@ -2,12 +2,27 @@
 
 1. In your own words, what is a Class?
 
+it is a category  of things.
+
 1. What is an attribute of a Class?
+
+it is a property that things have
 
 1. What is behavior of a Class?
 
+it is what a member of the class can do.
+
 1. In the space below, create a Dog class with at least 2 attributes and 2 behaviors:
+
+Dog class
+
+Attributes: gender and is_hungry
+Behaviors: eat and reproduce  
 
 1. How do you create an instance of a class?
 
+`Class.new`
+
 1. What questions do you still have about classes in Ruby?
+
+none at the moment.
