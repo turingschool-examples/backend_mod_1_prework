8cac84df8d755b37cd302e39f96c67b1a5e8892c
Kseniya Turing <kseniya.kudelich@gmail.com>
2020-10-31 19:54:08 -0600

    adding day 2 and day 3 excercises and updates

---
+++ day_2/exercises/array_methods.md
@@ -0,0 +1,29 @@
+.sort
+Sorts the array alphabetically or numerically
+
+.first and .last
+Returns first or last value in array
+
+.shuffle
+Mixes all values in array
+
+.at(3)
+Returns value in the third position in array
+
+.count
+Counts amount of values in array
+
+.insert(3, "apple")
+Inserts value(s) "apple" into the third position in array
+
+.pop
+Removes last value from an array
+
+.shift
+Removes first value in an array
+
+.join
+Mash items in array together, combines words and numbers into one with no spaces
+
+.push(" ") or << " "
+Adds an item in an array
--- day_3/exercises/if_statements.rb
+++ day_3/exercises/if_statements.rb
@@ -3,14 +3,14 @@
 # file by entering the following command in your terminal:
 # `ruby day_3/exercises/if_statements.rb`
 
-# Example: Using the weather variable below, write code that decides 
+# Example: Using the weather variable below, write code that decides
 # what you should take with you based on the following conditions:
   # if it is sunny, print "sunscreen"
   # if it is rainy, print "umbrella"
   # if it is snowy, print "coat"
   # if it is icy, print "yak traks"
 
-  weather = 'snowy'
+  weather = 'icy'
 
   if weather == 'sunny'
     p "sunscreen"
@@ -35,21 +35,23 @@
 
 # Right now, the program will print
 # out both "I have enough money for a gumball" and
-# "I don't have enough money for a gumball". Write a 
+# "I don't have enough money for a gumball". Write a
 # conditional statement that prints only one or the other.
 
 # Experiment with manipulating the value held within num_quarters
 # to make sure both conditions can be achieved.
 
-num_quarters = 0
+num_quarters = 4
 
-puts "I have enough money for a gumball"
-puts "I don't have enough money for a gumball"
-
+if num_quarters >= 2
+  puts "I have enough money for a gumball"
+else
+  puts "I don't have enough money for a gumball"
+end
 
 #####################
 # Using the variables defined below, write code that will tell you
-# if you have the ingredients to make a pizza. A pizza requires 
+# if you have the ingredients to make a pizza. A pizza requires
 # at least two cups of flour and sauce.
 
 # You should be able to change the variables to achieve the following outputs:
@@ -63,3 +65,37 @@ puts "I don't have enough money for a gumball"
 
 cups_of_flour = 1
 has_sauce = true
+
+if cups_of_flour >= 2 && has_sause = true
+  puts "I can make pizza!"
+else
+  puts "I cannot make pizza"
+end
+
+
+# Or - drafted based on a bear game
+
+puts "Let's make some pizza. How many cups of flour do you have?"
+
+print "> "
+cups_of_flour = $stdin.gets.chomp
+
+if cups_of_flour >= "2"
+  puts "Awesome! That's a good start. Do you have pizza sause?"
+  puts "1. Yes, I have pizza sause."
+  puts "2. No, I don't have pizza sause."
+
+  print "< "
+  has_sause = $stdin.gets.chomp
+
+  if has_sause == "1"
+    puts "Congratulations, we are making pizza. You have all the ingredients."
+  elsif has_sause == "2"
+    puts "You have to go to the grocery store, lil bud."
+  else
+    puts "What are you saying?"
+  end
+
+else
+  puts "Doesn't look like we are making pizza."
+end
