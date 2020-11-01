b1bf0d0af7c6fcc4f699247de599e86c8f4e9940
Andrew Johnston <av.johnst@gmail.com>
2020-11-01 13:57:45 -0700

    Added fizzbuzz.rb and 10_speckled_frogs.rb

---
+++ day_7/fizzbuzz.rb
@@ -0,0 +1,19 @@
+puts "How many numbers would you like me to run?"
+
+MAX = gets.chomp.to_i
+
+def fizzbuzz
+  (1..MAX).each do |n|
+    if n % 3 == 0
+      p "Fizz"
+    elsif n % 5 == 0
+      p "Buzz"
+    elsif n % 3 == 0 && n % 5 == 0
+      p "FizzBuzz"
+    else
+      p n
+    end
+  end
+end
+
+fizzbuzz
