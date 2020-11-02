7fbbbcfdc81beb8e0e49550f75162a186644b758
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-11-02 14:56:28 -0700

    Add Day 6 Work

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -1,4 +1,4 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
@@ -6,14 +6,30 @@
 
 class Burrito
   attr_reader :protein, :base, :toppings
+
   def initialize(protein, base, toppings)
     @protein  = protein
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(toppings)
+    @toppings.append(toppings)
+  end
+
+  def remove_topping(toppings)
+    @toppings.delete(toppings)
+  end
+
+  def change_protein(protein)
+    @protein = (protein)
+  end
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
+dinner.add_topping("pico")
+dinner.remove_topping("salsa")
+dinner.change_protein("Beef")
 p dinner.protein
 p dinner.base
 p dinner.toppings
--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -1,5 +1,31 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
 # YOUR CODE HERE
+class Person
+  attr_accessor :weight, :iq
+
+  def initialize(weight, iq)
+    @weight = weight
+    @iq = iq
+  end
+
+  def exercise(minutes)
+    @weight -= (minutes * 0.1)
+  end
+
+  def read_a_book(book)
+    @iq += book
+  end
+
+  def whoops_cheesecake(slices)
+    @weight += (slices * 1.2)
+  end
+end
+
+jerry = Person.new(210.0, 110)
+
+puts jerry.exercise(30)
+puts jerry.read_a_book(3)
+puts jerry.whoops_cheesecake(2)
