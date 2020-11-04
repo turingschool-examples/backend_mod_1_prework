44f5868f336f4ecd0c966dc694a3392920a85f16
Brisa <brisagarciaglz@gmail.com>
2020-11-04 14:49:15 -0700

    Add day 2 exercises

--- day_2/exercises/iteration.rb
+++ day_2/exercises/iteration.rb
@@ -15,14 +15,22 @@ end
 # "The <animal> is awesome!" for each animal:
 
 animals.each do |animal|
-  # YOUR CODE HERE
+puts "The #{animal} is awesome."
 end
 
-# Write code that stores an array of foods in a variable, 
+
+
+# Write code that stores an array of foods in a variable,
 # then iterates over that array to print
 # "Add <food> to shopping list" for each food item:
-# YOUR CODE HERE
-
-# Write code that stores an array of numbers in a variable, 
-# then iterates over that array to print doubles of each number: 
-# YOUR CODE HERE
+# I am making chicken quesadillas.  They are the best!
+food = ["chicken". "mushroom", "tortilla", "cheese", "lettuce"]
+foods.each do |product|
+  puts "Add #{product} to shopping list."
+end
+# Write code that stores an array of numbers in a variable,
+# then iterates over that array to print doubles of each number:
+numbers = [1,2,3,4,5,6,7,8,10]
+numbers.each do |num|
+  num x * 2
+end
