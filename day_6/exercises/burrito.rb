8d35b601305cb4e0bf1d6d0c7a07eefe465453b8
Khoa Nguyen <KhoaNg323@hotmail.com>
2020-11-04 00:55:53 -0700

    Add Day 6 Work

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -1,4 +1,4 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
@@ -6,11 +6,25 @@
 
 class Burrito
   attr_reader :protein, :base, :toppings
+
   def initialize(protein, base, toppings)
     @protein  = protein
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping
+    
+  end
+
+  def remove_topping
+
+  end
+
+  def change_protein
+
+  end
+
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -1,5 +1,29 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
-# YOUR CODE HERE
+class Person
+  attr_accessor :tattoos, :hair, :name
+
+  def initialize(hair, name)
+    @tattoos = true
+    @hair = hair
+    @name = name
+  end
+
+  def wear_sweater
+   @tattoos = false
+  end
+
+  def tip_hat
+    p "Hello"
+  end
+end
+
+kevin = Person.new("black", "Kevin")
+p kevin.tattoos
+p kevin.hair
+p kevin.name
+kevin.wear_sweater
+p kevin.wear_sweater
+kevin.tip_hat
