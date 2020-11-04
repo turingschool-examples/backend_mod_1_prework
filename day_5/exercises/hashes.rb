70590c14035d913ebc645b4abe17e565ae723ea4
Brisa <brisagarciaglz@gmail.com>
2020-11-04 15:33:46 -0700

    Add day 5 exercises

--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby day_5/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,21 +8,29 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+# I think i make up my own numnber of animals?
+zoo = {elephants: 7, gorillas: 4, penguins: 22, fish: 300}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
-# you created above:
-# YOUR CODE HERE
+# Write code that prints all of the 'keys' of the zoo variable
+zoo = {elephants: 7, gorillas: 4, penguins: 22, fish: 300}
 
-# Write code that prints all of the 'values' of the zoo variable 
-# you created above:
-# YOUR CODE HERE
+p zoo.keys
 
-# Write code that prints the value of the first animal of the zoo variable 
-# you created above:
-# YOUR CODE HERE
 
-# Write code that adds an animal to the zoo hash. 
+
+# Write code that prints all of the 'values' of the zoo variable
+# you created above:
+zoo = {elephants: 7, gorillas: 4, penguins: 22, fish: 300}
+p zoo.values
+
+# Write code that prints the value of the first animal of the zoo variable
+#zoo = {elephants: 7, gorillas: 4, penguins: 22, fish: 300}
+p zoo.fetch(:elephants)
+
+# Write code that adds an animal to the zoo hash.
+zoo = {elephants: 7, gorillas: 4, penguins: 22, fish: 300}
+zoo["armadillo"] = 1
+p zoo
 # Then, print the updated hash:
 # YOUR CODE HERE
