c1f8a19134c0a8abf16df69466d7fe3b2440e0ea
Alexa Morales <alexamsmyth@gmail.com>
2020-11-04 16:54:34 -0500

    Complete Day 7

--- day_7/ceasar_cipher.rb
+++ day_7/ceasar_cipher.rb
@@ -1,19 +1,45 @@
 ## CAESER SIPHER
-key = [*'A'..'Z', ' ']
-cipher = [*'D'..'Z', *'A'..'C', ' ']
 
-cipher_key = Hash[key.zip cipher]
-p cipher_key
+print "Enter your encryption level (1-25): "
+encription = (gets.to_i) + 1
 
-print "Enter your message: "
-message = gets.chomp
+# print "Enter your message: "
+# message = gets.chomp
 
-case_message = message.upcase
-case_message
 
-cipher_message = case_message.split("")
-cipher_message
+alphabet = [*'A'..'Z']
+numbers = [*1..26]
+base_key = Hash[alphabet.zip numbers]
+base_key
 
-encrypted = cipher_message.map {|key| cipher_key[key]}
+if encription <= 26
+  e_key = base_key.key(encription)
+  f_key = base_key.key((encription - 1))
 
-p encrypted_message = encrypted.join()
+  key = [*'A'..'Z', ' ']
+  cipher = [*(e_key..'Z'), *('A'..f_key), ' ']
+
+  cipher_key = Hash[key.zip cipher]
+
+  print "Enter your message: "
+  message = gets.chomp
+
+  p encrypted = (message.upcase.chars.map {|key| cipher_key[key]}).join()
+elsif encription == 27 || encription == 0
+  print "This will not encrypt your message. Please reenter a value between 1 and 25: "
+  try2 = (gets.to_i) + 1
+  e_key = base_key.key(try2)
+  f_key = base_key.key((try2 - 1))
+
+  key = [*'A'..'Z', ' ']
+  cipher = [*(e_key..'Z'), *('A'..f_key), ' ']
+
+  cipher_key = Hash[key.zip cipher]
+
+  print "Enter your message: "
+  message = gets.chomp
+
+  p encrypted = (message.upcase.chars.map {|key| cipher_key[key]}).join()
+else
+  "That entry is invalid. Program ending."
+end
