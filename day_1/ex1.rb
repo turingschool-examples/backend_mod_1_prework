4af6acf9476cb7f8d46f84dcec9916eb235245df
Brisa <brisagarciaglz@gmail.com>
2020-11-04 14:25:04 -0700

    add exercise 1)

--- day_1/ex1.rb
+++ day_1/ex1.rb
@@ -5,3 +5,12 @@ puts "This is fun."
 puts "Yay! Printing."
 puts "I'd rather you 'not'"
 puts 'I "said" do not touch this'
+
+
+#Study Drills
+
+puts "print another line"
+
+# a pound is for Comments
+# anything after teh pound is ignored by ruby
+# puts "this is the pound in action"
