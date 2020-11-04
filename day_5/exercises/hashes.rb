f972030bfe573a2ee8aaecd79fbfb5ff6cece333
Tanner Creque <tanner.creque@gmail.com>
2020-11-04 09:33:10 -0700

    Finish Day 5

--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -1,28 +1,30 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby day_5/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
 foods = {apples: 23, grapes: 507, eggs: 48}
-p foods
+puts "there are #{foods} in the inventory."
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {zebras: 23, lions: 56, tigers: 213}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
-# you created above:
-# YOUR CODE HERE
+# Write code that prints all of the 'keys' of the zoo variable
+# you created above:??
+zoo.keys
 
-# Write code that prints all of the 'values' of the zoo variable 
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+zoo.values
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
-# YOUR CODE HERE
+zoo["zebras"]
 
-# Write code that adds an animal to the zoo hash. 
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+zoo{bears}
+
+p zoo
