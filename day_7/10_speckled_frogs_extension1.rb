8e100e7c813bf87db3b003bd0898860c3d720300
joemecha <jwmecha@gmail.com>
2020-11-07 20:58:44 -0500

    Complete 10 Speckled Frogs exercise and extensions

---
+++ day_7/10_speckled_frogs_extension1.rb
@@ -0,0 +1,29 @@
+# Prints nursery rhyme based on starting number of frogs
+# Uses gem "to_words"
+
+# gem install 'to_words' # Un-comment out line 4 and comment out line 5 if gem install needed
+require 'to_words'
+
+def ten_speckled_frogs(frog_qty)
+  loop do
+    if frog_qty > 2
+      puts "#{frog_qty.to_words.capitalize} speckled frogs sat on a log eating some most delicious bugs.
+One jumped in the pool where it's nice and cool, then there were #{(frog_qty - 1).to_words} speckled frogs.\n"
+      puts ""
+      frog_qty -= 1
+    elsif frog_qty == 2
+      puts puts "#{frog_qty.to_words.capitalize} speckled frogs sat on a log eating some most delicious bugs.\n
+One jumped in the pool where it's nice and cool, then there was #{(frog_qty - 1).to_words} speckled frog!"
+      puts ""
+      frog_qty -= 1
+    elsif frog_qty == 1
+      puts puts "#{frog_qty.to_words.capitalize} speckled frog sat on a log eating some most delicious bugs.\n
+One jumped in the pool where it's nice and cool, then there were no more speckled frogs!"
+      frog_qty -= 1
+    else
+      break
+    end
+  end
+end
+
+ten_speckled_frogs(10)
