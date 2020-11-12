5c899b6b43dca4508facd484b916ccc36ef585e1
Katy La Tour <klatour324@gmail.com>
2020-11-12 14:46:31 -0600

    Refactor number method

--- day_7/projects/fizzbuzz.rb
+++ day_7/projects/fizzbuzz.rb
@@ -1,7 +1,8 @@
 # This is my solution for "FizzBuzz" when given the range 1 to 100:
 number = 1
 number.upto(100) do |num|
-  if num % 3 == 0 && num % 5 == 0 #Have to lead with this conditional otherwise "FizzBuzz" will not print to the console for 15
+  # Have to lead with this conditional otherwise "FizzBuzz" will not print to the console for 15
+  if num % 3 == 0 && num % 5 == 0
     puts "FizzBuzz"
   elsif num % 3 == 0
     puts "Fizz"
@@ -14,20 +15,28 @@ end
 
 # *** Bonus ***
 
-#Solution one passes in any value for i to number method and runs "FizzBuzz" accordingly:
+# Solution one passes in any value for max to number method and runs "FizzBuzz" accordingly:
 def number(max)
   1.upto(max) do |num|
-    str = "" #Looking at the problem from a different frame of reference and focusing on the output first as opposed to the input
-    str += "Fizz" if num % 3 == 0
-    str += "Buzz" if num % 5 == 0
-    str = num if str.empty?
+  # Looking at the problem from a different frame of reference and focusing on the output first (str) as opposed to the input (if/else block)
+    str = ""
+    
+    if num % 3 == 0 && num % 5 == 0
+      str += "FizzBuzz"
+    elsif num % 3 == 0
+      str += "Fizz"
+    elsif num % 5 == 0
+      str += "Buzz"
+    else
+      str = num if str.empty?
+    end
     puts str
   end
 end
 
 number(150)
 
-#Solution two prompts user for input via the command line and runs "FizzBuzz" for any range of numbers given by the user:
+# Solution two prompts user for input via the command line and runs "FizzBuzz" for any range of numbers given by the user:
 class FizzBuzz
   attr_accessor :min, :max
 
@@ -49,9 +58,16 @@ class FizzBuzz
   def start
     min.upto(max) do |num|
       str = ""
-      str += "Fizz" if num % 3 == 0
-      str += "Buzz" if num % 5 == 0
-      str = num if str.empty?
+
+      if num % 3 == 0 && num % 5 == 0
+        str += "FizzBuzz"
+      elsif num % 3 == 0
+        str += "Fizz"
+      elsif num % 5 == 0
+        str += "Buzz"
+      else
+        str = num if str.empty?
+      end
       puts str
     end
   end
