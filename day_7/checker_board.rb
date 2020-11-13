b8336c1d502d320b9fa622db4919007e67127287
Wil McCauley <wmccauley724@gmail.com>
2020-11-13 11:30:48 -0700

    Continue work on checkerboard solution.

---
+++ day_7/checker_board.rb
@@ -0,0 +1,38 @@
+# try reverse to polarize alternate lines?
+
+def make_board(size)
+
+  fill_array = ["X", " "]
+
+  row_counter = 1
+  col_counter = size
+  while row_counter <= size
+
+    row_str = ""
+    if row_counter % 2 == 0
+
+      row_str += fill_array[1]
+
+      if
+
+      #col_counter.times do
+      #  row_str += fill_array[row_counter % 2]
+      #end
+
+
+    # counter += 1
+    # puts row_str
+
+  end
+
+end
+
+
+
+
+
+fill_array[counter % 2]
+
+
+def build_row(col_count)
+  row_str +=
