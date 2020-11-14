1fcf4367b16719d8d89b70cb656ce3f40ac4d4a9
Wil McCauley <wmccauley724@gmail.com>
2020-11-14 12:11:04 -0700

    Add user input functionality, add comments, clean up output code.

--- day_7/checker_board.rb
+++ day_7/checker_board.rb
@@ -1,27 +1,36 @@
-# try reverse to polarize alternate lines?
-# build into arrays and then to_s
+# define checkerboard method taking user input
 
-def make_board(size)
+def make_board
 
+  # get a board size from user, and convert string to integer
+  puts "Please enter desired board size: "
+  size = gets.to_i
+
+  # declare initial arrays for row building
   out_array = []
   alt_array = []
-  counter = 1
+  #initialize counter to define row length
+  col_counter = 1
 
-  while counter <= size do
-    if counter % 2 == 0
+  # build a row of user defined length, and also store a reversed copy for alternation
+  while col_counter <= size do
+    if col_counter % 2 == 0
        out_array << " "
        alt_array << "X"
     else
       out_array << "X"
       alt_array << " "
     end
-    counter += 1
+    col_counter += 1
   end
 
+  # turn row arrays into strings for output
   out_row = out_array.join
   alt_row = alt_array.join
+  # initialize counter to make desired number of rows
   row_counter = 1
-  
+
+  # assemble and print a defined number of rows to create board, with alternating rows
   while row_counter <= size do
     if row_counter % 2 == 0
       puts alt_row
@@ -33,4 +42,4 @@ def make_board(size)
 end
 
 
-make_board(7)
+make_board
