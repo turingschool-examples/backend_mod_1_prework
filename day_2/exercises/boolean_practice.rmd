d31f310bd98a88588608400477008424ff962fa5
Joey Haas <josephthaas@gmail.comm>
2020-12-20 14:12:09 -0700

    Add Day 2 Work

---
+++ day_2/exercises/boolean_practice.rmd
@@ -0,0 +1,13 @@
+## Study Drills from Ex 28
+
+* Study Drill 1: <,>, <=,>=, !<,!>,!=,==
+* Study Drill 2:
+ * <: Less than
+ * >: Greater than
+ * <=: Less than or equal to
+ * >=: Greater than or equal to
+ * !<: Not less than
+ * !>: Not greater than
+ * !=: Not equal to
+ * ==: equal to
+ * 
---
+++ day_2/exercises/turings_iteration_and_each.rb
@@ -0,0 +1,35 @@
+# a = [1,2,3,4]
+# a.each do |number|
+#  2.times {puts number}
+# end
+
+# a = [1,2,3,4]
+# a.each do |number|
+#  3.times {puts number}
+# end
+
+# a = [1,2,3,4]
+# a.each do |number|
+  # p number if number.to_f%2==0
+# end
+
+# a = [1,2,3,4]
+# a.each do |number|
+  # p number if number.to_f%2==1
+# end
+
+# a = [1,2,3,4]
+# a.each do |number|
+#  p number * 2
+#end
+
+#a = ["Alice Smith", "Bob Evans", "Roy Rogers"]
+#a.each do |names|
+#  p names
+#end
+
+a = ["Alice Smith", "Bob Evans", "Roy Rogers"]
+a.each do |names|
+
+  p names
+end
