c237c897f8cb1206aa10db9de6703b3992dece3f
Joanna Cortes <jococopo@gmail.com>
2020-12-20 17:57:39 -0700

    Complete ex6

---
+++ day_1/ex6.rb
@@ -0,0 +1,34 @@
+# define variable, with integer (0 string inside string)
+types_of_people = 10
+# define variable, string with interpolation (0)
+x = "There are #{types_of_people} types of people."
+# define varible with string (0)
+binary = "binary"
+# define varible with string (0)
+do_not = "don't"
+# define variable, string with interpolation  (2)
+y = "Those who know #{binary} and those who #{do_not}."
+
+# print string with interpolation via varible
+puts x
+# print string with interpolation via varible
+puts y
+
+# print string with interpolation (1)
+puts "I said: #{x}."
+# print string with interpolation (1)
+puts "I also said: '#{y}'."
+
+# define variable, boolean
+hilarious = false
+# define avriable, string w/ interpolation (1)
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+# print string via variable
+puts joke_evaluation
+
+# define variable, string
+ w = "This is the left side of..."
+# define variable, string
+ e = "a string with a right side."
+# print and connect strings (2)
+ puts w + e
