a078c3eb1a10cc03cc6104246b8ffdbaeb6004c2
Joey Haas <josephthaas@gmail.comm>
2020-12-21 22:41:09 -0700

    Add Day 4 Work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -12,16 +12,24 @@ print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  p "#{name}"
 end
 
 print_name("Albus Dumbledore")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method:
-# YOUR CODE HERE
+def sum(a, b)
+  puts a + b
+end
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
+sum(1, 2)
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
 # Then call your method:
+def conc(a, b)
+  puts "To practice method creation i shall concatenatinate two words into one string, #{a} and #{b} voila. Do i pass?"
+end
+
+conc("turing","Is cool")
