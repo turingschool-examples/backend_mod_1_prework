c4986670d7b5e6814c04f6052f9bb3fed3d99bb8
Joanna Cortes <jococopo@gmail.com>
2020-12-24 08:13:49 -0700

    Complete ex6 study drills

--- day_1/ex6.rb
+++ day_1/ex6.rb
@@ -6,7 +6,7 @@ x = "There are #{types_of_people} types of people."
 binary = "binary"
 # define varible with string (0)
 do_not = "don't"
-# define variable, string with interpolation  (2)
+# define variable, string with interpolation  (1)
 y = "Those who know #{binary} and those who #{do_not}."
 
 # print string with interpolation via varible
@@ -21,7 +21,7 @@ puts "I also said: '#{y}'."
 
 # define variable, boolean
 hilarious = false
-# define avriable, string w/ interpolation (1)
+# define avriable, string w/ interpolation (0)
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 # print string via variable
 puts joke_evaluation
@@ -30,5 +30,10 @@ puts joke_evaluation
  w = "This is the left side of..."
 # define variable, string
  e = "a string with a right side."
-# print and connect strings (2)
+# print and connect strings (1)
  puts w + e
+
+# Find all the places where a string is put inside a string: done above
+# Are you sure there are only four places? How do you know? Maybe I like lying: It appears that there are 4 lines with stings inside of strings, but two of of those lines have 2 strings inside the larger string.
+# Explain why adding the two strings w and e with + makes a longer string: Because it puts two shorter strings together.
+# What happens when you change the strings to use ' (single-quote) instead of " (double-quote)? Do they still work? Try to guess why. It does appear to work, as both types of quotes indicate strings.
