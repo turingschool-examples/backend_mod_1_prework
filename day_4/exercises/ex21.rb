2dea3615e0b6c946f0f1a73084abfb982c155e92
Wyatt Wicks <wyattwicks@gmail.com>
2021-01-02 16:56:21 -0700

    Add Day 4 work

---
+++ day_4/exercises/ex21.rb
@@ -0,0 +1,46 @@
+def add(a, b)
+  puts "ADDING #{a} + #{b}"
+  a + b
+end
+
+def subtract(a, b)
+  puts "SUBTRACTING #{a} - #{b}"
+  return a - b
+end
+
+def multiply(a, b)
+  puts "MULTIPLYING #{a} * #{b}"
+  return a * b
+end
+
+def divide(a, b)
+  puts "DIVIDING #{a} / #{b}"
+  return a / b
+end
+
+
+puts "Let's do some math with just functions!"
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+
+# A puzzle for the extra credit, type it in anyway.
+puts "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(add(weight, 3), divide(iq, 2))))
+
+puts "That becomes: #{what}. Can you do it by hand?"
+
+answer = (43 + 6 + 2) * 4
+
+def form(a,b,c,d)
+  def first_part(a,b,c)
+    return a + b + c
+  end
+  return first_part * d
+end
---
+++ day_4/exercises/return.rb
@@ -0,0 +1,6 @@
+def add_three(number)
+  return number + 3
+end
+
+returned_value = add_three(4)
+puts returned_value
