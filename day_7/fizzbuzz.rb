a943fbac5475a12d2842c83223df506b746857b3
ignored-comment <info.konham@gmail.com>
2021-01-02 20:06:33 -0500

    Add Day 7 Work

--- day_7/fizzbuzz.rb
+++ day_7/fizzbuzz.rb
@@ -1,17 +1,26 @@
 # FizzBuzz
 def fizzbuzz (low, high)
-  while low <= high
+  while low < high
     if low % 15 === 0
-      puts "FizzBuzz"
+      print "FizzBuzz, "
     elsif low % 5 === 0
-      puts "Buzz"
+      print "Buzz, "
     elsif low % 3 === 0
-      puts "Fizz"
+      print "Fizz, "
     else
-      puts low
+      print "#{low}, "
     end
     low += 1
   end
+  if high % 15 === 0
+    print "FizzBuzz"
+  elsif high % 5 === 0
+    print "Buzz"
+  elsif high % 3 === 0
+    print "Fizz"
+  else
+    print high
+  end
 end
 
-fizzbuzz(25, 100)
+fizzbuzz(1, 999)
