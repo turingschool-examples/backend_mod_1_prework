e8bf61980961896039c50bd497b90915a4d24b48
Katherine McConnell <mcconnell.katherine01@gmail.com>
2021-01-03 22:05:56 -0700

    Add Day 2 Work

---
+++ day_2/exercises/ex28.rb
@@ -0,0 +1,96 @@
+# Exercise 28: Boolean Practice
+
+ true && true
+ # My guess: true
+ # The Answer: true
+
+false && true
+# My guess: false
+# The Answer: false
+
+1 == 1 && 2 == 1
+# My guess: true
+# The Answer: true
+
+"test" == "test"
+# My guess: true
+# The Answer: true
+
+1 == 1 || 2 != 1
+# My guess: false
+# The Answer: true
+
+true && 1 == 1
+# My guess: false
+# The Answer: false
+
+false && 0 != 0
+# My guess: false
+# The Answer: false
+
+true || 1 == 1
+# My guess: true
+# The Answer: true
+
+"test" == "testing"
+# My guess: false
+# The Answer: false
+
+1 != 0 && 2 == 1
+# My guess: false
+# The Answer: false
+
+"test" != "testing"
+# My guess: true
+# The Answer: true
+
+"test" == 1
+# My guess: false
+# The Answer: false
+
+!(true && false)
+# My guess: nil
+# The Answer: true
+
+!(1 == 1 && 0 != 1)
+# My guess: false
+# The Answer: false
+
+!(10 == 1 || 1000 == 1000)
+# My guess: ????
+# The Answer: false
+
+!(1 != 10 || 3 == 4)
+# My guess: false
+# The Answer: false
+
+!("testing" == "testing" && "Zed" == "Cool Guy")
+# My guess: true
+# The Answer: true
+
+1 == 1 && (!("testing" == 1 || 1 == 0))
+# My guess: true
+# The Answer: true
+
+"chunky" == "bacon" && (!(3 == 4 || 3 == 3))
+# My guess: true
+# The Answer: false
+
+3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))
+# My guess: false
+# The Answer: false
+
+# Study_Drills
+
+#1. < > <+ <- <= ......
+
+#2. != "not equal" reference https://www.tutorialspoint.com/ruby/ruby_operators.html
+
+#3. Practice Complete
+
+#4. N/A
+
+
+#Student_Questions
+
+#1, #2, #3 READ
--- day_2/exercises/iteration.rb
+++ day_2/exercises/iteration.rb
@@ -15,14 +15,20 @@ end
 # "The <animal> is awesome!" for each animal:
 
 animals.each do |animal|
-  # YOUR CODE HERE
+  p "The #{animal} is awesome!"
 end
 
-# Write code that stores an array of foods in a variable, 
+# Write code that stores an array of foods in a variable,
 # then iterates over that array to print
 # "Add <food> to shopping list" for each food item:
-# YOUR CODE HERE
+out_of = ["egg whites", "bread", "chicken", "almond milk"]
+  out_of.each do |item|
+    puts "Add #{item} to shopping list"
+  end
 
-# Write code that stores an array of numbers in a variable, 
-# then iterates over that array to print doubles of each number: 
-# YOUR CODE HERE
+# Write code that stores an array of numbers in a variable,
+# then iterates over that array to print doubles of each number:
+numbers = [5,7,3,4,1]
+  numbers.each do |age|
+    2.times {print age}
+  end
