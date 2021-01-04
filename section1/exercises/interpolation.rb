97a95d21e053effc37eb0881bd40088cf31a99c8
Eric Weissman <eric.weissman21@gmail.com>
2021-01-04 09:30:44 -0700

    Create section1 formatting

---
+++ section1/exercises/interpolation.rb
@@ -0,0 +1,25 @@
+# In the below exercises, write code that achieves
+# the desired result. To check your work, run this
+# file by entering the following command in your terminal:
+# `ruby day_1/exercises/interpolation.rb`
+
+# Example: Write code that uses the variables below to form a string that reads
+# "The Chudley Cannons are Ron's favorite Quidditch team":
+name = "Ron"
+team = "Chudley Cannons"
+
+p "The #{team} are #{name}'s favorite Quidditch team"
+
+# Write code that uses the variables below to form a string that reads
+# "The quick red fox jumped over the lazy brown dog":
+speedy = "quick red fox"
+slow_poke = "lazy brown dog"
+
+p # YOUR CODE HERE
+
+# Write code that uses the variables below to form a string that reads
+# "In a predictable result, the tortoise beat the hare!":
+slow_poke = "tortoise"
+speedy = "hare"
+
+# YOUR CODE HERE
---
+++ section1/exercises/numbers.rb
@@ -0,0 +1,16 @@
+# In the below exercises, write code that achieves
+# the desired result. To check your work, run this
+# file by entering the following command in your terminal:
+# `ruby day_1/exercises/numbers.rb`
+
+# Example: Write code that prints the result of the sum of 2 and 2:
+p 2 + 2
+
+# Write code that prints the result of 7 subtracted from 83:
+p #YOUR CODE HERE
+
+# Write code that prints the result of 6 multiplied by 53:
+# YOUR CODE HERE
+
+# Write code that prints the result of the modulo of 10 into 54:
+# YOUR CODE HERE
