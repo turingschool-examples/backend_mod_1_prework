1aa17f84ac3f41121f596d5bd409c83aacd13f5a
joepeterson <>
2021-01-05 11:09:55 -0700

    Add Day 7 Work

--- day_7/fizzbuzz.rb
+++ day_7/fizzbuzz.rb
@@ -1,32 +1,11 @@
-#(1..100).each do |num|
-#  if num % 3 == 0 and num % 5 == 0
-#    p "FizzBuzz"
-#  elsif num % 3 == 0
-#    p "Fizz"
-#  elsif num % 5 == 0
-#    p "Buzz"
-#  else
-#    p num
-#  end
-#end
-
-
-
-
-#Bonus
-
-#puts "Please enter the range of numbers you would like to FizzBuzz!"
-
-#print "> "
-#nums = $stdin.gets.chomp
-#nums.each do |num|
-#  if num % 3 == 0 and num % 5 == 0
-#    p "FizzBuzz"
-#  elsif num % "3" == 0
-#    p "Fizz"
-#  elsif num % 5 == 0
-#    p "Buzz"
-#  else
-#    p num
-#  end
-#end
+(1..100).each do |num|
+  if num % 3 == 0 and num % 5 == 0
+    p "FizzBuzz"
+  elsif num % 3 == 0
+    p "Fizz"
+  elsif num % 5 == 0
+    p "Buzz"
+  else
+    p num
+  end
+end
