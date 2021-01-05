4f3896c9f1e5356890c6864676af8bd7b3f8d479
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-01-05 09:25:00 -0700

    Add day 1

--- day_1/ex4.rb
+++ day_1/ex4.rb
@@ -16,7 +16,7 @@ carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
 
-puts "There are #{cars} cars available."
+puts "There are #{cars} cars available!"
 puts "There are only #{drivers} drivers available."
 puts "There will be #{cars_not_driven} empty cars today."
 puts "We can transport #{carpool_capacity} people today."
