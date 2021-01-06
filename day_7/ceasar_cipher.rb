c1b8f21c7a1e17af67974de99669b91b9a1bb17f
JoannaCoPo <>
2021-01-06 14:08:29 -0700

    Add Day 7 Work

--- day_7/ceasar_cipher.rb
+++ day_7/ceasar_cipher.rb
@@ -8,8 +8,8 @@ class Caeser
 
   def encode(string, key)
   string_to_ascii_array = string.chars.map {|char| char.ord}
-  shifted = string_to_ascii_array.map {|char| char+key}
-  shifted.map { |char| char.chr }.join
+  shift = string_to_ascii_array.map {|char| char+key}
+  shift.map { |char| char.chr }.join
   end
 end
 
