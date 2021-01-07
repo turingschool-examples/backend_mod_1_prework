b50541ea6f2e6da9838081e69aeab5bb47b99c62
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-01-06 16:38:01 -0800

    Update Day 5 hashes.rb

--- day_5/exercises/hashes.rb
+++ day_5/exercises/hashes.rb
@@ -10,7 +10,7 @@ p foods
 # Write code that prints a hash holding zoo animal inventory:
 # zoo = {
 #   'penguin' => 13,  # I wrote my code this way at first because I could not
-#   'anteater' => 3,  # get my individual value to pring out. I looked back at
+#   'anteater' => 3,  # get my individual value to print out. I looked back at
 #   'giraffe' => 6,   # the reading material and found that I needed the ':'
 #   'sloth' => 4,     # in order to call the single value 'penguin'. I also
 #   'meerkat' => 35   # found that it doesn't matter if the keys & values are
@@ -37,9 +37,10 @@ end
 
 # Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
-zoo["tortoise"] = 2
+zoo[:tortoise] = 2
   puts zoo
 
 # Here I am unable to get a ':' to print in front of the
 # tortoise key. I tried to put just zoo[tortose:] = 2
-# as well as zoo.tortoise: 2 and was unable to get that consistency.          
+# as well as zoo.tortoise: 2 and was unable to get that consistency.
+# Hahaha, I just figured it out!!!!
