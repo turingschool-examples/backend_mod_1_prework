564d8f4db4b671ffa8f340ed5e56da55a51f0fd0
Eric Weissman <eric.weissman21@gmail.com>
2021-01-18 13:10:55 -0700

    Adjust section 2 exercises

--- section2/exercises/methods.rb
+++ section2/exercises/methods.rb
@@ -18,10 +18,24 @@ end
 print_name("Albus Dumbledore")
 
 # Write a method that takes in 2 numbers as arguments and prints 
-# their sum. Then call your method:
+# their sum. Then call your method three times with different arguments passed in:
 # YOUR CODE HERE
 
 # Write a method that takes in two strings as arguments and prints 
 # a concatenation of those two strings. Example: The arguments could be 
 # (man, woman) and the end result might output: "When Harry Met Sally".  
-# Then call your method:
+# Then call your method three times with different arguments passed in. 
+
+
+#-------------------
+# PART 3: Naming is Hard
+#-------------------
+
+
+# Naming is notoriously hard in programming. It is a skill to name a variable or function concisely enough that it is reasonable to type, but descriptive enough that others can infer the meaning.
+
+# Look at the code you wrote for the previous YOU DO🎈 - what did you name the function, and why?
+# What did you name each parameter, and why?
+
+# EXPLAIN:
+
