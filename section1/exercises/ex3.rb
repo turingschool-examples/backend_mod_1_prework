6f7efe33656285d6d32dacb43e7a076911519dc2
Micha Bernhard <mib70@pitt.edu>
2021-08-05 17:29:10 -0600

    Move files to exercises folder

---
+++ section1/exercises/ex3.rb
@@ -0,0 +1,47 @@
+# + plus
+# - minus
+# / slash
+# * asterisk
+# % percent
+# < less-than
+# > greater-than
+# <= less-than-or-equal
+# >= greater-than-or-equal
+
+# This line prints the message in quotations
+puts "I will now count my chickens"
+
+# This line prints Hens and then the calculation
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# This line prints Roosters and then the calculation
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# This line prints the message in quotations
+puts "Now I will count the eggs:"
+
+# This line prints the result of the calculation
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# This line prints the message in quotations
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+# This line prints the result of the calculation
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# This line prints "What is 3 + 2" and then the result
+puts "What is 3 + 2? #{3.0 + 2.0}"
+# This line prints "What is 5 - 7" and then the result
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+# This line prints the message
+puts "Oh, that's why it's false."
+
+# This line prints the message
+puts "How about some more."
+
+# This line prints the message and then the result of the computation
+puts "Is it greater? #{5.0 > -2.0}"
+# This line prints the message and then the result of the computation
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# This line prints the message and then the result of the computation
+puts "Is it less or equal? #{5.0  <= -2.0}"
