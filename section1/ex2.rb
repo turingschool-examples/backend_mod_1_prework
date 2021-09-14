3c6a8d8d8e70275db49eeac7c3967b9c0c4a8ecc
Eric Chon <echon92@gmail.com>
2021-09-14 16:08:56 -0600

    Exersice two about octothorpes

---
+++ section1/ex2.rb
@@ -0,0 +1,9 @@
+# A comment this is so you can read your program later.
+# Anything after the # is ignored by ruby.
+
+ puts "I could have code like this." # and the comment after is ignored
+
+ # You can also use aq comment to "disable" or comment out a piece of code:
+ #puts "this wont run"
+
+ puts "this will run." 
