50382162ac581d5d37f813b083abef09f6609fb6
Eric Chon <echon92@gmail.com>
2021-09-21 17:20:15 -0600

    Completed section3 examples

---
+++ section3/ex34.rb
@@ -0,0 +1,29 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+#
+# 1. The animal at 1.
+# 2. The third animal (3rd) animal.
+# 3. The first (1st) animal.
+# 4. The animal at 3.
+# 5. The fifth (5th) animal.
+# 6. The animal at 2.
+# 7. The sixth (6th) animal.
+# 8. The animal at 4.
+#
+# 1. The animal at 1 is the second animal and is a ruby.
+#   The second animal is at 1 and is a ruby.
+# 2. The third animal is at 2 and is a peacock.
+#   The animal at 2 is the third animal and is a peacock.
+# 3. The first animal is at 0 and is a bear.
+#   The animal at 0 is the first animal and is a bear.
+# 4. The animal at 3 is the fourth animal and is a kangaroo.
+#   The fourth animal is at 3 and is a kangaroo.
+# 5. The fifth animal is at 4 and is a whale.
+#   The animal at 4 is the fifth animal and is a whale.
+# 6. The animal at 2 is the third animal and is a peacock.
+#  The third animal is at 2 and is a peakcock.
+# 7. The sixth animal is at 5 and is a platypus
+#   The animal at 5 is the sixth animal and is a platypus.
+# 8. The animal at 4 is the fifth animal and is a whale.
+#   The fifrth animal in at 4 and is a whale.
+
+  print "#{animals[5]}"
