9b256ff492ffd357cc2aa347e5e4e8062d095955
Eric Chon <echon92@gmail.com>
2021-09-21 17:23:10 -0600

    Completed arrays and hashes practice

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -19,46 +19,55 @@ print animals
 
 # EXAMPLE: Write code below that will print "Zebra" from the animals array
 # YOUR CODE HERE
-print animals[0]
+print animals[0] + ' '
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
-
+puts "Number of animals: #{animals.count}"
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
-
-
+animals[2] = "Gorilla"
+ p animals
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals[3] = "Dog"
 
 # YOU DO: Write code that will print the String "Elephant" in the animals array
-
+animals[4] = "Elephant"
+puts animals[4] + "."
 
 #-------------------
 # PART 2: Foods: Array Methods
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+foods = %w{Apple Bread Steak Fish}
+print foods
 
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
-
+puts foods.length
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
-
+foods[4] = "broccoli"
+puts foods[4]
 
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
+foods.pop
+print foods
 
-
-# YOU DO: Write code to add 3 new foods to the array. 
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
+foods << "Onions"
+foods << "Waffles"
+foods << "Peaches"
 
+print foods
 # YOU DO: Remove the food that is in index position 0.
+foods.shift
 
 #-------------------
 # PART 3: Where are Arrays used?
@@ -77,11 +86,9 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
-# 1:
-# 2:
-# 3:
-
-
+# 1: Youtube, list of suggested videos
+# 2: Youtube, list of videos watched (history)
+# 3: discord, list of users online
--- section3/exercises/hashes.rb
+++ section3/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section3/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,29 +8,30 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {apes: 4, lions: 2, owls: 1}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+print zoo.keys
 
-# Write code that prints all of the 'values' of the zoo variable 
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+print zoo.values
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
-# YOUR CODE HERE
+print "#{zoo[:apes]}"
 
-# Write code that adds an animal to the zoo hash. 
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+zoo[:whales] = 2
 
+p zoo
 
-#-------------------
+# -------------------
 # Part 2: Email
-#-------------------
+# -------------------
 
 
 # Think about all the pieces of information associated with one single email in your inbox.
@@ -40,15 +41,22 @@ p zoo
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
 # Write code that prints your email hash to the terminal.
+email = {
+  'Sender' => 'Eric',
+  'Subject' => 'School work',
+  'Receiver' => 'Daniel',
+  'Body' => 'This is for section3',
+  'Date' => 'September 21 2021'
+}
 
-
-# Write code that prints all of the 'keys' of the email hash 
+print email
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
-# YOUR CODE HERE
+puts email.keys
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
-# YOUR CODE HERE
+puts email.values
 
 
 #-------------------
@@ -64,7 +72,7 @@ p zoo
 # posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 
-# Frankly, that was a very simplified version of the Array the Instagram developers have 
+# Frankly, that was a very simplified version of the Array the Instagram developers have
 # written and work with. Still probably slightly simplified as we don't know what their code
 # actually looks like, but it may look more like this:
 
@@ -76,7 +84,7 @@ posts = [
     'timestamp' => "4:37 PM August 13, 2019",
     'number_likes' => 0,
     'comments' => []
-  }, 
+  },
   {
     'image_src' => "./images/holiday-party.png",
     'caption' => "What a great holiday party omg",
@@ -90,12 +98,24 @@ puts posts
 puts posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in an 
-# Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in an
+# Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same
 # key-value pairs you used in your email Object above.
 # Then, log the email Array to the console.
+
+email = {
+  'Sender' => 'Eric',
+  'Subject' => 'School work',
+  'Receiver' => 'Daniel',
+  'Body' => 'This is for section3',
+  'Date' => 'September 21 2021'
+}
+
+email.each do |key, value|
+  puts "The key is #{key} while the value is #{value}"
+end
