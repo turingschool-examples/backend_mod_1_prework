5f15c3b92df916c0bdb9e6dada8108951565b3ce
Eric Chon <echon92@gmail.com>
2021-09-22 11:50:10 -0600

    Completed burrito exercise

--- section4/exercises/burrito.rb
+++ section4/exercises/burrito.rb
@@ -1,4 +1,4 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
@@ -6,14 +6,36 @@
 
 class Burrito
   attr_reader :protein, :base, :toppings
-  def initialize(protein, base, toppings)
-    @protein  = protein
-    @base     = base
-    @toppings = toppings
+  def initialize(p, b, t)
+    @protein  = p
+    @base = b
+    @toppings = t
   end
+
+  def add_topping(t)
+    @toppings.push(t)
+  end
+
+  def remove_topping(t)
+    @toppings.delete(t)
+  end
+
+  def change_protein(p)
+    @protein = p
+  end
+
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+
+dinner.add_topping('more cheese')
+p dinner.toppings
+
+dinner.remove_topping('salsa')
+p dinner.toppings
+
+dinner.change_protein('Beef')
+p dinner.protein 
