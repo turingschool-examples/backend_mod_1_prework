56a8bdcd0c4118d7b0ba7da7f12dc179a74e0256
Kevin Ta <kvnta@yahoo.com>
2021-12-11 13:05:34 -0700

    Initial commit

---
+++ section1/ex10.rb
@@ -0,0 +1,23 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat =  "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\*  Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+tabby = "\t This is how you start a paragraph, with a tab."
+
+
+ puts "subject on top.\nWhat comes next?"
+
+puts "#{tabby} Then we add details in the paragraph.
+Details can be quotes too! Turing said \"Do your homework now!\""
