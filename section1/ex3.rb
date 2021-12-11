4d173ef687548348cb38b7f4a09e5bfabfd3e334
Kevin Ta <kvnta@yahoo.com>
2021-12-10 18:03:55 -0700

    Add fix to line

---
+++ section1/ex3.rb
@@ -0,0 +1,26 @@
+#Starting text
+puts "I will now count my chickens:"
+# First count of Hens equation. Hen output #
+puts "Hens #{25 + 30 / 6}"
+#Rooster output number and equation
+puts "Roosters #{100-25*3%4}"
+#more text
+puts "now I will count the eggs:"
+#Egg count equation
+puts 3+2+1-5+4%2-1/4+6
+# question if math is true
+puts "is it true that 3+2<5-7?"
+#Computing question above
+puts 3+2<5-7
+#question  for 3+2 and result
+puts "What is 3+2? #{3+2}"
+#Another question for 5-7 and result
+puts "What is 5-7? #{5-7}"
+# more text / response to equation above
+puts "Oh, that's why it's false."
+# new text for more questions
+puts "How about some more."
+# more  math question with computed true/ false answers
+puts "is it greater? #{5>-2}"
+puts "is it greater or equal? #{5>=-2}"
+puts "is it less or equal? #{5<= -2}"
