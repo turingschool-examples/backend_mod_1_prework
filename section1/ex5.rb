30bc6b829b6d1c25bf9fda4ddad4b8c95806b358
Kevin Ta <kvnta@yahoo.com>
2021-12-10 20:54:43 -0700

    add no change

--- section1/ex5.rb
+++ section1/ex5.rb
@@ -8,10 +8,10 @@ my_hair = 'Brown'
 
 puts "Let's talk about #{my_name}."
 puts "He's #{my_height} inches tall."
-puts " #{my_weight} pounds heavy."
+puts "#{my_weight} pounds heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{my_eyes} eyes and #{my_hair} hair."
 puts "His teeth are usually #{my_teeth} depending on the coffee."
 
 #this line is tricky, try to get it exactly right
-puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height +my_weight}."
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
