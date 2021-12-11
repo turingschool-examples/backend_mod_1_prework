308dadb5745ba837252379afcca3b01bc56090bb
Kevin Ta <kvnta@yahoo.com>
2021-12-11 13:51:23 -0700

    Initial commit

---
+++ section1/ex11.rb
@@ -0,0 +1,18 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weight?"
+weight =  gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+print "Do you like dogs? "
+dogs = gets.chomp
+print "What is your favorite of the breed? "
+breed = gets.chomp
+print "How many do you have at home? "
+count = gets.chomp
+
+puts "You said #{dogs} to if you like dogs, your favorite breed is #{breed},
+and you have  #{count} at home"
