913e7b58303ba88f1846254555e247a3d7fb751e
Kevin Ta <kvnta@yahoo.com>
2021-12-10 18:06:46 -0700

    Fix line 17

--- section1/ex5.rb
+++ section1/ex5.rb
@@ -14,4 +14,4 @@ puts "He's got #{my_eyes} eyes and #{my_hair} hair."
 puts "His teeth are usually #{my_teeth} depending on the coffee."
 
 #this line is tricky, try to get it exactly right
-puts "If I add #{my_age}, #{my_hieght}, and #{my_weight} I get #{my_age + my_height +my_weight}."
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height +my_weight}."
