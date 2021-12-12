f503553b1a8bd4d0bc4c24a2b1affa996ac397fa
Kevin Ta <kvnta@yahoo.com>
2021-12-12 01:29:46 -0700

    save

--- section1/ex10.rb
+++ section1/ex10.rb
@@ -2,12 +2,12 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat =  "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
-I'll do a list:
+fat_cat = '''
+I\'ll do a list:
 \t* Cat food
 \*  Fishies
 \t* Catnip\n\t* Grass
-"""
+'''
 
 puts tabby_cat
 puts persian_cat
