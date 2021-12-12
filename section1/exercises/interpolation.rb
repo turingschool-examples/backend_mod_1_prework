077c619f2a82fe34ba1d9ce22329a0bf39990a9f
Kevin <36166420+KevinT001@users.noreply.github.com>
2021-12-12 00:49:33 -0700

    Update interpolation.rb

--- section1/exercises/interpolation.rb
+++ section1/exercises/interpolation.rb
@@ -15,16 +15,19 @@ p "The #{team} are #{name}'s favorite Quidditch team"
 speedy = "quick red fox"
 slow_poke = "lazy brown dog"
 
-p # YOUR CODE HERE
+p " #{speedy} jumped over the #{slow_poke} "
 
 # Write code that uses the variables below to form a string that reads
 # "In a predictable result, the tortoise beat the hare!":
 slow_poke = "tortoise"
 speedy = "hare"
 
-# YOUR CODE HERE
+P "In a predictable result, the #{slow_poke} beat #{speedy}!"
 
 
 # YOU DO:
 # Declare three variables, name/content/data type of your choice. Think carefully about what you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!) Then, log out ONE sentence that incorporates all THREE variables.
-
+dog= "Corgi"
+big = "Chubby"
+bones = "8"
+p " The furry #{dog} munched on #{bones} chicken bones and was laughed at for being #{chubby}." 
