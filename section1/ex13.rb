e3c5e3f38b832a0e344dc5458e9710177c6520b8
Kevin Ta <kvnta@yahoo.com>
2021-12-11 22:04:05 -0700

    Initial commit

---
+++ section1/ex13.rb
@@ -0,0 +1,18 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+# study Drill below
+corgi, pitbull, pomeranian =ARGV
+puts "My favorite breeds of dogs are listed below."
+puts "My number one favorite is : #{corgi}"
+puts "My second favorite is : #{pitbull}"
+puts "My third favorite is: #{pomeranian}"
+
+
+burritos, popcorn, ramen = ARGV
+
+puts "The last thing I ate was: #{burritos}"
+puts "Before that I ate: #{popcorn}"
