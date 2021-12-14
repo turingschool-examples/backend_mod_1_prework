3454bdc23df89dc4d9578f704a5c084b9e1acaf4
Kevin Ta <kvnta@yahoo.com>
2021-12-13 19:03:45 -0700

    complete exercise

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,49 +11,67 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
+p "#{house_elf} , has been given a sock"
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+Caution = "Harry Potter must not return to Hogwarts!"
+
+p Caution
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
-p students
+p students + 2
+
+
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
 # YOUR CODE HERE
-p students
+p students - 2
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
 
-# IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+first_name = "Kevin"
+is_hungry = "sometimes"
+number_of_pets = "2"
 
-# Explain.
+
+# IN WORDS:
+# How did you decide to use the data type you did for each of the three variables above?
+# first_name and is_hungry are string values and number_of_pets have a numerical value as an integer.
 
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to different values (but same data type).
 # print all three variables to the terminal.
+first_name = "Ash Ketchum"
+is_hungry = "only at night"
+number_of_pets = 5
 
+p first_name
+p is_hungry
+p number_of_pets
 
+p "The trainer #{first_name} eats #{is_hungry} with #{number_of_pets} pokemon."
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
 
+p healthy_snacks + junk_food_snacks
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
-# Did you run this file in your terminal to make sure everything printed out to the terminal 
-  # as you would expect? 
+# Did you run this file in your terminal to make sure everything printed out to the terminal
+  # as you would expect?
+
+  #Yes I ran it in the terminal while working through this exercise.
