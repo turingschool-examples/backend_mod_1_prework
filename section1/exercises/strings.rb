25dd0779acca37a8d4033e7e8bf90ec4bf1d05cb
Sherman Au <shermau13@gmail.com>
2021-12-15 23:15:41 -0700

    Finish section 1 exercises

--- section1/exercises/strings.rb
+++ section1/exercises/strings.rb
@@ -4,12 +4,13 @@
 # `ruby section1/exercises/strings.rb`
 
 # Example: Write code that prints your name to the terminal:
-p "Alan Turing"
+p "Sherman Au"
 
 # Write code that prints `Welcome to Turing!` to the terminal:
-p #YOUR CODE HERE
+p "Welcome to Turing!"
 
 # Write code that prints `99 bottles of pop on the wall...` to the terminal:
-# YOUR CODE HERE
+p "99 bottles of pop on the wall..."
 
 # Write out code to log one line from your favorite song or movie.
+p "I never would have thought it could ever be this good and I know each moment's fleeting, but right now I feel like I'm right where I should be"
--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,38 +11,51 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+dobby_dialogue = "Harry Potter must not return to Hogwarts!"
+p dobby_dialogue
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
-p students
+new_students = 2
+p students + new_students
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
-p students
+missing_students = 2
+p students - missing_students
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
+first_name = "Momo"
+is_hungry = true
+number_of_pets = 3
+
+p first_name, is_hungry, number_of_pets
 
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
 
-# Explain.
-
+# Explain.:
+#- first_name variable would be a string because of conventional naming sense
+#- is_hungry variable closely resembles a boolean output because of the 'is' denotion
+#- number_of_pets variable would be a numerical designation rather than an array
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to different values (but same data type).
 # print all three variables to the terminal.
+first_name = "Boomi"
+is_hungry = false
+number_of_pets = 2
+
+ p first_name, is_hungry, number_of_pets
 
 
 # YOU DO:
@@ -51,9 +64,12 @@ healthy_snacks = 6;
 junk_food_snacks = 8;
 
 
+p healthy_snacks + junk_food_snacks
+
+
 #-------------------
 # FINAL CHECK
 #-------------------
 
-# Did you run this file in your terminal to make sure everything printed out to the terminal 
-  # as you would expect? 
+# Did you run this file in your terminal to make sure everything printed out to the terminal
+  # as you would expect?
