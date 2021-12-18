281cdfba148392632d225d50a5615b1b0c804f01
Kevin Ta <kvnta@yahoo.com>
2021-12-17 23:17:53 -0700

    Complete exercise

--- section4/exercises/person.rb
+++ section4/exercises/person.rb
@@ -1,5 +1,23 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
+class Person
+  attr_accessor :name, :sex
 
-# YOUR CODE HERE
+def initialize(name, sex)
+  @name = name
+  @sex = sex
+  end
+
+def sleep(snores)
+  puts "#{name} goes to bed at night and #{snores}."
+  end
+
+def gender(male)
+  puts "#{sex} is what #{@name} identifies with."
+  end
+end
+
+Kevin = Person.new("Kevin", "male")
+puts Kevin.sleep("snores")
+puts Kevin.gender("male")
