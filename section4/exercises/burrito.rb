7302ecb968d0ef8828ee8a624e8f5139a63e3e20
Sherman Au <shermau13@gmail.com>
2021-12-18 14:32:22 -0700

    Finish burrito.rb exercise

--- section4/exercises/burrito.rb
+++ section4/exercises/burrito.rb
@@ -5,11 +5,24 @@
 # 3. change_protein
 
 class Burrito
-  attr_reader :protein, :base, :toppings
+  attr_accessor :protein, :base, :toppings
+
   def initialize(protein, base, toppings)
     @protein  = protein
     @base     = base
     @toppings = toppings
+  end
+
+  def add_toppings(toppings)
+    self.toppings << toppings
+  end
+
+  def remove_toppings(toppings)
+    self.toppings.delete(toppings)
+  end
+
+  def change_protein(protein)
+    self.protein = protein
   end
 end
 
@@ -17,6 +30,8 @@ dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
-
-
-test
+p dinner.add_toppings('cilantro')
+dinner.remove_toppings('guacamole')
+p dinner.toppings
+dinner.change_protein("Carnitas")
+p dinner.protein
