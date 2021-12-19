f2dfcf88e810fb09b010cba48d5db2dc2b1a77ea
Kevin Ta <kvnta@yahoo.com>
2021-12-18 17:23:03 -0700

    Refactor arrays

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -23,43 +23,48 @@ print animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
-
+p animals.length
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
-
+animals[-1] = "Gorilla"
 
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals[3] = "Corgi"
 
 # YOU DO: Write code that will print the String "Elephant" in the animals array
+animals[2] = "Elephant"
 
+p animals[2]
 
 #-------------------
 # PART 2: Foods: Array Methods
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+foods = ["tacos", "chips", "noodles", "cookies"]
 
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
-
+foods.length
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
+foods.push("broccoli")
+p foods
 
 
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
 
-
-# YOU DO: Write code to add 3 new foods to the array. 
+foods.push("lettuce", "bread", "chocolate")
+p foods
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
 
 # YOU DO: Remove the food that is in index position 0.
-
+foods.delete_at(0)
 #-------------------
 # PART 3: Where are Arrays used?
 #-------------------
@@ -77,11 +82,9 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
-# 1:
-# 2:
-# 3:
-
-
+# 1: Itunes store would have arrays of artist per catagory
+# 2: Netflix would have arrays of genre titles
+# 3: Bar menu would have array of types of alcohol 
