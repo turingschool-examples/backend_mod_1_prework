78d6a9fda10e21351eb8b17bd538fc73d35026e4
Sean Culliton <smculliton@gmail.com>
2022-08-17 12:42:03 -0600

    Finish day1 questions

---
+++ day_1/ex3.rb
@@ -0,0 +1,25 @@
+#prints chicken phrase
+puts "I will now count my chickens:"
+#prints Hens and then 25 plus 30 divided by 6
+puts "Hens #{25.0 + 30 / 6}"
+#prints Roosters then the result of this math 
+puts "Roosters #{100.0 - 25 * 3 % 4}"
+#prints edd phrase 
+puts "Now I will count the eggs:"
+#prints the result of this math 
+puts 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+#prints the boolean value after evaluating whether 3 + 2 is less than 5 - 7
+puts 3.0 + 2 < 5 - 7
+#prints phrases, then the result of the math 
+puts "What is 3 + 2? #{3.0 + 2}"
+puts "What is 5 - 7? #{5.0 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+#prints phrases and then booleans as evaluated by concatenated values
+puts "Is it greater? #{5.0 > -2}"
+puts "Is it greater or equal? #{5.0 >= -2}"
+puts "Is it less or equal? #{5.0 <= -2}"
--- day_1/questions.md
+++ day_1/questions.md
@@ -2,16 +2,33 @@
 
 1. How would you print the string `"Hello World!"` to the terminal?
 
+puts "Hello World!"
+
 1. What character is used to indicate comments in a ruby file?
+
+octothorpe (#)
 
 1. Explain the difference between an integer and a float?
 
+A float contains a decimal point
+
 1. In the space below, create a variable `animal` that holds the string `"zebra"`
+
+animal = "zebra"
 
 1. How would you print the string `"zebra"` using the variable that you created above?
 
+puts animal 
+
 1. What is interpolation? Use interpolation to print a sentence using the variable `animal`.
+
+puts "I like #{animal}"
 
 1. What method is used to get input from a user?
 
+gets.chomp
+
 1. Name and describe two common string methods:
+
+string.length - returns integer of amount of characters in string 
+string.upcase - capitalizes every letter in string 
