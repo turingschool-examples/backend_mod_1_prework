77f43adea85eba16713f63ddd06f5b1648042f94
Bryan Flanagan <109649285+bflanagan138@users.noreply.github.com>
2022-08-18 16:06:09 -0600

    Update day 1 exercises 1-3

---
+++ ex3.rb
@@ -0,0 +1,28 @@
+#prints out statement that I'll count chickens
+puts "I will now count my chickens:"
+#30 divided by 6 plus 25 equals 30
+puts "Hens #{25 + 30 / 6}"
+#25 times 3 = 75 divided by 4 equals 18r 3. so 100 minus 3 equals 97
+puts "Rooster #{100 - 25 * 3 % 4}"
+#prints out statement about counting eggs
+puts "Now I will count the eggs:"
+#4 divided by 2 leaves no remainder, 1 divided by 4 is .25.
+#so 3 plus 2 plus 1 is 6 minus 5 is one plus 0 minus 0 is 1plus 6 is 7
+#because
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#3 plus 2 is 5. 5 minus 7 is -2. 5 is not less than -2 so it returns false
+puts 3 + 2 < 5 - 7
+#prints the question what is 3 plus 2. and follows with answer
+puts "What is 3 + 2? #{3 + 2}"
+#prints the question what is 5 minus 7. and follows with answer
+puts "What is 5 - 7? #{5-7}"
+#prints response to mathmatical illustrations above
+puts "Oh, that's why it's false."
+#suggests doing more math
+puts "How about some more."
+#prints question is 5 greater thatn -2 and follows with answer true
+puts "Is it greater? #{5 > -2}"
+#prints question is 5 greater or equal to -2 and follows with answer true
+puts "Is it greater or equal? #{5 >= -2}"
+#prints question is 5 less than or equal to -2 and follows with answer false
+puts "Is it less or equal #{5 <= -2}"
